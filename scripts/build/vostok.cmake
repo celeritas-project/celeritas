f5b142d90869c58673050c3394c81a1af6c7edc6
Seth R Johnson <johnsonsr@ornl.gov>
2020-07-29 13:16:05 -0400

    Update build scripts

--- scripts/build/vostok.cmake
+++ scripts/build/vostok.cmake
@@ -1,7 +1,7 @@
 # Options
-set(CELERITAS_USE_CUDA OFF CACHE BOOL "")
-set(CELERITAS_USE_ROOT OFF CACHE BOOL "")
-set(CELERITAS_USE_VECGEOM OFF CACHE BOOL "")
+set(CELERITAS_USE_CUDA    OFF CACHE BOOL "")
+set(CELERITAS_USE_ROOT    OFF CACHE BOOL "")
+set(CELERITAS_USE_VECGEOM ON  CACHE BOOL "")
 
 # Libraries
 string(REPLACE ":" ";" _rpath "$ENV{DYLD_FALLBACK_LIBRARY_PATH}")
@@ -9,5 +9,4 @@ set(CMAKE_BUILD_RPATH "${_rpath}" CACHE STRING "")
 set(CMAKE_INSTALL_RPATH "${_rpath};$ENV{prefix_dir}/lib" CACHE STRING "")
 
 # Add all the warnings, and enable color diagnostics when using Ninja
-set(CMAKE_CXX_FLAGS "-Wall -Wextra -pedantic -Werror -fcolor-diagnostics" CACHE STRING
-  "")
+set(CMAKE_CXX_FLAGS "-Wall -Wextra -pedantic -Werror -fcolor-diagnostics" CACHE STRING "")
