f71f9d5551162cc7a92e11acad9f39eeb17199f1
Seth R Johnson <johnsonsr@ornl.gov>
2020-09-21 23:13:22 -0400

    Fix filenames

--- src/base/StackAllocatorStore.t.hh
+++ src/base/StackAllocatorStore.t.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file StackAllocatorStore.cc
+//! \file StackAllocatorStore.t.hh
 //---------------------------------------------------------------------------//
 #include "StackAllocatorStore.hh"
 
--- src/physics/base/SecondaryAllocatorStore.cc
+++ src/physics/base/SecondaryAllocatorStore.cc
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file physics/base/StackAllocatorStore.t.cc
+//! \file StackAllocatorStore.cc
 //---------------------------------------------------------------------------//
 #include "Secondary.hh"
 #include "base/StackAllocatorStore.t.hh"
