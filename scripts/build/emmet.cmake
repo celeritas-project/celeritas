de7d66b7ce71f18f82849c7ae268477ea10b9dcd
Seth R Johnson <johnsonsr@ornl.gov>
2020-10-02 23:00:48 -0400

    Update scripts for CMake 3.18

--- scripts/build/emmet.cmake
+++ scripts/build/emmet.cmake
@@ -2,17 +2,16 @@ macro(set_cache_var var type val)
   set(${var} "${val}" CACHE "${type}" "emmet.sh")
 endmacro()
 
-# Celeritas options
+# Celeritas dependency options
 set_cache_var(CELERITAS_USE_CUDA BOOL ON)
-set_cache_var(CELERITAS_USE_Geant4 BOOL OFF)
+set_cache_var(CELERITAS_USE_Geant4 BOOL ON)
 set_cache_var(CELERITAS_USE_HepMC3 BOOL ON)
 set_cache_var(CELERITAS_USE_MPI BOOL ON)
 set_cache_var(CELERITAS_USE_ROOT BOOL ON)
 set_cache_var(CELERITAS_USE_VecGeom BOOL ON)
 set_cache_var(CELERITAS_GIT_SUBMODULE BOOL OFF)
 
-# set_cache_var rpath based on environment (loaded Spack modules); VecGeom does not
-# correctly set_cache_var rpath for downstream use
+# Set rpath based on environment
 string(REPLACE ":" ";" _rpath "$ENV{LD_RUN_PATH}")
 set_cache_var(CMAKE_BUILD_RPATH STRING "${_rpath}")
 set_cache_var(CMAKE_INSTALL_RPATH STRING "$ENV{prefix_dir}/lib;${_rpath}")
@@ -20,24 +19,17 @@ set_cache_var(CMAKE_INSTALL_RPATH STRING "$ENV{prefix_dir}/lib;${_rpath}")
 # Export compile commands for microsoft visual code
 set_cache_var(CMAKE_EXPORT_COMPILE_COMMANDS BOOL ON)
 
-# Use CUDA.
+# Build flags
 set_cache_var(BUILD_SHARED_LIBS BOOL ON)
-set_cache_var(CMAKE_CUDA_FLAGS STRING "-arch=sm_35")
-# TODO: when using CMake 3.18, replace the above line with this one:
-# set_cache_var(CMAKE_CUDA_ARCHITECTURES STRING "35")
+set_cache_var(CMAKE_CUDA_ARCHITECTURES STRING "35")
 set_cache_var(CMAKE_CUDA_FLAGS_DEBUG STRING "-g -G")
+# NOTE: CUDA error flags do not work with CMake 3.18
+#set_cache_var(CMAKE_CUDA_FLAGS STRING
+#  "-Werror all-warnings ${CMAKE_CUDA_FLAGS}")
+
 set_cache_var(CMAKE_BUILD_TYPE STRING "Debug")
-
-# Use MPI
-# XXX fexceptions gets added by the FindMPI script but it doesn't get escaped
-# and so causes nvcc to bomb.
-set_cache_var(MPI_CXX_COMPILE_OPTIONS STRING
-  "$<$<COMPILE_LANGUAGE:CUDA>:SHELL:-Xcompiler=>-fexceptions;-pthread")
-
-set_cache_var(MPI_CXX_SKIP_MPICXX BOOL TRUE)
-
-# Enable color diagnostics when using Ninja
 set_cache_var(CMAKE_CXX_FLAGS STRING
   "-Wall -Wextra -Werror -pedantic -fdiagnostics-color=always")
-set_cache_var(CMAKE_CUDA_FLAGS STRING
-  "-Werror all-warnings ${CMAKE_CUDA_FLAGS}")
+
+# MPI flags
+set_cache_var(MPI_CXX_SKIP_MPICXX BOOL TRUE)
--- scripts/build/emmet.sh
+++ scripts/build/emmet.sh
@@ -1,12 +1,16 @@
 #!/bin/sh -e
+
 BUILDSCRIPT_DIR="$(cd "$(dirname $BASH_SOURCE[0])" && pwd)"
 SOURCE_DIR="$(cd "${BUILDSCRIPT_DIR}" && git rev-parse --show-toplevel)"
+BUILD_DIR=${SOURCE_DIR}/build
 
-printf "\e[2;37mBuilding from ${SOURCE_DIR}\e[0m\n"
-cd $SOURCE_DIR
-mkdir build 2>/dev/null || true
-cd build
+printf "\e[2;37mBuilding in ${BUILD_DIR}\e[0m\n"
+mkdir ${BUILD_DIR} 2>/dev/null \
+  || printf "... \e[2;37mBuilding from existing cache\e[0m\n"
+cd ${BUILD_DIR}
 
+module purge
+module load cuda
 CELERITAS_ENV=$SPACK_ROOT/var/spack/environments/celeritas/.spack-env/view
 export PATH=$CELERITAS_ENV/bin:${PATH}
 export CMAKE_PREFIX_PATH=$CELERITAS_ENV:${CMAKE_PREFIX_PATH}
@@ -17,7 +21,7 @@ export CXX=/usr/bin/g++
 # module load vecgeom/8d0c478c-cuda
 
 cmake -C ${BUILDSCRIPT_DIR}/emmet.cmake -G Ninja \
-  -DCMAKE_INSTALL_PREFIX:PATH=$SOURCE_DIR/install \
+  -DCMAKE_INSTALL_PREFIX:PATH=${SOURCE_DIR}/install \
   ..
 ninja -v
 ctest -j32 --output-on-failure
