022a929a67da1f19e9eb27bed241e06f63d01891
Seth R Johnson <johnsonsr@ornl.gov>
2021-03-01 12:03:46 -0500

    Add range and inverse range calculators

---
+++ src/physics/grid/InverseRangeCalculator.hh
@@ -0,0 +1,60 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2021 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file InverseRangeCalculator.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include "base/Quantity.hh"
+#include "NonuniformGrid.hh"
+#include "XsGridInterface.hh"
+#include "UniformGrid.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Calculate the energy that would limit a particle to a particular range.
+ *
+ * This should provide the inverse of the result of \c RangeCalculator. The
+ * given \c range is not allowed to be greater than the maximum range in the
+ * physics data.
+ *
+ * The range must be monotonically increasing in energy, since it's defined as
+ * the integral of the inverse of the stopping power (which is always
+ * positive). For ranges shorter than the minimum energy in the table, the
+ * resulting energy is scaled:
+ * \f[
+    E = E_\textrm{min}} \left( \frac{r}{r_\textrm{min}} \right)^2
+ * \f]
+ * This scaling is the inverse of the off-the-end energy scaling in the
+ * RangeCalculator.
+ */
+class InverseRangeCalculator
+{
+  public:
+    //!@{
+    //! Type aliases
+    using Energy = Quantity<XsGridData::EnergyUnits>;
+    using Values = Pie<real_type, Ownership::const_reference, MemSpace::native>;
+    //!@}
+
+  public:
+    // Construct from state-independent data
+    inline CELER_FUNCTION
+    InverseRangeCalculator(const XsGridData& grid, const Values& values);
+
+    // Find and interpolate from the energy
+    inline CELER_FUNCTION Energy operator()(real_type range) const;
+
+  private:
+    UniformGrid               log_energy_;
+    NonuniformGrid<real_type> range_;
+};
+
+//---------------------------------------------------------------------------//
+} // namespace celeritas
+
+#include "InverseRangeCalculator.i.hh"
---
+++ src/physics/grid/RangeCalculator.hh
@@ -0,0 +1,56 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2021 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file RangeCalculator.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include "base/Quantity.hh"
+#include "XsGridInterface.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Find and interpolate range on a uniform log grid.
+ *
+ * \code
+    RangeCalculator calc_range(xs_grid, xs_params.reals);
+    real_type range = calc_range(particle);
+   \endcode
+ *
+ * Below the minimum tabulated energy, the range is scaled:
+ * \f[
+    r = r_\textrm{min}} \sqrt{ \frac{E}{E_\textrm{min}}}
+ * \f]
+ */
+class RangeCalculator
+{
+  public:
+    //!@{
+    //! Type aliases
+    using Energy = Quantity<XsGridData::EnergyUnits>;
+    using Values = Pie<real_type, Ownership::const_reference, MemSpace::native>;
+    //!@}
+
+  public:
+    // Construct from state-independent data
+    inline CELER_FUNCTION
+    RangeCalculator(const XsGridData& grid, const Values& values);
+
+    // Find and interpolate from the energy
+    inline CELER_FUNCTION real_type operator()(Energy energy) const;
+
+  private:
+    const XsGridData& data_;
+    const Values&     reals_;
+
+    CELER_FORCEINLINE_FUNCTION real_type get(size_type index) const;
+};
+
+//---------------------------------------------------------------------------//
+} // namespace celeritas
+
+#include "RangeCalculator.i.hh"
