f8f3572f0f2642e07d3b9a25360b9143f1cea9bb
Seth R Johnson <johnsonsr@ornl.gov>
2021-03-12 20:40:45 -0500

    Fix documentation

--- src/physics/em/EPlusGGMacroXsCalculator.i.hh
+++ src/physics/em/EPlusGGMacroXsCalculator.i.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file EPlusGGPEMacroXsCalculator.i.hh
+//! \file EPlusGGMacroXsCalculator.i.hh
 //---------------------------------------------------------------------------//
 
 #include "base/Algorithms.hh"
