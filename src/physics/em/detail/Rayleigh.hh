415ae4b85d9f0890b52954061ce030a63220b123
Soon Yung Jun <syjun@wc.fnal.gov>
2021-03-24 16:11:53 -0500

    add RayleighParameters to RayleighPointers

--- src/physics/em/detail/Rayleigh.hh
+++ src/physics/em/detail/Rayleigh.hh
@@ -9,29 +9,93 @@
 
 #include "base/Macros.hh"
 #include "base/Types.hh"
+#include "base/Collection.hh"
+#include "physics/base/Types.hh"
+#include "RayleighData.hh"
 
 namespace celeritas
 {
+struct ModelInteractPointers;
+
 namespace detail
 {
 //---------------------------------------------------------------------------//
 /*!
+ * Rayleigh angular parameters (form factor) based on modified fit formulas
+ * from Dermott E. Cullen, "A simple model of photon transport", Nucl.
+ * Instrum. Meth. Phys. Res. B v.101, (4), 499-510.
+ */
+template<Ownership W, MemSpace M>
+struct RayleighParameters
+{
+    using IntId = celeritas::ItemId<int>;
+
+    template<class T>
+    using Items = celeritas::Collection<T, W, M, IntId>;
+
+    Items<Array<real_type, detail::rayleigh_num_parameters>> data;
+
+    explicit CELER_FUNCTION operator bool() const { return !data.empty(); }
+
+    //! Assign from another set of parameters
+    template<Ownership W2, MemSpace M2>
+    RayleighParameters& operator=(const RayleighParameters<W2, M2>& other)
+    {
+        CELER_EXPECT(other);
+        data = other.data;
+
+        return *this;
+    }
+};
+
+//---------------------------------------------------------------------------//
+/*!
  * Device data for creating an interactor.
  */
-struct RayleighInteractorPointers
+template<Ownership W, MemSpace M>
+struct RayleighPointers
 {
-    //! ID of an electron
-    ParticleId electron_id;
+    //! Model ID
+    ModelId model_id;
+
     //! ID of a gamma
     ParticleId gamma_id;
-    // XXX additional data
+
+    //! Rayleigh angular parameters
+    RayleighParameters<W, M> params;
 
     //! Check whether the data is assigned
     explicit inline CELER_FUNCTION operator bool() const
     {
-        return electron_id && gamma_id; // XXX
+        return model_id && gamma_id && params;
+    }
+
+    //! Assign from another set of data
+    template<Ownership W2, MemSpace M2>
+    RayleighPointers& operator=(const RayleighPointers<W2, M2>& other)
+    {
+        CELER_EXPECT(other);
+        model_id = other.model_id;
+        gamma_id = other.gamma_id;
+        params   = other.params;
+        return *this;
     }
 };
+
+using RayleighDeviceRef
+    = RayleighPointers<Ownership::const_reference, MemSpace::device>;
+using RayleighHostRef
+    = RayleighPointers<Ownership::const_reference, MemSpace::host>;
+using RayleighNativePointers
+    = RayleighPointers<Ownership::const_reference, MemSpace::native>;
+
+//---------------------------------------------------------------------------//
+// KERNEL LAUNCHERS
+//---------------------------------------------------------------------------//
+
+// Launch the Livermore photoelectric interaction
+void rayleigh_interact(const RayleighDeviceRef&     pointers,
+                       const ModelInteractPointers& model);
 
 //---------------------------------------------------------------------------//
 } // namespace detail
