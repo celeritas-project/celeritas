aabd63449146283a7e2bdac5a000e813234e8117
Soon Yung Jun <syjun@wc.fnal.gov>
2021-04-01 12:51:02 -0500

    fix a bug in hc_factor, from unit_cast(units::MevMomentum{1.0} to unit_cast(units::MevEnergy{1.0}

--- src/physics/em/detail/RayleighInteractor.hh
+++ src/physics/em/detail/RayleighInteractor.hh
@@ -66,7 +66,7 @@ class RayleighInteractor
     //! cm/hc in the MeV energy unit
     static CELER_CONSTEXPR_FUNCTION real_type hc_factor()
     {
-        return units::centimeter * unit_cast(units::MevMomentum{1.0})
+        return units::centimeter * unit_cast(units::MevEnergy{1.0})
                / (constants::c_light * constants::h_planck);
     }
 
