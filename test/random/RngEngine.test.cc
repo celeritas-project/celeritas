de89c159119f6c8f14c8cbb47febfb1acaba0849
Seth R Johnson <johnsonsr@ornl.gov>
2021-04-08 13:20:10 +0100

    Add sequence engine for debugging RNG algorithms

--- test/random/RngEngine.test.cc
+++ test/random/RngEngine.test.cc
@@ -6,21 +6,158 @@
 //! \file RngEngine.test.cc
 //---------------------------------------------------------------------------//
 #include "random/RngParams.hh"
+#include "DiagnosticRngEngine.hh"
+#include "SequenceEngine.hh"
 
+#include <algorithm>
+#include <cmath>
+#include <limits>
 #include "celeritas_test.hh"
 #include "RngEngine.test.hh"
 #include "base/CollectionStateStore.hh"
+#include "random/distributions/GenerateCanonical.hh"
 
 using celeritas::CollectionStateStore;
+using celeritas::generate_canonical;
 using celeritas::RngParams;
 using celeritas::RngStateData;
 using namespace celeritas_test;
 
 //---------------------------------------------------------------------------//
-// TEST HARNESS
+// SEQUENCE ENGINE
 //---------------------------------------------------------------------------//
 
-class RngEngineTest : public celeritas::Test
+class SequenceEngineTest : public celeritas::Test
+{
+  public:
+    void SetUp() override
+    {
+        const double inv_32 = std::ldexp(1.0, -32.0);
+        const double inv_64 = std::ldexp(1.0, -64.0);
+        /*!
+         * Note: even the lowest *normalized* float value (1e-38) is below
+         * 2**-64, so the "min" values for both double and float are
+         * rounded up to 2**-64, which results in a value of 5e-20 for
+         * double.
+         *
+         * Also note that the follwing two values are ommited because they're
+         * accurate to only 1e-10 for doubles -- so our "promise" of
+         * reproducibility isn't quite right.
+         * \code
+         * std::nextafter(float(inv_64), 1.0f),
+         * std::nextafter(inv_32, 0.0),
+         * \endcode
+         */
+        values_ = {
+            0.0,
+            inv_64,
+            std::nextafter(inv_64, 1.0),
+            12345678u * inv_64,
+            0xffff0000u * inv_64,
+            // Note: the following two values are
+            std::nextafter(float(inv_32), 0.0f),
+            inv_32,
+            std::nextafter(inv_32, 1.0),
+            std::nextafter(float(inv_32), 1.0f),
+            std::nextafter(0.5f, 0.0f), // .5f - epsf
+            0x7fffffffu * inv_32,       // not exactly representable by float
+            std::nextafter(0.5, 0.0),   // .5 - eps
+            0.5,
+            std::nextafter(0.5, 1.0),   // .5 + eps
+            std::nextafter(0.5f, 1.0f), // .5f + epsf
+            std::nextafter(1.0f, 0.0f), // 1 - float eps
+            std::nextafter(1.0, 0.0),   // 1 - double eps
+        };
+    }
+    std::vector<double> values_;
+};
+
+TEST_F(SequenceEngineTest, manual)
+{
+    SequenceEngine::VecResult raw_vals{0x00000000u, 0x20210408u, 0xffffffffu};
+    SequenceEngine            engine(raw_vals);
+    EXPECT_EQ(0, engine.count());
+    EXPECT_EQ(3, engine.max_count());
+
+    // Call the engine operator for actual values
+    SequenceEngine::VecResult actual(raw_vals.size());
+    std::generate(actual.begin(), actual.end(), std::ref(engine));
+    EXPECT_EQ(3, engine.count());
+    EXPECT_VEC_EQ(raw_vals, actual);
+
+    // Past the end
+    EXPECT_THROW(engine(), celeritas::DebugError);
+}
+
+TEST_F(SequenceEngineTest, from_reals)
+{
+    auto engine = SequenceEngine::from_reals(celeritas::make_span(values_));
+    EXPECT_EQ(values_.size() * 2, engine.max_count());
+    SequenceEngine::VecResult actual(engine.max_count());
+    std::generate(actual.begin(), actual.end(), std::ref(engine));
+
+    const unsigned int expected[]
+        = {0u,          0u,          1u,          0u,          1u,
+           0u,          12345678u,   0u,          4294901760u, 0u,
+           4294967040u, 0u,          0u,          1u,          0u,
+           1u,          512u,        1u,          0u,          2147483520u,
+           0u,          2147483647u, 4294966272u, 2147483647u, 0u,
+           2147483648u, 2048u,       2147483648u, 0u,          2147483904u,
+           0u,          4294967040u, 4294965248u, 4294967295u};
+    EXPECT_VEC_EQ(expected, actual);
+
+    // Past the end
+    EXPECT_THROW(engine(), celeritas::DebugError);
+}
+
+TEST_F(SequenceEngineTest, double_canonical)
+{
+    auto engine = SequenceEngine::from_reals(celeritas::make_span(values_));
+    for (double expected : values_)
+    {
+        double actual = generate_canonical<double>(engine);
+        // Test within 2 ulp
+        EXPECT_DOUBLE_EQ(expected, actual)
+            << "for i=" << (engine.count() / 2 - 1);
+        EXPECT_LT(actual, 1.0);
+    }
+}
+
+TEST_F(SequenceEngineTest, float_canonical)
+{
+    auto engine = SequenceEngine::from_reals(celeritas::make_span(values_));
+    for (double expected : values_)
+    {
+        float actual = generate_canonical<float>(engine);
+        // Test within 2 ulp
+        EXPECT_FLOAT_EQ(static_cast<float>(expected), actual);
+        EXPECT_LT(actual, 1.0f);
+    }
+}
+
+//---------------------------------------------------------------------------//
+// DIAGNOSTIC ENGINE
+//---------------------------------------------------------------------------//
+
+TEST(DiagnosticEngineTest, from_reals)
+{
+    auto rng = DiagnosticRngEngine<std::mt19937>();
+    EXPECT_EQ(0, rng.count());
+    EXPECT_EQ(3499211612u, rng());
+    EXPECT_EQ(1, rng.count());
+    generate_canonical<double>(rng);
+    EXPECT_EQ(3, rng.count());
+    generate_canonical<float>(rng);
+    EXPECT_EQ(4, rng.count());
+    rng.reset_count();
+    EXPECT_EQ(0, rng.count());
+}
+
+//---------------------------------------------------------------------------//
+// CUDA RNG
+//---------------------------------------------------------------------------//
+
+class CudaRngEngineTest : public celeritas::Test
 {
   public:
     using RngDeviceStore = CollectionStateStore<RngStateData, MemSpace::device>;
@@ -30,7 +167,7 @@ class RngEngineTest : public celeritas::Test
     std::shared_ptr<RngParams> params;
 };
 
-TEST_F(RngEngineTest, TEST_IF_CELERITAS_CUDA(device))
+TEST_F(CudaRngEngineTest, TEST_IF_CELERITAS_CUDA(device))
 {
     // Create and initialize states
     RngDeviceStore rng_store(*params, 1024);
@@ -63,7 +200,7 @@ TEST_F(RngEngineTest, TEST_IF_CELERITAS_CUDA(device))
 //---------------------------------------------------------------------------//
 
 template<typename T>
-class RngEngineFloatTest : public RngEngineTest
+class CudaRngEngineFloatTest : public CudaRngEngineTest
 {
 };
 
@@ -82,12 +219,12 @@ void check_expected_float_samples(const std::vector<double>& v)
 }
 
 using FloatTypes = ::testing::Types<float, double>;
-TYPED_TEST_SUITE(RngEngineFloatTest, FloatTypes, );
+TYPED_TEST_SUITE(CudaRngEngineFloatTest, FloatTypes, );
 
 #if CELERITAS_USE_CUDA
-TYPED_TEST(RngEngineFloatTest, device)
+TYPED_TEST(CudaRngEngineFloatTest, device)
 #else
-TYPED_TEST(RngEngineFloatTest, DISABLED_device)
+TYPED_TEST(CudaRngEngineFloatTest, DISABLED_device)
 #endif
 {
     using RngDeviceStore = typename TestFixture::RngDeviceStore;
---
+++ test/random/SequenceEngine.hh
@@ -0,0 +1,95 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2021 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file SequenceEngine.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include <cstdint>
+#include <limits>
+#include <utility>
+#include <vector>
+#include "random/distributions/GenerateCanonical.hh"
+#include "base/Span.hh"
+
+namespace celeritas_test
+{
+//---------------------------------------------------------------------------//
+/*!
+ * "Random" number generator that returns a sequence of values.
+ *
+ * This is useful for testing algorithms that expect a random stream on the
+ * host: you can specify the sequence of values to return. When the sequence is
+ * used up, the next operator call will raise an exception.
+ *
+ * The factory function SequenceEngine::from_reals will create an integer
+ * sequence that exactly reproduces the given real numbers (which must be in
+ * the half-open interval \f$ [0, 1) \f$) using the \c generate_canonical
+ * function.
+ */
+class SequenceEngine
+{
+  public:
+    //@{
+    //! Typedefs
+    using result_type = std::uint32_t;
+    using VecResult   = std::vector<result_type>;
+    using LimitsT     = std::numeric_limits<result_type>;
+    using size_type   = VecResult::size_type;
+    //!@}
+
+  public:
+    // Nearly reproduce the given stream of reals with generate_canonical
+    inline static SequenceEngine
+    from_reals(celeritas::Span<const double> values);
+
+    // Construct from a sequence of integers (the sequence to reproduce)
+    explicit inline SequenceEngine(VecResult values);
+
+    // Get the next random number in the sequence; throw if past the end
+    inline result_type operator()();
+
+    size_type count() const { return iter_ - values_.begin(); }
+    size_type max_count() const { return values_.size(); }
+
+    //!@{
+    //! Engine limits
+    static constexpr result_type min() { return LimitsT::min(); }
+    static constexpr result_type max() { return LimitsT::max(); }
+    //!@}
+
+  private:
+    VecResult                 values_;
+    VecResult::const_iterator iter_;
+};
+
+//---------------------------------------------------------------------------//
+} // namespace celeritas_test
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Specialization of GenerateCanonical for SequenceEngine.
+ */
+template<class T>
+class GenerateCanonical<celeritas_test::SequenceEngine, T>
+{
+  public:
+    //!@{
+    //! Type aliases
+    using real_type   = T;
+    using result_type = real_type;
+    //!@}
+
+  public:
+    // Sample a random number
+    inline result_type operator()(celeritas_test::SequenceEngine& rng);
+};
+
+//---------------------------------------------------------------------------//
+} // namespace celeritas
+
+#include "SequenceEngine.i.hh"
