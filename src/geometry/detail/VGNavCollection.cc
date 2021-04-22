83ac474c46ba00a170197638cda08aecec419de3
Philippe Canal <pcanal@fnal.gov>
2021-04-22 11:28:04 -0500

    White space src/geometry/detail/VGNavCollection.cc

--- src/geometry/detail/VGNavCollection.cc
+++ src/geometry/detail/VGNavCollection.cc
@@ -1,4 +1,4 @@
-           //----------------------------------*-C++-*----------------------------------//
+//----------------------------------*-C++-*----------------------------------//
 // Copyright 2021 UT-Battelle, LLC, and other Celeritas developers.
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
