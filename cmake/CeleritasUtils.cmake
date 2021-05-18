f1a2f56b9ccc7504660c3ad63548a3bf59544952
Philippe Canal <pcanal@fnal.gov>
2021-05-18 10:03:18 -0500

    Add documentation

--- cmake/CeleritasUtils.cmake
+++ cmake/CeleritasUtils.cmake
@@ -26,9 +26,37 @@ CMake utility functions for Celeritas.
   Once upstream packages are updated, this can be replaced by ``find_package``.
 
 
+.. command:: celeritas_add_library
 
+  Add a library to the project using the specified source files *with* special handling
+  for the case where the library contains CUDA separatable code.
 
+  To support separatable CUDA code, the following 4 targets will be contruscted:
 
+ A object library used to compile the source code and share the result with the static and shared library
+ A static library used as input to nvcc -dlink
+ A shared “intermediary” library containing all the .o files but NO nvcc -dlink result
+ A shared “final” library containing the result of nvcc -dlink and linked against the above mentioned shared library.
+
+ An executable need to load exactly one result of nvcc -dlink (Whose input needs to be
+ the .o files from all the “cuda” library it uses/depends-on. So if the executable has cuda code,
+ it will call nvcc -dlink itself and link against the intermediary shared libraries.
+ If the executable has no cuda code, then it needs to link against the final library
+ (of its most derived dependency). If the executable has no cuda code but use two
+ independent cuda libraries, it will still need to run its own nvcc -dlink.
+
+  ::
+
+    celeritas_link_vecgeom_cuda(<name> [STATIC | SHARED | MODULE]
+            [EXCLUDE_FROM_ALL]
+            [<source>...])
+
+.. command: celeritas_target_link_libraries
+
+  Specify libraries or flags to use when linking a given target and/or its dependents, taking
+  in account the extra targets (see celeritas_add_library) needed to support CUDA separatable code
+  Usage requirements from linked library targets will be propagated. Usage requirements
+  of a target's dependencies affect compilation of its own sources.
 
 #]=======================================================================]
 include(FindPackageHandleStandardArgs)
@@ -59,6 +87,10 @@ define_property(TARGET PROPERTY CELERITAS_CUDA_OBJECT_LIBRARY
   FULL_DOCS "Name of the object (without nvlink step) library corresponding to this cuda library"
 )
 
+#
+# Internal routine to figure out if a list contains
+# CUDA source code.  Returns TRUE/FALSE in the OUTPUT_VARIABLE
+#
 function(celeritas_sources_contains_cuda OUTPUT_VARIABLE)
   set(_contains_cuda FALSE)
   foreach(_source ${ARGN})
@@ -78,6 +110,10 @@ function(celeritas_sources_contains_cuda OUTPUT_VARIABLE)
   set(${OUTPUT_VARIABLE} ${_contains_cuda} PARENT_SCOPE)
 endfunction()
 
+#
+# Internal routine to figure out if a target already contains
+# CUDA source code.  Returns TRUE/FALSE in the OUTPUT_VARIABLE
+#
 function(celeritas_lib_contains_cuda OUTPUT_VARIABLE target)
   celeritas_strip_alias(target ${target})
   get_target_property(_target_sources ${target} SOURCES)
@@ -86,6 +122,13 @@ function(celeritas_lib_contains_cuda OUTPUT_VARIABLE target)
   set(${OUTPUT_VARIABLE} ${_contains_cuda} PARENT_SCOPE)
 endfunction()
 
+#
+# celeritas_add_library
+#
+# Add a library taking into account whether it contains
+# or depends on separatable CUDA code.
+#
+#
 function(celeritas_add_library target)
 
   celeritas_sources_contains_cuda(_contains_cuda ${ARGN})
@@ -188,7 +231,7 @@ function(celeritas_depends_on OUTVARNAME lib potentialdepend)
 endfunction()
 
 
-
+# Return the 'real' target name whether the output is an alias or not.
 function(celeritas_strip_alias OUTVAR target)
   if(TARGET ${target})
     get_target_property(_target_alias ${target} ALIASED_TARGET)
@@ -199,7 +242,8 @@ function(celeritas_strip_alias OUTVAR target)
   set(${OUTVAR} ${target} PARENT_SCOPE)
 endfunction()
 
-
+# Retrieve the "middle" library, i.e. given a target, the
+# target name to be used as input to the linker of dependent libraries.
 function(celeritas_use_middle_lib_in_property target property)
   get_target_property(_target_libs ${target} ${property})
 
@@ -347,6 +391,10 @@ function(celeritas_target_link_libraries target)
 
 endfunction()
 
+#
+# Return a flat list of all the direct and indirect dependencies of 'target'
+# which are libraries containing CUDA separatable code.
+#
 function(celeritas_cuda_gather_dependencies outlist target)
   if(TARGET ${target})
     celeritas_strip_alias(target ${target})
