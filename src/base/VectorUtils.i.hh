f73125fd00e1ac5e1f2fd9c766714f6b711fcc9b
Paul Romano <paul.k.romano@gmail.com>
2021-10-19 13:57:51 -0500

    Add linspace helper function

--- src/base/VectorUtils.i.hh
+++ src/base/VectorUtils.i.hh
@@ -8,6 +8,7 @@
 
 #include <iterator>
 #include "Assert.hh"
+#include "base/Range.hh"
 
 namespace celeritas
 {
@@ -54,6 +55,36 @@ Span<T> move_extend(std::vector<T>&& ext, std::vector<T>* base)
                  std::make_move_iterator(ext.end()));
     ext.clear();
     return {base->data() + start, base->data() + base->size()};
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Return evenly spaced numbers over a specific interval
+ */
+template<class T>
+std::vector<real_type> linspace(T start, T stop, size_type n)
+{
+    CELER_EXPECT(n >= 0);
+    std::vector<real_type> result(n);
+
+    // Convert input values to real_type
+    real_type start_c = start;
+    real_type stop_c  = stop;
+
+    if (n == 1)
+    {
+        result[0] = start_c;
+    }
+    else if (n > 1)
+    {
+        real_type delta = (stop_c - start_c) / (n - 1);
+        for (auto i : range(n - 1))
+        {
+            result[i] = start_c + delta * i;
+        }
+        result[n - 1] = stop_c;
+    }
+    return result;
 }
 
 //---------------------------------------------------------------------------//
