0c2a01f44f5f4cf6df0995a9b29ca24c17a8e420
Seth R Johnson <johnsonsr@ornl.gov>
2021-10-20 09:48:20 -0400

    Add stream printing to surfaces

---
+++ src/orange/surfaces/SurfaceIO.hh
@@ -0,0 +1,28 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2021 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file SurfaceIO.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include <iosfwd>
+#include "SurfaceTypeTraits.hh"
+#include "../Types.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+//!@{
+//! Print surfaces to a stream.
+template<Axis T>
+std::ostream& operator<<(std::ostream& os, const PlaneAligned<T>& s);
+
+template<Axis T>
+std::ostream& operator<<(std::ostream& os, const CylCentered<T>& s);
+
+std::ostream& operator<<(std::ostream& os, const GeneralQuadric& s);
+//!@}
+//---------------------------------------------------------------------------//
+} // namespace celeritas
