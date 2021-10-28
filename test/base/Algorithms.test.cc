b5731f4f99dd7bebef05a2847152d57982b09afe
Seth R Johnson <johnsonsr@ornl.gov>
2021-10-28 07:20:07 -0400

    Add device-friendly forward and rearrange Algorithms

--- test/base/Algorithms.test.cc
+++ test/base/Algorithms.test.cc
@@ -8,16 +8,41 @@
 #include "base/Algorithms.hh"
 
 #include <algorithm>
+#include <functional>
+#include <type_traits>
 #include "celeritas_test.hh"
+
+struct Foo
+{
+};
+
+template<class Expected, class T>
+void test_forward_impl(T&& val)
+{
+    EXPECT_TRUE(
+        (std::is_same<Expected, decltype(celeritas::forward<T>(val))>::value));
+}
 
 //---------------------------------------------------------------------------//
 // TESTS
 //---------------------------------------------------------------------------//
 
-TEST(AlgorithmsTest, minmax)
+TEST(UtilityTest, forward)
 {
-    EXPECT_EQ(1, celeritas::min<int>(1, 2));
-    EXPECT_EQ(2, celeritas::max<int>(1, 2));
+    Foo       foo;
+    const Foo cfoo;
+
+    test_forward_impl<Foo&>(foo);
+    test_forward_impl<const Foo&>(cfoo);
+    test_forward_impl<Foo&&>(Foo{});
+}
+
+TEST(UtilityTest, move)
+{
+    Foo foo;
+
+    EXPECT_TRUE((std::is_same<Foo&&, decltype(celeritas::move(foo))>::value));
+    EXPECT_TRUE((std::is_same<Foo&&, decltype(celeritas::move(Foo{}))>::value));
 }
 
 TEST(AlgorithmsTest, clamp)
@@ -28,16 +53,6 @@ TEST(AlgorithmsTest, clamp)
     EXPECT_DOUBLE_EQ(1.2345, clamp_to_nonneg(1.2345));
     EXPECT_DOUBLE_EQ(0.0, clamp_to_nonneg(-123));
     EXPECT_TRUE(std::isnan(celeritas::clamp_to_nonneg(nan)));
-}
-
-TEST(AlgorithmsTest, ipow)
-{
-    EXPECT_DOUBLE_EQ(1, celeritas::ipow<0>(0.0));
-    EXPECT_EQ(123.456, celeritas::ipow<1>(123.456));
-    EXPECT_EQ(8, (celeritas::ipow<3>(2)));
-    EXPECT_FLOAT_EQ(0.001f, celeritas::ipow<3>(0.1f));
-    EXPECT_EQ(1e4, celeritas::ipow<4>(10.0));
-    EXPECT_TRUE((std::is_same<int, decltype(celeritas::ipow<4>(5))>::value));
 }
 
 TEST(AlgorithmsTest, lower_bound)
@@ -62,22 +77,47 @@ TEST(AlgorithmsTest, lower_bound)
     }
 }
 
+TEST(AlgorithmsTest, minmax)
+{
+    EXPECT_EQ(1, celeritas::min<int>(1, 2));
+    EXPECT_EQ(2, celeritas::max<int>(1, 2));
+}
+
 TEST(AlgorithmsTest, min_element)
 {
     std::vector<int> v;
+
     auto             min_element_idx = [&v]() {
         return celeritas::min_element(v.begin(), v.end()) - v.begin();
+    };
+    auto min_element_gt_idx = [&v]() {
+        return celeritas::min_element(v.begin(), v.end(), std::greater<int>())
+               - v.begin();
     };
 
     // Empty vector will return 0, which is off-the-end
     EXPECT_EQ(0, min_element_idx());
+    EXPECT_EQ(0, min_element_gt_idx());
 
     v = {100};
     EXPECT_EQ(0, min_element_idx());
+    EXPECT_EQ(0, min_element_gt_idx());
 
     v = {10, 2, 100, 3, -1};
     EXPECT_EQ(4, min_element_idx());
+    EXPECT_EQ(2, min_element_gt_idx());
 
     v[2] = -100;
     EXPECT_EQ(2, min_element_idx());
+    EXPECT_EQ(0, min_element_gt_idx());
+}
+
+TEST(MathTest, ipow)
+{
+    EXPECT_DOUBLE_EQ(1, celeritas::ipow<0>(0.0));
+    EXPECT_EQ(123.456, celeritas::ipow<1>(123.456));
+    EXPECT_EQ(8, (celeritas::ipow<3>(2)));
+    EXPECT_FLOAT_EQ(0.001f, celeritas::ipow<3>(0.1f));
+    EXPECT_EQ(1e4, celeritas::ipow<4>(10.0));
+    EXPECT_TRUE((std::is_same<int, decltype(celeritas::ipow<4>(5))>::value));
 }
