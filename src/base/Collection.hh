9771463f33ed74278d5c2289f476d57effb22fc9
Seth R Johnson <johnsonsr@ornl.gov>
2021-11-20 09:10:22 -0500

    Change constexpr to forceinline
    I'm surprised this worked since vector/DeviceVector do not have
    constexpr attributes on those functions.

--- src/base/Collection.hh
+++ src/base/Collection.hh
@@ -236,9 +236,17 @@ class Collection
     inline CELER_FUNCTION SpanT      operator[](AllItemsT);
     inline CELER_FUNCTION SpanConstT operator[](AllItemsT) const;
 
-    // Direct accesors to underlying data
-    CELER_CONSTEXPR_FUNCTION size_type size() const;
-    CELER_CONSTEXPR_FUNCTION bool      empty() const;
+    //!@{
+    //! Direct accesors to underlying data
+    CELER_FORCEINLINE_FUNCTION size_type size() const
+    {
+        return this->storage().size();
+    }
+    CELER_FORCEINLINE_FUNCTION bool empty() const
+    {
+        return this->storage().empty();
+    }
+    //!@}
 
   private:
     //// DATA ////
