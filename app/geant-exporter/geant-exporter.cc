f84bb549ac5fc331bdeb9d0568d53f163182d844
Stefano C Tognini <togninis@ornl.gov>
2021-12-14 14:14:44 -0500

    Remove useless includes

--- app/geant-exporter/geant-exporter.cc
+++ app/geant-exporter/geant-exporter.cc
@@ -54,32 +54,6 @@
 #include "GeantLoggerAdapter.hh"
 #include "GeantExceptionHandler.hh"
 
-//////////////////////////////////
-#include <iostream>
-#include "globals.hh"
-#include "G4UnitsTable.hh"
-#include "G4DataVector.hh"
-
-#include "G4ParticleTable.hh"
-#include "G4Gamma.hh"
-#include "G4Positron.hh"
-#include "G4Electron.hh"
-#include "G4Proton.hh"
-
-#include "G4NistManager.hh"
-#include "G4Material.hh"
-#include "G4Element.hh"
-
-#include <G4EmParameters.hh>
-#include "G4LivermorePhotoElectricModel.hh"
-#include "G4KleinNishinaCompton.hh"
-#include "G4BetheHeitlerModel.hh"
-#include "G4eeToTwoGammaModel.hh"
-#include "G4MollerBhabhaModel.hh"
-#include "G4SeltzerBergerModel.hh"
-#include "G4BetheBlochModel.hh"
-//////////////////////////////////
-
 using namespace geant_exporter;
 namespace celer_pdg = celeritas::pdg;
 using celeritas::ImportData;
