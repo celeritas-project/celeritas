eba30874b049ce3ff4f5ba11f1f0dc9647d20d1a
Seth R Johnson <johnsonsr@ornl.gov>
2022-01-09 09:50:03 -0500

    Update copyrights

--- src/physics/em/generated/LivermorePEInteract.hh
+++ src/physics/em/generated/LivermorePEInteract.hh
@@ -1,5 +1,5 @@
 //----------------------------------*-hh-*-----------------------------------//
-// Copyright 2021 UT-Battelle, LLC, and other Celeritas developers.
+// Copyright 2021-2022 UT-Battelle, LLC, and other Celeritas developers.
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
