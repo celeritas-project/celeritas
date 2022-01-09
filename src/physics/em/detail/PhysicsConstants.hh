d6e59297df3500586e91a83b0039a2ec9591a9aa
Seth R Johnson <johnsonsr@ornl.gov>
2022-01-09 10:09:43 -0500

    Fix documentation

--- src/physics/em/detail/PhysicsConstants.hh
+++ src/physics/em/detail/PhysicsConstants.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file PhysicsConstants
+//! \file PhysicsConstants.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
