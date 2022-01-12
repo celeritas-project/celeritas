3357303abd9dd2eee295e478bb80dc9193551cc5
Seth R Johnson <johnsonsr@ornl.gov>
2022-01-12 18:45:53 -0500

    Move HepMC3 target creation into FindHepMC3.cmake

--- cmake/FindHepMC3.cmake
+++ cmake/FindHepMC3.cmake
@@ -15,4 +15,13 @@ find_package(HepMC3 QUIET CONFIG)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(HepMC3 CONFIG_MODE)
 
+# HepMC3's cmake config file does not currently provide an imported target.
+if(NOT TARGET HepMC3::HepMC3)
+  add_library(HepMC3::HepMC3 UNKNOWN IMPORTED)
+  set_target_properties(HepMC3::HepMC3 PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${HEPMC3_INCLUDE_DIR}"
+    IMPORTED_LOCATION "${HEPMC3_LIB}"
+  )
+endif()
+
 #-----------------------------------------------------------------------------#
