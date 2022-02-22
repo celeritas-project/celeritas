1aa98ce0bacf44ab8660dcbc128f58f72963dfd7
Soon Yung Jun <syjun@wc.fnal.gov>
2022-02-22 09:12:01 -0600

    Added units and additional comments, and renamed some of variables

--- src/physics/em/detail/UrbanMscData.hh
+++ src/physics/em/detail/UrbanMscData.hh
@@ -16,36 +16,43 @@ namespace detail
 {
 //---------------------------------------------------------------------------//
 /*!
- * UrbanMscModel settable parameters
+ * UrbanMscModel settable parameters and default values.
+ *
+ * \f$ \tau = t/\lambda \f$ where t is the true path length and \f$ \lambda \f$
+ * is the mean free path of the multiple scattering. The range and safety
+ * factors are used in step limitation algorithms and default values are
+ * chosen to balance between simulation time and precision.
  */
 struct UrbanMscParameters
 {
-    real_type tau_small{1e-16};   //!< small value of tau
-    real_type tau_big{8};         //!< big value of tau
-    real_type tau_limit{1e-6};    //!< limit of tau
-    real_type lambda_limit{0.01}; //!< lambda limit (default: 1 mm)
-    real_type range_fact{0.04};   //!< range_factor for e-/e+ (0.02 for mu/h)
-    real_type safety_fact{0.6};   //!< safety factor
+    real_type tau_small{1e-16};                       //!< small value of tau
+    real_type tau_big{8};                             //!< big value of tau
+    real_type tau_limit{1e-6};                        //!< limit of tau
+    real_type lambda_limit{0.01 * units::centimeter}; //!< lambda limit
+    real_type range_fact{0.04}; //!< range_factor for e-/e+ (0.2 for muon/h)
+    real_type safety_fact{0.6}; //!< safety factor
 };
 
 //---------------------------------------------------------------------------//
 /*!
- * UrbanMsc material data: G4UrbanMscModel::mscData
+ * UrbanMsc material data (see G4UrbanMscModel::mscData) is a set of
+ * precalculated material dependent parameters used in sampling the angular
+ * distribution of MSC, \f$ \cos\theta \f$. All parameters are unitless.
  */
 struct UrbanMscMaterialData
 {
-    real_type zeff;     //!< effective atomic_number
-    real_type z23;      //!< ipow<4>(log(zeff)/6)
-    real_type coeffth1; //!< correction in theta_0 formula
-    real_type coeffth2; //!< correction in theta_0 formula
-    real_type coeffc1;  //!< coefficient of tail parameters
-    real_type coeffc2;  //!< coefficient of tail parameters
-    real_type coeffc3;  //!< coefficient of tail parameters
-    real_type coeffc4;  //!< coefficient of tail parameters
-    real_type stepmina; //!< coefficient of the step minimum calculation
-    real_type stepminb; //!< coefficient of the step minimum calculation
-    real_type doverra;  //!< distance/range for e-/e+
-    real_type doverrb;  //!< distance/range for mu/h
+    real_type zeff;        //!< effective atomic_number
+    real_type z23;         //!< ipow<4>(log(zeff)/6)
+    real_type coeffth1;    //!< correction in theta_0 formula
+    real_type coeffth2;    //!< correction in theta_0 formula
+    real_type coeffc1;     //!< coefficient of tail parameters
+    real_type coeffc2;     //!< coefficient of tail parameters
+    real_type coeffc3;     //!< coefficient of tail parameters
+    real_type coeffc4;     //!< coefficient of tail parameters
+    real_type stepmin_a;   //!< coefficient of the step minimum calculation
+    real_type stepmin_b;   //!< coefficient of the step minimum calculation
+    real_type d_over_r;    //!< the maximum distance/range for e-/e+
+    real_type d_over_r_mh; //!< the maximum distance/range for muon/h
 };
 
 //---------------------------------------------------------------------------//
