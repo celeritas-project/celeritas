4e60903a28c34c9f706484b1bc381ba4b75b735e
Soon Yung Jun <syjun@wc.fnal.gov>
2022-02-24 21:49:40 -0600

    Add a reference and make a variable name more clearer (w->z16)

--- src/physics/em/UrbanMscModel.cc
+++ src/physics/em/UrbanMscModel.cc
@@ -114,7 +114,8 @@ void UrbanMscModel::build_data(HostValue* data, const MaterialParams& materials)
 //---------------------------------------------------------------------------//
 /*!
  * Build UrbanMsc data per material.
- * Tabulated data based on G4UrbanMscModel::InitialiseModelCache()
+ * Tabulated data based on G4UrbanMscModel::InitialiseModelCache() and
+ * documented in section 8.1.5 of the Geant4 10.7 Physics Reference Manual.
  */
 auto UrbanMscModel::calc_material_data(const MaterialView& material_view)
     -> MaterialData
@@ -137,13 +138,13 @@ auto UrbanMscModel::calc_material_data(const MaterialView& material_view)
     real_type log_z = std::log(zeff);
 
     // Correction in the (modified Highland-Lynch-Dahl) theta_0 formula
-    real_type w   = std::exp(log_z / 6);
-    real_type fz  = 0.990395 + w * (-0.168386 + w * 0.093286);
+    real_type z16 = std::exp(log_z / 6);
+    real_type fz  = 0.990395 + z16 * (-0.168386 + z16 * 0.093286);
     data.coeffth1 = fz * (1 - 8.7780e-2 / zeff);
     data.coeffth2 = fz * (4.0780e-2 + 1.7315e-4 * zeff);
 
     // Tail parameters
-    real_type z13 = ipow<2>(w);
+    real_type z13 = ipow<2>(z16);
     data.coeffc1  = 2.3785 - z13 * (4.1981e-1 - z13 * 6.3100e-2);
     data.coeffc2  = 4.7526e-1 + z13 * (1.7694 - z13 * 3.3885e-1);
     data.coeffc3  = 2.3683e-1 - z13 * (1.8111 - z13 * 3.2774e-1);
