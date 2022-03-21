e24838c256ca81414f39825f4a9fd2702792b049
Seth R Johnson <johnsonsr@ornl.gov>
2022-03-21 11:13:22 -0400

    Update environment to load geant4 data

--- scripts/env/summit.sh
+++ scripts/env/summit.sh
@@ -6,6 +6,8 @@ module load DefApps/default cuda/11.0 gcc/9.1
 module unload python
 export PATH=${_celer_base}/spack/view/bin:$PATH
 export CMAKE_PREFIX_PATH=${_celer_base}/spack/view:${CMAKE_PREFIX_PATH}
+export MODULEPATH=/ccs/proj/csc404/spack/share/spack/lmod/linux-rhel8-ppc64le/gcc/9.1.0:$MODULEPATH
+module load geant4-data
 
 
 # NOTE: these might only be necessary when we add MPI support
