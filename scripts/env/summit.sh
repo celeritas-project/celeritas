fadd06ea357c3f1923aca7ffb6601889064aa663
Seth R Johnson <johnsonsr@ornl.gov>
2022-03-24 09:43:05 -0400

    Disable cxx extensions and explicitly specify compilers

--- scripts/env/summit.sh
+++ scripts/env/summit.sh
@@ -7,4 +7,7 @@ module unload cmake python
 export PATH=${_celer_base}/spack/view/bin:$PATH
 export CMAKE_PREFIX_PATH=${_celer_base}/spack/view:${CMAKE_PREFIX_PATH}
 export MODULEPATH=/ccs/proj/csc404/spack/share/spack/lmod/linux-rhel8-ppc64le/Core:$MODULEPATH
+export CXX=${OLCF_GCC_ROOT}/bin/g++
+export CC=${OLCF_GCC_ROOT}/bin/gcc
+export CUDACXX=${OLCF_CUDA_ROOT}/bin/nvcc
 module load geant4-data
