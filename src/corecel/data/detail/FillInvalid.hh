36859b0d5074d0ce7a48ecfda63edf200fcb1a45
Seth R Johnson <johnsonsr@ornl.gov>
2022-05-18 06:35:43 -0400

    Fix documentation

--- src/corecel/data/detail/FillInvalid.hh
+++ src/corecel/data/detail/FillInvalid.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file corecel/data/detail/InvalidValueTraits.hh
+//! \file corecel/data/detail/FillInvalid.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
