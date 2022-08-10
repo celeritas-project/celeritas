9afb714e637cbf34956c2911e0c1ec7736b37d8f
Seth R Johnson <johnsonsr@ornl.gov>
2022-08-10 11:22:45 -0400

    Add process builder

---
+++ src/celeritas/phys/ProcessBuilder.hh
@@ -0,0 +1,81 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2022 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celeritas/phys/ProcessBuilder.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include "celeritas/io/ImportProcess.hh"
+
+#include "Process.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+class MaterialParams;
+class ParticleParams;
+struct ImportData;
+class ImportedProcesses;
+
+//---------------------------------------------------------------------------//
+/*!
+ * Construct Celeritas EM processes from imported data.
+ *
+ * Note that imported data may have multiple duplicate "process" entries, one
+ * per particle type.
+ */
+class ProcessBuilder
+{
+  public:
+    //!@{
+    //! \name Type aliases
+    using SPProcess       = std::shared_ptr<Process>;
+    using SPConstParticle = std::shared_ptr<const ParticleParams>;
+    using SPConstMaterial = std::shared_ptr<const MaterialParams>;
+    //!@}
+
+    struct Options
+    {
+        bool brem_combined{false};
+    };
+
+  public:
+    // Construct from shared data
+    ProcessBuilder(const ImportData& data,
+                   Options           options,
+                   SPConstParticle   particle,
+                   SPConstMaterial   material);
+
+    // Default destructor
+    ~ProcessBuilder();
+
+    // Create a process from the data
+    SPProcess operator()(ImportProcessClass ipc);
+
+  private:
+    //// DATA ////
+
+    std::shared_ptr<const ParticleParams> particle_;
+    std::shared_ptr<const MaterialParams> material_;
+    std::shared_ptr<ImportedProcesses>    processes_;
+    bool                                  brem_combined_;
+    bool                                  enable_lpm_;
+    bool                                  use_integral_xs_;
+
+    //// HELPER FUNCTIONS ////
+
+    auto build_annihilation() -> SPProcess;
+    auto build_compton() -> SPProcess;
+    auto build_conversion() -> SPProcess;
+    auto build_coulomb_scat() -> SPProcess;
+    auto build_ebrems() -> SPProcess;
+    auto build_eioni() -> SPProcess;
+    auto build_msc() -> SPProcess;
+    auto build_photoelectric() -> SPProcess;
+    auto build_rayleigh() -> SPProcess;
+};
+
+//---------------------------------------------------------------------------//
+} // namespace celeritas
