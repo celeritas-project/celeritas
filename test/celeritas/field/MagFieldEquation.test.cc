72996dd41c293875b77a456322bf816336f471de
Seth R Johnson <johnsonsr@ornl.gov>
2022-09-01 19:53:09 -0400

    Add MagFieldEquation test

---
+++ test/celeritas/field/MagFieldEquation.test.cc
@@ -0,0 +1,87 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2022 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celeritas/field/MagFieldEquation.test.cc
+//---------------------------------------------------------------------------//
+#include "celeritas/field/MagFieldEquation.hh"
+
+#include "celeritas_test.hh"
+
+using celeritas::units::ElementaryCharge;
+
+namespace celeritas
+{
+namespace test
+{
+//---------------------------------------------------------------------------//
+
+Real3 dummy_field(const Real3& pos)
+{
+    // Rotate and scale
+    Real3 result;
+    result[0] = real_type(0.5) * pos[1];
+    result[1] = real_type(1.0) * pos[2];
+    result[2] = real_type(2.0) * pos[0];
+    return result;
+}
+
+template<class FieldT>
+CELER_FUNCTION decltype(auto)
+make_equation(FieldT&& field, ElementaryCharge charge)
+{
+    using Equation_t = celeritas::MagFieldEquation<FieldT>;
+    return Equation_t{::celeritas::forward<FieldT>(field), charge};
+}
+
+void print_expected(const OdeState& s)
+{
+    cout << "/*** BEGIN CODE ***/\n"
+         << "EXPECT_VEC_SOFT_EQ(Real3(" << repr(s.pos) << "), result.pos);\n"
+         << "EXPECT_VEC_SOFT_EQ(Real3(" << repr(s.mom) << "), result.mom);\n"
+         << "/*** END CODE ***/\n";
+}
+
+//---------------------------------------------------------------------------//
+
+TEST(MagFieldEquationTest, charged)
+{
+    auto eval = make_equation(dummy_field, ElementaryCharge{3});
+    {
+        OdeState result = eval(OdeState{.pos = {1, 2, 3}, .mom = {0, 0, 1}});
+        EXPECT_VEC_SOFT_EQ(Real3({0, 0, 1}), result.pos);
+        EXPECT_VEC_SOFT_EQ(Real3({-0.02698132122, 0.00899377374, 0}),
+                           result.mom);
+    }
+    {
+        OdeState result
+            = eval(OdeState{.pos = {0.5, -2, -1}, .mom = {1, 2, 3}});
+        EXPECT_VEC_SOFT_EQ(
+            Real3({0.26726124191242, 0.53452248382485, 0.80178372573727}),
+            result.pos);
+        EXPECT_VEC_SOFT_EQ(
+            Real3({0.012018435696159, -0.009614748556927, 0.0024036871392318}),
+            result.mom);
+    }
+    if (CELERITAS_DEBUG)
+    {
+        EXPECT_THROW(eval(OdeState{.pos = {0.5, -2, -1}, .mom = {0, 0, 0}}),
+                     DebugError);
+    }
+}
+
+TEST(MagFieldEquationTest, neutral)
+{
+    auto eval = make_equation(dummy_field, ElementaryCharge{0});
+    {
+        OdeState s{.pos = {0.5, -2, -1}, .mom = {0, 0, 1}};
+        OdeState result = eval(s);
+        EXPECT_VEC_SOFT_EQ(Real3({0, 0, 1}), result.pos);
+        EXPECT_VEC_SOFT_EQ(Real3({0, 0, 0}), result.mom);
+    }
+}
+
+//---------------------------------------------------------------------------//
+} // namespace test
+} // namespace celeritas
