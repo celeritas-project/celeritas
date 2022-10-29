63f3d8ff524c28a00fdc277c714ab926dcee32e1
Seth R Johnson <johnsonsr@ornl.gov>
2022-10-29 13:33:15 -0400

    Complete test and correctly propagate CXX version requirement

--- scripts/ci/test-installation/example.cc
+++ scripts/ci/test-installation/example.cc
@@ -5,13 +5,16 @@
 //---------------------------------------------------------------------------//
 //! \file scripts/ci/test-installation/example.cc
 //---------------------------------------------------------------------------//
-
 #include "corecel/io/Logger.hh"
 #include "corecel/sys/Device.hh"
+#include "corecel/sys/MpiCommunicator.hh"
 #include "corecel/sys/ScopedMpiInit.hh"
 
 int main(int argc, char* argv[])
 {
+    using celeritas::MpiCommunicator;
+    using celeritas::ScopedMpiInit;
+
     // Initialize MPI
     ScopedMpiInit   scoped_mpi(&argc, &argv);
     MpiCommunicator comm
