fde376b4b67b47f0bf8771960e795eab5079134c
Seth R Johnson <johnsonsr@ornl.gov>
2022-11-06 10:42:54 -0500

    Fix tracklauncher location

--- src/celeritas/global/detail/TrackLauncherImpl.hh
+++ src/celeritas/global/detail/TrackLauncherImpl.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/track/detail/TrackLauncherImpl.hh
+//! \file celeritas/global/detail/TrackLauncherImpl.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
