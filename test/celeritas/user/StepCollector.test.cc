e0dabb89f6a0dda85c136656ca79b006ccc9f154
Seth R Johnson <johnsonsr@ornl.gov>
2022-11-15 13:43:40 -0600

    Only check volume IDs for ORANGE

--- test/celeritas/user/StepCollector.test.cc
+++ test/celeritas/user/StepCollector.test.cc
@@ -110,8 +110,11 @@ TEST_F(KnStepCollectorTest, single_step)
     EXPECT_VEC_EQ(expected_track, result.track);
     static const int expected_step[] = {1, 1, 1, 1, 1, 1, 1, 1};
     EXPECT_VEC_EQ(expected_step, result.step);
-    static const int expected_volume[] = {1, 1, 1, 1, 1, 1, 1, 1};
-    EXPECT_VEC_EQ(expected_volume, result.volume);
+    if (!CELERITAS_USE_VECGEOM)
+    {
+        static const int expected_volume[] = {1, 1, 1, 1, 1, 1, 1, 1};
+        EXPECT_VEC_EQ(expected_volume, result.volume);
+    }
     static const double expected_pos[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     EXPECT_VEC_SOFT_EQ(expected_pos, result.pos);
@@ -131,8 +134,11 @@ TEST_F(KnStepCollectorTest, two_step)
     EXPECT_VEC_EQ(expected_track, result.track);
     static const int expected_step[] = {1, 2, 1, 2, 1, 2, 1, 2};
     EXPECT_VEC_EQ(expected_step, result.step);
-    static const int expected_volume[] = {1, 1, 1, 1, 1, 2, 1, 2};
-    EXPECT_VEC_EQ(expected_volume, result.volume);
+    if (!CELERITAS_USE_VECGEOM)
+    {
+        static const int expected_volume[] = {1, 1, 1, 1, 1, 2, 1, 2};
+        EXPECT_VEC_EQ(expected_volume, result.volume);
+    }
     static const double expected_pos[] = {0, 0, 0, 2.6999255778482, 0, 0, 0, 0, 0, 3.5717683161497, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 5, 0, 0};
     EXPECT_VEC_SOFT_EQ(expected_pos, result.pos);
     static const double expected_dir[] = {1, 0, 0, 0.45619379667222, 0.14402721708137, -0.87814769863479, 1, 0, 0, 0.8985574206844, -0.27508545475671, -0.34193940152356, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0};
@@ -159,8 +165,11 @@ TEST_F(TestEm3CollectorTest, four_step)
         EXPECT_VEC_EQ(expected_track, result.track);
         static const int expected_step[] = {1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4};
         EXPECT_VEC_EQ(expected_step, result.step);
-        static const int expected_volume[] = {1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2};
-        EXPECT_VEC_EQ(expected_volume, result.volume);
+        if (!CELERITAS_USE_VECGEOM)
+        {
+            static const int expected_volume[] = {1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2};
+            EXPECT_VEC_EQ(expected_volume, result.volume);
+        }
         static const double expected_pos[] = {-22, 0, 0, -20, 0.62729376699828, 0, -19.974881515749, 0.63919659778087, 0.0048217121175519, -19.934025722828, 0.64565957014582, 0.023962145128973, -22, 0, 0, -20, -0.6272937669977, 0, -19.968347491864, -0.64477993221197, 0.0094975131089902, -19.926616737736, -0.65875813304638, 0.027537430238836, -22, 0, 0, -20, 0.62729376699805, 0, -19.972019729151, 0.66426222006667, -0.0037691265086884, -19.982100079658, 0.68574627311443, 0.027941516714081, -22, 0, 0, -20, -0.62729376699778, 0, -19.975083374908, -0.65817577010885, -0.0029322565536058, -19.959498010488, -0.70081357929493, 0.0044325483363873};
         EXPECT_VEC_SOFT_EQ(expected_pos, result.pos);
         static const double expected_dir[] = {1, 0, 0, 0.82087264698346, 0.57111128288133, 0, 0.86898307494434, 0.46974361575277, 0.15559354395281, 0.93933735506149, 0.33063938350342, -0.091230101734915, 1, 0, 0, 0.8208726469842, -0.57111128288028, 0, 0.97025863720971, -0.23236953975431, 0.067842272325867, 0.075841950532517, -0.590557814686, 0.80342359067475, 1, 0, 0, 0.82087264698375, 0.57111128288092, 0, -0.21530637869043, 0.77276783694, 0.59705362697691, -0.48956806668328, 0.50481519308475, 0.71097449245019, 1, 0, 0, 0.8208726469841, -0.57111128288041, 0, 0.50418543298446, -0.77669926497628, 0.37753847612074, 0.043343361560974, -0.9667738756985, -0.25193178893401};
