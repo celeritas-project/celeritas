d3bce312d46568274a1052e7c06cb58656bd5457
Seth R Johnson <johnsonsr@ornl.gov>
2022-12-17 07:57:55 -0500

    Document SetupOptions

--- src/accel/SetupOptions.hh
+++ src/accel/SetupOptions.hh
@@ -25,6 +25,7 @@ struct SetupOptions
     {
         return static_cast<size_type>(-1);
     }
+
     // TODO: names of sensitive detectors
     // TODO: along-step construction option/callback
 
@@ -32,16 +33,27 @@ struct SetupOptions
     // temporary GDML and re-read)
     std::string geometry_file;
 
+    //!@{
+    //! \name Celeritas stepper options
+    //! Number of track "slots" to be transported simultaneously
     size_type   max_num_tracks{};
+    //! Maximum number of events in use
     size_type   max_num_events{};
+    //! Limit on number of step iterations before aborting
     size_type   max_steps = no_max_steps();
+    //! Maximum number of track initializers (primaries+secondaries)
     size_type   initializer_capacity{};
+    //! At least the average number of secondaries per track slot
     real_type   secondary_stack_factor{};
-    bool        sync{};
+    //! Sync the GPU at every kernel for error checking
+    bool sync{false};
+    //!@}
 
-    // CUDA options
+    //!@{
+    //! \name CUDA options
     size_type cuda_stack_size{};
     size_type cuda_heap_size{};
+    //!@}
 };
 
 //---------------------------------------------------------------------------//
