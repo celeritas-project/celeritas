0feae99e3802b6f35aa2708901e2b8f70bf2d9fb
Seth R Johnson <johnsonsr@ornl.gov>
2022-12-23 07:43:44 -0600

    Revert "Use a raw pointer for SensitiveHitsCollection"
    This reverts commit 5be75e0575c3cf97902ac6e1c58bf8e7687f2cca.

--- app/demo-geant-integration/SensitiveDetector.cc
+++ app/demo-geant-integration/SensitiveDetector.cc
@@ -7,7 +7,6 @@
 //---------------------------------------------------------------------------//
 #include "SensitiveDetector.hh"
 
-#include <G4AutoDelete.hh>
 #include <G4HCofThisEvent.hh>
 #include <G4SDManager.hh>
 #include <G4Step.hh>
@@ -21,7 +20,7 @@ namespace demo_geant
 {
 //---------------------------------------------------------------------------//
 SensitiveDetector::SensitiveDetector(std::string name)
-    : G4VSensitiveDetector(name), hcid_(-1)
+    : G4VSensitiveDetector(name), hcid_{-1}, collection_{nullptr}
 {
     this->collectionName.insert(name + "_HC");
 }
@@ -32,16 +31,23 @@ SensitiveDetector::SensitiveDetector(std::string name)
  */
 void SensitiveDetector::Initialize(G4HCofThisEvent* hce)
 {
-    collection_ = new SensitiveHitsCollection(this->SensitiveDetectorName,
-                                              this->collectionName[0]);
+    auto collection = std::make_unique<SensitiveHitsCollection>(
+        this->SensitiveDetectorName, this->collectionName[0]);
+
     if (hcid_ < 0)
     {
-        // Initialize during the first event
-        hcid_ = G4SDManager::GetSDMpointer()->GetCollectionID(collection_);
+        // Initialize during the first event. The SD collection was registered
+        // inside DetectorConstruction::ConstructSDandField on the local
+        // thread.
+        hcid_ = G4SDManager::GetSDMpointer()->GetCollectionID(collection.get());
         CELER_ASSERT(hcid_ >= 0);
     }
-    hce->AddHitsCollection(hcid_, collection_);
-    G4AutoDelete::Register(collection_);
+
+    // Save a pointer to the collection we just made before tranferring
+    // ownership to the HC manager for the event.
+    collection_ = collection.get();
+    hce->AddHitsCollection(hcid_, collection.release());
+    CELER_ENSURE(collection_);
 }
 
 //---------------------------------------------------------------------------//
@@ -50,7 +56,6 @@ void SensitiveDetector::Initialize(G4HCofThisEvent* hce)
  */
 bool SensitiveDetector::ProcessHits(G4Step* step, G4TouchableHistory*)
 {
-    CELER_EXPECT(collection_);
     auto edep = step->GetTotalEnergyDeposit();
 
     if (edep == 0)
