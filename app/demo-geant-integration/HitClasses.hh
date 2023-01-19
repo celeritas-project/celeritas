03f428c0e61cde925552c654116989f25107b00f
Soon Yung Jun <syjun@fnal.gov>
2023-01-19 13:33:44 -0600

    Remove unnecessary declaration of classes

--- app/demo-geant-integration/HitClasses.hh
+++ app/demo-geant-integration/HitClasses.hh
@@ -5,16 +5,5 @@
 //---------------------------------------------------------------------------//
 //! \file demo-geant-integration/HitClasses.hh
 //---------------------------------------------------------------------------//
-#include <G4VHit.hh>
-
 #include "HitRootIO.hh"
 #include "SensitiveHit.hh"
-//---------------------------------------------------------------------------//
-/*!
- * Declaration of classes for generating ROOT dictionaries
- */
-demo_geant::HitRootEvent e;
-std::vector<G4VHit*> vh;
-std::vector<demo_geant::SensitiveHit*> vsh;
-std::map<std::string, std::vector<G4VHit*>> mhc;
-#undef __G4String
