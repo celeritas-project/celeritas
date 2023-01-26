3833c40571c6b9a22d9243d10aed41080f951ea7
Seth R Johnson <johnsonsr@ornl.gov>
2023-01-26 12:36:01 -0500

    Add kernel context exception

---
+++ src/celeritas/global/KernelContextException.cc
@@ -0,0 +1,164 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2023 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celeritas/global/KernelContextException.cc
+//---------------------------------------------------------------------------//
+#include "KernelContextException.hh"
+
+#include "corecel/cont/ArrayIO.hh"
+#include "corecel/io/JsonPimpl.hh"
+#if CELERITAS_USE_JSON
+#    include "corecel/cont/Array.json.hh"
+#    include "corecel/math/QuantityIO.json.hh"
+#endif
+
+#include "CoreTrackView.hh"
+
+namespace celeritas
+{
+namespace
+{
+//---------------------------------------------------------------------------//
+#if CELERITAS_USE_JSON
+template<class V, class S>
+void insert_if_valid(char const* key,
+                     OpaqueId<V, S> const& val,
+                     nlohmann::json* obj)
+{
+    if (val)
+    {
+        (*obj)[key] = val.unchecked_get();
+    }
+}
+#endif
+template<class V, class S>
+std::ostream& operator<<(std::ostream& os, OpaqueId<V, S> const& v)
+{
+    if (v)
+    {
+        os << v.unchecked_get();
+    }
+    else
+    {
+        os << '?';
+    }
+    return os;
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace
+
+//---------------------------------------------------------------------------//
+/*!
+ * Construct with track data and kernel label.
+ */
+KernelContextException::KernelContextException(CoreHostRef const& data,
+                                               ThreadId thread,
+                                               std::string&& label)
+    : thread_(thread), label_(std::move(label))
+{
+    try
+    {
+        if (!thread)
+        {
+            // Make sure the thread is valid before trying to construct
+            // detailed debug informatino
+            throw std::exception();
+        }
+        CoreTrackView core(data.params, data.states, thread);
+        this->initialize(core);
+    }
+    catch (...)
+    {
+        // Ignore all exceptions while trying to process diagnostic information
+        what_ = label_ + " (error processing track state)";
+    }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * This class type's description.
+ */
+char const* KernelContextException::type() const
+{
+    return "KernelContextException";
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Save context to a JSON object.
+ */
+void KernelContextException::output(JsonPimpl* json) const
+{
+#if CELERITAS_USE_JSON
+    nlohmann::json j;
+#    define KCE_INSERT_IF_VALID(ATTR) insert_if_valid(#ATTR, ATTR##_, &j)
+
+    KCE_INSERT_IF_VALID(thread);
+    KCE_INSERT_IF_VALID(event);
+    KCE_INSERT_IF_VALID(track);
+    if (track_)
+    {
+        KCE_INSERT_IF_VALID(parent);
+        j["num_steps"] = num_steps_;
+        KCE_INSERT_IF_VALID(particle);
+        j["energy"] = energy_;
+        j["pos"] = pos_;
+        j["dir"] = dir_;
+        KCE_INSERT_IF_VALID(volume);
+        KCE_INSERT_IF_VALID(surface);
+        KCE_INSERT_IF_VALID(next_surface);
+    }
+    if (!label_.empty())
+    {
+        j["label"] = label_;
+    }
+#    undef KCE_INSERT_IF_VALID
+    json->obj = std::move(j);
+#else
+    (void)sizeof(json);
+#endif
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Save context to a JSON object.
+ */
+void KernelContextException::initialize(CoreTrackView const& core)
+{
+    {
+        auto const&& sim = core.make_sim_view();
+        event_ = sim.event_id();
+        track_ = sim.track_id();
+        parent_ = sim.parent_id();
+        num_steps_ = sim.num_steps();
+    }
+    {
+        auto const&& par = core.make_particle_view();
+        particle_ = par.particle_id();
+        energy_ = par.energy();
+    }
+    {
+        auto const&& geo = core.make_geo_view();
+        pos_ = geo.pos();
+        dir_ = geo.dir();
+        volume_ = geo.volume_id();
+        surface_ = geo.surface_id();
+        next_surface_ = geo.next_surface_id();
+    }
+    {
+        // Construct std::exception message
+        std::ostringstream os;
+        os << "kernel context: thread " << thread_ << " in '" << label_ << "'";
+        if (track_)
+        {
+            os << ", track " << track_ << " of event " << event_;
+        }
+        what_ = os.str();
+    }
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
---
+++ test/celeritas/global/KernelContextException.test.cc
@@ -0,0 +1,203 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2023 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celeritas/global/KernelContextException.test.cc
+//---------------------------------------------------------------------------//
+#include "celeritas/global/KernelContextException.hh"
+
+#include <exception>
+
+#include "corecel/Assert.hh"
+#include "corecel/Macros.hh"
+#include "corecel/io/JsonPimpl.hh"
+#include "celeritas/global/Stepper.hh"
+#include "celeritas/phys/PDGNumber.hh"
+#include "celeritas/phys/ParticleParams.hh"
+#include "celeritas/phys/Primary.hh"
+
+#include "../SimpleTestBase.hh"
+#include "StepperTestBase.hh"
+#include "celeritas_test.hh"
+
+using celeritas::units::MevEnergy;
+
+namespace celeritas
+{
+namespace test
+{
+namespace
+{
+std::string get_json_str(KernelContextException const& e)
+{
+#if CELERITAS_USE_JSON
+    JsonPimpl jp;
+    e.output(&jp);
+    return jp.obj.dump();
+#else
+    (void)sizeof(e);
+    return {};
+#endif
+}
+}  // namespace
+//---------------------------------------------------------------------------//
+
+class KernelContextExceptionTest : public SimpleTestBase, public StepperTestBase
+{
+  protected:
+    std::vector<Primary> make_primaries(size_type count) const override
+    {
+        Primary p;
+        p.particle_id = this->particle()->find(pdg::gamma());
+        p.energy = MevEnergy{10};
+        p.position = {0, 1, 0};
+        p.direction = {0, 0, 1};
+        p.time = 0;
+
+        CELER_ASSERT(p.particle_id);
+
+        std::vector<Primary> result(count, p);
+
+        for (auto i : range(count))
+        {
+            result[i].event_id = EventId{i / (count / 2)};
+            result[i].track_id = TrackId{i % (count / 2)};
+        }
+        return result;
+    }
+
+    size_type max_average_steps() const override { return 1000; }
+
+    void check_exception(std::exception_ptr eptr)
+    {
+        try
+        {
+            std::rethrow_exception(eptr);
+        }
+        catch (KernelContextException const& e)
+        {
+            caught_kce = true;
+            if (this->check_kce)
+            {
+                this->check_kce(e);
+            }
+            try
+            {
+                std::rethrow_if_nested(e);
+            }
+            catch (DebugError const& e)
+            {
+                caught_debug = true;
+                EXPECT_STREQ("test.cc", e.details().file);
+            }
+        }
+    }
+
+    std::function<void(KernelContextException const&)> check_kce;
+    bool caught_debug = false;
+    bool caught_kce = false;
+};
+
+TEST_F(KernelContextExceptionTest, typical)
+{
+    // Create some track slots
+    Stepper<MemSpace::host> step(this->make_stepper_input(16));
+
+    // Initialize some primaries and take a step
+    auto primaries = this->make_primaries(8);
+    step(make_span(primaries));
+
+    // Check for these values based on the step count and thread ID below
+    this->check_kce = [](KernelContextException const& e) {
+        EXPECT_STREQ(
+            "kernel context: thread 15 in 'test-kernel', track 3 of event 1",
+            e.what());
+        EXPECT_EQ(ThreadId{15}, e.thread());
+        EXPECT_EQ(EventId{1}, e.event());
+        EXPECT_EQ(TrackId{3}, e.track());
+        EXPECT_EQ(TrackId{}, e.parent());
+        EXPECT_EQ(1, e.num_steps());
+        EXPECT_EQ(ParticleId{0}, e.particle());
+        EXPECT_EQ(10, e.energy().value());
+        EXPECT_VEC_SOFT_EQ((Real3{0, 1, 5}), e.pos());
+        EXPECT_VEC_SOFT_EQ((Real3{0, 0, 1}), e.dir());
+        if (!CELERITAS_USE_VECGEOM)
+        {
+            EXPECT_EQ(VolumeId{2}, e.volume());
+            EXPECT_EQ(SurfaceId{11}, e.surface());
+        }
+        EXPECT_EQ(SurfaceId{}, e.next_surface());
+        if (CELERITAS_USE_JSON && !CELERITAS_USE_VECGEOM)
+        {
+            EXPECT_EQ(
+                R"json({"dir":[0.0,0.0,1.0],"energy":[10.0,"MeV"],"event":1,"label":"test-kernel","num_steps":1,"particle":0,"pos":[0.0,1.0,5.0],"surface":11,"thread":15,"track":3,"volume":2})json",
+                get_json_str(e));
+        }
+    };
+    // Since tracks are initialized back to front, the thread ID must be toward
+    // the end
+    CELER_TRY_HANDLE_CONTEXT(
+        throw DebugError({DebugErrorType::internal, "false", "test.cc", 0}),
+        this->check_exception,
+        KernelContextException(step.core_data(), ThreadId{15}, "test-kernel"));
+    EXPECT_TRUE(this->caught_debug);
+    EXPECT_TRUE(this->caught_kce);
+}
+
+TEST_F(KernelContextExceptionTest, uninitialized_track)
+{
+    // Create some track slots
+    Stepper<MemSpace::host> step(this->make_stepper_input(8));
+
+    // Initialize some primaries and take a step
+    auto primaries = this->make_primaries(4);
+    step(make_span(primaries));
+
+    this->check_kce = [](KernelContextException const& e) {
+        EXPECT_STREQ("kernel context: thread 1 in 'test-kernel'", e.what());
+        EXPECT_EQ(ThreadId{1}, e.thread());
+        EXPECT_EQ(EventId{}, e.event());
+        EXPECT_EQ(TrackId{}, e.track());
+        if (CELERITAS_USE_JSON)
+        {
+            EXPECT_EQ(R"json({"label":"test-kernel","thread":1})json",
+                      get_json_str(e));
+        }
+    };
+    CELER_TRY_HANDLE_CONTEXT(
+        throw DebugError({DebugErrorType::internal, "false", "test.cc", 0}),
+        this->check_exception,
+        KernelContextException(step.core_data(), ThreadId{1}, "test-kernel"));
+    EXPECT_TRUE(this->caught_debug);
+    EXPECT_TRUE(this->caught_kce);
+}
+
+TEST_F(KernelContextExceptionTest, bad_thread)
+{
+    // Create some track slots
+    Stepper<MemSpace::host> step(this->make_stepper_input(4));
+
+    // Initialize some primaries and take a step
+    auto primaries = this->make_primaries(8);
+    step(make_span(primaries));
+
+    this->check_kce = [](KernelContextException const& e) {
+        EXPECT_STREQ("dumb-kernel (error processing track state)", e.what());
+        EXPECT_EQ(ThreadId{}, e.thread());
+        if (CELERITAS_USE_JSON)
+        {
+            EXPECT_EQ(R"json({"label":"dumb-kernel"})json", get_json_str(e));
+        }
+    };
+    CELER_TRY_HANDLE_CONTEXT(
+        throw DebugError({DebugErrorType::internal, "false", "test.cc", 0}),
+        this->check_exception,
+        KernelContextException(step.core_data(), ThreadId{}, "dumb-kernel"));
+    EXPECT_TRUE(this->caught_debug);
+    EXPECT_TRUE(this->caught_kce);
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace test
+}  // namespace celeritas
