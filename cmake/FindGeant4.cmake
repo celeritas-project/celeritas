6c0e4d6c3aa8aac57a5c2efbbdf323c02cc7b011
Ben Morgan <ben.morgan@warwick.ac.uk>
2023-01-30 16:52:17 +0000

    Support linking to Geant4 static libraries
    If Geant4 is installed with only static libraries, the FindGeant4
    wrapper will fail as it directly sets properties on the unavailable
    Geant4::G4global shared target.
    
    Check that the shared Geant4::G4global target exists before setting
    its properties. Fallback to the static target if the shared does not
    exist. This is safe against dual installs, as Geant4 will prefer
    shared targets in this case.

--- cmake/FindGeant4.cmake
+++ cmake/FindGeant4.cmake
@@ -16,7 +16,11 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Geant4 CONFIG_MODE)
 
 if(Geant4_FOUND AND Geant4_VERSION VERSION_GREATER_EQUAL 11 AND CELERITAS_USE_CUDA)
-  target_compile_features(Geant4::G4global INTERFACE cuda_std_17)
+  if(TARGET Geant4::G4global)
+    target_compile_features(Geant4::G4global INTERFACE cuda_std_17)
+  else()
+    target_compile_features(Geant4::G4global-static INTERFACE cuda_std_17)
+  endif()
 endif()
 
 #-----------------------------------------------------------------------------#
