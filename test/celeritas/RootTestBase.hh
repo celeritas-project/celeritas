ad853eae33923e545f490fffedf7d5b5169714b3
Seth R Johnson <johnsonsr@ornl.gov>
2023-02-02 10:41:41 -0500

    Add a ROOT test base

---
+++ test/celeritas/RootTestBase.hh
@@ -0,0 +1,35 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2023 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celeritas/RootTestBase.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include "ImportedDataTestBase.hh"
+
+namespace celeritas
+{
+namespace test
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Test harness for loading problem data from a ROOT file
+ */
+class RootTestBase : public ImportedDataTestBase
+{
+  protected:
+    // Access lazily loaded static ROOT data
+    ImportData const& imported_data() const final;
+
+  private:
+    struct ImportHelper;
+    class CleanupGeantEnvironment;
+
+    static ImportHelper& import_helper();
+};
+
+//---------------------------------------------------------------------------//
+}  // namespace test
+}  // namespace celeritas
