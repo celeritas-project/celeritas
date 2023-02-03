00cc9ec0f16ece0522d78470e272655980c3443d
Seth R Johnson <johnsonsr@ornl.gov>
2023-02-03 09:57:37 -0500

    Move vector utils and define logspace

--- src/celeritas/grid/VectorUtils.hh
+++ src/celeritas/grid/VectorUtils.hh
@@ -3,8 +3,8 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file corecel/math/VectorUtils.hh
-//! \brief Helper functions for std::vector
+//! \file celeritas/grid/VectorUtils.hh
+//! \brief Grid creation helpers
 //---------------------------------------------------------------------------//
 #pragma once
 
@@ -16,7 +16,11 @@ namespace celeritas
 {
 //---------------------------------------------------------------------------//
 // Return evenly spaced numbers over a specific interval
-std::vector<real_type> linspace(real_type start, real_type stop, size_type n);
+std::vector<double> linspace(double start, double stop, size_type n);
+
+//---------------------------------------------------------------------------//
+// Return logarithmically spaced numbers over a specific interval
+std::vector<double> logspace(double start, double stop, size_type n);
 
 //---------------------------------------------------------------------------//
 }  // namespace celeritas
