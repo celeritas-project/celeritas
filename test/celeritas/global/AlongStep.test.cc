ac64dbd1fbd5da7b971c2e236d66535776554a2a
Seth R. Johnson <johnsonsr@ornl.gov>
2023-02-13 12:46:13 -0500

    Check for unlikely "zero speed" condition (#646)
    * Add test that triggers speed assertion
    * Allow unusual zero speed

--- test/celeritas/global/AlongStep.test.cc
+++ test/celeritas/global/AlongStep.test.cc
@@ -10,6 +10,7 @@
 #include "celeritas/phys/PDGNumber.hh"
 #include "celeritas/phys/ParticleParams.hh"
 
+#include "../MockTestBase.hh"
 #include "../SimpleTestBase.hh"
 #include "AlongStepTestBase.hh"
 #include "celeritas_test.hh"
@@ -24,7 +25,10 @@ namespace test
 
 class KnAlongStepTest : public SimpleTestBase, public AlongStepTestBase
 {
-  public:
+};
+
+class MockAlongStepTest : public MockTestBase, public AlongStepTestBase
+{
 };
 
 #define Em3AlongStepTest TEST_IF_CELERITAS_GEANT(Em3AlongStepTest)
@@ -83,6 +87,52 @@ TEST_F(KnAlongStepTest, basic)
         EXPECT_SOFT_EQ(1, result.angle);
         EXPECT_SOFT_EQ(3.3386159562990149e-14, result.time);
         EXPECT_SOFT_EQ(0.0010008918838569024, result.step);
+        EXPECT_EQ("physics-discrete-select", result.action);
+    }
+}
+
+TEST_F(MockAlongStepTest, basic)
+{
+    size_type num_tracks = 10;
+    Input inp;
+    inp.particle_id = this->particle()->find("celeriton");
+    {
+        inp.energy = MevEnergy{1};
+        auto result = this->run(inp, num_tracks);
+        EXPECT_SOFT_EQ(0.29312, result.eloss);
+        EXPECT_SOFT_EQ(0.48853333333333, result.displacement);
+        EXPECT_SOFT_EQ(1, result.angle);
+        EXPECT_SOFT_EQ(1.881667426791e-11, result.time);
+        EXPECT_SOFT_EQ(0.48853333333333, result.step);
+        EXPECT_EQ("eloss-range", result.action);
+    }
+    {
+        inp.energy = MevEnergy{1e-6};
+        auto result = this->run(inp, num_tracks);
+        EXPECT_SOFT_EQ(1e-06, result.eloss);
+        EXPECT_SOFT_EQ(5.2704627669473e-05, result.displacement);
+        EXPECT_SOFT_EQ(1, result.angle);
+        EXPECT_SOFT_EQ(1.2431209185653e-12, result.time);
+        EXPECT_SOFT_EQ(5.2704627669473e-05, result.step);
+        EXPECT_EQ("physics-discrete-select", result.action);
+    }
+    {
+        inp.energy = MevEnergy{1e-12};
+        auto result = this->run(inp, num_tracks);
+        EXPECT_SOFT_EQ(1e-12, result.eloss);
+        EXPECT_SOFT_EQ(5.2704627669473e-08, result.displacement);
+        EXPECT_SOFT_EQ(1, result.angle);
+        EXPECT_SOFT_EQ(1.2430647328325e-12, result.time);
+        EXPECT_SOFT_EQ(5.2704627669473e-08, result.step);
+        EXPECT_EQ("physics-discrete-select", result.action);
+    }
+    {
+        inp.energy = MevEnergy{1e-18};
+        auto result = this->run(inp, num_tracks);
+        EXPECT_SOFT_EQ(5.2704627669473e-11, result.displacement);
+        EXPECT_SOFT_EQ(1, result.angle);
+        EXPECT_SOFT_EQ(0, result.time);
+        EXPECT_SOFT_EQ(5.2704627669473e-11, result.step);
         EXPECT_EQ("physics-discrete-select", result.action);
     }
 }
