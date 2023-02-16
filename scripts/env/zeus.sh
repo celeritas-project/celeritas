76c25994e81f784a33eb2226231cf7af31d24b0a
Julien Esseiva <jesseiva@lbl.gov>
2023-02-15 16:49:04 -0800

    vecgeom and geant4 version used by AtlasExternals

--- scripts/env/zeus.sh
+++ scripts/env/zeus.sh
@@ -8,4 +8,4 @@ asetup none,gcc11,cmakesetup,siteroot=cvmfs
 lsetup "views LCG_102b_ATLAS_11 x86_64-centos7-gcc11-opt"
 set -e
 module load cuda/11.8.0
-source /bld3/build/celeritas/geant4/geant4-v11.1.0-install/bin/geant4.sh
+source /bld3/build/celeritas/geant4/geant4-v10.6.0-install/bin/geant4.sh
