83e65dce0e712d63f248079c53b4080dcdc37a33
Seth R. Johnson <johnsonsr@ornl.gov>
2023-02-19 13:34:54 -0500

    Release v0.1.5 (#663)
    * Fix doxygen
    * Fix displayed version for dev documentation builds
    * Add release notes for v0.1.5

--- src/orange/detail/LevelStateAccessor.hh
+++ src/orange/detail/LevelStateAccessor.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file orange/LevelStateAccessor.hh
+//! \file orange/detail/LevelStateAccessor.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
