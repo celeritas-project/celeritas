e545c6d1839c8975897538e3fa7e9af2eced6a3d
Julien Esseiva <jesseiva@lbl.gov>
2023-02-21 18:31:13 -0800

    make sure that we use gcc11.2, removed duplicate cmake preset

--- scripts/env/zeus.sh
+++ scripts/env/zeus.sh
@@ -4,8 +4,10 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 # we need to clear exit on unchecked error for atlasLocalSetup...
 set +e
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-asetup none,gcc11,cmakesetup,siteroot=cvmfs
 lsetup "views LCG_102b_ATLAS_11 x86_64-centos7-gcc11-opt"
+asetup none,gcc112,cmakesetup,siteroot=cvmfs
 set -e
+
+# not in standard Atlas build env but we need it for cuda-enabled vecgeom
 module load cuda/11.8.0
 source /bld3/build/celeritas/geant4/geant4-v10.6.0-install/bin/geant4.sh
