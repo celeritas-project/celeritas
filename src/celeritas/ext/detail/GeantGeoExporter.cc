6806434b0afee09ee7d70b41b98225f3161034d9
Seth R Johnson <johnsonsr@ornl.gov>
2023-04-04 13:52:54 -0400

    Don't add suffixes to GDML output if volume names already have them

--- src/celeritas/ext/detail/GeantGeoExporter.cc
+++ src/celeritas/ext/detail/GeantGeoExporter.cc
@@ -7,7 +7,10 @@
 //---------------------------------------------------------------------------//
 #include "GeantGeoExporter.hh"
 
+#include <algorithm>
 #include <G4GDMLParser.hh>
+#include <G4LogicalVolume.hh>
+#include <G4LogicalVolumeStore.hh>
 #include <G4Threading.hh>
 #include <G4VPhysicalVolume.hh>
 #include <G4Version.hh>
@@ -61,8 +64,19 @@ void GeantGeoExporter::operator()(std::string const& filename) const
 #if G4VERSION_NUMBER >= 1070
     parser.SetOutputFileOverwrite(true);
 #endif
-    constexpr bool append_pointers = true;
-    parser.Write(filename, world_, append_pointers);
+
+    G4LogicalVolumeStore* lv_store = G4LogicalVolumeStore::GetInstance();
+    CELER_ASSERT(lv_store);
+    bool all_uniquified = std::all_of(
+        lv_store->begin(), lv_store->end(), [](G4LogicalVolume* lv) {
+            std::string const& name = lv->GetName();
+            return name.find("0x") != std::string::npos;
+        });
+
+    parser.Write(filename, world_, /* append_pointers = */ !all_uniquified);
+    CELER_LOG(debug) << "Wrote temporary GDML to " << filename
+                     << (all_uniquified ? "without " : "with")
+                     << "additional pointer suffix";
 }
 
 //---------------------------------------------------------------------------//
