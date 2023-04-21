4ffd118eb32422d48c83af2a21db42807ffafd3f
Biondo, Elliott <biondoed@ornl.gov>
2023-04-20 23:36:06 -0400

    Add HyperslabIndexer class

---
+++ src/corecel/data/HyperslabIndexer.hh
@@ -0,0 +1,107 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2023 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file corecel/data/StreamStore.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include "corecel/Assert.hh"
+#include "corecel/Types.hh"
+#include "corecel/cont/Array.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Class for indexing into flattened N-dimensional data.
+ *
+ * Indexing is in standard C iteration order, such that final dimension
+ * "changes fastest". For example, when indexing into a 3D grid (N=3) with
+ * (i=0, j=0, k=1) the resulting index will be 1.
+ */
+template<class T, size_type N>
+class HyperslabIndexer
+{
+  public:
+    //!@{
+    //! \name Type aliases
+    using index_type = T;
+    using Coords = Array<size_type, N>;
+    //!@}
+
+  public:
+    // Construct with the an array denoting the size of each dimension
+    explicit inline CELER_FUNCTION HyperslabIndexer(Coords const& dims);
+
+    //// METHODS ////
+
+    //! Convert N-dimensional coordinates to an index
+    inline CELER_FUNCTION index_type index(Coords const& coords) const;
+
+    //! Convert an index to N-dimensional coordinates
+    inline CELER_FUNCTION Coords coords(index_type index) const;
+
+  private:
+    //// DATA ////
+
+    Coords dims_;
+};
+
+//---------------------------------------------------------------------------//
+// INLINE DEFINITIONS
+//---------------------------------------------------------------------------//
+/*!
+ * Construct from array denoting the sizes of each dimension
+ */
+template<class T, size_type N>
+CELER_FUNCTION HyperslabIndexer<T, N>::HyperslabIndexer(Coords const& dims)
+    : dims_(dims)
+{
+    for (auto dim : dims_)
+    {
+        CELER_EXPECT(dim > 0);
+    }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Convert N-dimensional coordinates to an index.
+ */
+template<class T, size_type N>
+CELER_FUNCTION typename HyperslabIndexer<T, N>::index_type
+HyperslabIndexer<T, N>::index(Coords const& coords) const
+{
+    index_type index = 0;
+    index_type offset = 1;
+    for (int i = dims_.size() - 1; i >= 0; i--)
+    {
+        index += coords[i] * offset;
+        offset *= dims_[i];
+    }
+
+    return index;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Convert an index into N-dimensional coordinates.
+ */
+template<class T, size_type N>
+CELER_FUNCTION typename HyperslabIndexer<T, N>::Coords
+HyperslabIndexer<T, N>::coords(index_type index) const
+{
+    Coords coords;
+
+    for (int i = dims_.size() - 1; i >= 0; i--)
+    {
+        coords[i] = index % dims_[i];
+        index = (index - coords[i]) / dims_[i];
+    }
+
+    return coords;
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
---
+++ test/corecel/data/HyperslabIndexer.test.cc
@@ -0,0 +1,110 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2020-2023 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file corecel/data/HyperslabIndexer.test.cc
+//---------------------------------------------------------------------------//
+
+#include "corecel/data/HyperslabIndexer.hh"
+
+#include "corecel/cont/Range.hh"
+
+#include "celeritas_test.hh"
+
+namespace celeritas
+{
+namespace test
+{
+//---------------------------------------------------------------------------//
+
+TEST(HyerslabIndexerTest, 2D)
+{
+    HyperslabIndexer<size_type, 2> hi({3, 4});
+
+    size_type index = 0;
+    for (size_type i : range(3))
+    {
+        for (size_type j : range(4))
+        {
+            Array<size_type, 2> coords{i, j};
+            EXPECT_EQ(coords, hi.coords(index));
+            EXPECT_EQ(index, hi.index(coords));
+            index++;
+        }
+    }
+}
+
+TEST(HyerslabIndexerTest, 3D)
+{
+    HyperslabIndexer<size_type, 3> hi({3, 4, 5});
+
+    size_type index = 0;
+    for (size_type i : range(3))
+    {
+        for (size_type j : range(4))
+        {
+            for (size_type k : range(5))
+            {
+                Array<size_type, 3> coords{i, j, k};
+                EXPECT_EQ(coords, hi.coords(index));
+                EXPECT_EQ(index, hi.index(coords));
+                index++;
+            }
+        }
+    }
+}
+
+TEST(HyerslabIndexerTest, 4D)
+{
+    HyperslabIndexer<size_type, 4> hi({4, 6, 3, 2});
+
+    size_type index = 0;
+    for (size_type i : range(4))
+    {
+        for (size_type j : range(6))
+        {
+            for (size_type k : range(3))
+            {
+                for (size_type l : range(2))
+                {
+                    Array<size_type, 4> coords{i, j, k, l};
+                    EXPECT_EQ(coords, hi.coords(index));
+                    EXPECT_EQ(index, hi.index(coords));
+                    index++;
+                }
+            }
+        }
+    }
+}
+
+TEST(HyerslabIndexerTest, 5D_with_ones)
+{
+    HyperslabIndexer<size_type, 5> hi({3, 1, 4, 1, 5});
+
+    size_type index = 0;
+    for (size_type i : range(3))
+    {
+        for (size_type j : range(4))
+        {
+            for (size_type k : range(5))
+            {
+                Array<size_type, 5> coords{i, 0, j, 0, k};
+                EXPECT_EQ(coords, hi.coords(index));
+                EXPECT_EQ(index, hi.index(coords));
+                index++;
+            }
+        }
+    }
+}
+
+#if CELERITAS_DEBUG
+TEST(HyerslabIndexerTest, error)
+{
+    EXPECT_THROW((HyperslabIndexer<size_type, 3>({2, 0, 3})), DebugError);
+}
+#endif
+
+//---------------------------------------------------------------------------//
+}  // namespace test
+}  // namespace celeritas
