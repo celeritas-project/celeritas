1a664d516b86013592fbea47bb71903cd33cf50d
Amanda Lund <alund@anl.gov>
2023-04-29 19:25:35 +0000

    Different diagnostic results with ORANGE

--- test/celeritas/user/Diagnostic.test.cc
+++ test/celeritas/user/Diagnostic.test.cc
@@ -81,19 +81,40 @@ TEST_F(TestEm3DiagnosticTest, host)
     auto result = this->run<MemSpace::host>(256, 32);
 
     // Check action diagnostic results
-    static const size_type expected_action_counts[]
-        = {0u,  0u, 0u, 0u, 0u,  0u,   0u,    1580u, 1168u, 0u,   1019u, 55u,
-           0u,  0u, 0u, 0u, 22u, 90u,  283u,  0u,    0u,    575u, 0u,    0u,
-           19u, 0u, 0u, 0u, 0u,  127u, 0u,    20u,   14u,   0u,   467u,  386u,
-           0u,  0u, 0u, 0u, 0u,  0u,   1769u, 289u,  284u,  0u,   15u,   10u,
-           0u,  0u, 0u, 0u, 0u,  0u,   0u,    0u,    0u};
-    EXPECT_VEC_EQ(expected_action_counts, result.action_counts);
-
-    if (CELERITAS_USE_JSON)
+    if (CELERITAS_CORE_GEO == CELERITAS_CORE_GEO_VECGEOM)
     {
-        EXPECT_EQ(
-            R"json({"actions":{"annihil-2-gamma e+":127,"brems-combined e+":386,"brems-combined e-":467,"conv-bethe-heitler gamma":19,"eloss-range e+":55,"eloss-range e-":1019,"geo-boundary e+":284,"geo-boundary e-":289,"geo-boundary gamma":1769,"geo-propagation-limit e+":10,"geo-propagation-limit e-":15,"ioni-moller-bhabha e+":14,"ioni-moller-bhabha e-":20,"msc-range e+":1168,"msc-range e-":1580,"photoel-livermore gamma":575,"physics-integral-rejected e+":90,"physics-integral-rejected e-":22,"scat-klein-nishina gamma":283}})json",
-            this->action_output());
+        static const size_type expected_action_counts[] = {
+            0u,  0u, 0u, 0u, 0u,  0u,   0u,    1580u, 1168u, 0u,   1019u, 55u,
+            0u,  0u, 0u, 0u, 22u, 90u,  283u,  0u,    0u,    575u, 0u,    0u,
+            19u, 0u, 0u, 0u, 0u,  127u, 0u,    20u,   14u,   0u,   467u,  386u,
+            0u,  0u, 0u, 0u, 0u,  0u,   1769u, 289u,  284u,  0u,   15u,   10u,
+            0u,  0u, 0u, 0u, 0u,  0u,   0u,    0u,    0u};
+        EXPECT_VEC_EQ(expected_action_counts, result.action_counts);
+
+        if (CELERITAS_USE_JSON)
+        {
+            EXPECT_EQ(
+                R"json({"actions":{"annihil-2-gamma e+":127,"brems-combined e+":386,"brems-combined e-":467,"conv-bethe-heitler gamma":19,"eloss-range e+":55,"eloss-range e-":1019,"geo-boundary e+":284,"geo-boundary e-":289,"geo-boundary gamma":1769,"geo-propagation-limit e+":10,"geo-propagation-limit e-":15,"ioni-moller-bhabha e+":14,"ioni-moller-bhabha e-":20,"msc-range e+":1168,"msc-range e-":1580,"photoel-livermore gamma":575,"physics-integral-rejected e+":90,"physics-integral-rejected e-":22,"scat-klein-nishina gamma":283}})json",
+                this->action_output());
+        }
+    }
+    else
+    {
+        // ORANGE results are slightly different
+        static const size_type expected_action_counts[] = {
+            0u,  0u, 0u, 0u, 0u,  0u,   0u,    1549u, 1186u, 0u,   1010u, 59u,
+            0u,  0u, 0u, 0u, 24u, 87u,  298u,  0u,    0u,    567u, 0u,    0u,
+            19u, 0u, 0u, 0u, 0u,  124u, 0u,    19u,   15u,   0u,   476u,  391u,
+            0u,  0u, 0u, 0u, 0u,  0u,   1813u, 281u,  274u,  0u,   0u,    0u,
+            0u,  0u, 0u, 0u, 0u,  0u,   0u,    0u,    0u};
+        EXPECT_VEC_EQ(expected_action_counts, result.action_counts);
+
+        if (CELERITAS_USE_JSON)
+        {
+            EXPECT_EQ(
+                R"json({"actions":{"annihil-2-gamma e+":124,"brems-combined e+":391,"brems-combined e-":476,"conv-bethe-heitler gamma":19,"eloss-range e+":59,"eloss-range e-":1010,"geo-boundary e+":274,"geo-boundary e-":281,"geo-boundary gamma":1813,"ioni-moller-bhabha e+":15,"ioni-moller-bhabha e-":19,"msc-range e+":1186,"msc-range e-":1549,"photoel-livermore gamma":567,"physics-integral-rejected e+":87,"physics-integral-rejected e-":24,"scat-klein-nishina gamma":298}})json",
+                this->action_output());
+        }
     }
 }
 
