3b6edeecb7a1c432456a9379c61a0ccffdcea4f4
Seth R Johnson <johnsonsr@ornl.gov>
2023-05-02 11:51:32 -0400

    Use Ptr instead of Ref for interactions

--- src/celeritas/em/generated/RelativisticBremInteract.cu
+++ src/celeritas/em/generated/RelativisticBremInteract.cu
@@ -18,6 +18,8 @@
 #include "celeritas/em/launcher/RelativisticBremLauncher.hh"
 #include "celeritas/phys/InteractionLauncher.hh"
 
+using celeritas::MemSpace;
+
 namespace celeritas
 {
 namespace generated
@@ -25,12 +27,13 @@ namespace generated
 namespace
 {
 __global__ void relativistic_brem_interact_kernel(
-    celeritas::DeviceCRef<celeritas::CoreParamsData> const params,
-    celeritas::DeviceRef<celeritas::CoreStateData> const state,
-    celeritas::RelativisticBremDeviceRef const model_data)
+    celeritas::CRefPtr<celeritas::CoreParamsData, MemSpace::device> const params,
+    celeritas::RefPtr<celeritas::CoreStateData, MemSpace::device> const state,
+    celeritas::RelativisticBremDeviceRef const model_data,
+    celeritas::size_type size)
 {
     auto tid = celeritas::KernelParamCalculator::thread_id();
-    if (!(tid < state.size()))
+    if (!(tid < size))
         return;
 
     auto launch = celeritas::make_interaction_launcher(
@@ -49,7 +52,10 @@ void relativistic_brem_interact(
     CELER_LAUNCH_KERNEL(relativistic_brem_interact,
                         celeritas::device().default_block_size(),
                         state.size(),
-                        params.ref<MemSpace::native>(), state.ref(), model_data);
+                        params.ptr<MemSpace::native>(),
+                        state.ptr(),
+                        model_data,
+                        state.size());
 }
 
 }  // namespace generated
