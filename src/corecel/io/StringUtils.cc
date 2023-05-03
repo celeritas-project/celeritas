6be7193fa41e856df0d81c5d76ad02eb45a8b1ed
Seth R Johnson <johnsonsr@ornl.gov>
2023-05-03 08:57:14 -0400

    Fix log mesages for G4GDML loading
    The ScopedStreamRedirect was wrapping Celeritas log messages.

--- src/corecel/io/StringUtils.cc
+++ src/corecel/io/StringUtils.cc
@@ -8,6 +8,7 @@
 #include "StringUtils.hh"
 
 #include <algorithm>
+#include <cctype>
 #include <string>
 
 namespace celeritas
@@ -16,7 +17,7 @@ namespace celeritas
 /*!
  * Whether the string starts with another string.
  */
-bool starts_with(std::string const& main_string, std::string const& prefix)
+bool starts_with(std::string_view main_string, std::string_view prefix)
 {
     if (main_string.size() < prefix.size())
         return false;
@@ -30,13 +31,39 @@ bool starts_with(std::string const& main_string, std::string const& prefix)
 /*!
  * Whether the string ends with another string.
  */
-bool ends_with(std::string const& main_string, std::string const& suffix)
+bool ends_with(std::string_view main_string, std::string_view suffix)
 {
     if (main_string.size() < suffix.size())
         return false;
 
     return std::equal(
         main_string.end() - suffix.size(), main_string.end(), suffix.begin());
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Whether the character is whitespace or unprintable.
+ */
+bool is_ignored_trailing(unsigned char c)
+{
+    return std::isspace(c) || !std::isprint(c);
+}
+
+//---------------------------------------------------------------------------//
+// Return a string view with leading and trailing whitespace removed
+std::string_view trim(std::string_view input)
+{
+    auto start = input.begin();
+    auto stop = input.end();
+    while (start != stop && is_ignored_trailing(*start))
+    {
+        ++start;
+    }
+    while (start != stop && is_ignored_trailing(*(stop - 1)))
+    {
+        --stop;
+    }
+    return {start, static_cast<std::size_t>(stop - start)};
 }
 
 //---------------------------------------------------------------------------//
