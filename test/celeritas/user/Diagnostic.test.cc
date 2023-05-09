dc65df45bc988a3ba28075e656c8ba08267d911a
Seth R Johnson <johnsonsr@ornl.gov>
2023-05-09 08:32:01 -0400

    Update diagnostic tests
    Interestingly geo propagation limit seems to be fixed? Results are now consistent with ORANGE (!)

--- test/celeritas/user/Diagnostic.test.cc
+++ test/celeritas/user/Diagnostic.test.cc
@@ -79,95 +79,47 @@ TEST_F(TestEm3DiagnosticTest, host)
 {
     auto result = this->run<MemSpace::host>(256, 32);
 
+    static char const* expected_nonzero_actions[]
+        = {"annihil-2-gamma e+",
+           "brems-combined e+",
+           "brems-combined e-",
+           "conv-bethe-heitler gamma",
+           "eloss-range e+",
+           "eloss-range e-",
+           "geo-boundary e+",
+           "geo-boundary e-",
+           "geo-boundary gamma",
+           "ioni-moller-bhabha e+",
+           "ioni-moller-bhabha e-",
+           "msc-range e+",
+           "msc-range e-",
+           "photoel-livermore gamma",
+           "physics-integral-rejected e+",
+           "physics-integral-rejected e-",
+           "scat-klein-nishina gamma"};
+    EXPECT_VEC_EQ(expected_nonzero_actions, result.nonzero_actions);
+
     // Check action diagnostic results
-    if (CELERITAS_CORE_GEO == CELERITAS_CORE_GEO_VECGEOM)
+    if (this->is_ci_build())
     {
-        static char const* expected_nonzero_actions[]
-            = {"annihil-2-gamma e+",
-               "brems-combined e+",
-               "brems-combined e-",
-               "conv-bethe-heitler gamma",
-               "eloss-range e+",
-               "eloss-range e-",
-               "geo-boundary e+",
-               "geo-boundary e-",
-               "geo-boundary gamma",
-               "geo-propagation-limit e+",
-               "geo-propagation-limit e-",
-               "ioni-moller-bhabha e+",
-               "ioni-moller-bhabha e-",
-               "msc-range e+",
-               "msc-range e-",
-               "photoel-livermore gamma",
-               "physics-integral-rejected e+",
-               "physics-integral-rejected e-",
-               "scat-klein-nishina gamma"};
-        EXPECT_VEC_EQ(expected_nonzero_actions, result.nonzero_actions);
+        static const size_type expected_actions[] = {
+            0u,   0u,    0u,    0u,    0u,    0u,  0u, 298u, 567u, 19u, 0u,
+            0u,   0u,    0u,    0u,    1813u, 0u,  0u, 0u,   0u,   0u,  0u,
+            0u,   0u,    1549u, 1010u, 0u,    24u, 0u, 0u,   0u,   0u,  19u,
+            476u, 0u,    0u,    281u,  0u,    0u,  0u, 0u,   0u,   0u,  0u,
+            0u,   1186u, 59u,   0u,    87u,   0u,  0u, 0u,   124u, 15u, 391u,
+            0u,   0u,    274u,  0u,    0u,    0u,  0u, 0u};
+        EXPECT_VEC_EQ(expected_actions, result.actions);
 
-        if (this->is_ci_build())
-        {
-            static const size_type expected_actions[]
-                = {0u,   0u,  0u,   0u,    0u,    0u,    0u,  283u, 575u, 19u,
-                   0u,   0u,  0u,   0u,    0u,    1769u, 0u,  0u,   0u,   0u,
-                   0u,   0u,  0u,   1580u, 1019u, 0u,    22u, 0u,   0u,   0u,
-                   0u,   20u, 467u, 0u,    0u,    289u,  15u, 0u,   0u,   0u,
-                   0u,   0u,  0u,   1168u, 55u,   0u,    90u, 0u,   0u,   0u,
-                   127u, 14u, 386u, 0u,    0u,    284u,  10u, 0u,   0u,   0u};
-            EXPECT_VEC_EQ(expected_actions, result.actions);
-
-            static const size_type expected_steps[]
-                = {0u, 327u, 196u, 81u, 41u, 29u, 26u, 17u, 13u, 6u, 7u,
-                   1u, 3u,   4u,   2u,  1u,  0u,  2u,  1u,  0u,  1u, 1u,
-                   0u, 737u, 43u,  17u, 9u,  11u, 7u,  5u,  9u,  5u, 4u,
-                   9u, 11u,  9u,   10u, 14u, 4u,  5u,  2u,  2u,  4u, 22u,
-                   0u, 0u,   5u,   2u,  1u,  4u,  5u,  8u,  4u,  7u, 7u,
-                   7u, 7u,   13u,  8u,  7u,  3u,  2u,  5u,  6u,  1u, 27u};
-            EXPECT_VEC_EQ(expected_steps, result.steps);
-        }
-    }
-    else
-    {
-        // ORANGE results are slightly different
-        static char const* expected_nonzero_actions[]
-            = {"annihil-2-gamma e+",
-               "brems-combined e+",
-               "brems-combined e-",
-               "conv-bethe-heitler gamma",
-               "eloss-range e+",
-               "eloss-range e-",
-               "geo-boundary e+",
-               "geo-boundary e-",
-               "geo-boundary gamma",
-               "ioni-moller-bhabha e+",
-               "ioni-moller-bhabha e-",
-               "msc-range e+",
-               "msc-range e-",
-               "photoel-livermore gamma",
-               "physics-integral-rejected e+",
-               "physics-integral-rejected e-",
-               "scat-klein-nishina gamma"};
-        EXPECT_VEC_EQ(expected_nonzero_actions, result.nonzero_actions);
-
-        if (this->is_ci_build())
-        {
-            static const size_type expected_actions[]
-                = {0u,   0u,  0u,   0u,    0u,    0u,    0u,  298u, 567u, 19u,
-                   0u,   0u,  0u,   0u,    0u,    1813u, 0u,  0u,   0u,   0u,
-                   0u,   0u,  0u,   1549u, 1010u, 0u,    24u, 0u,   0u,   0u,
-                   0u,   19u, 476u, 0u,    0u,    281u,  0u,  0u,   0u,   0u,
-                   0u,   0u,  0u,   1186u, 59u,   0u,    87u, 0u,   0u,   0u,
-                   124u, 15u, 391u, 0u,    0u,    274u,  0u,  0u,   0u,   0u};
-            EXPECT_VEC_EQ(expected_actions, result.actions);
-
-            static const size_type expected_steps[]
-                = {0u, 316u, 209u, 91u, 33u, 35u, 21u, 20u, 7u,  11u, 10u,
-                   1u, 2u,   3u,   3u,  1u,  1u,  1u,  0u,  0u,  1u,  1u,
-                   0u, 742u, 39u,  11u, 7u,  10u, 6u,  10u, 11u, 5u,  3u,
-                   7u, 11u,  11u,  10u, 14u, 4u,  4u,  3u,  3u,  3u,  23u,
-                   0u, 2u,   2u,   1u,  1u,  6u,  5u,  7u,  4u,  6u,  8u,
-                   7u, 7u,   13u,  8u,  7u,  3u,  2u,  5u,  6u,  2u,  24u};
-            EXPECT_VEC_EQ(expected_steps, result.steps);
-        }
+        // ORANGE resuts are slightly different
+        static const size_type expected_steps[]
+            = {0u, 316u, 209u, 91u, 33u, 35u, 21u, 20u, 7u,  11u, 10u,
+               1u, 2u,   3u,   3u,  1u,  1u,  1u,  0u,  0u,  1u,  1u,
+               0u, 742u, 39u,  11u, 7u,  10u, 6u,  10u, 11u, 5u,  3u,
+               7u, 11u,  11u,  10u, 14u, 4u,  4u,  3u,  3u,  3u,  23u,
+               0u, 2u,   2u,   1u,  1u,  6u,  5u,  7u,  4u,  6u,  8u,
+               7u, 7u,   13u,  8u,  7u,  3u,  2u,  5u,  6u,  2u,  24u};
+        EXPECT_VEC_EQ(expected_steps, result.steps);
     }
 }
 
