2598813283aa81b73f7d8d6f07ee59b293344c76
Seth R Johnson <johnsonsr@ornl.gov>
2023-05-09 12:09:49 -0400

    Add output for opaque ID and quantity

---
+++ src/corecel/OpaqueIdIO.hh
@@ -0,0 +1,35 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2023 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file corecel/OpaqueIdIO.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include <ostream>
+
+#include "OpaqueId.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Output an opaque ID's value or a placeholder if unavailable.
+ */
+template<class V, class S>
+std::ostream& operator<<(std::ostream& os, OpaqueId<V, S> const& v)
+{
+    if (v)
+    {
+        os << v.unchecked_get();
+    }
+    else
+    {
+        os << "<invalid>";
+    }
+    return os;
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
---
+++ src/corecel/math/QuantityIO.hh
@@ -0,0 +1,29 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2023 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file corecel/math/QuantityIO.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include <ostream>
+#include "Quantity.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Output an quantity with its label.
+ */
+template<class UnitT, class ValueT>
+std::ostream& operator<<(std::ostream& os, Quantity<UnitT, ValueT> const& q)
+{
+    static_assert(sizeof(UnitT::label()) > 0,
+                  "Unit does not have a 'label' definition");
+    os << q.value() << " [" << UnitT::label() << ']';
+    return os;
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
