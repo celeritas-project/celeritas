8f56faee49f4f42f9e096f285a98c62a8170f750
Julien Esseiva <jesseiva@lbl.gov>
2023-05-22 17:27:57 -0700

    replace sync_threads by kernel calls

--- src/celeritas/track/detail/TrackSortUtils.cu
+++ src/celeritas/track/detail/TrackSortUtils.cu
@@ -68,42 +68,20 @@ void sort_impl(TrackSlots const& track_slots, F&& func)
 }
 
 template<class F>
-__device__ void tracks_per_action_impl(DeviceRef<CoreStateData> const& states,
-                                       F&& action_accessor)
+CELER_FUNCTION void
+tracks_per_action_impl(DeviceRef<CoreStateData> const& states,
+                       F&& action_accessor)
 {
     ThreadId tid = celeritas::KernelParamCalculator::thread_id();
-    Span<ThreadId> offsets = states.thread_offsets[AllItems<ThreadId>{}];
-
-    if (tid < states.thread_offsets.size())
-    {
-        // fill
-        offsets[tid.get()] = ThreadId{};
-    }
-
-    __syncthreads();
 
     if ((tid < states.size()) && tid.get() != 0)
     {
+        Span<ThreadId> offsets = states.thread_offsets[AllItems<ThreadId>{}];
         ActionId current_action = action_accessor(tid);
         ActionId previous_action = action_accessor(tid - 1);
         if (current_action && current_action != previous_action)
         {
             offsets[current_action.get()] = tid;
-        }
-    }
-
-    __syncthreads();
-    // TODO: on CPU? Most likely faster and no need for a barrier
-    if (tid.get() == 0)
-    {
-        offsets.back() = ThreadId{states.size()};
-        for (auto thread_id = offsets.end() - 2; thread_id >= offsets.begin();
-             --thread_id)
-        {
-            if (*thread_id == ThreadId{})
-            {
-                *thread_id = *(thread_id + 1);
-            }
         }
     }
 }
@@ -130,6 +108,46 @@ __global__ void tracks_per_action_kernel(DeviceRef<CoreStateData> const states,
         default:
             CELER_ASSERT_UNREACHABLE();
     }
+}
+
+// TODO: On host
+__global__ void
+tracks_per_action_reduce_kernel(DeviceRef<CoreStateData> const states)
+{
+    ThreadId tid = celeritas::KernelParamCalculator::thread_id();
+    if (tid.get() == 0)
+    {
+        Span<ThreadId> offsets = states.thread_offsets[AllItems<ThreadId>{}];
+        offsets.back() = ThreadId{states.size()};
+        for (auto thread_id = offsets.end() - 2; thread_id >= offsets.begin();
+             --thread_id)
+        {
+            if (*thread_id == ThreadId{})
+            {
+                *thread_id = *(thread_id + 1);
+            }
+        }
+    }
+}
+
+void tracks_per_action(DeviceRef<CoreStateData> const& states, TrackOrder order)
+{
+    auto start = device_pointer_cast(states.thread_offsets.data());
+    thrust::fill(start, start + states.thread_offsets.size(), ThreadId{});
+    CELER_DEVICE_CHECK_ERROR();
+
+    CELER_LAUNCH_KERNEL(tracks_per_action,
+                        celeritas::device().default_block_size(),
+                        states.size(),
+                        states,
+                        order);
+    CELER_DEVICE_CHECK_ERROR();
+
+    CELER_LAUNCH_KERNEL(tracks_per_action_reduce,
+                        celeritas::device().default_block_size(),
+                        states.size(),
+                        states);
+    CELER_DEVICE_CHECK_ERROR();
 }
 
 //---------------------------------------------------------------------------//
@@ -184,22 +202,12 @@ void sort_tracks(DeviceRef<CoreStateData> const& states, TrackOrder order)
             sort_impl(
                 states.track_slots,
                 along_action_comparator{states.sim.along_step_action.data()});
-            CELER_LAUNCH_KERNEL(tracks_per_action,
-                                celeritas::device().default_block_size(),
-                                states.size(),
-                                states,
-                                order);
-            CELER_DEVICE_CHECK_ERROR();
+            tracks_per_action(states, order);
             return;
         case TrackOrder::sort_step_limit_action:
             sort_impl(states.track_slots,
                       step_limit_comparator{states.sim.step_limit.data()});
-            CELER_LAUNCH_KERNEL(tracks_per_action,
-                                celeritas::device().default_block_size(),
-                                states.size(),
-                                states,
-                                order);
-            CELER_DEVICE_CHECK_ERROR();
+            tracks_per_action(states, order);
             return;
         default:
             CELER_ASSERT_UNREACHABLE();
