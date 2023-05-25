b4e767372d02fc0a261fb86b673299bc33f7a6fd
Julien Esseiva <jesseiva@lbl.gov>
2023-05-24 17:49:05 -0700

    adapt grid size for AlongStepUniform

--- src/celeritas/global/alongstep/AlongStepUniformMscAction.cu
+++ src/celeritas/global/alongstep/AlongStepUniformMscAction.cu
@@ -21,6 +21,7 @@
 #include "celeritas/global/CoreParams.hh"
 #include "celeritas/global/CoreState.hh"
 #include "celeritas/global/TrackLauncher.hh"
+#include "celeritas/track/TrackInitParams.hh"
 
 #include "detail/AlongStepImpl.hh"
 #include "detail/MeanELoss.hh"
@@ -34,6 +35,7 @@ __global__ void along_step_apply_msc_step_limit_kernel(
     CRefPtr<CoreParamsData, MemSpace::device> const params,
     RefPtr<CoreStateData, MemSpace::device> const state,
     ActionId const along_step_id,
+    ThreadId const offset,
     DeviceCRef<UrbanMscData> const msc_data)
 {
     auto launch = make_along_step_track_launcher(
@@ -42,7 +44,7 @@ __global__ void along_step_apply_msc_step_limit_kernel(
         along_step_id,
         detail::apply_msc_step_limit<UrbanMsc>,
         UrbanMsc{msc_data});
-    launch(KernelParamCalculator::thread_id());
+    launch(KernelParamCalculator::thread_id() + offset.get());
 }
 
 //---------------------------------------------------------------------------//
@@ -50,6 +52,7 @@ __global__ void along_step_apply_uniform_propagation_kernel(
     CRefPtr<CoreParamsData, MemSpace::device> const params,
     RefPtr<CoreStateData, MemSpace::device> const state,
     ActionId const along_step_id,
+    ThreadId const offset,
     UniformFieldParams const field)
 {
     auto launch = make_along_step_track_launcher(
@@ -61,7 +64,7 @@ __global__ void along_step_apply_uniform_propagation_kernel(
             return make_mag_field_propagator<DormandPrinceStepper>(
                 UniformField(field.field), field.options, particle, geo);
         });
-    launch(KernelParamCalculator::thread_id());
+    launch(KernelParamCalculator::thread_id() + offset.get());
 }
 
 //---------------------------------------------------------------------------//
@@ -69,6 +72,7 @@ __global__ void along_step_apply_msc_kernel(
     CRefPtr<CoreParamsData, MemSpace::device> const params,
     RefPtr<CoreStateData, MemSpace::device> const state,
     ActionId const along_step_id,
+    ThreadId const offset,
     DeviceCRef<UrbanMscData> const msc_data)
 {
     auto launch = make_along_step_track_launcher(*params,
@@ -76,25 +80,27 @@ __global__ void along_step_apply_msc_kernel(
                                                  along_step_id,
                                                  detail::apply_msc<UrbanMsc>,
                                                  UrbanMsc{msc_data});
-    launch(KernelParamCalculator::thread_id());
+    launch(KernelParamCalculator::thread_id() + offset.get());
 }
 
 //---------------------------------------------------------------------------//
 __global__ void along_step_update_time_kernel(
     CRefPtr<CoreParamsData, MemSpace::device> const params,
     RefPtr<CoreStateData, MemSpace::device> const state,
-    ActionId const along_step_id)
+    ActionId const along_step_id,
+    ThreadId const offset)
 {
     auto launch = make_along_step_track_launcher(
         *params, *state, along_step_id, detail::update_time);
-    launch(KernelParamCalculator::thread_id());
+    launch(KernelParamCalculator::thread_id() + offset.get());
 }
 
 //---------------------------------------------------------------------------//
 __global__ void along_step_apply_mean_eloss_kernel(
     CRefPtr<CoreParamsData, MemSpace::device> const params,
     RefPtr<CoreStateData, MemSpace::device> const state,
-    ActionId const along_step_id)
+    ActionId const along_step_id,
+    ThreadId const offset)
 {
     using detail::MeanELoss;
 
@@ -103,18 +109,19 @@ __global__ void along_step_apply_mean_eloss_kernel(
                                                  along_step_id,
                                                  detail::apply_eloss<MeanELoss>,
                                                  MeanELoss{});
-    launch(KernelParamCalculator::thread_id());
+    launch(KernelParamCalculator::thread_id() + offset.get());
 }
 
 //---------------------------------------------------------------------------//
 __global__ void along_step_update_track_kernel(
     CRefPtr<CoreParamsData, MemSpace::device> const params,
     RefPtr<CoreStateData, MemSpace::device> const state,
-    ActionId const along_step_id)
+    ActionId const along_step_id,
+    ThreadId const offset)
 {
     auto launch = make_along_step_track_launcher(
         *params, *state, along_step_id, detail::update_track);
-    launch(KernelParamCalculator::thread_id());
+    launch(KernelParamCalculator::thread_id() + offset.get());
 }
 
 //---------------------------------------------------------------------------//
@@ -127,45 +134,71 @@ __global__ void along_step_update_track_kernel(
 void AlongStepUniformMscAction::execute(CoreParams const& params,
                                         CoreStateDevice& state) const
 {
+    // TODO: function to round up, single check with tuple
+    auto action_range = state.get_action_range(this->action_id());
+    auto const grid_size = [&] {
+        if (params.init()->host_ref().track_order
+            == TrackOrder::sort_along_step_action)
+        {
+            auto n_threads = action_range.size();
+            auto bs = celeritas::device().default_block_size();
+            return n_threads + bs - (n_threads % bs);
+        }
+        return state.size();
+    }();
+    auto const offset = [&] {
+        if (params.init()->host_ref().track_order
+            == TrackOrder::sort_along_step_action)
+        {
+            return action_range.front();
+        }
+        return ThreadId{0};
+    }();
     CELER_LAUNCH_KERNEL(along_step_apply_msc_step_limit,
                         celeritas::device().default_block_size(),
-                        state.size(),
+                        grid_size,
                         params.ptr<MemSpace::native>(),
                         state.ptr(),
                         this->action_id(),
+                        offset,
                         device_data_.msc);
     CELER_LAUNCH_KERNEL(along_step_apply_uniform_propagation,
                         celeritas::device().default_block_size(),
-                        state.size(),
+                        grid_size,
                         params.ptr<MemSpace::native>(),
                         state.ptr(),
                         this->action_id(),
+                        offset,
                         field_params_);
     CELER_LAUNCH_KERNEL(along_step_apply_msc,
                         celeritas::device().default_block_size(),
-                        state.size(),
+                        grid_size,
                         params.ptr<MemSpace::native>(),
                         state.ptr(),
                         this->action_id(),
+                        offset,
                         device_data_.msc);
     CELER_LAUNCH_KERNEL(along_step_update_time,
                         celeritas::device().default_block_size(),
-                        state.size(),
+                        grid_size,
                         params.ptr<MemSpace::native>(),
                         state.ptr(),
-                        this->action_id());
+                        this->action_id(),
+                        offset);
     CELER_LAUNCH_KERNEL(along_step_apply_mean_eloss,
                         celeritas::device().default_block_size(),
-                        state.size(),
+                        grid_size,
                         params.ptr<MemSpace::native>(),
                         state.ptr(),
-                        this->action_id());
+                        this->action_id(),
+                        offset);
     CELER_LAUNCH_KERNEL(along_step_update_track,
                         celeritas::device().default_block_size(),
-                        state.size(),
+                        grid_size,
                         params.ptr<MemSpace::native>(),
                         state.ptr(),
-                        this->action_id());
+                        this->action_id(),
+                        offset);
 }
 
 //---------------------------------------------------------------------------//
