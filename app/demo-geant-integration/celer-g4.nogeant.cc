a6662a15f907e28e56c1086cdbeb66070465b6d3
Seth R Johnson <johnsonsr@ornl.gov>
2023-05-27 10:28:59 -0400

    Always build demo-geant-integration

---
+++ app/demo-geant-integration/celer-g4.nogeant.cc
@@ -0,0 +1,20 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2023 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celer-g4/celer-g4.nogeant.cc
+//---------------------------------------------------------------------------//
+#include <iostream>
+
+//---------------------------------------------------------------------------//
+/*!
+ * Execute and run.
+ */
+int main(int, char* argv[])
+{
+    std::cerr << argv[0]
+              << ": Geant4 is not enabled in this build of Celeritas"
+              << std::endl;
+    return EXIT_FAILURE;
+}
