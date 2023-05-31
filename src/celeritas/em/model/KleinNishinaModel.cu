751e57804fa599e66d0b6c912905ec4b71e9d6b2
Julien Esseiva <jesseiva@lbl.gov>
2023-05-31 13:11:09 -0700

    refactor klein-nishina

---
+++ src/celeritas/em/model/KleinNishinaModel.cu
@@ -0,0 +1,36 @@
+//---------------------------------*-CUDA-*----------------------------------//
+// Copyright 2023 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celeritas/em/model/KleinNishinaModel.cu
+//---------------------------------------------------------------------------//
+#include "KleinNishinaModel.hh"
+
+#include "celeritas/em/executor/KleinNishinaExecutor.hh"
+#include "celeritas/global/ActionLauncher.device.hh"
+#include "celeritas/global/CoreParams.hh"
+#include "celeritas/global/CoreState.hh"
+#include "celeritas/global/TrackExecutor.hh"
+#include "celeritas/phys/InteractionApplier.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Interact with device data.
+ */
+void KleinNishinaModel::execute(CoreParams const& params,
+                                CoreStateDevice& state) const
+{
+    auto execute = make_action_track_executor(
+        params.ptr<MemSpace::native>(),
+        state.ptr(),
+        this->action_id(),
+        InteractionApplier{KleinNishinaExecutor{this->host_ref()}});
+    static ActionLauncher<decltype(execute)> const launch_kernel(*this);
+    launch_kernel(state, execute);
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
