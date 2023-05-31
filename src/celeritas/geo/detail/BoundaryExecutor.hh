9b09b08323aa841888314193d818ca8237127596
Julien Esseiva <jesseiva@lbl.gov>
2023-05-31 14:13:42 -0700

    fix header

--- src/celeritas/geo/detail/BoundaryExecutor.hh
+++ src/celeritas/geo/detail/BoundaryExecutor.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/geo/detail/BoundaryActionExecutor.hh
+//! \file celeritas/geo/detail/BoundaryExecutor.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
