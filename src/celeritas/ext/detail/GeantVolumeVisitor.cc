38041c94ab6543261f2f1aae1ca4c582c1fac227
Seth R Johnson <johnsonsr@ornl.gov>
2023-06-02 18:17:27 -0400

    Save import data from reflected volumes

--- src/celeritas/ext/detail/GeantVolumeVisitor.cc
+++ src/celeritas/ext/detail/GeantVolumeVisitor.cc
@@ -10,6 +10,7 @@
 #include <G4GDMLWriteStructure.hh>
 #include <G4LogicalVolume.hh>
 #include <G4MaterialCutsCouple.hh>
+#include <G4ReflectionFactory.hh>
 #include <G4VSolid.hh>
 
 #include "corecel/Assert.hh"
@@ -60,6 +61,13 @@ void GeantVolumeVisitor::visit(G4LogicalVolume const& logical_volume)
     else if (unique_volumes_)
     {
         volume.name = this->generate_name(logical_volume);
+    }
+
+    if (auto const* lv = G4ReflectionFactory::Instance()->GetConstituentLV(
+            const_cast<G4LogicalVolume*>(&logical_volume)))
+    {
+        // Visit underlying reflection volume
+        this->visit(*lv);
     }
 
     // Recursive: repeat for every daughter volume, if there are any
