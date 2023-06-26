ce4c7b99059a6480db335d3c87fb96b9e5e4a36d
Julien Esseiva <jesseiva@lbl.gov>
2023-06-26 14:50:47 -0700

    Register string message

--- src/corecel/sys/ScopedDeviceProfiling.cuda.cc
+++ src/corecel/sys/ScopedDeviceProfiling.cuda.cc
@@ -5,7 +5,12 @@
 //---------------------------------------------------------------------------//
 //! \file corecel/sys/ScopedDeviceProfiling.cuda.cc
 //---------------------------------------------------------------------------//
+
 #include "ScopedDeviceProfiling.hh"
+
+#include <mutex>
+#include <shared_mutex>
+#include <unordered_map>
 
 #include "celeritas_config.h"
 #include "corecel/device_runtime_api.h"
@@ -22,11 +27,66 @@ namespace celeritas
 {
 namespace
 {
+
+//---------------------------------------------------------------------------//
+/*!
+ * Global registry for strings used by NVTX.
+ * This Implemented as a free function instead of a static in
+ * ScopedDeviceProfiling to hide the NVTX dependency from user of the
+ * interface.
+ */
+std::unordered_map<std::string, nvtxStringHandle_t>& message_registry()
+{
+    static std::unordered_map<std::string, nvtxStringHandle_t> registry;
+    return registry;
+}
+
 nvtxDomainHandle_t domain_handle()
 {
     static nvtxDomainHandle_t domain = nvtxDomainCreateA("celeritas");
     return domain;
 }
+
+//---------------------------------------------------------------------------//
+/*!
+ * Retrieve the handle for a given message, insert it if it doesn't already
+ * exists
+ */
+nvtxStringHandle_t message_handle_for(std::string const& message)
+{
+    static std::shared_mutex mutex;
+
+    {
+        std::shared_lock lock(mutex);
+        auto message_handle = message_registry().find(message);
+        if (message_handle != message_registry().end())
+        {
+            return message_handle->second;
+        }
+    }
+    // we did not find the handle, insert it
+    std::unique_lock lock(mutex);
+    auto message_handle = message_registry().find(message);
+    // recheck that nobody inserted the same message as we had to release the
+    // shared lock not strictly necessary as insert will do nothing if the key
+    // already exists
+    if (message_handle == message_registry().end())
+    {
+        auto handle
+            = nvtxDomainRegisterStringA(domain_handle(), message.c_str());
+        message_registry().insert({message, handle});
+        return handle;
+    }
+    else
+    {
+        return message_handle->second;
+    }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Create EventAttribute with a specific name
+ */
 nvtxEventAttributes_t make_attributes(std::string const& name)
 {
     nvtxEventAttributes_t attributes;
@@ -34,8 +94,8 @@ nvtxEventAttributes_t make_attributes(std::string const& name)
     attributes.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE;
     attributes.colorType = NVTX_COLOR_ARGB;
     attributes.color = 0xff00ff00;
-    attributes.messageType = NVTX_MESSAGE_TYPE_ASCII;
-    attributes.message.ascii = name.c_str();
+    attributes.messageType = NVTX_MESSAGE_TYPE_REGISTERED;
+    attributes.message.registered = message_handle_for(name);
     attributes.payloadType = NVTX_PAYLOAD_TYPE_INT32;
     attributes.payload.iValue = 0;
     attributes.category = 0;
@@ -48,11 +108,8 @@ nvtxEventAttributes_t make_attributes(std::string const& name)
  */
 ScopedDeviceProfiling::ScopedDeviceProfiling(std::string const& name)
 {
-    if (celeritas::device())
-    {
-        nvtxEventAttributes_t attributes_ = make_attributes(name);
-        nvtxDomainRangePushEx(domain_handle(), &attributes_);
-    }
+    nvtxEventAttributes_t attributes_ = make_attributes(name);
+    nvtxDomainRangePushEx(domain_handle(), &attributes_);
 }
 
 //---------------------------------------------------------------------------//
@@ -61,10 +118,7 @@ ScopedDeviceProfiling::ScopedDeviceProfiling(std::string const& name)
  */
 ScopedDeviceProfiling::~ScopedDeviceProfiling()
 {
-    if (celeritas::device())
-    {
-        nvtxDomainRangePop(domain_handle());
-    }
+    nvtxDomainRangePop(domain_handle());
 }
 
 //---------------------------------------------------------------------------//
