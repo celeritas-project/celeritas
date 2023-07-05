f023033149552e8b4862c7b01da2d606209c78f6
Seth R Johnson <johnsonsr@ornl.gov>
2023-07-05 17:58:28 -0400

    Add demonstration of pathological chord update

--- test/celeritas/field/FieldDriver.test.cc
+++ test/celeritas/field/FieldDriver.test.cc
@@ -20,7 +20,8 @@
 #include "celeritas/field/MagFieldEquation.hh"
 #include "celeritas/field/MakeMagFieldPropagator.hh"
 #include "celeritas/field/Types.hh"
-#include "celeritas/field/UniformField.hh"
+#include "celeritas/field/UniformZField.hh"
+#include "celeritas/field/ZHelixStepper.hh"
 #include "celeritas/field/detail/FieldUtils.hh"
 
 #include "DiagnosticStepper.hh"
@@ -146,16 +147,17 @@ TEST_F(FieldDriverTest, types)
 {
     FieldDriverOptions driver_options;
     auto driver = make_mag_field_driver<DormandPrinceStepper>(
-        UniformField({0, 0, 1}), driver_options, electron_charge());
+        UniformZField(1), driver_options, electron_charge());
 
     // Make sure object is holding things by value
     EXPECT_TRUE(
         (std::is_same<
-            FieldDriver<DormandPrinceStepper<MagFieldEquation<UniformField>>>,
+            FieldDriver<DormandPrinceStepper<MagFieldEquation<UniformZField>>>,
             decltype(driver)>::value));
     // Size: field vector, q / c, reference to options
-    EXPECT_EQ(sizeof(Real3) + sizeof(real_type) + sizeof(FieldDriverOptions*),
-              sizeof(driver));
+    EXPECT_EQ(
+        sizeof(real_type) + sizeof(real_type) + sizeof(FieldDriverOptions*),
+        sizeof(driver));
 }
 
 // Field strength changes quickly with z, so different chord steps require
@@ -174,7 +176,7 @@ TEST_F(FieldDriverTest, unpleasant_field)
     // Vary by a factor of 1024 over the radius of curvature
     auto stepper = make_mag_field_stepper<DiagnosticDPStepper>(
         ExpZField{field_strength, radius / 10}, units::ElementaryCharge{-1});
-    FieldDriver<decltype(stepper)&> driver{driver_options, stepper};
+    FieldDriver driver{driver_options, stepper};
 
     OdeState state;
     state.pos = {radius, 0, 0};
@@ -208,7 +210,7 @@ TEST_F(FieldDriverTest, horrible_field)
     auto stepper = make_mag_field_stepper<DiagnosticDPStepper>(
         HorribleZField{field_strength, radius / 10},
         units::ElementaryCharge{-1});
-    FieldDriver<decltype(stepper)&> driver{driver_options, stepper};
+    FieldDriver driver{driver_options, stepper};
 
     OdeState state;
     state.pos = {radius, 0, -radius / 5};
@@ -232,6 +234,48 @@ TEST_F(FieldDriverTest, horrible_field)
         << state.pos;
 }
 
+/*!
+ * Demonstrate the misbehavior of the chord finder for tightly circling
+ * particles.
+ */
+TEST_F(FieldDriverTest, pathological_chord)
+{
+    FieldDriverOptions driver_options;
+    driver_options.max_nsteps = std::numeric_limits<short int>::max();
+
+    real_type field_strength = 1.0 * units::tesla;
+    MevEnergy e{1.0};
+    real_type radius = this->calc_curvature(e, field_strength);
+
+    OdeState state;
+    state.pos = {radius, 0, 0};
+    state.mom = this->calc_momentum(e, {0, std::sqrt(1 - ipow<2>(0.2)), 0.2});
+
+    DiagnosticStepper stepper{ZHelixStepper{MagFieldEquation{
+        UniformZField{field_strength}, units::ElementaryCharge{-1}}}};
+    FieldDriver driver{driver_options, stepper};
+
+    std::vector<unsigned int> counts;
+    std::vector<real_type> lengths;
+
+    for (auto rev : {0.01, 1.0, 2.0, 4.0, 8.0})
+    {
+        stepper.reset_count();
+        auto end = driver.advance(rev * 2 * constants::pi * radius, state);
+        counts.push_back(stepper.count());
+        lengths.push_back(end.step);
+    }
+
+    static unsigned int const expected_counts[] = {1u, 6u, 1u, 1u, 1u};
+    static double const expected_lengths[] = {0.029802281646312,
+                                              0.30937398137671,
+                                              5.9604563292623,
+                                              11.920912658525,
+                                              23.841825317049};
+    EXPECT_VEC_EQ(expected_counts, counts);
+    EXPECT_VEC_SOFT_EQ(expected_lengths, lengths);
+}
+
 TEST_F(FieldDriverTest, step_counts)
 {
     FieldDriverOptions driver_options;
@@ -239,8 +283,8 @@ TEST_F(FieldDriverTest, step_counts)
 
     real_type field_strength = 1.0 * units::tesla;
     auto stepper = make_mag_field_stepper<DiagnosticDPStepper>(
-        UniformField({0, 0, field_strength}), units::ElementaryCharge{-1});
-    FieldDriver<decltype(stepper)&> driver{driver_options, stepper};
+        UniformZField{field_strength}, units::ElementaryCharge{-1});
+    FieldDriver driver{driver_options, stepper};
 
     std::vector<real_type> radii;
     std::vector<unsigned int> counts;
@@ -295,9 +339,7 @@ TEST_F(FieldDriverTest, step_counts)
 TEST_F(RevolutionFieldDriverTest, advance)
 {
     auto driver = make_mag_field_driver<DormandPrinceStepper>(
-        UniformField({0, 0, 1.0 * units::tesla}),
-        driver_options,
-        electron_charge());
+        UniformZField{1.0 * units::tesla}, driver_options, electron_charge());
 
     // Test parameters and the sub-step size
     real_type circumference = 2 * constants::pi * test_params.radius;
@@ -339,9 +381,7 @@ TEST_F(RevolutionFieldDriverTest, advance)
 TEST_F(RevolutionFieldDriverTest, accurate_advance)
 {
     auto driver = make_mag_field_driver<DormandPrinceStepper>(
-        UniformField({0, 0, 1.0 * units::tesla}),
-        driver_options,
-        electron_charge());
+        UniformZField{1.0 * units::tesla}, driver_options, electron_charge());
 
     // Test parameters and the sub-step size
     real_type circumference = 2 * constants::pi * test_params.radius;
