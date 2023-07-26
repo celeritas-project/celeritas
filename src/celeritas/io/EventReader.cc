7e01ebcad6b0cd638c194d5c87016e80f84af51b
Seth R Johnson <johnsonsr@ornl.gov>
2023-07-26 13:50:42 -0400

    Simplify names

--- src/celeritas/io/EventReader.cc
+++ src/celeritas/io/EventReader.cc
@@ -55,10 +55,10 @@ EventReader::EventReader(std::string const& filename, SPConstParticles params)
 auto EventReader::operator()() -> result_type
 {
     // Parse the next event from the record
-    HepMC3::GenEvent gen_event;
+    HepMC3::GenEvent evt;
     {
         ScopedTimeAndRedirect temp_{"HepMC3"};
-        reader_->read_event(gen_event);
+        reader_->read_event(evt);
     }
     // There are no more events
     if (reader_->failed())
@@ -67,24 +67,23 @@ auto EventReader::operator()() -> result_type
     }
 
     EventId const event_id{event_count_++};
-    if (static_cast<EventId::size_type>(gen_event.event_number())
-        != event_id.get())
+    if (static_cast<EventId::size_type>(evt.event_number()) != event_id.get())
     {
         CELER_LOG_LOCAL(warning)
-            << "Overwriting event ID " << gen_event.event_number()
+            << "Overwriting event ID " << evt.event_number()
             << " from file with sequential event ID " << event_id.get();
     }
 
     // Convert the energy units to MeV and the length units to cm
-    gen_event.set_units(HepMC3::Units::MEV, HepMC3::Units::CM);
+    evt.set_units(HepMC3::Units::MEV, HepMC3::Units::CM);
 
     std::set<int> missing_pdg;
 
     result_type result;
     int track_id = 0;
-    for (auto gen_particle : gen_event.particles())
+    for (auto par : evt.particles())
     {
-        HepMC3::GenParticleData const& part_data = gen_particle->data();
+        HepMC3::GenParticleData const& part_data = par->data();
         if (part_data.status <= 0)
         {
             // Skip particles that should not be tracked
@@ -101,7 +100,7 @@ auto EventReader::operator()() -> result_type
 
         // Get the PDG code and check if this particle type is defined for
         // the current physics
-        PDGNumber pdg{gen_particle->pid()};
+        PDGNumber pdg{par->pid()};
         ParticleId particle_id{params_->find(pdg)};
         if (CELER_UNLIKELY(!particle_id))
         {
@@ -119,7 +118,7 @@ auto EventReader::operator()() -> result_type
         primary.track_id = TrackId(track_id++);
 
         // Get the position of the primary
-        auto const& pos = gen_particle->production_vertex()->position();
+        auto const& pos = par->production_vertex()->position();
         primary.position = {pos.x() * units::centimeter,
                             pos.y() * units::centimeter,
                             pos.z() * units::centimeter};
@@ -128,13 +127,12 @@ auto EventReader::operator()() -> result_type
         primary.time = pos.t() * units::centimeter / constants::c_light;
 
         // Get the direction of the primary
-        primary.direction = {gen_particle->momentum().px(),
-                             gen_particle->momentum().py(),
-                             gen_particle->momentum().pz()};
+        primary.direction = {
+            par->momentum().px(), par->momentum().py(), par->momentum().pz()};
         normalize_direction(&primary.direction);
 
         // Get the energy of the primary
-        primary.energy = units::MevEnergy{gen_particle->momentum().e()};
+        primary.energy = units::MevEnergy{par->momentum().e()};
 
         result.push_back(primary);
     }
