61445de1fa205c835eea2e1a8a1a0d25559f6606
Seth R Johnson <johnsonsr@ornl.gov>
2023-08-02 07:57:04 -0400

    fixup! Save chord estimate from previous field driver advances

--- test/celeritas/field/FieldPropagator.test.cc
+++ test/celeritas/field/FieldPropagator.test.cc
@@ -1313,7 +1313,7 @@ TEST_F(SimpleCmsTest, vecgeom_failure)
             // Repeated substep bisection failed; particle is bumped
             EXPECT_SOFT_EQ(1e-6, result.distance);
             // Minor floating point differences could make this 98 or so
-            EXPECT_SOFT_NEAR(real_type(94), real_type(stepper.count()), 0.05);
+            EXPECT_SOFT_NEAR(real_type(1149), real_type(stepper.count()), 0.05);
             EXPECT_FALSE(result.looping);
         }
     }
