3f985b822ecddb68b0a1a8b3b9e3d4d7082b355c
Seth R Johnson <johnsonsr@ornl.gov>
2023-08-22 16:03:18 -0400

    Add variant transform test

--- test/orange/transform/VariantTransform.test.cc
+++ test/orange/transform/VariantTransform.test.cc
@@ -74,6 +74,49 @@ TEST_F(VariantTransformTest, transformer)
     }
 }
 
+TEST_F(VariantTransformTest, variant_types)
+{
+    using std::holds_alternative;
+    Translation tl{Real3{0, 1, 0}};
+    Transformation tf{make_rotation(Axis::z, Turn{0.25}), Real3{0, 0, 2}};
+
+    EXPECT_TRUE(holds_alternative<std::monostate>(apply_transform({}, {})));
+    EXPECT_TRUE(holds_alternative<Translation>(apply_transform({}, tl)));
+    EXPECT_TRUE(holds_alternative<Transformation>(apply_transform({}, tf)));
+
+    EXPECT_TRUE(holds_alternative<Translation>(apply_transform(tl, {})));
+    EXPECT_TRUE(holds_alternative<Translation>(apply_transform(tl, tl)));
+    EXPECT_TRUE(holds_alternative<Transformation>(apply_transform(tl, tf)));
+
+    EXPECT_TRUE(holds_alternative<Transformation>(apply_transform(tf, {})));
+    EXPECT_TRUE(holds_alternative<Transformation>(apply_transform(tf, tl)));
+    EXPECT_TRUE(holds_alternative<Transformation>(apply_transform(tf, tf)));
+
+    auto result = apply_transform(tl, tf);
+    if (auto* result_tf = std::get_if<Transformation>(&result))
+    {
+        EXPECT_EQ(result_tf->rotation(), tf.rotation());
+        EXPECT_VEC_SOFT_EQ((Real3{0, 1, 2}), result_tf->translation());
+    }
+    else
+    {
+        FAIL() << "wrong type";
+    }
+
+    Transformation tf2{make_rotation(Axis::x, Turn{0.3}), Real3{1, 0, 2}};
+    result = apply_transform(tf, tf2);
+    if (auto* result_tf = std::get_if<Transformation>(&result))
+    {
+        Real3 pos{1, 2, 3};
+        EXPECT_VEC_SOFT_EQ(tf.transform_up(tf2.transform_up(pos)),
+                           result_tf->transform_up(pos));
+    }
+    else
+    {
+        FAIL() << "wrong type";
+    }
+}
+
 //---------------------------------------------------------------------------//
 }  // namespace test
 }  // namespace celeritas
