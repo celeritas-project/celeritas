d88520aafc38f2690018329e967e669a4148c90b
Seth R Johnson <johnsonsr@ornl.gov>
2023-08-25 17:28:11 -0400

    Refactor surface action using functors

--- src/orange/surf/SurfaceTypeTraits.hh
+++ src/orange/surf/SurfaceTypeTraits.hh
@@ -15,16 +15,38 @@ namespace celeritas
 {
 //---------------------------------------------------------------------------//
 /*!
+ * Helper struct for mapping enums to classes.
+ *
+ * \todo This will be moved later for reuse in other type-deleted storage.
+ */
+template<class E, E EV, class T>
+struct EnumToClass
+{
+    using enum_type = E;
+    using type = T;
+
+    static constexpr enum_type value = EV;
+
+    constexpr operator E() const noexcept { return value; }
+    constexpr enum_type operator()() const noexcept { return value; }
+};
+
+//---------------------------------------------------------------------------//
+/*!
  * Map surface enumeration to surface type.
+ *
+ * This class can be passed as a "tag" to functors that can then retrieve its
+ * value or the associated class. It can be implicitly converted into a
+ * SurfaceType enum for use in template parameters.
  */
 template<SurfaceType S>
 struct SurfaceTypeTraits;
 
-#define ORANGE_SURFACE_TRAITS(ENUM_VALUE, CLS)        \
-    template<>                                        \
-    struct SurfaceTypeTraits<SurfaceType::ENUM_VALUE> \
-    {                                                 \
-        using type = CLS;                             \
+#define ORANGE_SURFACE_TRAITS(ENUM_VALUE, CLS)                          \
+    template<>                                                          \
+    struct SurfaceTypeTraits<SurfaceType::ENUM_VALUE>                   \
+        : public EnumToClass<SurfaceType, SurfaceType::ENUM_VALUE, CLS> \
+    {                                                                   \
     }
 
 // clang-format off
@@ -48,6 +70,46 @@ ORANGE_SURFACE_TRAITS(gq,  GeneralQuadric);
 // clang-format on
 
 #undef ORANGE_SURFACE_TRAITS
+
+//---------------------------------------------------------------------------//
+/*!
+ * Expand a macro to a switch statement over all possible surface types.
+ *
+ * The \c func argument should be a functor that takes a single argument which
+ * is a SurfaceTypeTraits instance.
+ */
+template<class F>
+constexpr decltype(auto) visit_surface_type(F&& func, SurfaceType st)
+{
+#define ORANGE_ST_VISIT_CASE(TYPE)          \
+    case SurfaceType::TYPE:                 \
+        return celeritas::forward<F>(func)( \
+            SurfaceTypeTraits<SurfaceType::TYPE>{})
+
+    switch (st)
+    {
+        ORANGE_ST_VISIT_CASE(px);
+        ORANGE_ST_VISIT_CASE(py);
+        ORANGE_ST_VISIT_CASE(pz);
+        ORANGE_ST_VISIT_CASE(cxc);
+        ORANGE_ST_VISIT_CASE(cyc);
+        ORANGE_ST_VISIT_CASE(czc);
+        ORANGE_ST_VISIT_CASE(sc);
+        ORANGE_ST_VISIT_CASE(cx);
+        ORANGE_ST_VISIT_CASE(cy);
+        ORANGE_ST_VISIT_CASE(cz);
+        ORANGE_ST_VISIT_CASE(p);
+        ORANGE_ST_VISIT_CASE(s);
+        ORANGE_ST_VISIT_CASE(kx);
+        ORANGE_ST_VISIT_CASE(ky);
+        ORANGE_ST_VISIT_CASE(kz);
+        ORANGE_ST_VISIT_CASE(sq);
+        ORANGE_ST_VISIT_CASE(gq);
+        default:
+            CELER_ASSERT_UNREACHABLE();
+    }
+#undef ORANGE_ST_VISIT_CASE
+}
 
 //---------------------------------------------------------------------------//
 }  // namespace celeritas
--- src/orange/surf/detail/SurfaceAction.hh
+++ src/orange/surf/detail/SurfaceAction.hh
@@ -34,11 +34,17 @@ template<class F>
 class SurfaceAction
 {
   public:
+    //!@{
+    //! \name Type aliases
+    using result_type = decltype(std::declval<F>()(std::declval<Sphere>()));
+    //!@}
+
+  public:
     // Construct from surfaces and action
     inline CELER_FUNCTION SurfaceAction(Surfaces const& surfaces, F&& action);
 
     // Apply to the surface specified by a surface ID
-    inline CELER_FUNCTION decltype(auto) operator()(LocalSurfaceId id);
+    inline CELER_FUNCTION result_type operator()(LocalSurfaceId id);
 
     //! Access the resulting action
     CELER_FUNCTION F const& action() const { return action_; }
@@ -67,52 +73,6 @@ struct StaticSurfaceAction
 };
 
 //---------------------------------------------------------------------------//
-// PRIVATE MACRO DEFINITIONS
-//---------------------------------------------------------------------------//
-
-//! Call a function for the given type, using the type as the argument.
-#define ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, TYPE) \
-    case SurfaceType::TYPE:                        \
-        FUNC(typename SurfaceTypeTraits<SurfaceType::TYPE>::type)
-
-/*!
- * Expand a macro to a switch statement over all possible surface types.
- *
- * The \c FUNC argument should be a macro that:
- * - takes a single argument which is a surface class identifier (e.g.
- *   \c GeneralQuadric)
- * - calls \c return or \c break
- *
- * The \c ST argument must be a value of type \c SurfaceType.
- */
-#define ORANGE_SURF_DISPATCH_IMPL(FUNC, ST)            \
-    do                                                 \
-    {                                                  \
-        switch (ST)                                    \
-        {                                              \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, px);  \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, py);  \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, pz);  \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, cxc); \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, cyc); \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, czc); \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, sc);  \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, cx);  \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, cy);  \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, cz);  \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, p);   \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, s);   \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, kx);  \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, ky);  \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, kz);  \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, sq);  \
-            ORANGE_SURF_DISPATCH_CASE_IMPL(FUNC, gq);  \
-            case SurfaceType::size_:                   \
-                CELER_ASSERT_UNREACHABLE();            \
-        }                                              \
-    } while (0)
-
-//---------------------------------------------------------------------------//
 // INLINE DEFINITIONS
 //---------------------------------------------------------------------------//
 /*!
@@ -131,15 +91,16 @@ SurfaceAction<F>::SurfaceAction(Surfaces const& surfaces, F&& action)
  */
 template<class F>
 CELER_FUNCTION auto SurfaceAction<F>::operator()(LocalSurfaceId id)
-    -> decltype(auto)
+    -> result_type
 {
     CELER_EXPECT(id < surfaces_.num_surfaces());
-#define ORANGE_SA_APPLY_IMPL(SURFACE) \
-    return action_(surfaces_.make_surface<SURFACE>(id));
 
-    ORANGE_SURF_DISPATCH_IMPL(ORANGE_SA_APPLY_IMPL, surfaces_.surface_type(id));
-#undef ORANGE_SA_APPLY_IMPL
-    CELER_ASSERT_UNREACHABLE();
+    return visit_surface_type(
+        [this, id](auto st_traits) -> result_type {
+            using S = typename decltype(st_traits)::type;
+            return action_(surfaces_.make_surface<S>(id));
+        },
+        surfaces_.surface_type(id));
 }
 
 //---------------------------------------------------------------------------//
@@ -150,14 +111,14 @@ template<template<class> class T>
 CELER_FUNCTION decltype(auto)
 StaticSurfaceAction<T>::operator()(SurfaceType type) const
 {
-#define ORANGE_SSA_GET(SURFACE) return T<SURFACE>()();
-    ORANGE_SURF_DISPATCH_IMPL(ORANGE_SSA_GET, type);
-#undef ORANGE_SSA_GET
-    CELER_ASSERT_UNREACHABLE();
+    return visit_surface_type(
+        [](auto st_traits) {
+            using S = typename decltype(st_traits)::type;
+            return T<S>{}();
+        },
+        type);
 }
 
 //---------------------------------------------------------------------------//
-#undef ORANGE_SURF_DISPATCH_CASE_IMPL
-#undef ORANGE_SURF_DISPATCH_IMPL
 }  // namespace detail
 }  // namespace celeritas
