cb953e5470dc56fd422a1e22870b1378a769e84f
Biondo, Elliott <biondoed@ornl.gov>
2023-09-06 14:22:27 -0400

    Fixing missing paren

--- src/orange/univ/RectArrayTracker.hh
+++ src/orange/univ/RectArrayTracker.hh
@@ -159,208 +159,210 @@ CELER_FUNCTION auto RectArrayTracker::initialize(LocalState const& state) const
             }
             coords[to_int(ax)] = index;
         }
-
-        VolumeIndexer to_index(
-            {record_.dims[2], record_.dims[1], record_.dims[0]});
-        return {LocalVolumeId{to_index({coords[2], coords[1], coords[0]})}, {}};
     }
 
-    //---------------------------------------------------------------------------//
-    /*!
-     * Find the local volume on the opposite side of a surface.
-     */
-    CELER_FUNCTION auto RectArrayTracker::cross_boundary(
-        LocalState const& state) const->Initialization
+    VolumeIndexer to_index({record_.dims[2], record_.dims[1], record_.dims[0]});
+    return {LocalVolumeId{to_index({coords[2], coords[1], coords[0]})}, {}};
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Find the local volume on the opposite side of a surface.
+ */
+CELER_FUNCTION auto
+RectArrayTracker::cross_boundary(LocalState const& state) const
+    -> Initialization
+{
+    CELER_EXPECT(state.surface && state.volume);
+
+    // Find the coords of the current volume
+    VolumeIndexer to_index(record_.dims);
+    VolumeInverseIndexer to_coords(record_.dims);
+    auto coords = to_coords(state.volume.unchecked_get());
+    auto ax_idx = this->find_surface_axis_idx(state.surface.id());
+
+    // Value for incrementing the axial coordinate upon crossing
+    int inc = (state.surface.sense() == Sense::outside) ? -1 : 1;
+
+    detail::OnLocalSurface new_surface(state.surface.id(),
+                                       flip_sense(state.surface.sense()));
+
+    if ((coords[ax_idx] == 0 && inc == -1)
+        || (coords[ax_idx] == record_.dims[ax_idx] - 1 && inc == 1))
     {
-        CELER_EXPECT(state.surface && state.volume);
+        // Crossimg out
+        return {{}, new_surface};
+    }
+    else
+    {
+        coords[ax_idx] += inc;
+        return {LocalVolumeId(to_index(coords)), new_surface};
+    }
+}
 
-        // Find the coords of the current volume
-        VolumeIndexer to_index(record_.dims);
-        VolumeInverseIndexer to_coords(record_.dims);
-        auto coords = to_coords(state.volume.unchecked_get());
-        auto ax_idx = this->find_surface_axis_idx(state.surface.id());
+//---------------------------------------------------------------------------//
+/*!
+ * Calculate distance-to-intercept for the next surface.
+ */
+CELER_FUNCTION auto RectArrayTracker::intersect(LocalState const& state) const
+    -> Intersection
+{
+    Intersection result = this->intersect_impl(state, detail::IsFinite{});
+    return result;
+}
 
-        // Value for incrementing the axial coordinate upon crossing
-        int inc = (state.surface.sense() == Sense::outside) ? -1 : 1;
+//---------------------------------------------------------------------------//
+/*!
+ * Calculate distance-to-intercept for the next surface, with max distance.
+ */
+CELER_FUNCTION auto
+RectArrayTracker::intersect(LocalState const& state, real_type max_dist) const
+    -> Intersection
+{
+    CELER_EXPECT(max_dist > 0);
+    Intersection result
+        = this->intersect_impl(state, detail::IsNotFurtherThan{max_dist});
+    if (!result)
+    {
+        result.distance = max_dist;
+    }
+    return result;
+}
 
-        detail::OnLocalSurface new_surface(state.surface.id(),
-                                           flip_sense(state.surface.sense()));
+//---------------------------------------------------------------------------//
+/*!
+ * Calculate nearest distance to a surface in any direction.
+ *
+ * On an axis-aligned rectlinear grid the minimum distance to any surface
+ * is always always occurs along a line parallel to an axis.
+ */
+CELER_FUNCTION real_type RectArrayTracker::safety(Real3 const& pos,
+                                                  LocalVolumeId volid) const
+{
+    CELER_EXPECT(volid && volid.get() < this->num_volumes());
 
-        if ((coords[ax_idx] == 0 && inc == -1)
-            || (coords[ax_idx] == record_.dims[ax_idx] - 1 && inc == 1))
+    VolumeInverseIndexer to_coords(record_.dims);
+    auto coords = to_coords(volid.unchecked_get());
+
+    real_type min_dist = numeric_limits<real_type>::infinity();
+
+    for (auto ax : range(Axis::size_))
+    {
+        auto grid = this->make_grid(ax);
+        for (auto i : range(2))
         {
-            // Crossimg out
-            return {{}, new_surface};
+            auto target_coord = coords[to_int(ax)] + i;
+            real_type target = grid[target_coord];
+            min_dist = min(min_dist, std::fabs(pos[to_int(ax)] - target));
         }
-        else
+    }
+
+    CELER_ENSURE(min_dist >= 0
+                 && min_dist < numeric_limits<real_type>::infinity());
+    return min_dist;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Calculate the local surface normal.
+ */
+CELER_FUNCTION autouniv
+    / RectArrayTracker
+          .hh
+          RectArrayTracker::normal(Real3 const&,
+                                   LocalSurfaceId surf) const->Real3
+{
+    CELER_EXPECT(surf && surf.get() < this->num_surfaces());
+    size_type ax = this->find_surface_axis_idx(surf);
+
+    Real3 normal{0, 0, 0};
+    normal[ax] = 1;
+
+    return normal;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * DaughterId of universe embedded in a given volume.
+ */
+CELER_FORCEINLINE_FUNCTION DaughterId
+RectArrayTracker::daughter(LocalVolumeId vol) const
+{
+    CELER_EXPECT(vol && vol.get() < this->num_volumes());
+    return record_.daughters[vol];
+}
+
+//---------------------------------------------------------------------------//
+// PRIVATE INLINE DEFINITIONS
+//---------------------------------------------------------------------------//
+/*!
+ * Calculate distance-to-intercept for the next surface.
+ */
+template<class F>
+CELER_FUNCTION auto
+RectArrayTracker::intersect_impl(LocalState const& state, F is_valid) const
+    -> Intersection
+{
+    CELER_EXPECT(state.volume && !state.temp_sense.empty());
+
+    auto coords
+        = VolumeInverseIndexer{record_.dims}(state.volume.unchecked_get());
+
+    Intersection result;
+    Sense sense;
+    SurfaceIndexer to_index(record_.surface_indexer_data);
+
+    for (auto ax : range(Axis::size_))
+    {
+        auto dir = state.dir[to_int(ax)];
+
+        // Ignore any stationary axis
+        if (dir == 0)
         {
-            coords[ax_idx] += inc;
-            return {LocalVolumeId(to_index(coords)), new_surface};
-        }
-    }
-
-    //---------------------------------------------------------------------------//
-    /*!
-     * Calculate distance-to-intercept for the next surface.
-     */
-    CELER_FUNCTION auto RectArrayTracker::intersect(LocalState const& state)
-        const->Intersection
-    {
-        Intersection result = this->intersect_impl(state, detail::IsFinite{});
-        return result;
-    }
-
-    //---------------------------------------------------------------------------//
-    /*!
-     * Calculate distance-to-intercept for the next surface, with max distance.
-     */
-    CELER_FUNCTION auto RectArrayTracker::intersect(
-        LocalState const& state, real_type max_dist) const->Intersection
-    {
-        CELER_EXPECT(max_dist > 0);
-        Intersection result
-            = this->intersect_impl(state, detail::IsNotFurtherThan{max_dist});
-        if (!result)
-        {
-            result.distance = max_dist;
-        }
-        return result;
-    }
-
-    //---------------------------------------------------------------------------//
-    /*!
-     * Calculate nearest distance to a surface in any direction.
-     *
-     * On an axis-aligned rectlinear grid the minimum distance to any surface
-     * is always always occurs along a line parallel to an axis.
-     */
-    CELER_FUNCTION real_type RectArrayTracker::safety(
-        Real3 const& pos, LocalVolumeId volid) const
-    {
-        CELER_EXPECT(volid && volid.get() < this->num_volumes());
-
-        VolumeInverseIndexer to_coords(record_.dims);
-        auto coords = to_coords(volid.unchecked_get());
-
-        real_type min_dist = numeric_limits<real_type>::infinity();
-
-        for (auto ax : range(Axis::size_))
-        {
-            auto grid = this->make_grid(ax);
-            for (auto i : range(2))
-            {
-                auto target_coord = coords[to_int(ax)] + i;
-                real_type target = grid[target_coord];
-                min_dist = min(min_dist, std::fabs(pos[to_int(ax)] - target));
-            }
-        }
-
-        CELER_ENSURE(min_dist >= 0
-                     && min_dist < numeric_limits<real_type>::infinity());
-        return min_dist;
-    }
-
-    //---------------------------------------------------------------------------//
-    /*!
-     * Calculate the local surface normal.
-     */
-    CELER_FUNCTION autouniv
-        / RectArrayTracker
-              .hh
-              RectArrayTracker::normal(Real3 const&, LocalSurfaceId surf)
-                  const->Real3
-    {
-        CELER_EXPECT(surf && surf.get() < this->num_surfaces());
-        size_type ax = this->find_surface_axis_idx(surf);
-
-        Real3 normal{0, 0, 0};
-        normal[ax] = 1;
-
-        return normal;
-    }
-
-    //---------------------------------------------------------------------------//
-    /*!
-     * DaughterId of universe embedded in a given volume.
-     */
-    CELER_FORCEINLINE_FUNCTION DaughterId RectArrayTracker::daughter(
-        LocalVolumeId vol) const
-    {
-        CELER_EXPECT(vol && vol.get() < this->num_volumes());
-        return record_.daughters[vol];
-    }
-
-    //---------------------------------------------------------------------------//
-    // PRIVATE INLINE DEFINITIONS
-    //---------------------------------------------------------------------------//
-    /*!
-     * Calculate distance-to-intercept for the next surface.
-     */
-    template<class F>
-    CELER_FUNCTION auto RectArrayTracker::intersect_impl(
-        LocalState const& state, F is_valid) const->Intersection
-    {
-        CELER_EXPECT(state.volume && !state.temp_sense.empty());
-
-        auto coords
-            = VolumeInverseIndexer{record_.dims}(state.volume.unchecked_get());
-
-        Intersection result;
-        Sense sense;
-        SurfaceIndexer to_index(record_.surface_indexer_data);
-
-        for (auto ax : range(Axis::size_))
-        {
-            auto dir = state.dir[to_int(ax)];
-
-            // Ignore any stationary axis
-            if (dir == 0)
-            {
-                continue;
-            }
-
-            auto target_coord = coords[to_int(ax)] + static_cast<int>(dir > 0);
-
-            auto target_value = this->make_grid(ax)[target_coord];
-
-            real_type dist = (target_value
-                              - static_cast<real_type>(state.pos[to_int(ax)]))
-                             / state.dir[to_int(ax)];
-
-            if (dist > 0 && is_valid(dist) && dist < result.distance)
-            {
-                result.distance = dist;
-
-                sense = dir > 0 ? Sense::inside : Sense::outside;
-                auto local_surface = LocalSurfaceId(to_index(
-                    {static_cast<size_type>(to_int(ax)), target_coord}));
-                result.surface = detail::OnLocalSurface(local_surface, sense);
-            }
+            continue;
         }
 
-        univ / RectArrayTracker.hh return result;
+        auto target_coord = coords[to_int(ax)] + static_cast<int>(dir > 0);
+
+        auto target_value = this->make_grid(ax)[target_coord];
+
+        real_type dist
+            = (target_value - static_cast<real_type>(state.pos[to_int(ax)]))
+              / state.dir[to_int(ax)];
+
+        if (dist > 0 && is_valid(dist) && dist < result.distance)
+        {
+            result.distance = dist;
+
+            sense = dir > 0 ? Sense::inside : Sense::outside;
+            auto local_surface = LocalSurfaceId(
+                to_index({static_cast<size_type>(to_int(ax)), target_coord}));
+            result.surface = detail::OnLocalSurface(local_surface, sense);
+        }
     }
 
-    //---------------------------------------------------------------------------//
-    /*!
-     * Find the index of axis (x/y/z) we are about to cross:
-     */
-    CELER_FUNCTION size_type RectArrayTracker::find_surface_axis_idx(
-        LocalSurfaceId s) const
-    {
-        SurfaceInverseIndexer to_axis(record_.surface_indexer_data);
-        return to_axis(s.unchecked_get())[0];
-    }
+    univ / RectArrayTracker.hh return result;
+}
 
-    //---------------------------------------------------------------------------//
-    /*!
-     * Create Grid object for a given axis.
-     */
-    CELER_FUNCTION RectArrayTracker::Grid RectArrayTracker::make_grid(Axis ax)
-        const
-    {
-        return Grid(record_.grid[to_int(ax)], params_.reals);
-    }
+//---------------------------------------------------------------------------//
+/*!
+ * Find the index of axis (x/y/z) we are about to cross:
+ */
+CELER_FUNCTION size_type
+RectArrayTracker::find_surface_axis_idx(LocalSurfaceId s) const
+{
+    SurfaceInverseIndexer to_axis(record_.surface_indexer_data);
+    return to_axis(s.unchecked_get())[0];
+}
 
-    //---------------------------------------------------------------------------//
+//---------------------------------------------------------------------------//
+/*!
+ * Create Grid object for a given axis.
+ */
+CELER_FUNCTION RectArrayTracker::Grid RectArrayTracker::make_grid(Axis ax) const
+{
+    return Grid(record_.grid[to_int(ax)], params_.reals);
+}
+
+//---------------------------------------------------------------------------//
 }  // namespace celeritas
