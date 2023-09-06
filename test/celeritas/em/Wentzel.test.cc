a88578d5f7503d29eecf62ccc3a2d5373ba3f79e
Seth R Johnson <johnsonsr@ornl.gov>
2023-09-06 13:09:00 -0400

    Rename interactor test to match model name

--- test/celeritas/em/Wentzel.test.cc
+++ test/celeritas/em/Wentzel.test.cc
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/em/CoulombScattering.test.cc
+//! \file celeritas/em/Wentzel.test.cc
 //---------------------------------------------------------------------------//
 #include "celeritas/Quantities.hh"
 #include "celeritas/Units.hh"
