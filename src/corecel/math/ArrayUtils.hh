f3318e6a35985ae7fbf926993729b28a3e699f6e
Seth R Johnson <johnsonsr@ornl.gov>
2023-09-10 07:19:37 -0400

    Add helper function for constructing unit vector

--- src/corecel/math/ArrayUtils.hh
+++ src/corecel/math/ArrayUtils.hh
@@ -43,6 +43,12 @@ template<class T, size_type N>
 [[nodiscard]] inline CELER_FUNCTION T norm(Array<T, N> const& vec);
 
 //---------------------------------------------------------------------------//
+// Construct a vector with unit magnitude
+template<class T, size_type N>
+[[nodiscard]] inline CELER_FUNCTION Array<T, N>
+make_unit_vector(Array<T, N> const& v);
+
+//---------------------------------------------------------------------------//
 // Calculate the Euclidian (2) distance between two points
 template<class T, size_type N>
 [[nodiscard]] inline CELER_FUNCTION T distance(Array<T, N> const& x,
@@ -126,6 +132,24 @@ CELER_FUNCTION T norm(Array<T, N> const& v)
 
 //---------------------------------------------------------------------------//
 /*!
+ * Construct a unit vector.
+ *
+ * Unit vectors have an Euclidian norm magnitude of 1.
+ */
+template<class T, size_type N>
+CELER_FUNCTION Array<T, N> make_unit_vector(Array<T, N> const& v)
+{
+    Array<T, N> result{v};
+    const T scale_factor = 1 / norm(result);
+    for (auto& el : result)
+    {
+        el *= scale_factor;
+    }
+    return result;
+}
+
+//---------------------------------------------------------------------------//
+/*!
  * Calculate the Euclidian (2) distance between two points.
  */
 template<class T, size_type N>
@@ -142,6 +166,8 @@ CELER_FUNCTION T distance(Array<T, N> const& x, Array<T, N> const& y)
 //---------------------------------------------------------------------------//
 /*!
  * Divide the given vector by its Euclidian norm.
+ *
+ * TODO: deprecate and replace with \c make_unit_vector ?
  */
 template<class T>
 CELER_FUNCTION void normalize_direction(Array<T, 3>* direction)
