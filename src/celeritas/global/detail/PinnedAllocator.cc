f36095670244ddbb08fa40e66bf623f0230ffaa6
Julien Esseiva <jesseiva@lbl.gov>
2023-09-14 16:13:36 -0700

    filepath

--- src/celeritas/global/detail/PinnedAllocator.cc
+++ src/celeritas/global/detail/PinnedAllocator.cc
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file corecel/data/detail/PinnedAllocator.cc
+//! \file celeritas/global/detail/PinnedAllocator.cc
 //---------------------------------------------------------------------------//
 #include "PinnedAllocator.hh"
 
--- src/celeritas/global/detail/PinnedAllocator.hh
+++ src/celeritas/global/detail/PinnedAllocator.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file corecel/data/detail/PinnedAllocator.hh
+//! \file celeritas/global/detail/PinnedAllocator.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
