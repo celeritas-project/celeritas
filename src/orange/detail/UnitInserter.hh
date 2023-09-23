ae361e151f56ea35a4ac5ad31c7f0043db2e197e
Seth R Johnson <johnsonsr@ornl.gov>
2023-09-23 10:32:04 +0900

    Add builders as unit inserter member data

--- src/orange/detail/UnitInserter.hh
+++ src/orange/detail/UnitInserter.hh
@@ -10,6 +10,7 @@
 #include <vector>
 
 #include "corecel/Types.hh"
+#include "corecel/data/CollectionBuilder.hh"
 #include "orange/OrangeData.hh"
 #include "orange/OrangeTypes.hh"
 #include "orange/construct/OrangeInput.hh"
@@ -46,6 +47,18 @@ class UnitInserter
     Data* orange_data_{nullptr};
     BIHBuilder build_bih_tree_;
     TransformInserter insert_transform_;
+
+    CollectionBuilder<SimpleUnitRecord> simple_units_;
+
+    CollectionBuilder<LocalSurfaceId> local_surface_ids_;
+    CollectionBuilder<LocalVolumeId> local_volume_ids_;
+    CollectionBuilder<OpaqueId<real_type>> real_ids_;
+    CollectionBuilder<logic_int> logic_ints_;
+    CollectionBuilder<real_type> reals_;
+    CollectionBuilder<SurfaceType> surface_types_;
+    CollectionBuilder<Connectivity> connectivities_;
+    CollectionBuilder<VolumeRecord> volume_records_;
+    CollectionBuilder<Daughter> daughters_;
 
     //// HELPER METHODS ////
 
