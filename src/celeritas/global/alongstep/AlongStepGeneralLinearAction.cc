dbfe992a5aee5da5f5af9179095c7392c2420393
Amanda Lund <alund@anl.gov>
2023-09-25 23:25:10 +0000

    Move CPU along-step logic inside loop over tracks

--- src/celeritas/global/alongstep/AlongStepGeneralLinearAction.cc
+++ src/celeritas/global/alongstep/AlongStepGeneralLinearAction.cc
@@ -91,26 +91,27 @@ void AlongStepGeneralLinearAction::execute(CoreParams const& params,
                 std::forward<decltype(execute_track)>(execute_track)));
     };
 
-    if (this->has_msc())
-    {
-        launch_impl(
-            MscStepLimitApplier{UrbanMsc{msc_->ref<MemSpace::native>()}});
-    }
-    launch_impl(PropagationApplier{LinearPropagatorFactory{}});
-    if (this->has_msc())
-    {
-        launch_impl(MscApplier{UrbanMsc{msc_->ref<MemSpace::native>()}});
-    }
-    launch_impl(detail::TimeUpdater{});
-    if (this->has_fluct())
-    {
-        launch_impl(ElossApplier{FluctELoss{fluct_->ref<MemSpace::native>()}});
-    }
-    else
-    {
-        launch_impl(ElossApplier{MeanELoss{}});
-    }
-    launch_impl(TrackUpdater{});
+    launch_impl([&](CoreTrackView const& track) {
+        if (this->has_msc())
+        {
+            MscStepLimitApplier{UrbanMsc{msc_->ref<MemSpace::native>()}}(track);
+        }
+        PropagationApplier{LinearPropagatorFactory{}}(track);
+        if (this->has_msc())
+        {
+            MscApplier{UrbanMsc{msc_->ref<MemSpace::native>()}}(track);
+        }
+        TimeUpdater{}(track);
+        if (this->has_fluct())
+        {
+            ElossApplier{FluctELoss{fluct_->ref<MemSpace::native>()}}(track);
+        }
+        else
+        {
+            ElossApplier{MeanELoss{}}(track);
+        }
+        TrackUpdater{}(track);
+    });
 }
 
 //---------------------------------------------------------------------------//
