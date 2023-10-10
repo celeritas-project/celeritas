676fc6fef571d5084988987a6cad199d53af4da7
Seth R Johnson <johnsonsr@ornl.gov>
2023-10-10 16:11:51 -0400

    Add JSON export of orange input

--- src/orange/construct/detail/OrangeInputIOImpl.json.cc
+++ src/orange/construct/detail/OrangeInputIOImpl.json.cc
@@ -9,12 +9,15 @@
 
 #include <vector>
 
+#include "corecel/cont/Range.hh"
+#include "corecel/io/Join.hh"
 #include "corecel/io/StringEnumMapper.hh"
-#include "orange/OrangeTypes.hh"
 
 namespace celeritas
 {
 namespace detail
+{
+namespace
 {
 //---------------------------------------------------------------------------//
 /*!
@@ -55,9 +58,77 @@ struct SurfaceEmplacer
 
 //---------------------------------------------------------------------------//
 /*!
+ * Convert a logic token to a string.
+ */
+void logic_to_stream(std::ostream& os, logic_int val)
+{
+    if (logic::is_operator_token(val))
+    {
+        os << to_char(static_cast<logic::OperatorToken>(val));
+    }
+    else
+    {
+        // Just a face ID
+        os << val;
+    }
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace
+
+//---------------------------------------------------------------------------//
+/*!
+ * Read a transform from a JSON object
+ */
+VariantTransform import_transform(nlohmann::json const& src)
+{
+    std::vector<real_type> data;
+    src.get_to(data);
+    if (data.size() == 0)
+    {
+        return NoTransformation{};
+    }
+    else if (data.size() == 3)
+    {
+        return Translation{Translation::StorageSpan{make_span(data)}};
+    }
+    else if (data.size() == 12)
+    {
+        return Transformation{Transformation::StorageSpan{make_span(data)}};
+    }
+    else
+    {
+        CELER_VALIDATE(false,
+                       << "invalid number of elements in transform: "
+                       << data.size());
+    }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Write a transform to arrays suitable for JSON export.
+ */
+nlohmann::json export_transform(VariantTransform const& t)
+{
+    // Append the transform data as a single array. Rely on the size to
+    // unpack it.
+    return std::visit(
+        [](auto&& tr) -> nlohmann::json {
+            auto result = nlohmann::json::array();
+            for (auto v : tr.data())
+            {
+                result.push_back(v);
+            }
+            return result;
+        },
+        t);
+}
+
+//---------------------------------------------------------------------------//
+/*!
  * Read surface data from an ORANGE JSON file.
  */
-std::vector<VariantSurface> read_surfaces(nlohmann::json const& j)
+std::vector<VariantSurface> import_zipped_surfaces(nlohmann::json const& j)
 {
     // Read and convert types
     auto const& type_labels = j.at("types").get<std::vector<std::string>>();
@@ -79,6 +150,35 @@ std::vector<VariantSurface> read_surfaces(nlohmann::json const& j)
         data_idx += sizes[i];
     }
     return result;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Write surface data to a JSON object.
+ */
+nlohmann::json export_zipped_surfaces(std::vector<VariantSurface> const& all_s)
+{
+    std::vector<std::string> surface_types;
+    std::vector<real_type> surface_data;
+    std::vector<size_type> surface_sizes;
+
+    for (auto const& var_s : all_s)
+    {
+        std::visit(
+            [&](auto&& s) {
+                auto d = s.data();
+                surface_types.push_back(to_cstring(s.surface_type()));
+                surface_data.insert(surface_data.end(), d.begin(), d.end());
+                surface_sizes.push_back(d.size());
+            },
+            var_s);
+    }
+
+    return nlohmann::json::object({
+        {"types", std::move(surface_types)},
+        {"data", std::move(surface_data)},
+        {"sizes", std::move(surface_sizes)},
+    });
 }
 
 //---------------------------------------------------------------------------//
