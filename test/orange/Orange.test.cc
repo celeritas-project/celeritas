bf50d9eeb3d48722e7d7f39ef3e641655073c992
Seth R Johnson <johnsonsr@ornl.gov>
2023-10-10 08:33:28 -0400

    Move orange types to a different test

--- test/orange/Orange.test.cc
+++ test/orange/Orange.test.cc
@@ -29,45 +29,6 @@ namespace test
 {
 //---------------------------------------------------------------------------//
 
-TEST(OrangeTypes, tolerances)
-{
-    using TolT = Tolerance<>;
-    EXPECT_FALSE(TolT{});
-
-    EXPECT_SOFT_EQ(1e-10, ipow<2>(TolT::sqrt_quadratic()));
-
-    {
-        SCOPED_TRACE("Default tolerance");
-        auto const tol = TolT::from_default();
-        EXPECT_TRUE(tol);
-        EXPECT_SOFT_NEAR(
-            std::sqrt(std::numeric_limits<real_type>::epsilon()), tol.rel, 0.5);
-        EXPECT_SOFT_EQ(tol.rel, tol.abs);
-        if constexpr (std::is_same_v<real_type, double>)
-        {
-            EXPECT_SOFT_EQ(1e-8, tol.rel);
-        }
-    }
-    {
-        SCOPED_TRACE("Tolerance with other length scale");
-        auto const tol = TolT::from_default(1e-4);
-        EXPECT_SOFT_EQ(1e-8, tol.rel);
-        EXPECT_SOFT_EQ(1e-12, tol.abs);
-    }
-    {
-        SCOPED_TRACE("Tolerance with arbitrary relative");
-        auto const tol = TolT::from_relative(1e-5);
-        EXPECT_SOFT_EQ(1e-5, tol.rel);
-        EXPECT_SOFT_EQ(1e-5, tol.abs);
-    }
-    {
-        SCOPED_TRACE("Tolerance with arbitrary relative and length scale");
-        auto const tol = TolT::from_relative(1e-5, 0.1);
-        EXPECT_SOFT_EQ(1e-5, tol.rel);
-        EXPECT_SOFT_EQ(1e-6, tol.abs);
-    }
-}
-
 class OrangeTest : public OrangeGeoTestBase
 {
   protected:
