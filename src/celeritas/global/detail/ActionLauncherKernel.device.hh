a7625c334ab0847eba5005e18916cd00c4404fe0
Julien Esseiva <esseivaju@users.noreply.github.com>
2023-10-26 06:19:15 -0700

    Support device-specific launch bounds (#986)

--- src/celeritas/global/detail/ActionLauncherKernel.device.hh
+++ src/celeritas/global/detail/ActionLauncherKernel.device.hh
@@ -59,9 +59,8 @@ __global__ void __launch_bounds__(CELERITAS_MAX_BLOCK_SIZE)
 // Instantiated if F::Applier defines the first launch bounds argument
 template<class F,
          class A_ = typename F::Applier,
-         std::enable_if_t<kernel_max_blocks<A_>, bool> = true,
-         int T_ = A_::max_block_size>
-__global__ void __launch_bounds__(T_)
+         std::enable_if_t<kernel_max_blocks<A_>, bool> = true>
+__global__ void __launch_bounds__(A_::max_block_size)
     launch_action_impl(Range<ThreadId> const thread_range, F execute_thread)
 {
     launch_kernel_impl(thread_range, execute_thread);
@@ -70,18 +69,13 @@ __global__ void __launch_bounds__(T_)
 // Instantiated if F::Applier defines two arguments for launch bounds
 template<class F,
          class A_ = typename F::Applier,
-         std::enable_if_t<kernel_max_blocks_min_warps<A_>, bool> = true,
-         int T_ = A_::max_block_size,
+         std::enable_if_t<kernel_max_blocks_min_warps<A_>, bool> = true>
+__global__ void
 #if CELERITAS_USE_CUDA
-         int B_ = (A_::min_warps_per_eu * 32) / T_>
+    __launch_bounds__( A_::max_block_size, (A_::min_warps_per_eu * 32) / A_::max_block_size)
 #elif CELERITAS_USE_HIP
-         // https://rocm.docs.amd.com/projects/HIP/en/latest/reference/kernel_language.html#porting-from-cuda-launch-bounds
-         int B_ = A_::min_warps_per_eu>
-#else
-#    error \
-        "Compiling device code without setting either CELERITAS_USE_CUDA or CELERITAS_USE_HIP"
+    __launch_bounds__( A_::max_block_size,  A_::min_warps_per_eu)
 #endif
-__global__ void __launch_bounds__(T_, B_)
     launch_action_impl(Range<ThreadId> const thread_range, F execute_thread)
 {
     launch_kernel_impl(thread_range, execute_thread);
