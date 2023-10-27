4a969d078b8a5203e86f24d28df8937654766a21
Seth R Johnson <johnsonsr@ornl.gov>
2023-10-26 19:52:57 -0500

    Move CSG tree tests

--- test/orange/construct/CsgTreeUtils.test.cc
+++ test/orange/construct/CsgTreeUtils.test.cc
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file orange/CsgTreeUtils.test.cc
+//! \file orange/construct/CsgTreeUtils.test.cc
 //---------------------------------------------------------------------------//
 #include "orange/construct/CsgTreeUtils.hh"
 
