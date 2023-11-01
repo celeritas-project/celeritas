725abe57aee4ddd15c538cceca89343d2e7c4e0e
Seth R Johnson <johnsonsr@ornl.gov>
2023-11-01 13:45:04 -0400

    Fix double/float issues

--- test/orange/Orange.test.cc
+++ test/orange/Orange.test.cc
@@ -118,13 +118,13 @@ class ShiftTrackerTest : public OrangeTest
         track = {pos, dir};
     }
 
-    void distance_to_boundary(double& distance)
+    void distance_to_boundary(real_type& distance)
     {
         auto track = this->make_track_view();
         distance = track.find_next_step().distance;
     }
 
-    void move_to_point(double distance)
+    void move_to_point(real_type distance)
     {
         auto track = this->make_track_view();
         track.find_next_step();
@@ -1054,9 +1054,8 @@ TEST_F(HexArrayTest, track_out)
     OrangeTrackView geo = this->make_track_view();
 
     // Initialize
-    Real3 pos
-        = {-6.9258369494022292, -4.9982766629573767, -10.8378536157757495};
-    Real3 dir = {0.6750034206933703, -0.3679917428721818, 0.6394939086732125};
+    Real3 pos{-6.9258369494022292, -4.9982766629573767, -10.8378536157757495};
+    Real3 dir{0.6750034206933703, -0.3679917428721818, 0.6394939086732125};
 
     geo = Initializer_t{pos, dir};
 
@@ -1066,8 +1065,8 @@ TEST_F(HexArrayTest, track_out)
                                                celeritas::VolumeId{57},
                                                celeritas::VolumeId{2}};
 
-    std::vector<double> d2b;
-    std::vector<double> refd2b = {1.99143, 5.30607, 0.306368, 5.98808};
+    std::vector<real_type> d2b;
+    std::vector<real_type> refd2b = {1.99143, 5.30607, 0.306368, 5.98808};
 
     while (!geo.is_outside())
     {
@@ -1081,45 +1080,49 @@ TEST_F(HexArrayTest, track_out)
     }
 
     EXPECT_VEC_EQ(refids, vids);
-    EXPECT_VEC_CLOSE(d2b, refd2b, 1e-5, 1e-5);
+    EXPECT_VEC_CLOSE(d2b, refd2b, real_type(1e-5), real_type(1e-5));
 }
 
-TEST_F(ShiftTrackerTest, track)
+TEST_F(ShiftTrackerTest, host)
 {
-    std::vector<Real3> pos = {{-0.5466, 1.1298, -1.8526},
-                              {1.5968, -4.3272, -3.0764},
-                              {-1.2053, -2.7582, -0.1715},
-                              {-2.3368, -1.7800, 1.2726},
-                              {4.0610, 1.5512, 2.8693},
-                              {-1.5469, 1.6592, -0.6909},
-                              {-3.6040, -0.7626, -1.7840},
-                              {4.3726, -2.5543, -0.0444},
-                              {1.7047, 1.6042, 4.4779},
-                              {-0.8630, -4.8264, 3.1796}};
-    std::vector<Real3> mu_phi = {{0.215991, 1.114365},
-                                 {-0.887921, 1.414178},
-                                 {0.727041, 5.874378},
-                                 {0.822052, 3.051407},
-                                 {0.576156, 3.585084},
-                                 {-0.243608, 0.901952},
-                                 {0.486739, 2.328782},
-                                 {0.966572, 4.876337},
-                                 {-0.798997, 0.149136},
-                                 {0.748980, 1.677583}};
+    std::vector<Real3> pos{
+        {-0.5466, 1.1298, -1.8526},
+        {1.5968, -4.3272, -3.0764},
+        {-1.2053, -2.7582, -0.1715},
+        {-2.3368, -1.7800, 1.2726},
+        {4.0610, 1.5512, 2.8693},
+        {-1.5469, 1.6592, -0.6909},
+        {-3.6040, -0.7626, -1.7840},
+        {4.3726, -2.5543, -0.0444},
+        {1.7047, 1.6042, 4.4779},
+        {-0.8630, -4.8264, 3.1796},
+    };
+    std::vector<Array<real_type, 2>> mu_phi{
+        {0.215991, 1.114365},
+        {-0.887921, 1.414178},
+        {0.727041, 5.874378},
+        {0.822052, 3.051407},
+        {0.576156, 3.585084},
+        {-0.243608, 0.901952},
+        {0.486739, 2.328782},
+        {0.966572, 4.876337},
+        {-0.798997, 0.149136},
+        {0.748980, 1.677583},
+    };
 
     std::vector<unsigned int> steps(10, 0);
 
     for (auto n : range(pos.size()))
     {
         auto costheta = mu_phi[n][0];
-        auto sintheta = std::sqrt(1.0 - costheta * costheta);
+        auto sintheta = std::sqrt(1 - costheta * costheta);
         auto phi = mu_phi[n][1];
         Real3 dir
             = {sintheta * std::cos(phi), sintheta * std::sin(phi), costheta};
 
         this->initialize(pos[n], dir);
 
-        auto dbnd = std::numeric_limits<double>::max();
+        auto dbnd = std::numeric_limits<real_type>::max();
         auto cell = this->invalid_id();
         BoundaryState bnd_state = BoundaryState::INSIDE;
 
