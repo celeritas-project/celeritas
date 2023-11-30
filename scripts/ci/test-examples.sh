16f3f32f77588d7bd9f784156e3c01a5931cd7d7
Seth R Johnson <johnsonsr@ornl.gov>
2023-11-30 08:36:28 -0500

    REVERTME: delete hip and restore everything to last known working
    Somehow looks like docker isn't closing and reopening????

--- scripts/ci/test-examples.sh
+++ scripts/ci/test-examples.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 # Copyright 2022-2023 UT-Battelle, LLC, and other Celeritas developers.
 # See the top-level COPYRIGHT file for details.
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -11,14 +11,7 @@ if [ -z "${CELER_INSTALL_DIR}" ]; then
 fi
 export CMAKE_PREFIX_PATH=${CELER_INSTALL_DIR}:${CMAKE_PREFIX_PATH}
 
-test -d "${CELER_INSTALL_DIR}" || (
-  echo "CELER_INSTALL_DIR=${CELER_INSTALL_DIR} is not a directory"
-  exit 1
-)
-test -n "${CMAKE_PRESET}" || (
-  echo "CMAKE_PRESET is undefined"
-  exit 1
-)
+test -n "${CMAKE_PRESET}" || (echo "CMAKE_PRESET is undefined"; exit 1)
 
 build_local() {
   git clean -fxd .
@@ -35,11 +28,9 @@ cd "${CELER_SOURCE_DIR}/example/minimal"
 build_local
 ./minimal
 
-# Only run on configurations with '-vecgeom'
-if [ "${CMAKE_PRESET#*"-vecgeom"}" != "${CMAKE_PRESET}" ]; then
+if [ "${CMAKE_PRESET}" = "vecgeom-demos" ]; then
+  # The 'accel' example requires Geant4 and VecGeom
   cd "${CELER_SOURCE_DIR}/example/accel"
   build_local
   ctest -V --no-tests=error
-else
-  echo "Skipping 'accel' test: vecgeom appears not to be available"
 fi
