1782ddbdb3ca0285413cefec39d60380da087a62
Seth R. Johnson <johnsonsr@ornl.gov>
2023-12-05 17:37:30 -0500

    Support JSON via stdin for celer-g4 (#1048)

--- app/celer-g4/test-harness.py
+++ app/celer-g4/test-harness.py
@@ -91,20 +91,64 @@ inp = {
 with open(inp_file, "w") as f:
     json.dump(inp, f, indent=1)
 
+kwargs = {}
+if use_celeritas:
+    # IO through streams should work with celeritas or g4 as driver, but just
+    # do it here as an example
+    inp_file = "-"
+    inp["output_file"] = "-"
+    env = dict(environ)
+    kwargs = dict(
+        input=json.dumps(inp).encode(),
+        stdout=subprocess.PIPE,
+        env=env,
+    )
+
+
 print("Running", exe, inp_file, file=stderr)
-result = subprocess.run([exe, inp_file])
+result = subprocess.run([exe, inp_file], **kwargs)
+
+if use_celeritas:
+    # Decode the output, fail if the run failed
+    out_text = result.stdout.decode()
+    try:
+        j = json.loads(out_text)
+    except json.decoder.JSONDecodeError as e:
+        print(f"error ({e}): expected a JSON object but got the following stdout:")
+        with open(out_file, 'w') as f:
+            f.write(out_text)
+        print("Wrote text to", out_file)
+        if result.returncode:
+            print("fatal:", str(e))
+            exit(result.returncode)
+    else:
+        with open(out_file, 'w') as f:
+            json.dump(j, f, indent=1)
 
 if result.returncode:
     print("fatal: run failed with error", result.returncode)
+    if use_celeritas:
+        try:
+            j = json.loads(result.stdout.decode())
+        except:
+            pass
+        else:
+            outfilename = f'{run_name}.out.failed.json'
+            with open(outfilename, 'w') as f:
+                json.dump(j, f, indent=1)
+            print("Failure written to", outfilename, file=stderr)
+            j = {}
+
     exit(result.returncode)
 
-with open(out_file) as f:
-    result = json.load(f)
+if not use_celeritas:
+    # Load written file
+    with open(out_file) as f:
+        j = json.load(f)
 
-pprint(result["result"])
+    # Rewrite with indentation
+    with open(out_file, 'w') as f:
+        json.dump(j, f, indent=1)
 
-# Rewrite with indentation
-with open(out_file, 'w') as f:
-    json.dump(result, f, indent=1)
-
+pprint(j["result"])
 
