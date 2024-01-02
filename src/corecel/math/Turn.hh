e3becdeac663fbcb17e91b0c9325b8324695967c
Seth R Johnson <johnsonsr@ornl.gov>
2024-01-02 13:48:14 -0500

    Define integer quarter turn helper class

--- src/corecel/math/Turn.hh
+++ src/corecel/math/Turn.hh
@@ -7,6 +7,8 @@
 //---------------------------------------------------------------------------//
 #pragma once
 
+#include <cmath>
+
 #include "corecel/Types.hh"
 
 #include "Algorithms.hh"
@@ -24,8 +26,21 @@ struct TwoPi
 };
 
 //---------------------------------------------------------------------------//
+//! Unit for pi/2 radians
+struct HalfPi
+{
+    static real_type value() { return static_cast<real_type>(m_pi / 2); }
+    //! Text label for output
+    static char const* label() { return "qtr"; }
+};
+
+//---------------------------------------------------------------------------//
 //! Quantity denoting a full turn
 using Turn = Quantity<TwoPi, real_type>;
+
+//---------------------------------------------------------------------------//
+//! Quantity for an integer number of turns for axis swapping
+using QuarterTurn = Quantity<HalfPi, int>;
 
 //---------------------------------------------------------------------------//
 //!@{
@@ -43,6 +58,24 @@ CELER_FORCEINLINE_FUNCTION real_type cos(Turn r)
 CELER_FORCEINLINE_FUNCTION void sincos(Turn r, real_type* sinv, real_type* cosv)
 {
     return sincospi(r.value() * 2, sinv, cosv);
+}
+
+CELER_FORCEINLINE_FUNCTION int cos(QuarterTurn r)
+{
+    constexpr int cosval[] = {1, 0, -1, 0};
+    return cosval[std::abs(r.value()) % 4];
+}
+
+CELER_FORCEINLINE_FUNCTION int sin(QuarterTurn r)
+{
+    // Define in terms of the symmetric "cos"
+    return cos(QuarterTurn{r.value() - 1});
+}
+
+CELER_FORCEINLINE_FUNCTION void sincos(QuarterTurn r, int* sinv, int* cosv)
+{
+    *sinv = sin(r);
+    *cosv = cos(r);
 }
 //!@}
 
