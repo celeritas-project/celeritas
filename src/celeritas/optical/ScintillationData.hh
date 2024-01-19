c6c19dbb8efa50ec7b387810a17e8d0551e89a05
Soon Yung Jun <soon.yung.jun@cern.ch>
2024-01-19 17:41:34 -0600

    Remove ScintillationData and move ScintillationSpectra to ScintillationData

--- src/celeritas/optical/ScintillationData.hh
+++ src/celeritas/optical/ScintillationData.hh
@@ -18,6 +18,24 @@ namespace celeritas
 {
 //---------------------------------------------------------------------------//
 /*!
+ * Material dependent scintillation spectrum.
+ *
+ * Support up to 3 components: [fast, medium, slow]
+ */
+struct ScintillationSpectrum
+{
+    static inline constexpr size_type size = 3;
+    using Real3 = Array<real_type, size>;
+
+    Real3 yield_prob{};  //!< Probability of the yield
+    Real3 lambda_mean{};  //!< Mean wavelength
+    Real3 lambda_sigma{};  //!< Standard dev. of wavelength
+    Real3 rise_time{};  //!< Rise time
+    Real3 fall_time{};  //!< Decay time
+};
+
+//---------------------------------------------------------------------------//
+/*!
  *  Scintillation data tabulated with the optical material id.
  */
 template<Ownership W, MemSpace M>
@@ -28,19 +46,19 @@ struct ScintillationData
 
     //// MEMBER DATA ////
 
-    OpticalMaterialItems<ScintillationSpectra> spectra;
+    OpticalMaterialItems<ScintillationSpectrum> spectrum;
 
     //// MEMBER FUNCTIONS ////
 
     //! Whether all data are assigned and valid
-    explicit CELER_FUNCTION operator bool() const { return !spectra.empty(); }
+    explicit CELER_FUNCTION operator bool() const { return !spectrum.empty(); }
 
     //! Assign from another set of data
     template<Ownership W2, MemSpace M2>
     ScintillationData& operator=(ScintillationData<W2, M2> const& other)
     {
         CELER_EXPECT(other);
-        spectra = other.spectra;
+        spectrum = other.spectrum;
         return *this;
     }
 };
