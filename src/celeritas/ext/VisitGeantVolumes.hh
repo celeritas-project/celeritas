7b9954b0f78c7a9c8cac5fe1aa4959df18a1879b
Seth R Johnson <johnsonsr@ornl.gov>
2024-02-01 14:20:31 -0500

    fixup! Refactor Geant4 volume visitor to an actual volume visitor

--- src/celeritas/ext/VisitGeantVolumes.hh
+++ src/celeritas/ext/VisitGeantVolumes.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file geocel/g4/VisitGeantVolumes.hh
+//! \file celeritas/ext/VisitGeantVolumes.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
