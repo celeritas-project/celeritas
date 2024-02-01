780ced580274176e1d1028f3a9a0a187501af5c2
Seth R Johnson <johnsonsr@ornl.gov>
2024-02-01 15:39:50 -0500

    fixup! Split generic geo test into multiple

--- test/celeritas/ext/GeantGeoTestBase.cc
+++ test/celeritas/ext/GeantGeoTestBase.cc
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/GeantGeoTestBase.cc
+//! \file celeritas/ext/GeantGeoTestBase.cc
 //---------------------------------------------------------------------------//
 #include "GeantGeoTestBase.hh"
 
--- test/celeritas/ext/GeantGeoTestBase.hh
+++ test/celeritas/ext/GeantGeoTestBase.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/g4/GeantGeoTestBase.hh
+//! \file celeritas/ext/GeantGeoTestBase.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
