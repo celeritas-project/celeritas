eb010290c57695879693e634ec6a54291e7f4508
Seth R Johnson <johnsonsr@ornl.gov>
2024-02-01 15:46:48 -0500

    fixup! Split generic geo test into multiple

--- test/celeritas/OrangeTestBase.hh
+++ test/celeritas/OrangeTestBase.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/GenericOrangeTestBase.hh
+//! \file celeritas/OrangeTestBase.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
