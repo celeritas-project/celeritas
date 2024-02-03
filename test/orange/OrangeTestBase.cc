eef03206b8b477e975600bc733dd0ab9bf7511b6
Seth R Johnson <johnsonsr@ornl.gov>
2024-02-03 09:13:52 -0500

    fixup! Reorganize test files

--- test/orange/OrangeTestBase.cc
+++ test/orange/OrangeTestBase.cc
@@ -3,33 +3,24 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/OrangeTestBase.hh
+//! \file celeritas/OrangeTestBase.cc
 //---------------------------------------------------------------------------//
-#pragma once
+#include "OrangeTestBase.hh"
 
 #include "orange/OrangeData.hh"
 #include "orange/OrangeParams.hh"
 #include "orange/OrangeTrackView.hh"
 
-#include "GenericGeoTestBase.hh"
+#include "geocel/CheckedGeoTrackView.t.hh"
+#include "geocel/GenericGeoTestBase.t.hh"
 
 namespace celeritas
 {
 namespace test
 {
 //---------------------------------------------------------------------------//
-template<>
-struct GenericGeoTraits<OrangeParams>
-{
-    template<MemSpace M>
-    using StateStore = CollectionStateStore<OrangeStateData, M>;
-
-    using TrackView = OrangeTrackView;
-    static inline char const* ext = ".org.json";
-    static inline char const* name = "ORANGE";
-};
-
-using OrangeTestBase = GenericGeoTestBase<OrangeParams>;
+template class CheckedGeoTrackView<OrangeTrackView>;
+template class GenericGeoTestBase<OrangeParams>;
 
 //---------------------------------------------------------------------------//
 }  // namespace test
--- test/orange/OrangeTestBase.hh
+++ test/orange/OrangeTestBase.hh
@@ -3,24 +3,33 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/OrangeTestBase.cc
+//! \file celeritas/OrangeTestBase.hh
 //---------------------------------------------------------------------------//
-#include "OrangeTestBase.hh"
+#pragma once
 
 #include "orange/OrangeData.hh"
 #include "orange/OrangeParams.hh"
 #include "orange/OrangeTrackView.hh"
 
-#include "CheckedGeoTrackView.t.hh"
-#include "GenericGeoTestBase.t.hh"
+#include "geocel/GenericGeoTestBase.hh"
 
 namespace celeritas
 {
 namespace test
 {
 //---------------------------------------------------------------------------//
-template class CheckedGeoTrackView<OrangeTrackView>;
-template class GenericGeoTestBase<OrangeParams>;
+template<>
+struct GenericGeoTraits<OrangeParams>
+{
+    template<MemSpace M>
+    using StateStore = CollectionStateStore<OrangeStateData, M>;
+
+    using TrackView = OrangeTrackView;
+    static inline char const* ext = ".org.json";
+    static inline char const* name = "ORANGE";
+};
+
+using OrangeTestBase = GenericGeoTestBase<OrangeParams>;
 
 //---------------------------------------------------------------------------//
 }  // namespace test
