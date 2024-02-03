c25e4bf3704f333098ba9d89ce9cf798e9684f53
Seth R Johnson <johnsonsr@ornl.gov>
2024-02-03 09:40:57 -0500

    fixup! Move more source files

--- src/geocel/vg/detail/SurfNavigator.hh
+++ src/geocel/vg/detail/SurfNavigator.hh
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //---------------------------------------------------------------------------//
 /*!
- * \file SurfNavigator.hh
+ * \file geocel/vg/detail/SurfNavigator.hh
  * \brief Navigation methods using the surface model.
  *
  * Original source:
