1fda240c33a7c550d0b3ded9454909c51e192a7b
Seth R Johnson <johnsonsr@ornl.gov>
2024-02-05 08:55:47 -0500

    Fix doxygen

--- test/orange/OrangeTestBase.hh
+++ test/orange/OrangeTestBase.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/OrangeTestBase.hh
+//! \file orange/OrangeTestBase.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
