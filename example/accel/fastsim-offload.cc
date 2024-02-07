2a0228e4dee69ecec17f2dd81768e29e5b88242a
Seth R Johnson <johnsonsr@ornl.gov>
2024-02-06 21:23:44 -0500

    Fix paths in example

--- example/accel/fastsim-offload.cc
+++ example/accel/fastsim-offload.cc
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file accel/fastsim-offload.cc
+//! \file example/accel/fastsim-offload.cc
 //---------------------------------------------------------------------------//
 
 #include <algorithm>
--- example/accel/simple-offload.cc
+++ example/accel/simple-offload.cc
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file accel/simple-offload.cc
+//! \file scripts/ci/test-installation/example-accel.cc
 //---------------------------------------------------------------------------//
 
 #include <algorithm>
