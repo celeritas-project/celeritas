de0952185b255c43547868de504c9c00707b8c8a
Soon Yung Jun <syjun@fnal.gov>
2024-02-22 10:48:54 -0600

    Add a helper class to load neutron elastic cgit commit -m section data (G4PARTICLEXSDATA/neutron/el)

---
+++ src/celeritas/io/NeutronXsReader.cc
@@ -0,0 +1,99 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2024 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celeritas/io/NeutronXsReader.cc
+//---------------------------------------------------------------------------//
+#include "NeutronXsReader.hh"
+
+#include <fstream>
+#include <vector>
+
+#include "corecel/Assert.hh"
+#include "corecel/Types.hh"
+#include "corecel/io/Logger.hh"
+#include "corecel/math/Algorithms.hh"
+#include "corecel/sys/Environment.hh"
+#include "celeritas/Units.hh"
+#include "celeritas/io/ImportLivermorePE.hh"
+#include "celeritas/io/ImportPhysicsVector.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Construct the reader using the G4PARTICLEXSDATA environment variable to get
+ * the path to the data.
+ */
+NeutronXsReader::NeutronXsReader()
+{
+    std::string const& dir = celeritas::getenv("G4PARTICLEXSDATA");
+    CELER_VALIDATE(!dir.empty(),
+                   << "environment variable G4PARTICLEXSDATA is not defined"
+                      " (needed to locate Livermore data)");
+    path_ = dir + "/neutron";
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Construct the reader with the path to the directory containing the data.
+ */
+NeutronXsReader::NeutronXsReader(char const* path) : path_(path)
+{
+    CELER_EXPECT(!path_.empty());
+    if (path_.back() == '/')
+    {
+        path_.pop_back();
+    }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Read the data for the given element.
+ */
+NeutronXsReader::result_type
+NeutronXsReader::operator()(AtomicNumber atomic_number) const
+{
+    CELER_EXPECT(atomic_number && atomic_number < AtomicNumber{92});
+
+    std::string z_str = std::to_string(atomic_number.unchecked_get());
+    CELER_LOG(debug) << "Reading neutron elastic xs data for Z=" << z_str;
+
+    result_type result;
+
+    // Read neutron elastic cross section data for the given atomic_number
+    {
+        std::string filename = path_ + "/el" + z_str;
+        std::ifstream infile;
+        infile.open(filename);
+        CELER_VALIDATE(infile.is_open(),
+                       << "failed to open '" << filename
+                       << "' (should contain cross section data)");
+
+        // Set the physics vector type
+        result.vector_type = ImportPhysicsVectorType::free;
+
+        // Read tabulated energies and cross sections
+        double energy_min = 0.;
+        double energy_max = 0.;
+        size_type size = 0;
+        infile >> energy_min >> energy_max >> size >> size;
+        result.x.resize(size);
+        result.y.resize(size);
+        for (size_type i = 0; i < size; ++i)
+        {
+            CELER_ASSERT(infile);
+            infile >> result.x[i] >> result.y[i];
+            // Convert to the celeritas native length 1/[len^2] from
+	    // clhep::mm^2 as stored in G4PARTICLEXS/neutron/el data
+            result.y[i] *= ipow<2>(units::millimeter);
+        }
+        infile.close();
+    }
+
+    return result;
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
---
+++ src/celeritas/io/NeutronXsReader.hh
@@ -0,0 +1,46 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2024 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celeritas/io/NeutronXsReader.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include <string>
+
+#include "celeritas/phys/AtomicNumber.hh"
+
+#include "ImportPhysicsVector.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Load the neutron elastic cross section (G4PARTICLEXSDATA/neutron/elZ) data.
+ */
+class NeutronXsReader
+{
+  public:
+    //!@{
+    //! \name Type aliases
+    using result_type = ImportPhysicsVector;
+    //!@}
+
+  public:
+    // Construct the reader and locate the data using the environment variable
+    NeutronXsReader();
+
+    // Construct the reader from the path to the data directory
+    explicit NeutronXsReader(char const* path);
+
+    // Read the data for the given element
+    result_type operator()(AtomicNumber atomic_number) const;
+
+  private:
+    // Directory containing the neutron elastic cross section data
+    std::string path_;
+};
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
