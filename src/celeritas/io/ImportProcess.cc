c4482bbf6cfb58530bf99e65354d9ba368b5d3b7
Soon Yung Jun <syjun@fnal.gov>
2024-02-22 10:57:05 -0600

    Add a neutron_elastic process

--- src/celeritas/io/ImportProcess.cc
+++ src/celeritas/io/ImportProcess.cc
@@ -63,6 +63,7 @@ char const* to_cstring(ImportProcessClass value)
         "mu_brems",
         "mu_pair_prod",
         "gamma_general",
+        "neutron_elastic",
     };
     return to_cstring_impl(value);
 }
@@ -92,6 +93,7 @@ char const* to_geant_name(ImportProcessClass value)
         "muBrems",  // mu_brems,
         "muPairProd",  // mu_pair_prod,
         "GammaGeneralProc",  // gamma_general,
+        "neutronElasticProc",  // neutron_elastic,
     };
     return to_name_impl(value);
 }
