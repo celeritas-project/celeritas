af302a278f6584be2ba6df0ce0645edfeff319ae
Seth R Johnson <johnsonsr@ornl.gov>
2024-02-22 22:23:30 -0500

    Revert "REVERTME: move second-order convex regions to a follow-on PR"
    This reverts commit 581659ab04d0cf35f2e2859d20ed72c33be32959.

--- src/orange/orangeinp/ConvexRegion.cc
+++ src/orange/orangeinp/ConvexRegion.cc
@@ -76,6 +76,178 @@ void Box::build(ConvexSurfaceBuilder& insert_surface) const
 }
 
 //---------------------------------------------------------------------------//
+// CONE
+//---------------------------------------------------------------------------//
+/*!
+ * Construct with Z halfwidth and lo, hi radii.
+ */
+Cone::Cone(Real2 const& radii, real_type halfheight)
+    : radii_{radii}, hh_{halfheight}
+{
+    for (auto i : range(2))
+    {
+        CELER_VALIDATE(radii_[i] >= 0, << "negative radius: " << radii_[i]);
+    }
+    CELER_VALIDATE(radii_[0] != radii_[1], << "radii cannot be equal");
+    CELER_VALIDATE(hh_ > 0, << "nonpositive halfheight: " << hh_);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Build surfaces.
+ *
+ * The inner bounding box of a cone
+ */
+void Cone::build(ConvexSurfaceBuilder& insert_surface) const
+{
+    // Build the bottom and top planes
+    insert_surface(Sense::outside, PlaneZ{-hh_});
+    insert_surface(Sense::inside, PlaneZ{hh_});
+
+    // Calculate the cone using lo and hi radii
+    real_type const lo{radii_[0]};
+    real_type const hi{radii_[1]};
+
+    // Arctangent of the opening angle of the cone (opposite / adjacent)
+    real_type const tangent = std::fabs(lo - hi) / (2 * hh_);
+
+    // Calculate vanishing point (origin)
+    real_type vanish_z = 0;
+    if (lo > hi)
+    {
+        // Cone opens downward (base is on bottom)
+        vanish_z = -hh_ + lo / tangent;
+        CELER_ASSERT(vanish_z > 0);
+    }
+    else
+    {
+        // Cone opens upward
+        vanish_z = hh_ - hi / tangent;
+        CELER_ASSERT(vanish_z < 0);
+    }
+
+    // Build the cone surface along the given axis
+    ConeZ cone{Real3{0, 0, vanish_z}, tangent};
+    insert_surface(cone);
+
+    // Set radial extents of exterior bbox
+    insert_surface(Sense::inside, make_xyradial_bbox(std::fmax(lo, hi)));
+
+    // Calculating the interior bounding box:
+    // - Represent a radial slice of the cone as a right triangle with base b
+    //   (aka the higher radius) and height h (translated vanishing point)
+    // - An interior bounding box (along the xy diagonal cut!) will satisfy
+    //   r = b - tangent * z
+    // - Maximizing the area of that box gives r = b / 2, i.e. z = h / 2
+    // - Truncate z so that it's not outside of the half-height
+    // - project that radial slice onto the xz plane by multiplying 1/sqrt(2)
+    real_type const b = std::fmax(lo, hi);
+    real_type const h = b / tangent;
+    real_type const z = std::fmin(h / 2, 2 * hh_);
+    real_type const r = b - tangent * z;
+
+    // Now convert from "triangle above z=0" to "cone centered on z=0"
+    auto const [zmin, zmax] = [&]() -> std::pair<real_type, real_type> {
+        if (lo > hi)
+            return {-hh_, -hh_ + z};  // Base is on bottom
+        else
+            return {hh_ - z, hh_};  // Base is on top
+    }();
+    CELER_ASSERT(zmin < zmax);
+    real_type const rbox = (constants::sqrt_two / 2) * r;
+    BBox const interior_bbox{{-rbox, -rbox, zmin}, {rbox, rbox, zmax}};
+
+    // Check that the corners are actually inside the cone
+    CELER_ASSERT(cone.calc_sense(interior_bbox.lower() * real_type(1 - 1e-5))
+                 == SignedSense::inside);
+    CELER_ASSERT(cone.calc_sense(interior_bbox.upper() * real_type(1 - 1e-5))
+                 == SignedSense::inside);
+    insert_surface(Sense::outside, interior_bbox);
+}
+
+//---------------------------------------------------------------------------//
+// CYLINDER
+//---------------------------------------------------------------------------//
+/*!
+ * Construct with radius.
+ */
+Cylinder::Cylinder(real_type radius, real_type halfheight)
+    : radius_{radius}, hh_{halfheight}
+{
+    CELER_VALIDATE(radius_ > 0, << "nonpositive radius: " << radius_);
+    CELER_VALIDATE(hh_ > 0, << "nonpositive half-height: " << hh_);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Build surfaces.
+ */
+void Cylinder::build(ConvexSurfaceBuilder& insert_surface) const
+{
+    insert_surface(Sense::outside, PlaneZ{-hh_});
+    insert_surface(Sense::inside, PlaneZ{hh_});
+    insert_surface(CCylZ{radius_});
+}
+
+//---------------------------------------------------------------------------//
+// ELLIPSOID
+//---------------------------------------------------------------------------//
+/*!
+ * Construct with half-widths.
+ */
+Ellipsoid::Ellipsoid(Real3 const& radii) : radii_{radii}
+{
+    for (auto ax : range(Axis::size_))
+    {
+        CELER_VALIDATE(radii_[to_int(ax)] > 0,
+                       << "nonpositive radius " << to_char(ax)
+                       << " axis: " << radii_[to_int(ax)]);
+    }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Build surfaces.
+ */
+void Ellipsoid::build(ConvexSurfaceBuilder& insert_surface) const
+{
+    // Second-order coefficients are product of the other two squared radii;
+    // Zeroth-order coefficient is the product of all three squared radii
+    Real3 rsq;
+    for (auto ax : range(to_int(Axis::size_)))
+    {
+        rsq[ax] = ipow<2>(radii_[ax]);
+    }
+
+    Real3 abc{1, 1, 1};
+    real_type g = -1;
+    for (auto ax : range(to_int(Axis::size_)))
+    {
+        g *= rsq[ax];
+        for (auto nax : range(to_int(Axis::size_)))
+        {
+            if (ax != nax)
+            {
+                abc[ax] *= rsq[nax];
+            }
+        }
+    }
+
+    insert_surface(SimpleQuadric{abc, Real3{0, 0, 0}, g});
+
+    // Set exterior bbox
+    insert_surface(Sense::inside, BBox{-radii_, radii_});
+
+    // Set an interior bbox with maximum volume: a scaled inscribed cube
+    Real3 inner_radii = radii_;
+    for (real_type& r : inner_radii)
+    {
+        r *= 1 / constants::sqrt_three;
+    }
+    insert_surface(Sense::outside, BBox{-inner_radii, inner_radii});
+}
+
+//---------------------------------------------------------------------------//
 // PRISM
 //---------------------------------------------------------------------------//
 /*!
@@ -143,6 +315,26 @@ void Prism::build(ConvexSurfaceBuilder& insert_surface) const
     interior_bbox.shrink(Bound::lo, Axis::z, -hh_);
     interior_bbox.shrink(Bound::hi, Axis::z, hh_);
     insert_surface(Sense::outside, interior_bbox);
+}
+
+//---------------------------------------------------------------------------//
+// SPHERE
+//---------------------------------------------------------------------------//
+/*!
+ * Construct with radius.
+ */
+Sphere::Sphere(real_type radius) : radius_{radius}
+{
+    CELER_VALIDATE(radius_ > 0, << "nonpositive radius: " << radius_);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Build surfaces.
+ */
+void Sphere::build(ConvexSurfaceBuilder& insert_surface) const
+{
+    insert_surface(SphereCentered{radius_});
 }
 
 //---------------------------------------------------------------------------//
