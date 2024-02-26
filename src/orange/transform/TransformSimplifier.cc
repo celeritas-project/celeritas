5c50bf1917d16a1a9be6eceb13e536d37f111611
Seth R Johnson <johnsonsr@ornl.gov>
2024-02-26 07:36:06 -0500

    Add transform simplifier

---
+++ src/orange/transform/TransformSimplifier.cc
@@ -0,0 +1,45 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2024 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file orange/transform/TransformSimplifier.cc
+//---------------------------------------------------------------------------//
+#include "TransformSimplifier.hh"
+
+#include "corecel/math/ArrayUtils.hh"
+#include "orange/MatrixUtils.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Translation may simplify to no transformation.
+ */
+VariantTransform TransformSimplifier::operator()(Translation const& t)
+{
+    if (norm(t.translation()) <= eps_)
+    {
+        // Distance to origin is less than tolerance
+        return NoTransformation{};
+    }
+    return t;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * One transformation step can simplify to translation.
+ */
+VariantTransform TransformSimplifier::operator()(Transformation const& t)
+{
+    real_type tr = trace(t.rotation());
+    if (tr >= 3 - ipow<2>(eps_))
+    {
+        // Rotation results in no more then epsilon movement
+        return (*this)(Translation{t.translation()});
+    }
+    return t;
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
