31666047fc795f91852eec8a002a36c43650fdbb
Soon Yung Jun <syjun@fnal.gov>
2024-03-06 01:34:43 -0600

    Updates for review comments by sethrj

--- src/celeritas/neutron/interactor/detail/MomentumTransferSampler.hh
+++ src/celeritas/neutron/interactor/detail/MomentumTransferSampler.hh
@@ -67,7 +67,7 @@ class MomentumTransferSampler
     Mass target_mass_;
 
     // Atomic mass number (A) of the target
-    AtomicMassNumber a_;
+    AtomicMassNumber amass_;
     bool heavy_target_{false};
     // Momentum magnitude of the incident neutron (GeV/c)
     Momentum neutron_p_;
@@ -101,6 +101,12 @@ class MomentumTransferSampler
     {
         return Momentum{14};
     }
+
+    // Limit of the slope square
+    static CELER_CONSTEXPR_FUNCTION real_type tolerance_slope_sq()
+    {
+        return 1e-7;
+    }
 };
 
 //---------------------------------------------------------------------------//
@@ -119,8 +125,8 @@ MomentumTransferSampler::MomentumTransferSampler(NeutronElasticRef const& shared
     : par_(shared.coeffs[target.isotope_id()].par)
     , neutron_mass_(shared.neutron_mass)
     , target_mass_(target.nuclear_mass())
-    , a_(target.atomic_mass_number())
-    , heavy_target_(a_.get() > 6)
+    , amass_(target.atomic_mass_number())
+    , heavy_target_(amass_.get() > 6)
     , neutron_p_(neutron_p)
     , max_q_sq_(calc_max_q_sq(neutron_p_))
     , par_q_sq_(calc_par_q_sq(neutron_p_))
@@ -147,19 +153,20 @@ CELER_FUNCTION auto MomentumTransferSampler::operator()(Engine& rng)
     // Sample \f$ Q^{2} \f$
     real_type q_sq = 0;
 
-    if (a_ == AtomicMassNumber{1})
+    if (amass_ == AtomicMassNumber{1})
     {
         // Special case for the \f$ n + p \rightarrow n + p \f$ channel
-        real_type r_1 = -std::expm1(-max_q_sq_ * par_q_sq_.slope[0]);
-        real_type r_2 = -std::expm1(-max_q_sq_ * par_q_sq_.slope[1]);
+        real_type const r[2] = {-std::expm1(-max_q_sq_ * par_q_sq_.slope[0]),
+                                -std::expm1(-max_q_sq_ * par_q_sq_.slope[1])};
 
-        real_type i_1 = r_1 * par_q_sq_.expnt[0];
-        real_type i_2 = r_2 * par_q_sq_.expnt[1] / par_q_sq_.slope[1];
+        real_type const mi[2]
+            = {r[0] * par_q_sq_.expnt[0],
+               r[1] * par_q_sq_.expnt[1] / par_q_sq_.slope[1]};
 
         // Sample by t-channel and u-channel (charge exchange)
-        q_sq = (BernoulliDistribution(i_1 / (i_1 + i_2))(rng))
-                   ? sample_q_sq(r_1, rng) / par_q_sq_.slope[0]
-                   : max_q_sq_ - sample_q_sq(r_2, rng) / par_q_sq_.slope[1];
+        q_sq = (BernoulliDistribution(mi[0] / (mi[0] + mi[1]))(rng))
+                   ? sample_q_sq(r[0], rng) / par_q_sq_.slope[0]
+                   : max_q_sq_ - sample_q_sq(r[1], rng) / par_q_sq_.slope[1];
     }
     else
     {
@@ -168,32 +175,30 @@ CELER_FUNCTION auto MomentumTransferSampler::operator()(Engine& rng)
         constexpr real_type one_fifth{0.2};
         constexpr real_type one_seventh = 1 / real_type(7);
 
-        real_type r_1 = -std::expm1(
-            -max_q_sq_ * (par_q_sq_.slope[0] + max_q_sq_ * par_q_sq_.ss));
+        real_type const r[4]
+            = {-std::expm1(-max_q_sq_
+                           * (par_q_sq_.slope[0] + max_q_sq_ * par_q_sq_.ss)),
+               -std::expm1(
+                   -(heavy_target_ ? ipow<5>(max_q_sq_) : ipow<3>(max_q_sq_))
+                   * par_q_sq_.slope[1]),
+               -std::expm1(-(heavy_target_ ? ipow<7>(max_q_sq_) : max_q_sq_)
+                           * par_q_sq_.slope[2]),
+               -std::expm1(-max_q_sq_ * par_q_sq_.slope[3])};
 
-        // power 3 for low-A and power 5 for high-A
-        real_type factor = heavy_target_ ? ipow<5>(max_q_sq_)
-                                         : ipow<3>(max_q_sq_);
-        real_type r_2 = -std::expm1(-factor * par_q_sq_.slope[1]);
+        real_type mi[6] = {};
+        for (auto i : range(4))
+        {
+            mi[i] = r[i] * par_q_sq_.expnt[i];
+        }
+        mi[4] = mi[0] + mi[1];
+        mi[5] = mi[2] + mi[4];
 
-        // power 1 for low-A and power 7 for high-A
-        factor = heavy_target_ ? ipow<7>(max_q_sq_) : max_q_sq_;
-        real_type r_3 = -std::expm1(-factor * par_q_sq_.slope[2]);
-        real_type r_4 = -std::expm1(-max_q_sq_ * par_q_sq_.slope[3]);
-
-        real_type i_1 = r_1 * par_q_sq_.expnt[0];
-        real_type i_2 = r_2 * par_q_sq_.expnt[1];
-        real_type i_3 = r_3 * par_q_sq_.expnt[2];
-        real_type i_4 = r_4 * par_q_sq_.expnt[3];
-        real_type i_5 = i_1 + i_2;
-        real_type i_6 = i_3 + i_5;
-
-        real_type rand = (i_4 + i_6) * generate_canonical(rng);
-        if (rand < i_1)
+        real_type rand = (mi[3] + mi[5]) * generate_canonical(rng);
+        if (rand < mi[0])
         {
             real_type tss = 2 * par_q_sq_.ss;
-            q_sq = this->sample_q_sq(r_1, rng) / par_q_sq_.slope[0];
-            if (std::fabs(tss) > 1e-7)
+            q_sq = this->sample_q_sq(r[0], rng) / par_q_sq_.slope[0];
+            if (std::fabs(tss) > this->tolerance_slope_sq())
             {
                 q_sq = (std::sqrt(par_q_sq_.slope[0]
                                   * (par_q_sq_.slope[0] + 2 * tss * q_sq))
@@ -201,16 +206,15 @@ CELER_FUNCTION auto MomentumTransferSampler::operator()(Engine& rng)
                        / tss;
             }
         }
-        else if (rand < i_5)
+        else if (rand < mi[4])
         {
-            q_sq = clamp_to_nonneg(this->sample_q_sq(r_2, rng)
+            q_sq = clamp_to_nonneg(this->sample_q_sq(r[1], rng)
                                    / par_q_sq_.slope[1]);
-            q_sq = (heavy_target_) ? std::pow(q_sq, one_fifth)
-                                   : std::pow(q_sq, one_third);
+            q_sq = std::pow(q_sq, heavy_target_ ? one_fifth : one_third);
         }
-        else if (rand < i_6)
+        else if (rand < mi[5])
         {
-            q_sq = clamp_to_nonneg(this->sample_q_sq(r_3, rng)
+            q_sq = clamp_to_nonneg(this->sample_q_sq(r[2], rng)
                                    / par_q_sq_.slope[2]);
             if (heavy_target_)
             {
@@ -219,7 +223,7 @@ CELER_FUNCTION auto MomentumTransferSampler::operator()(Engine& rng)
         }
         else
         {
-            q_sq = this->sample_q_sq(r_4, rng) / par_q_sq_.slope[3];
+            q_sq = this->sample_q_sq(r[3], rng) / par_q_sq_.slope[3];
             // u reduced for light-A (starts from 0)
             if (!heavy_target_)
             {
@@ -249,13 +253,13 @@ CELER_FUNCTION real_type MomentumTransferSampler::calc_max_q_sq(Momentum p) cons
     // Momentum and mass square of the incident neutron
     real_type target_mass = value_as<Mass>(target_mass_);
 
-    real_type p2 = ipow<2>(value_as<units::MevMomentum>(p));
-    real_type m2 = ipow<2>(value_as<Mass>(neutron_mass_));
-    real_type a2 = ipow<2>(target_mass);
+    real_type p_sq = ipow<2>(value_as<units::MevMomentum>(p));
+    real_type m_sq = ipow<2>(value_as<Mass>(neutron_mass_));
+    real_type a_sq = ipow<2>(target_mass);
 
     // Return the maximum momentum transfer in the value of clhep::GeV square
-    return 4 * a2 * p2 * ipow<2>(this->to_gev())
-           / (2 * target_mass * std::sqrt(p2 + m2) + m2 + a2);
+    return 4 * a_sq * p_sq * ipow<2>(this->to_gev())
+           / (2 * target_mass * std::sqrt(p_sq + m_sq) + m_sq + a_sq);
 }
 
 //---------------------------------------------------------------------------//
@@ -278,7 +282,7 @@ auto MomentumTransferSampler::calc_par_q_sq(Momentum neutron_p) const
 
     ExchangeParameters result;
 
-    if (a_ == AtomicMassNumber{1})
+    if (amass_ == AtomicMassNumber{1})
     {
         // Special case for the \f$ n + p \rightarrow n + p \f$ channel
         real_type dl1 = lp - par_[3];
@@ -320,37 +324,37 @@ auto MomentumTransferSampler::calc_par_q_sq(Momentum neutron_p) const
 
         if (!heavy_target_)
         {
-            real_type pah = std::pow(p, real_type(0.5) * a_.get());
+            real_type pah = std::pow(p, real_type(0.5) * amass_.get());
             real_type pa = ipow<2>(pah);
             real_type pa2 = ipow<2>(pa);
-            result.expnt[0] = par_[0] / (1. + par_[1] * p4 * pa)
+            result.expnt[0] = par_[0] / (1 + par_[1] * p4 * pa)
                               + par_[2] / (p4 + par_[3] * p4 / pa2)
                               + (par_[4] * ipow<2>(dl) + par_[5])
-                                    / (1. + par_[6] / p2);
+                                    / (1 + par_[6] / p2);
             result.slope[0] = (par_[7] + par_[8] * p2) / (p4 + par_[9] / pah)
                               + par_[10];
-            result.ss = par_[11] / (1. + par_[12] / p2)
+            result.ss = par_[11] / (1 + par_[12] / p2)
                         + par_[13] / (p6 / pa + par_[14] / p16);
             result.expnt[1] = par_[15] / (pa / p2 + par_[16] / p4) + par_[17];
             result.slope[1] = par_[18] * std::pow(p, par_[19])
                               + par_[20] / (p8 + par_[21] / p16);
             result.expnt[2] = par_[22] / (pa * p + par_[23] / pa) + par_[24];
             result.slope[2] = par_[25] / (p3 + par_[26] / p6)
-                              + par_[27] / (1. + par_[28] / p2);
+                              + par_[27] / (1 + par_[28] / p2);
             result.expnt[3]
                 = p2
                   * (pah * par_[29] * std::exp(-pah * par_[30])
-                     + par_[31] / (1. + par_[32] * std::pow(p, par_[33])));
-            result.slope[3] = par_[34] * pa / p2 / (1. + pa * par_[35]);
+                     + par_[31] / (1 + par_[32] * std::pow(p, par_[33])));
+            result.slope[3] = par_[34] * pa / p2 / (1 + pa * par_[35]);
         }
         else
         {
-            result.expnt[0] = par_[0] / (1. + par_[1] / p4)
+            result.expnt[0] = par_[0] / (1 + par_[1] / p4)
                               + par_[2] / (p4 + par_[3] / p2)
                               + par_[4] / (p5 + par_[5] / p16);
             result.slope[0] = (par_[6] / p8 + par_[10])
                                   / (p + par_[7] / std::pow(p, par_[11]))
-                              + par_[8] / (1. + par_[9] / p4);
+                              + par_[8] / (1 + par_[9] / p4);
             result.ss = par_[12] / (p4 / std::pow(p, par_[14]) + par_[13] / p4);
             result.expnt[1] = par_[15] / p4
                                   / (std::pow(p, par_[16]) + par_[17] / p12)
@@ -358,15 +362,15 @@ auto MomentumTransferSampler::calc_par_q_sq(Momentum neutron_p) const
             result.slope[1] = par_[19] / std::pow(p, par_[20])
                               + par_[21] / std::pow(p, par_[22]);
             result.expnt[2] = par_[23] / std::pow(p, par_[26])
-                                  / (1. + par_[27] / p12)
+                                  / (1 + par_[27] / p12)
                               + par_[24] / (1. + par_[25] / p6);
             result.slope[2] = par_[28] / p8 + par_[29] / p2
-                              + par_[30] / (1. + par_[31] / p8);
+                              + par_[30] / (1 + par_[31] / p8);
             result.expnt[3]
-                = (par_[32] / p4 + par_[37] / p) / (1. + par_[33] / p10)
-                  + (par_[34] + par_[35] * dl * dl) / (1. + par_[36] / p12);
-            result.slope[3] = par_[38] / (1. + par_[39] / p)
-                              + par_[40] * p4 / (1. + par_[41] * p5);
+                = (par_[32] / p4 + par_[37] / p) / (1 + par_[33] / p10)
+                  + (par_[34] + par_[35] * dl * dl) / (1 + par_[36] / p12);
+            result.slope[3] = par_[38] / (1 + par_[39] / p)
+                              + par_[40] * p4 / (1 + par_[41] * p5);
         }
     }
 
