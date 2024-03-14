ac73ba9d7838aea9f38de790d89993d4edc57cef
Stefano Tognini <togninis@ornl.gov>
2024-03-14 10:52:12 -0400

    Update `GeantImporter` test

--- test/celeritas/ext/GeantImporter.test.cc
+++ test/celeritas/ext/GeantImporter.test.cc
@@ -1342,10 +1342,10 @@ TEST_F(LarSphere, optical)
     auto const& scint = optical.scintillation;
     EXPECT_TRUE(scint);
     EXPECT_EQ(1, scint.resolution_scale);
-    EXPECT_EQ(50000, scint.yield);
-    EXPECT_EQ(3, scint.components.size());
+    EXPECT_EQ(50000, scint.material.yield);
+    EXPECT_EQ(3, scint.material.components.size());
     std::vector<double> components;
-    for (auto const& comp : scint.components)
+    for (auto const& comp : scint.material.components)
     {
         components.push_back(comp.yield);
         components.push_back(to_cm(comp.lambda_mean));
