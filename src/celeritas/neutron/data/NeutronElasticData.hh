382fdd7c35a8de7b796651cdbd283bcefc73869f
Soon Yung Jun <syjun@fnal.gov>
2024-04-01 15:36:07 -0500

    Change NeutronInelasticIds to NeutronInelasticScalars and move other scalar data to NeutronInelasticScalars

--- src/celeritas/neutron/data/NeutronElasticData.hh
+++ src/celeritas/neutron/data/NeutronElasticData.hh
@@ -47,15 +47,33 @@ struct ChipsDiffXsCoefficients
 
 //---------------------------------------------------------------------------//
 /*!
- * Model and particles IDs for neutron--nucleus elastic scattering.
+ * Scalar data for neutron-nucleus elastic scattering.
  */
-struct NeutronElasticIds
+struct NeutronElasticScalars
 {
-    ActionId action;
-    ParticleId neutron;
+    //! Action and particle IDs
+    ActionId action_id;
+    ParticleId neutron_id;
 
-    //! Whether the IDs are assigned
-    explicit CELER_FUNCTION operator bool() const { return action && neutron; }
+    //! Particle mass * c^2 [MeV]
+    units::MevMass neutron_mass;
+
+    //! Model's minimum and maximum energy limit [MeV]
+    static CELER_CONSTEXPR_FUNCTION units::MevEnergy min_valid_energy()
+    {
+        return units::MevEnergy{1e-5};
+    }
+
+    static CELER_CONSTEXPR_FUNCTION units::MevEnergy max_valid_energy()
+    {
+        return units::MevEnergy{2e+4};
+    }
+
+    //! Whether data are assigned
+    explicit CELER_FUNCTION operator bool() const
+    {
+        return action_id && neutron_id && neutron_mass > zero_quantity();
+    }
 };
 
 //---------------------------------------------------------------------------//
@@ -76,11 +94,8 @@ struct NeutronElasticData
 
     //// MEMBER DATA ////
 
-    //! Model and particle IDs
-    NeutronElasticIds ids;
-
-    //! Particle mass * c^2 [MeV]
-    units::MevMass neutron_mass;
+    //! Scalar data
+    NeutronElasticScalars scalars;
 
     //! Microscopic (element) cross section data (G4PARTICLEXS/neutron/elZ)
     ElementItems<GenericGridData> micro_xs;
@@ -88,6 +103,7 @@ struct NeutronElasticData
     //! A-dependent coefficients for the momentum transfer of the CHIPS model
     IsotopeItems<ChipsDiffXsCoefficients> coeffs;
 
+<<<<<<< HEAD
     // Backend data
     Items<real_type> reals;
 
@@ -109,6 +125,13 @@ struct NeutronElasticData
     {
         return ids && neutron_mass > zero_quantity() && !micro_xs.empty()
                && !coeffs.empty() && !reals.empty();
+=======
+    //! Whether the data are assigned
+    explicit CELER_FUNCTION operator bool() const
+    {
+        return scalars && !reals.empty() && !micro_xs.empty()
+               && !coeffs.empty();
+>>>>>>> 8a25bcfe2 (Change NeutronElasticIds to NeutronElasticScalars and move other scalar data to NeutronElasticScalars)
     }
 
     //! Assign from another set of data
@@ -116,8 +139,13 @@ struct NeutronElasticData
     NeutronElasticData& operator=(NeutronElasticData<W2, M2> const& other)
     {
         CELER_EXPECT(other);
+<<<<<<< HEAD
         ids = other.ids;
         neutron_mass = other.neutron_mass;
+=======
+        scalars = other.scalars;
+        reals = other.reals;
+>>>>>>> 8a25bcfe2 (Change NeutronElasticIds to NeutronElasticScalars and move other scalar data to NeutronElasticScalars)
         micro_xs = other.micro_xs;
         coeffs = other.coeffs;
         reals = other.reals;
