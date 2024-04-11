11bc45b12573e9b913d5dbfe784811c9c7c6abc0
Seth R. Johnson <johnsonsr@ornl.gov>
2024-04-11 08:52:16 -0400

    Convert Geant4 logical and physical volumes (#1170)
    * Add logical volume converter
    
    * Add physical volume converter
    
    * Add test for physical volume converter
    
    * Add backdoor option to ignore fatal exceptions in logic converter
    
    * Add additional tests
    
    * Create a NoTransformation in the simple case
    
    * Check for identity and remove now-unneeded function
    
    * Remove gendered hierarchy names
    
    * Add scale factor to volume converter
    
    * Add helper for genericizing pointers
    
    * IWYU
    
    * Use material instead of physics-based material
    
    * Incorporate review feedback

--- src/orange/g4org/SolidConverter.cc
+++ src/orange/g4org/SolidConverter.cc
@@ -143,7 +143,7 @@ auto SolidConverter::operator()(arg_type solid_base) -> result_type
 
 //---------------------------------------------------------------------------//
 /*!
- * Convert a Geant4 solid to a VecGeom sphere with equivalent capacity.
+ * Convert a Geant4 solid to a sphere with equivalent capacity.
  */
 auto SolidConverter::to_sphere(arg_type solid_base) const -> result_type
 {
@@ -279,6 +279,9 @@ auto SolidConverter::displaced(arg_type solid_base) -> result_type
     G4VSolid* g4daughter = solid.GetConstituentMovedSolid();
     CELER_ASSERT(g4daughter);
     auto daughter = (*this)(*g4daughter);
+
+    // Note that GetDirectTransform is the combination of GetFrameTranslation
+    // and GetFrameRotation .
     return std::make_shared<Transformed>(
         daughter, transform_(solid.GetDirectTransform()));
 }
