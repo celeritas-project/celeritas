05d4c64732ab49634583b2d3dd78272cc0d73a15
Seth R Johnson <johnsonsr@ornl.gov>
2024-04-22 07:32:47 -0400

    Define "is enabled" geo traits helper

--- src/geocel/g4/GeantGeoTraits.hh
+++ src/geocel/g4/GeantGeoTraits.hh
@@ -7,6 +7,7 @@
 //---------------------------------------------------------------------------//
 #pragma once
 
+#include "celeritas_config.h"
 #include "geocel/GeoTraits.hh"
 
 namespace celeritas
--- src/geocel/vg/VecgeomGeoTraits.hh
+++ src/geocel/vg/VecgeomGeoTraits.hh
@@ -7,6 +7,7 @@
 //---------------------------------------------------------------------------//
 #pragma once
 
+#include "celeritas_config.h"
 #include "geocel/GeoTraits.hh"
 
 namespace celeritas
@@ -19,6 +20,7 @@ struct VecgeomParamsData;
 template<Ownership W, MemSpace M>
 struct VecgeomStateData;
 
+#if CELERITAS_USE_VECGEOM
 //---------------------------------------------------------------------------//
 /*!
  * Traits specialization for VecGeom geometry.
@@ -38,11 +40,18 @@ struct GeoTraits<VecgeomParams>
     using TrackView = VecgeomTrackView;
 
     //! Descriptive name for the geometry
-    static inline char const* name = "VecGeom";
+    static constexpr inline char const* name = "VecGeom";
 
     //! TO BE REMOVED: "native" file extension for this geometry
-    static inline char const* ext = ".gdml";
+    static constexpr inline char const* ext = ".gdml";
 };
+#else
+//! VecGeom is unavailable
+template<>
+struct GeoTraits<VecgeomParams> : NotConfiguredGeoTraits
+{
+};
+#endif
 
 //---------------------------------------------------------------------------//
 }  // namespace celeritas
