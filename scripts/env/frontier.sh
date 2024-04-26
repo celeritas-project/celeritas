d0eb89b51aa0070176ed7655abb251f14f4c88ac
Seth R Johnson <johnsonsr@ornl.gov>
2024-04-26 15:48:49 -0400

    Update frontier environment

--- scripts/env/frontier.sh
+++ scripts/env/frontier.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -e
 
-_celer_base=$PROJWORK/csc404/celeritas-frontier
+PROJID=hep143
+_celer_view=${PROJWORK}/${PROJID}/opt-view
+_tool_view=/ccs/proj/${PROJID}/opt-view
 
-# From spack compiler toolchain
-module load amd/5.6.0 PrgEnv-amd/8.3.3 craype-x86-trento libfabric/1.15.2.0 cray-pmi/6.1.8
+module load PrgEnv-amd/8.5.0 cpe/23.12 amd/5.7.1 craype-x86-trento libfabric/1.15.2.0
+# Disable warning "Using generic mem* routines instead of tuned routines"
 export RFE_811452_DISABLE=1
-export LD_LIBRARY_PATH=/opt/cray/pe/pmi/6.1.8/lib:$LD_LIBRARY_PATH:/opt/cray/pe/gcc-libs:/opt/cray/libfabric/1.15.2.0/lib64
-export LIBRARY_PATH=/opt/rocm-5.6.0/lib:/opt/rocm-5.6.0/lib64:$LIBRARY_PATH
 
 # Avoid linking multiple different libsci (one with openmp, one without)
 module unload cray-libsci
@@ -23,13 +23,13 @@ export CC=${CRAYPE_DIR}/bin/cc
 # module load craype-accel-amd-gfx90a
 
 # Set up celeritas
-export SPACK_ROOT=/ccs/proj/csc404/spack-frontier
-export PATH=${_celer_base}/spack/view/bin:$PATH
-export CMAKE_PREFIX_PATH=${_celer_base}/spack/view:${CMAKE_PREFIX_PATH}
-export MODULEPATH=${SPACK_ROOT}/share/spack/lmod/cray-sles15-x86_64/Core:${MODULEPATH}
+export SPACK_ROOT=/ccs/proj/hep143/spack
+export PATH=${_celer_view}/bin:${_tool_view}/bin:$PATH
+export CMAKE_PREFIX_PATH=${_celer_view}:${CMAKE_PREFIX_PATH}
+export MODULEPATH=${PROJWORK}/${PROJID}/share/lmod/linux-sles15-x86_64/Core:${MODULEPATH}
 
 # Set up Geant4 data 
-module load geant4-data
+module load geant4-data/11.1
 test -n "${G4ENSDFSTATEDATA}"
 test -e "${G4ENSDFSTATEDATA}"
 
