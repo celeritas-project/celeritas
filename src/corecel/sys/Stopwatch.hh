cc3699e634da08c7489e57239b9fcdb5f8a89efb
Julien Esseiva <jesseiva@lbl.gov>
2024-05-13 17:05:29 -0700

    env var to enable Stopwatch

--- src/corecel/sys/Stopwatch.hh
+++ src/corecel/sys/Stopwatch.hh
@@ -10,6 +10,9 @@
 #include <chrono>
 
 #include "corecel/Types.hh"
+#include "corecel/io/Logger.hh"
+
+#include "Environment.hh"
 
 namespace celeritas
 {
@@ -18,7 +21,9 @@ namespace celeritas
  * Simple timer.
  *
  * The stopwatch starts counting upward at construction and can be reset by
- * assigning a new stopwatch instance.
+ * assigning a new stopwatch instance. It needs to be enabled by setting the
+ * CELER_ENABLE_STOPWATCH environment variable, otherwise returns no elapsed
+ * time.
  *
  * \code
     Stopwatch get_elapsed_time;
@@ -30,18 +35,20 @@ namespace celeritas
  */
 class Stopwatch
 {
+  private:
+    using Clock = std::chrono::high_resolution_clock;
+    using TimePoint = Clock::time_point;
+    using Duration = Clock::duration;
+
   public:
     // Start the count at construction
     inline Stopwatch();
 
     // Get the current elapsed time [s]
     inline double operator()() const;
+    inline static TimePoint now();
 
   private:
-    using Clock = std::chrono::high_resolution_clock;
-    using TimePoint = Clock::time_point;
-    using Duration = Clock::duration;
-
     TimePoint start_;
 };
 
@@ -51,7 +58,7 @@ class Stopwatch
 /*!
  * Start the count at construction.
  */
-Stopwatch::Stopwatch() : start_(Clock::now()) {}
+Stopwatch::Stopwatch() : start_(Stopwatch::now()) {}
 
 //---------------------------------------------------------------------------//
 /*!
@@ -61,8 +68,23 @@ double Stopwatch::operator()() const
 {
     using DurationSec = std::chrono::duration<double>;
 
-    auto duration = Clock::now() - start_;
+    auto duration = Stopwatch::now() - start_;
     return std::chrono::duration_cast<DurationSec>(duration).count();
+}
+
+inline auto Stopwatch::now() -> TimePoint
+{
+    static bool const result = [] {
+        if (!celeritas::getenv("CELER_ENABLE_STOPWATCH").empty())
+        {
+            CELER_LOG(info) << "Enabling timing information since the "
+                               "'CELER_ENABLE_STOPWATCH' "
+                               "environment variable is present and non-empty";
+            return true;
+        }
+        return false;
+    }();
+    return result ? Clock::now() : TimePoint::min();
 }
 
 //---------------------------------------------------------------------------//
