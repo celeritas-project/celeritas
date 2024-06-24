6bfd5f1b40dcec65c12ee8e8d07a75341314a5c4
Seth R Johnson <johnsonsr@ornl.gov>
2024-06-24 12:20:44 -0400

    Fix docstring path

--- src/celeritas/io/ImportMaterial.cc
+++ src/celeritas/io/ImportMaterial.cc
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/io/ImportPhysMaterial.cc
+//! \file celeritas/io/ImportMaterial.cc
 //---------------------------------------------------------------------------//
 #include "ImportMaterial.hh"
 
