266e508a8d5e7f8131d3b21b1ccb59825b39089e
VHLM2001 <viu@ornl.gov>
2024-07-17 14:57:14 -0400

    Changed assertion to be based on remaining iterations

--- src/corecel/math/RegulaFalsiRootFinder.hh
+++ src/corecel/math/RegulaFalsiRootFinder.hh
@@ -112,7 +112,7 @@ CELER_FUNCTION real_type RegulaFalsi<F>::operator()(real_type left,
         }
     } while (std::fabs(f_root) > tol_ && --remaining_iters > 0);
 
-    CELER_ENSURE(std::fabs(f_root) <= tol_);
+    CELER_ENSURE(remaining_iters > 0);
 
     return root;
 }
