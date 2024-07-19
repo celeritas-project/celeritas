4dbc0283e5f90f73a5f5eff2e5a16057c8401347
Soon Yung Jun <syjun@cern.ch>
2024-07-19 18:49:40 -0500

    Fix the file path/name

--- src/celeritas/grid/TwodGridBuilder.hh
+++ src/celeritas/grid/TwodGridBuilder.hh
@@ -3,7 +3,7 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/grid/GenericGridBuilder.hh
+//! \file celeritas/grid/TwodGridBuilder.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
