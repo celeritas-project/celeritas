d1d0e9bab62837d47777d9d75dfcc50e6db6d4e4
Vidor Heli Lujan Montiel <viu@ornl.gov>
2024-07-24 14:10:23 -0400

    Add more root finding algorithms (#1333)
    * Bisection Method
    
    * Illinois Method
    
    * Inplementing comments
    
    * Assertion for bisection method
    
    however if after 50 iterations its still not converged I think there's another problem
    
    * Quick fixes
    
    * Make all methods have the same maximum iterations.

--- test/corecel/math/BisectionRootFinder.test.cc
+++ test/corecel/math/BisectionRootFinder.test.cc
@@ -3,9 +3,9 @@
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file corecel/math/RegulaFalsiRootFinder.test.cc
+//! \file corecel/math/BisectionRootFinder.test.cc
 //---------------------------------------------------------------------------//
-#include "corecel/math/RegulaFalsiRootFinder.hh"
+#include "corecel/math/BisectionRootFinder.hh"
 
 #include <cmath>
 #include <functional>
@@ -25,39 +25,39 @@ namespace test
 {
 
 // Solve: (x-2)(x+2) = 0
-TEST(RegulaFalsi, root_two)
+TEST(Bisection, root_two)
 {
     auto root = [](real_type t) { return (t - 2) * (t + 2); };
 
     constexpr real_type tol = SoftEqual<>{}.rel();
 
-    RegulaFalsiRootFinder find_root{root, tol};
+    BisectionRootFinder find_root{root, tol};
 
     EXPECT_SOFT_EQ(2.0, find_root(1.75, 2.25));
     EXPECT_SOFT_EQ(-2.0, find_root(-2.25, -1.75));
 }
 
 // Solve: x^2 - x - 1 = 0
-TEST(RegulaFalsi, golden_ratio)
+TEST(Bisection, golden_ratio)
 {
     auto root = [](real_type t) { return ipow<2>(t) - t - 1; };
 
     constexpr real_type tol = SoftEqual<>{}.rel();
 
-    RegulaFalsiRootFinder find_root{root, tol};
+    BisectionRootFinder find_root{root, tol};
 
     EXPECT_SOFT_EQ(1.618033988749, find_root(1.5, 1.75));
     EXPECT_SOFT_EQ(-0.6180339887498, find_root(-0.75, -0.5));
 }
 
 // Solve first 3 roots: cos(x) = 0
-TEST(RegulaFalsi, trigometric)
+TEST(Bisection, trigometric)
 {
     auto root = [](real_type t) { return std::cos(t); };
 
     constexpr real_type tol = SoftEqual<>{}.rel();
 
-    RegulaFalsiRootFinder find_root{root, tol};
+    BisectionRootFinder find_root{root, tol};
 
     EXPECT_SOFT_EQ(pi * 0.5, find_root(0, pi));
     EXPECT_SOFT_EQ(pi * 1.5, find_root(pi, 2 * pi));
@@ -73,7 +73,7 @@ TEST(RegulaFalsi, trigometric)
  * Point (1.5,0.5)
  * Direction (-0.7071067812,0.7071067812)
  */
-TEST(RegulaFalsi, exponential_intersect)
+TEST(Bisection, exponential_intersect)
 {
     real_type x = 1.5;
     real_type y = 0.5;
@@ -86,7 +86,7 @@ TEST(RegulaFalsi, exponential_intersect)
 
     constexpr real_type tol = SoftEqual<>{}.rel();
 
-    RegulaFalsiRootFinder find_root{root, tol};
+    BisectionRootFinder find_root{root, tol};
 
     EXPECT_SOFT_EQ(1.0, find_root(0.5, 1.5));
 }
