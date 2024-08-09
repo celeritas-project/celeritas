e55696c21630aa15a9115614de95db0b25f28c91
Seth R Johnson <johnsonsr@ornl.gov>
2024-08-09 10:19:29 -0400

    Print volumes and add mermaid support to csg-to-dot tool

--- scripts/user/orange-csg-to-dot.py
+++ scripts/user/orange-csg-to-dot.py
@@ -33,8 +33,35 @@ rankdir=TB;
     def write_edge(self, i, e):
         self.write(f"{i:02d} -> {e:02d};\n")
 
-def write_tree(gen, tree, labels):
-    for (i, node, labs) in zip(count(), tree, labels or repeat(None)):
+
+
+class MermaidGenerator:
+    def __init__(self, f):
+        self.f = f
+        self.write = f.write
+        self.write("flowchart TB\n")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.write("")
+
+    def write_node(self, i, value):
+        self.write(f"  n{i:02d}[\"{value}\"]\n")
+
+    def write_volume(self, i, value):
+        self.write(f"  v{i:02d}([\"{value}\"])\n")
+        self.write(f"  v{i:02d} <--> n{i:02d}\n")
+
+    def write_edge(self, i, e):
+        self.write(f"  n{i:02d} --> n{e:02d}\n")
+
+def write_tree(gen, csg_unit):
+    tree = csg_unit["tree"]
+    labels = csg_unit["metadata"] or repeat(None)
+
+    for (i, node, labs) in zip(count(), tree, labels):
         if isinstance(node, str):
             # True (or false??)
             gen.write_node(i, node)
@@ -64,20 +91,22 @@ def write_volumes(gen, volumes):
     for v in volumes:
         gen.write_volume(v["csg_node"], v["label"])
 
-def run(infile, outfile, universe):
+def run(infile, outfile, gencls, universe):
     tree = json.load(infile)
-    metadata = None
-    volumes = []
     if universe is not None:
-        # Load from a 'proto' debug file
+        # Load from a .csg.json debug file
         csg_unit = tree[universe]
-        tree = csg_unit["tree"]
-        metadata = csg_unit["metadata"]
-        volumes = csg_unit["volumes"]
+    else:
+        csg_unit = {
+            "tree": tree,
+            "metadata": None,
+            "label": "CSG tree",
+        }
 
-    with DotGenerator(outfile) as gen:
-        write_tree(gen, tree, metadata)
-        write_volumes(gen, volumes)
+    with gencls(outfile) as gen:
+        write_tree(gen, csg_unit)
+        if (vols := csg_unit.get("volumes")):
+            write_volumes(gen, vols)
 
 def main():
     import argparse
@@ -87,6 +116,8 @@ def main():
     parser.add_argument(
         "input",
         help="Input filename (- for stdin)")
+    parser.add_argument('-T', '--type', default=None,
+                        help="Output type: 'dot' or 'mermaid'")
     parser.add_argument('-u', '--universe', type=int, default=None,
                         help="Universe ID if a 'csg.json' debug file")
     parser.add_argument(
@@ -100,12 +131,30 @@ def main():
     else:
         infile = open(args.input)
 
+    if not args.type:
+        if args.output and args.output.endswith(".dot"):
+            gencls = DotGenerator
+        else:
+            gencls = MermaidGenerator
+    else:
+        gencls_dict = {
+            "dot": DotGenerator,
+            "mermaid": MermaidGenerator,
+        }
+        try:
+            gencls = gencls_dict[args.type]
+        except KeyError:
+            valid = ",".join(gencls_dict)
+            print(f"invalid type {args.type}: valid types are {valid}",
+                  file=sys.stderr)
+
+
     if not args.output:
         outfile = sys.stdout
     else:
         outfile = open(args.output, 'w')
 
-    run(infile, outfile, args.universe)
+    run(infile, outfile, gencls, args.universe)
 
 if __name__ == "__main__":
     main()
