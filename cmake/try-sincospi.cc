045da1fd002e84718acf8dc44a6a0d360cdb9995
Seth R Johnson <johnsonsr@ornl.gov>
2024-08-19 13:15:27 -0400

    Expand cospi testing

--- cmake/try-sincospi.cc
+++ cmake/try-sincospi.cc
@@ -12,22 +12,25 @@
 #    error "Expected CELERITAS_SINCOSPI_PREFIX to be defined"
 #endif
 
-#define CELER_CONCAT(PREFIX, FUNC) PREFIX##FUNC
-#define CELER_CONCAT_WRAP(PREFIX, FUNC) CELER_CONCAT(PREFIX, FUNC)
-#define CELER_MANGLED(FUNC) CELER_CONCAT_WRAP(CELERITAS_SINCOSPI_PREFIX, FUNC)
+// These macros are identical to those in `corecel/math/Algorithms.hh` but the
+// only important thing is that they correctly test the prefixed function.
+#define CELER_CONCAT_IMPL(PREFIX, FUNC) PREFIX##FUNC
+#define CELER_CONCAT(PREFIX, FUNC) CELER_CONCAT_IMPL(PREFIX, FUNC)
+#define CELER_SINCOS_MANGLED(FUNC) \
+    CELER_CONCAT(CELERITAS_SINCOSPI_PREFIX, FUNC)
 
 int main()
 {
     double s, c;
-    CELER_MANGLED(sinpi)(2.0);
-    CELER_MANGLED(cospi)(2.0);
-    CELER_MANGLED(sincos)(3.1415, &s, &c);
-    CELER_MANGLED(sincospi)(2.0, &s, &c);
+    CELER_SINCOS_MANGLED(sinpi)(2.0);
+    CELER_SINCOS_MANGLED(cospi)(2.0);
+    CELER_SINCOS_MANGLED(sincos)(3.1415, &s, &c);
+    CELER_SINCOS_MANGLED(sincospi)(2.0, &s, &c);
 
     float sf, cf;
-    CELER_MANGLED(sinpif)(2.0f);
-    CELER_MANGLED(cospif)(2.0f);
-    CELER_MANGLED(sincosf)(3.1415f, &sf, &cf);
-    CELER_MANGLED(sincospif)(2.0f, &sf, &cf);
+    CELER_SINCOS_MANGLED(sinpif)(2.0f);
+    CELER_SINCOS_MANGLED(cospif)(2.0f);
+    CELER_SINCOS_MANGLED(sincosf)(3.1415f, &sf, &cf);
+    CELER_SINCOS_MANGLED(sincospif)(2.0f, &sf, &cf);
     return 0;
 }
