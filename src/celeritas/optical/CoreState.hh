615a35ee863f16075ae46da11b53e489d6507d92
Seth R Johnson <johnsonsr@ornl.gov>
2024-08-27 13:06:45 -0400

    WIP: core state

---
+++ src/celeritas/optical/CoreState.hh
@@ -0,0 +1,45 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2024 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celeritas/optical/CoreState.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+namespace celeritas
+{
+namespace optical
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Brief class description.
+ *
+ * Optional detailed class description, and possibly example usage:
+ * \code
+    CoreState ...;
+   \endcode
+ */
+class CoreState
+{
+  public:
+    //!@{
+    //! \name Type aliases
+    //!@}
+
+  public:
+    // Construct with defaults
+    inline CoreState();
+};
+
+//---------------------------------------------------------------------------//
+// INLINE DEFINITIONS
+//---------------------------------------------------------------------------//
+/*!
+ * Construct with defaults.
+ */
+CoreState::CoreState() {}
+
+//---------------------------------------------------------------------------//
+}  // namespace optical
+}  // namespace celeritas
