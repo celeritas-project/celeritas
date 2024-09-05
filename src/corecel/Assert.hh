9a718843a1096764e12e7b03e71021eaadc8e159
Seth R Johnson <johnsonsr@ornl.gov>
2024-09-05 11:50:30 -0400

    Revert changes to assertion for now

--- src/corecel/Assert.hh
+++ src/corecel/Assert.hh
@@ -189,7 +189,7 @@
         ::celeritas::unreachable();     \
     } while (0)
 
-#if !CELER_DEVICE_COMPILE
+#if !CELER_DEVICE_COMPILE || defined(__HIP__)
 #    define CELER_RUNTIME_THROW(WHICH, WHAT, COND) \
         throw ::celeritas::RuntimeError({          \
             WHICH,                                 \
@@ -198,13 +198,10 @@
             __FILE__,                              \
             __LINE__,                              \
         })
-#elif CELERITAS_DEBUG
+#else
 #    define CELER_RUNTIME_THROW(WHICH, WHAT, COND)                           \
         CELER_DEBUG_FAIL("Runtime errors cannot be thrown from device code", \
                          unreachable);
-#else
-// Avoid printf statements which can add substantially to local memory
-#    define CELER_RUNTIME_THROW(WHICH, WHAT, COND) ::celeritas::unreachable()
 #endif
 
 #if CELERITAS_DEBUG
@@ -222,7 +219,7 @@
 #    define CELER_ASSERT_UNREACHABLE() ::celeritas::unreachable()
 #endif
 
-#if !CELER_DEVICE_COMPILE
+#if !CELER_DEVICE_COMPILE || defined(__HIP__)
 #    define CELER_VALIDATE(COND, MSG)                            \
         do                                                       \
         {                                                        \
