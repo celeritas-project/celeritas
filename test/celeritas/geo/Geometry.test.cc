2b1908e668793fbaf456751d4bc2e7f9e106470f
Seth R Johnson <johnsonsr@ornl.gov>
2024-10-23 12:23:54 +0200

    Fix tests

--- test/celeritas/geo/Geometry.test.cc
+++ test/celeritas/geo/Geometry.test.cc
@@ -272,7 +272,7 @@ TEST_F(SimpleCmsTest, output)
     if (CELERITAS_CORE_GEO == CELERITAS_CORE_GEO_VECGEOM)
     {
         EXPECT_JSON_EQ(
-            R"json({"_category":"internal","_label":"geometry","bbox":[[-1000.001,-1000.001,-2000.001],[1000.001,1000.001,2000.001]],"max_depth":1,"supports_safety":true,"volumes":{"label":["vacuum_tube","si_tracker","em_calorimeter","had_calorimeter","sc_solenoid","fe_muon_chambers","world"]}})json",
+            R"json({"_category":"internal","_label":"geometry","bbox":[[-1000.001,-1000.001,-2000.001],[1000.001,1000.001,2000.001]],"max_depth":2,"supports_safety":true,"volumes":{"label":["vacuum_tube","si_tracker","em_calorimeter","had_calorimeter","sc_solenoid","fe_muon_chambers","world"]}})json",
             to_string(out));
     }
     else if (CELERITAS_CORE_GEO == CELERITAS_CORE_GEO_ORANGE
