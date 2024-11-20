46652b9bce9f6e49551858e97d4db678854fa83e
Hayden Hollenbeck <hollenbeck.hayden@gmail.com>
2024-11-20 17:00:47 -0500

    Moved ModelImporter tests to use mock data

--- test/celeritas/optical/ModelImporter.test.cc
+++ test/celeritas/optical/ModelImporter.test.cc
@@ -9,8 +9,6 @@
 
 #include <algorithm>
 
-#include "celeritas/ext/RootImporter.hh"
-#include "celeritas/ext/ScopedRootErrorHandler.hh"
 #include "celeritas/io/ImportData.hh"
 #include "celeritas/mat/MaterialParams.hh"
 #include "celeritas/optical/MaterialParams.hh"
@@ -18,6 +16,7 @@
 #include "celeritas/optical/model/AbsorptionModel.hh"
 #include "celeritas/optical/model/RayleighModel.hh"
 
+#include "MockImportedData.hh"
 #include "celeritas_test.hh"
 
 namespace celeritas
@@ -29,7 +28,7 @@ namespace test
 using namespace ::celeritas::test;
 //---------------------------------------------------------------------------//
 
-class ModelImporterTest : public ::celeritas::test::Test
+class ModelImporterTest : public MockImportedData
 {
   protected:
     using SPConstMaterial = std::shared_ptr<MaterialParams const>;
@@ -38,33 +37,19 @@ class ModelImporterTest : public ::celeritas::test::Test
 
     static void SetUpTestCase()
     {
-        ScopedRootErrorHandler scoped_root_error;
-        RootImporter import_from_root(
-            Test::test_data_path("celeritas", "lar-sphere.root").c_str());
-        import_data() = import_from_root();
-        core_material()
-            = ::celeritas::MaterialParams::from_import(import_data());
-
-        {
-            MaterialParams::Input inp;
-            inp.properties.reserve(import_data().optical_materials.size());
-            for (auto const& mat : import_data().optical_materials)
-            {
-                inp.properties.push_back(mat.properties);
-            }
-            inp.volume_to_mat = {OpticalMaterialId{}};
-
-            material() = std::make_shared<MaterialParams const>(std::move(inp));
-        }
-
-        CELER_ENSURE(material() && core_material());
+        auto& data = import_data();
+        data.optical_models = import_models();
+        data.optical_materials = import_materials();
+        data.units = units::NativeTraits::label();
     }
 
     ModelImporter
     build_importer(ModelImporter::UserBuildMap const& user_build = {})
     {
-        return ModelImporter(
-            import_data(), material(), core_material(), user_build);
+        return ModelImporter(import_data(),
+                             this->optical_materials(),
+                             this->core_materials(),
+                             user_build);
     }
 
     static ImportData& import_data()
@@ -73,51 +58,70 @@ class ModelImporterTest : public ::celeritas::test::Test
         return import_data_;
     }
 
-    static SPConstMaterial& material()
+    template<class ModelT>
+    void check_import_model(ModelImporter const& model_importer,
+                            ImportModelClass imc)
     {
-        static SPConstMaterial m;
-        return m;
-    }
+        // Create a model builder
+        auto build = model_importer(imc);
+        ASSERT_TRUE(build);
 
-    static SPConstCoreMaterial& core_material()
-    {
-        static SPConstCoreMaterial m;
-        return m;
+        // Create a model
+        ActionId action_id{0};
+        auto model = (*build)(action_id);
+        ASSERT_TRUE(model);
+
+        // Check we have the correct model
+        EXPECT_EQ(action_id, model->action_id());
+        EXPECT_TRUE(std::dynamic_pointer_cast<ModelT const>(model));
+
+        // Get expected MFP tables
+        auto expected_iter = std::find_if(
+            this->import_models().begin(),
+            this->import_models().end(),
+            [imc](auto const& m) { return m.model_class == imc; });
+        ASSERT_FALSE(expected_iter == this->import_models().end());
+
+        // Build imported tables
+        auto mfp_builder = this->create_mfp_builder();
+        for (auto mat : range(OpticalMaterialId{
+                 this->optical_materials()->num_materials()}))
+        {
+            model->build_mfps(mat, mfp_builder);
+        }
+
+        // Check tables (i.e. models have correct data after being built)
+        this->check_built_table_exact(expected_iter->mfp_table,
+                                      mfp_builder.grid_ids());
     }
 };
 
+//---------------------------------------------------------------------------//
+// Test building absorption
 TEST_F(ModelImporterTest, build_absorption)
 {
     auto model_importer = this->build_importer();
 
-    auto build = model_importer(ImportModelClass::absorption);
-    ASSERT_TRUE(build);
-
-    ActionId action_id{0};
-    auto model = (*build)(action_id);
-    ASSERT_TRUE(model);
-
-    ASSERT_EQ(action_id, model->action_id());
-    ASSERT_TRUE(std::dynamic_pointer_cast<AbsorptionModel const>(model));
+    this->check_import_model<AbsorptionModel>(model_importer,
+                                              ImportModelClass::absorption);
 }
 
+//---------------------------------------------------------------------------//
+// Test building Rayleigh scattering
 TEST_F(ModelImporterTest, build_rayleigh)
 {
     auto model_importer = this->build_importer();
 
-    auto build = model_importer(ImportModelClass::rayleigh);
-    ASSERT_TRUE(build);
-
-    ActionId action_id{0};
-    auto model = (*build)(action_id);
-    ASSERT_TRUE(model);
-
-    ASSERT_EQ(action_id, model->action_id());
-    ASSERT_TRUE(std::dynamic_pointer_cast<RayleighModel const>(model));
+    this->check_import_model<RayleighModel>(model_importer,
+                                            ImportModelClass::rayleigh);
 }
 
+//---------------------------------------------------------------------------//
+// Test building WLS
 TEST_F(ModelImporterTest, build_wls) {}
 
+//---------------------------------------------------------------------------//
+// Test user ignore options
 TEST_F(ModelImporterTest, warn_and_ignore)
 {
     ModelImporter::UserBuildMap user_map{
@@ -129,7 +133,8 @@ TEST_F(ModelImporterTest, warn_and_ignore)
     auto model_importer = this->build_importer(user_map);
 
     EXPECT_FALSE(model_importer(ImportModelClass::absorption));
-    EXPECT_TRUE(model_importer(ImportModelClass::rayleigh));
+    this->check_import_model<RayleighModel>(model_importer,
+                                            ImportModelClass::rayleigh);
     EXPECT_FALSE(model_importer(ImportModelClass::wls));
 }
 
