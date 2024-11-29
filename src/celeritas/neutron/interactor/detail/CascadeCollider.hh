6e61bc5a9a9f7eee4f631136782c067660566fa9
Amanda Lund <alund@anl.gov>
2024-11-29 17:28:19 +0000

    Fix up

--- src/celeritas/neutron/interactor/detail/CascadeCollider.hh
+++ src/celeritas/neutron/interactor/detail/CascadeCollider.hh
@@ -148,11 +148,12 @@ CELER_FUNCTION auto CascadeCollider::operator()(Engine& rng) -> FinalState
     {
         // Find cos\theta from tabulated angular data for a given CDF
         Grid costheta_grid(cdf_grid.y, shared_.reals);
-        auto calc = TwodGridCalculator(cdf_grid, shared_.reals)(kin_energy_);
-        auto calc_cdf = [&calc, &costheta_grid](size_type i) {
-            return calc(costheta_grid[i]);
-        };
-        cos_theta = InverseCdfFinder(costheta_grid, calc_cdf)(cdf);
+        auto calc_cdf
+            = TwodGridCalculator(cdf_grid, shared_.reals)(kin_energy_);
+        cos_theta = InverseCdfFinder(Grid(cdf_grid.y, shared_.reals),
+                                     [&calc_cdf, &costheta_grid](size_type i) {
+                                         return calc_cdf(costheta_grid[i]);
+                                     })(cdf);
     }
     else
     {
