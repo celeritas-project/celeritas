442b06d72b3bf6f5ad6dc36c257faacb7f69e8c8
Seth R Johnson <johnsonsr@ornl.gov>
2024-11-30 08:19:03 -0500

    Add import data trimmer

---
+++ src/celeritas/io/ImportDataTrimmer.cc
@@ -0,0 +1,363 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2024 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celeritas/io/ImportDataTrimmer.cc
+//---------------------------------------------------------------------------//
+#include "ImportDataTrimmer.hh"
+
+#include "corecel/Assert.hh"
+#include "corecel/cont/Range.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+struct ImportDataTrimmer::GridFilterer
+{
+    std::size_t stride;
+    std::size_t orig_size;
+
+    // Whether to keep the data at this index
+    inline bool operator()(std::size_t i) const;
+};
+
+//---------------------------------------------------------------------------//
+/*!
+ * Construct from input parameters.
+ */
+ImportDataTrimmer::ImportDataTrimmer(Input const& inp) : options_{inp}
+{
+    CELER_EXPECT(options_.max_size >= 2);
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportData* data)
+{
+    if (options_.materials)
+    {
+        (*this)(&data->isotopes);
+        (*this)(&data->elements);
+        (*this)(&data->geo_materials);
+        (*this)(&data->phys_materials);
+        (*this)(&data->optical_materials);
+    }
+
+    if (options_.physics)
+    {
+        (*this)(&data->particles);
+        (*this)(&data->processes);
+        (*this)(&data->msc_models);
+        (*this)(&data->regions);
+        (*this)(&data->volumes);
+    }
+
+    for (auto& e : data->elements)
+    {
+        (*this)(&e);
+    }
+
+    for (auto& m : data->geo_materials)
+    {
+        (*this)(&m);
+    }
+
+    for (auto& m : data->phys_materials)
+    {
+        (*this)(&m);
+    }
+
+    for (auto& p : data->processes)
+    {
+        (*this)(&p);
+    }
+
+    for (auto& m : data->msc_models)
+    {
+        (*this)(&m);
+    }
+
+    (*this)(&data->sb_data);
+    (*this)(&data->livermore_pe_data);
+    (*this)(&data->neutron_elastic_data);
+    (*this)(&data->atomic_relaxation_data);
+    (*this)(&data->mu_pair_production_data);
+
+    for (auto& m : data->optical_models)
+    {
+        (*this)(&m);
+    }
+
+    for (auto& m : data->optical_materials)
+    {
+        (*this)(&m);
+    }
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportElement* data)
+{
+    CELER_EXPECT(data);
+
+    if (options_.materials)
+    {
+        (*this)(&data->isotopes_fractions);
+    }
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportGeoMaterial* data)
+{
+    CELER_EXPECT(data);
+
+    if (options_.materials)
+    {
+        (*this)(&data->elements);
+    }
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportPhysMaterial* data)
+{
+    CELER_EXPECT(data);
+
+    // TODO: remap IDs?
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportOpticalMaterial* data)
+{
+    CELER_EXPECT(data);
+
+    // TODO: trim WLS components?
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportOpticalModel* data)
+{
+    CELER_EXPECT(data);
+
+    if (options_.materials)
+    {
+        (*this)(&data->mfp_table);
+    }
+
+    for (auto& mfp_grid : data->mfp_table)
+    {
+        (*this)(&mfp_grid);
+    }
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportModelMaterial* data)
+{
+    CELER_EXPECT(data);
+
+    (*this)(&data->energy);
+
+    if (options_.materials)
+    {
+        (*this)(&data->micro_xs);
+    }
+
+    for (auto& xs_vec : data->micro_xs)
+    {
+        (*this)(&xs_vec);
+    }
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportModel* data)
+{
+    CELER_EXPECT(data);
+
+    if (options_.materials)
+    {
+        (*this)(&data->materials);
+    }
+
+    for (auto& mm : data->materials)
+    {
+        (*this)(&mm);
+    }
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportMscModel* data)
+{
+    CELER_EXPECT(data);
+
+    (*this)(&data->xs_table);
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportMuPairProductionTable* data)
+{
+    CELER_EXPECT(data);
+
+    if (!*data)
+    {
+        return;
+    }
+
+    (*this)(&data->atomic_number);
+    (*this)(&data->physics_vectors);
+
+    for (auto& pv : data->physics_vectors)
+    {
+        (*this)(&pv);
+    }
+
+    CELER_ENSURE(*data);
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportParticle* data)
+{
+    CELER_EXPECT(data);
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportProcess* data)
+{
+    CELER_EXPECT(data);
+
+    if (options_.materials)
+    {
+        (*this)(&data->tables);
+    }
+    if (options_.physics)
+    {
+        (*this)(&data->models);
+    }
+
+    for (auto& m : data->models)
+    {
+        (*this)(&m);
+    }
+
+    for (auto& t : data->tables)
+    {
+        (*this)(&t);
+    }
+
+    CELER_ENSURE(*data);
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportPhysicsVector* data)
+{
+    CELER_EXPECT(data);
+    (*this)(&data->x);
+    (*this)(&data->y);
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportPhysicsTable* data)
+{
+    CELER_EXPECT(data);
+    if (options_.materials)
+    {
+        (*this)(&data->physics_vectors);
+    }
+
+    for (auto& pv : data->physics_vectors)
+    {
+        (*this)(&pv);
+    }
+}
+
+//---------------------------------------------------------------------------//
+void ImportDataTrimmer::operator()(ImportPhysics2DVector* data)
+{
+    auto x_filter = this->make_filterer(data->x.size());
+    auto y_filter = this->make_filterer(data->y.size());
+
+    // Trim x and y grid
+    (*this)(&data->x);
+    (*this)(&data->y);
+
+    std::vector<double> new_value;
+    new_value.reserve(data->x.size() * data->y.size());
+
+    auto src = data->value.cbegin();
+    for (auto i : range(x_filter.orig_size))
+    {
+        for (auto j : range(y_filter.orig_size))
+        {
+            if (x_filter(i) && y_filter(j))
+            {
+                new_value.push_back(*src);
+            }
+            ++src;
+        }
+    }
+    CELER_ASSERT(src == data->value.cend());
+
+    data->value = std::move(new_value);
+
+    CELER_ENSURE(*data);
+}
+
+//---------------------------------------------------------------------------//
+template<class T>
+void ImportDataTrimmer::operator()(std::vector<T>* vec)
+{
+    CELER_EXPECT(vec);
+
+    std::vector<T> result;
+    result.reserve(options_.max_size);
+
+    auto filter = this->make_filterer(vec->size());
+    for (auto i : range(vec->size()))
+    {
+        if (filter(i))
+        {
+            result.push_back((*vec)[i]);
+        }
+    }
+    *vec = std::move(result);
+}
+
+//---------------------------------------------------------------------------//
+template<class K, class T, class C, class A>
+void ImportDataTrimmer::operator()(std::map<K, T, C, A>* m)
+{
+    CELER_EXPECT(m);
+
+    std::map<K, T, C, A> result;
+    auto filter = this->make_filterer(m->size());
+    auto iter = m->begin();
+    for (auto i : range(filter.orig_size))
+    {
+        auto cur_iter = iter++;
+        if (filter(i))
+        {
+            result.insert(m->extract(cur_iter));
+        }
+    }
+    *m = std::move(result);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Whether to keep the data at this index.
+ */
+auto ImportDataTrimmer::make_filterer(std::size_t vec_size) const -> GridFilterer
+{
+    auto stride = vec_size >= 2 ? vec_size / (options_.max_size - 1) : 1;
+
+    return {stride, vec_size};
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Whether to keep the data at this index.
+ */
+bool ImportDataTrimmer::GridFilterer::operator()(std::size_t i) const
+{
+    return i % stride == 0 || i + 1 == orig_size;
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
---
+++ src/celeritas/io/ImportDataTrimmer.hh
@@ -0,0 +1,82 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 2024 UT-Battelle, LLC, and other Celeritas developers.
+// See the top-level COPYRIGHT file for details.
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file celeritas/io/ImportDataTrimmer.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include "corecel/Types.hh"
+#include "corecel/math/NumericLimits.hh"
+
+#include "ImportData.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Reduce the amount of imported/exported data for testing.
+ */
+class ImportDataTrimmer
+{
+  public:
+    struct Input
+    {
+        //! Remove materials/elements which might affect the problem
+        bool materials{false};
+        //! Reduce the number of physics models and processes
+        bool physics{false};
+        //! Maximum number of elements in a vector (approximate)
+        size_type max_size{numeric_limits<size_type>::max()};
+    };
+
+  public:
+    // Construct with a unit system
+    explicit ImportDataTrimmer(Input const& inp);
+
+    //!@{
+    //! Trim imported data
+    void operator()(ImportData* data);
+    void operator()(ImportElement* data);
+    void operator()(ImportGeoMaterial* data);
+    void operator()(ImportModel* data);
+    void operator()(ImportModelMaterial* data);
+    void operator()(ImportMscModel* data);
+    void operator()(ImportMuPairProductionTable* data);
+    void operator()(ImportOpticalMaterial* data);
+    void operator()(ImportOpticalModel* data);
+    void operator()(ImportParticle* data);
+    void operator()(ImportPhysMaterial* data);
+    void operator()(ImportProcess* data);
+    //!@}
+
+    //!@{
+    //! Trim objects
+    void operator()(ImportPhysicsVector* data);
+    void operator()(ImportPhysicsTable* data);
+    void operator()(ImportPhysics2DVector* data);
+    //!@}
+
+  private:
+    //// TYPES ////
+
+    struct GridFilterer;
+
+    //// DATA ////
+
+    Input options_;
+
+    //// HELPERS ////
+
+    GridFilterer make_filterer(std::size_t vec_size) const;
+
+    template<class T>
+    void operator()(std::vector<T>* vec);
+
+    template<class K, class T, class C, class A>
+    void operator()(std::map<K, T, C, A>* m);
+};
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
