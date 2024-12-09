a717be7fcb52a77996f2f19245c09ab5834b8cc0
Julien Esseiva <jesseiva@lbl.gov>
2024-12-09 15:07:08 -0800

    use Postfix logic builder as base

--- src/orange/orangeinp/detail/LogicBuilderPolicies.cc
+++ src/orange/orangeinp/detail/LogicBuilderPolicies.cc
@@ -25,116 +25,6 @@ namespace detail
  *
  * The surface mapping vector is *optional*.
  */
-InfixLogicBuilderPolicy::InfixLogicBuilderPolicy(CsgTree const& tree,
-                                                 VecSurface const* vs,
-                                                 VecLogic* logic)
-    : visit_node_{tree}, mapping_{vs}, logic_{logic}
-{
-    CELER_EXPECT(logic_);
-}
-
-//---------------------------------------------------------------------------//
-/*!
- * Build from a node ID.
- */
-void InfixLogicBuilderPolicy::operator()(NodeId const& n)
-{
-    visit_node_(*this, n);
-}
-
-//---------------------------------------------------------------------------//
-/*!
- * Append the "true" token.
- */
-void InfixLogicBuilderPolicy::operator()(True const&)
-{
-    logic_->push_back(logic::ltrue);
-}
-
-//---------------------------------------------------------------------------//
-/*!
- * Explicit "False" should never be possible for a CSG cell.
- *
- * The 'false' standin is always aliased to "not true" in the CSG tree.
- */
-void InfixLogicBuilderPolicy::operator()(False const&)
-{
-    CELER_ASSERT_UNREACHABLE();
-}
-
-//---------------------------------------------------------------------------//
-/*!
- * Push a surface ID.
- */
-void InfixLogicBuilderPolicy::operator()(Surface const& s)
-{
-    CELER_EXPECT(s.id < logic::lbegin);
-    // Get index of original surface or remapped
-    logic_int sidx = [this, sid = s.id] {
-        if (!mapping_)
-        {
-            return sid.unchecked_get();
-        }
-        else
-        {
-            // Remap by finding position of surface in our mapping
-            auto iter = find_sorted(mapping_->begin(), mapping_->end(), sid);
-            CELER_ASSERT(iter != mapping_->end());
-            return logic_int(iter - mapping_->begin());
-        }
-    }();
-
-    logic_->push_back(sidx);
-}
-
-//---------------------------------------------------------------------------//
-/*!
- * Push an aliased node.
- *
- * Aliased node shouldn't be reachable if the tree is fully simplified, but
- * could be reachable for testing purposes.
- */
-void InfixLogicBuilderPolicy::operator()(Aliased const& n)
-{
-    (*this)(n.node);
-}
-
-//---------------------------------------------------------------------------//
-/*!
- * Visit a negated node and append 'not'.
- */
-void InfixLogicBuilderPolicy::operator()(Negated const& n)
-{
-    logic_->push_back(logic::lnot);
-    (*this)(n.node);
-}
-
-//---------------------------------------------------------------------------//
-/*!
- * Visit daughter nodes and append the conjunction.
- */
-void InfixLogicBuilderPolicy::operator()(Joined const& n)
-{
-    CELER_EXPECT(n.nodes.size() > 1);
-    logic_->push_back(logic::lopen);
-    // Visit first node, then add conjunction for subsequent nodes
-    auto iter = n.nodes.begin();
-    (*this)(*iter++);
-
-    while (iter != n.nodes.end())
-    {
-        logic_->push_back(n.op);
-        (*this)(*iter++);
-    }
-    logic_->push_back(logic::lclose);
-}
-
-//---------------------------------------------------------------------------//
-/*!
- * Construct with pointer to the logic expression.
- *
- * The surface mapping vector is *optional*.
- */
 PostfixLogicBuilderPolicy::PostfixLogicBuilderPolicy(CsgTree const& tree,
                                                      VecSurface const* vs,
                                                      VecLogic* logic)
@@ -236,6 +126,58 @@ void PostfixLogicBuilderPolicy::operator()(Joined const& n)
         (*this)(*iter++);
         logic_->push_back(n.op);
     }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Build from a node ID.
+ */
+void InfixLogicBuilderPolicy::operator()(NodeId const& n)
+{
+    this->visite_node()(*this, n);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Push an aliased node.
+ *
+ * Aliased node shouldn't be reachable if the tree is fully simplified, but
+ * could be reachable for testing purposes.
+ */
+void InfixLogicBuilderPolicy::operator()(Aliased const& n)
+{
+    (*this)(n.node);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Visit a negated node and append 'not'.
+ */
+void InfixLogicBuilderPolicy::operator()(Negated const& n)
+{
+    this->logic().push_back(logic::lnot);
+    (*this)(n.node);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Visit daughter nodes and append the conjunction.
+ */
+void InfixLogicBuilderPolicy::operator()(Joined const& n)
+{
+    CELER_EXPECT(n.nodes.size() > 1);
+    auto& logic = this->logic();
+    logic.push_back(logic::lopen);
+    // Visit first node, then add conjunction for subsequent nodes
+    auto iter = n.nodes.begin();
+    (*this)(*iter++);
+
+    while (iter != n.nodes.end())
+    {
+        logic.push_back(n.op);
+        (*this)(*iter++);
+    }
+    logic.push_back(logic::lclose);
 }
 
 //---------------------------------------------------------------------------//
--- src/orange/orangeinp/detail/LogicBuilderPolicies.hh
+++ src/orange/orangeinp/detail/LogicBuilderPolicies.hh
@@ -23,58 +23,6 @@ namespace detail
 {
 //---------------------------------------------------------------------------//
 /*!
- * Recursively construct a logic vector from a node with infix operation.
- *
- * This is an implementation detail of the \c InfixLogicBuilder class. The
- * user invokes this class with a node ID (usually representing a cell), and
- * then this class recurses into the daughters using a tree visitor.
- */
-class InfixLogicBuilderPolicy
-{
-  public:
-    //!@{
-    //! \name Type aliases
-    using VecLogic = std::vector<logic_int>;
-    using VecSurface = std::vector<LocalSurfaceId>;
-    //!@}
-
-    static_assert(std::is_same_v<LocalSurfaceId::size_type, logic_int>,
-                  "unsupported: add enum logic conversion for different-sized "
-                  "face and surface ints");
-
-  public:
-    // Construct with optional mapping and logic vector to append to
-    InfixLogicBuilderPolicy(CsgTree const& tree,
-                            VecSurface const* vs,
-                            VecLogic* logic);
-
-    //! Build from a node ID
-    void operator()(NodeId const& n);
-
-    //!@{
-    //! \name Visit a node directly
-    // Append 'true'
-    void operator()(True const&);
-    // False is never explicitly part of the node tree
-    void operator()(False const&);
-    // Append a surface ID
-    void operator()(Surface const&);
-    // Aliased nodes should never be reachable explicitly
-    void operator()(Aliased const&);
-    // Visit a negated node and append 'not'
-    void operator()(Negated const&);
-    // Visit daughter nodes and append the conjunction.
-    void operator()(Joined const&);
-    //!@}
-
-  private:
-    ContainerVisitor<CsgTree const&, NodeId> visit_node_;
-    VecSurface const* mapping_;
-    VecLogic* logic_;
-};
-
-//---------------------------------------------------------------------------//
-/*!
  * Recursively construct a logic vector from a node with postfix operation.
  *
  * This is an implementation detail of the \c PostfixLogicBuilder class. The
@@ -122,10 +70,52 @@ class PostfixLogicBuilderPolicy
     void operator()(Joined const&);
     //!@}
 
+  protected:
+    VecLogic& logic() { return *logic_; }
+    ContainerVisitor<CsgTree const&, NodeId>& visite_node()
+    {
+        return visit_node_;
+    }
+
   private:
     ContainerVisitor<CsgTree const&, NodeId> visit_node_;
     VecSurface const* mapping_;
     VecLogic* logic_;
+};
+
+//---------------------------------------------------------------------------//
+/*!
+ * Recursively construct a logic vector from a node with infix operation.
+ *
+ * This is an implementation detail of the \c InfixLogicBuilder class. The
+ * user invokes this class with a node ID (usually representing a cell), and
+ * then this class recurses into the daughters using a tree visitor.
+ */
+class InfixLogicBuilderPolicy : public PostfixLogicBuilderPolicy
+{
+  public:
+    //!@{
+    //! \name Type aliases
+    using PostfixLogicBuilderPolicy::VecLogic;
+    using PostfixLogicBuilderPolicy::VecSurface;
+    //!@}
+
+  public:
+    using PostfixLogicBuilderPolicy::PostfixLogicBuilderPolicy;
+
+    //! Build from a node ID
+    void operator()(NodeId const& n);
+
+    //!@{
+    //! \name Visit a node directly
+    using PostfixLogicBuilderPolicy::operator();
+    // Aliased nodes should never be reachable explicitly
+    void operator()(Aliased const&);
+    // Visit a negated node and append 'not'
+    void operator()(Negated const&);
+    // Visit daughter nodes and append the conjunction.
+    void operator()(Joined const&);
+    //!@}
 };
 
 //---------------------------------------------------------------------------//
