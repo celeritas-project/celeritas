f75ec7a840ae8200c63cd00ca1825580ac26b58a
Julien Esseiva <jesseiva@lbl.gov>
2024-12-09 16:02:44 -0800

    inline implementation

--- src/orange/orangeinp/detail/LogicBuilderPolicies.hh
+++ src/orange/orangeinp/detail/LogicBuilderPolicies.hh
@@ -7,18 +7,18 @@
 //---------------------------------------------------------------------------//
 #pragma once
 
-#include <utility>
 #include <vector>
 
 #include "corecel/cont/VariantUtils.hh"
+#include "corecel/math/Algorithms.hh"
 #include "orange/OrangeTypes.hh"
+#include "orange/orangeinp/CsgTree.hh"
 #include "orange/orangeinp/CsgTypes.hh"
 
 namespace celeritas
 {
 namespace orangeinp
 {
-class CsgTree;
 namespace detail
 {
 //---------------------------------------------------------------------------//
@@ -49,27 +49,27 @@ class PostfixLogicBuilderPolicy
 
   public:
     // Construct with optional mapping and logic vector to append to
-    PostfixLogicBuilderPolicy(CsgTree const& tree,
-                              VecSurface const* vs,
-                              VecLogic* logic);
+    inline PostfixLogicBuilderPolicy(CsgTree const& tree,
+                                     VecSurface const* vs,
+                                     VecLogic* logic);
 
     //! Build from a node ID
-    void operator()(NodeId const& n);
+    inline void operator()(NodeId const& n);
 
     //!@{
     //! \name Visit a node directly
     // Append 'true'
-    void operator()(True const&);
+    inline void operator()(True const&);
     // False is never explicitly part of the node tree
-    void operator()(False const&);
+    inline void operator()(False const&);
     // Append a surface ID
-    void operator()(Surface const&);
+    inline void operator()(Surface const&);
     // Aliased nodes should never be reachable explicitly
-    void operator()(Aliased const&);
+    inline void operator()(Aliased const&);
     // Visit a negated node and append 'not'
-    void operator()(Negated const&);
+    inline void operator()(Negated const&);
     // Visit daughter nodes and append the conjunction.
-    void operator()(Joined const&);
+    inline void operator()(Joined const&);
     //!@}
 
   protected:
@@ -112,19 +112,180 @@ class InfixLogicBuilderPolicy : public PostfixLogicBuilderPolicy
     using PostfixLogicBuilderPolicy::PostfixLogicBuilderPolicy;
 
     //! Build from a node ID
-    void operator()(NodeId const& n);
+    inline void operator()(NodeId const& n);
 
     //!@{
     //! \name Visit a node directly
     using PostfixLogicBuilderPolicy::operator();
     // Aliased nodes should never be reachable explicitly
-    void operator()(Aliased const&);
+    inline void operator()(Aliased const&);
     // Visit a negated node and append 'not'
-    void operator()(Negated const&);
+    inline void operator()(Negated const&);
     // Visit daughter nodes and append the conjunction.
-    void operator()(Joined const&);
+    inline void operator()(Joined const&);
     //!@}
 };
+
+//---------------------------------------------------------------------------//
+/*!
+ * Construct with pointer to the logic expression.
+ *
+ * The surface mapping vector is *optional*.
+ */
+PostfixLogicBuilderPolicy::PostfixLogicBuilderPolicy(CsgTree const& tree,
+                                                     VecSurface const* vs,
+                                                     VecLogic* logic)
+    : visit_node_{tree}, mapping_{vs}, logic_{logic}
+{
+    CELER_EXPECT(logic_);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Build from a node ID.
+ */
+void PostfixLogicBuilderPolicy::operator()(NodeId const& n)
+{
+    visit_node_(*this, n);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Append the "true" token.
+ */
+void PostfixLogicBuilderPolicy::operator()(True const&)
+{
+    logic_->push_back(logic::ltrue);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Explicit "False" should never be possible for a CSG cell.
+ *
+ * The 'false' standin is always aliased to "not true" in the CSG tree.
+ */
+void PostfixLogicBuilderPolicy::operator()(False const&)
+{
+    CELER_ASSERT_UNREACHABLE();
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Push a surface ID.
+ */
+void PostfixLogicBuilderPolicy::operator()(Surface const& s)
+{
+    CELER_EXPECT(s.id < logic::lbegin);
+    // Get index of original surface or remapped
+    logic_int sidx = [this, sid = s.id] {
+        if (!mapping_)
+        {
+            return sid.unchecked_get();
+        }
+        else
+        {
+            // Remap by finding position of surface in our mapping
+            auto iter = find_sorted(mapping_->begin(), mapping_->end(), sid);
+            CELER_ASSERT(iter != mapping_->end());
+            return logic_int(iter - mapping_->begin());
+        }
+    }();
+
+    logic_->push_back(sidx);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Push an aliased node.
+ *
+ * Aliased node shouldn't be reachable if the tree is fully simplified, but
+ * could be reachable for testing purposes.
+ */
+void PostfixLogicBuilderPolicy::operator()(Aliased const& n)
+{
+    (*this)(n.node);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Visit a negated node and append 'not'.
+ */
+void PostfixLogicBuilderPolicy::operator()(Negated const& n)
+{
+    (*this)(n.node);
+    logic_->push_back(logic::lnot);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Visit daughter nodes and append the conjunction.
+ */
+void PostfixLogicBuilderPolicy::operator()(Joined const& n)
+{
+    CELER_EXPECT(n.nodes.size() > 1);
+
+    // Visit first node, then add conjunction for subsequent nodes
+    auto iter = n.nodes.begin();
+    (*this)(*iter++);
+
+    while (iter != n.nodes.end())
+    {
+        (*this)(*iter++);
+        logic_->push_back(n.op);
+    }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Build from a node ID.
+ */
+void InfixLogicBuilderPolicy::operator()(NodeId const& n)
+{
+    this->visite_node()(*this, n);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Push an aliased node.
+ *
+ * Aliased node shouldn't be reachable if the tree is fully simplified, but
+ * could be reachable for testing purposes.
+ */
+void InfixLogicBuilderPolicy::operator()(Aliased const& n)
+{
+    (*this)(n.node);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Visit a negated node and append 'not'.
+ */
+void InfixLogicBuilderPolicy::operator()(Negated const& n)
+{
+    this->logic().push_back(logic::lnot);
+    (*this)(n.node);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Visit daughter nodes and append the conjunction.
+ */
+void InfixLogicBuilderPolicy::operator()(Joined const& n)
+{
+    CELER_EXPECT(n.nodes.size() > 1);
+    auto& logic = this->logic();
+    logic.push_back(logic::lopen);
+    // Visit first node, then add conjunction for subsequent nodes
+    auto iter = n.nodes.begin();
+    (*this)(*iter++);
+
+    while (iter != n.nodes.end())
+    {
+        logic.push_back(n.op);
+        (*this)(*iter++);
+    }
+    logic.push_back(logic::lclose);
+}
 
 //---------------------------------------------------------------------------//
 }  // namespace detail
