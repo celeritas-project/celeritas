d0f1b831259ccde948fc30a7d20aee1c198a0464
Seth R Johnson <johnsonsr@ornl.gov>
2025-01-02 09:44:54 -0500

    Update copyrights

--- cmake/CeleritasOptionUtils.cmake
+++ cmake/CeleritasOptionUtils.cmake
@@ -1,6 +1,5 @@
-#----------------------------------*-CMake-*----------------------------------#
-# Copyright 2020-2024 UT-Battelle, LLC, and other Celeritas developers.
-# See the top-level COPYRIGHT file for details.
+#------------------------------- -*- cmake -*- -------------------------------#
+# Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 #[=======================================================================[.rst:
 
