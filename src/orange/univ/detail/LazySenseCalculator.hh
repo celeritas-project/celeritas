8293f626b9f6d59459c71da7854ae6ae4a40dd88
Julien Esseiva <jesseiva@lbl.gov>
2025-01-09 11:59:49 -0800

    include

--- src/orange/univ/detail/LazySenseCalculator.hh
+++ src/orange/univ/detail/LazySenseCalculator.hh
@@ -8,7 +8,6 @@
 
 #include "corecel/Assert.hh"
 #include "corecel/Macros.hh"
-#include "corecel/cont/Span.hh"
 #include "orange/OrangeTypes.hh"
 #include "orange/SenseUtils.hh"
 #include "orange/surf/LocalSurfaceVisitor.hh"
