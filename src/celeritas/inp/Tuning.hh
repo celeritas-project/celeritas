41798cd905d8beefe2bc869b31221da0cf045f6b
Seth R Johnson <johnsonsr@ornl.gov>
2025-01-23 07:22:30 -0800

    Add docs, make secondaries optional

--- src/celeritas/inp/Tuning.hh
+++ src/celeritas/inp/Tuning.hh
@@ -16,14 +16,28 @@ namespace inp
 {
 //---------------------------------------------------------------------------//
 /*!
- * Set up per-process state capacities.
+ * Set up per-process state/buffer capacities.
+ *
+ * Increasing these values increases resource requirements with the trade-off
+ * of (usually!) improving performance. A larger number of \c tracks in flight
+ * means improved performance on GPU because the standard kernel size
+ * increases, but it also means higher memory usage because of the larger
+ * number of full states. More \c initializers are necessary for more (and
+ * higher-energy) tracks when lots of particles are in flight and producing new
+ * child particles. More \c secondaries may be necessary if physical processes
+ * that produce many daughters (e.g., atomic relaxation or Bertini cascade) are
+ * active. The number of \c events in flight primarily increases the number of
+ * active tracks, possible initializers, and produced secondaries (NOTE: see
+ * [#1233](https://github.com/celeritas-project/celeritas/issues/1233) ).
+ * Finally, the number of \c primaries is the maximum number of pending tracks
+ * from an external application before running a kernel to construct \c
+ * initializers and execute the stpeping loop.
  *
  * Capacities are defined as the number per application process: this means
  * that in a multithreaded context it implies "strong scaling" (i.e., the
  * allocations are divided among threads), and in a multiprocess context it
  * implies "weak scaling" (the problem size grows with the number of
  * processes).
- *
  * In other words, if used in a multithread "event-parallel" context, each
  * state gets the specified \c tracks divided by the number of threads.  When
  * used in MPI parallel (e.g., one process per GPU), each process \em rank has
@@ -39,22 +53,22 @@ namespace inp
  * \todo Split this into "core" state capacity and "optical" state capacity?
  * Core contains \c events and \c secondaries .
  * \todo Instead of a special value \c events=0, make a variant or something
- * more descriptive
+ * more descriptive?
+ * \todo Some of these parameters will be more automated in the future.
  */
 struct StateCapacity
 {
-    //! Maximum number of track slots to be simultaneously stepped
-    size_type tracks{};
+    //! Maximum number of primaries that can be buffered before stepping
+    size_type primaries{};
     //! Maximum number of queued primaries+secondaries
     size_type initializers{};
-    //! Maximum number of secondaries (automatic if zero)
-    size_type secondaries{};
+    //! Maximum number of track slots to be simultaneously stepped
+    size_type tracks{};
+    //! Maximum number of secondaries created per step
+    std::optional<size_type> secondaries;
 
     //! Maximum number of simultaneous events (zero for Geant4 integration)
     size_type events{0};
-
-    //! Maximum number of primaries that can be buffered before stepping
-    size_type primaries{};
 };
 
 //---------------------------------------------------------------------------//
