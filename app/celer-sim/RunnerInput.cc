c82c9d85639f071363017e60f243d28a4571a8a4
Seth R Johnson <johnsonsr@ornl.gov>
2025-01-27 17:47:40 -0500

    Add note about streams

--- app/celer-sim/RunnerInput.cc
+++ app/celer-sim/RunnerInput.cc
@@ -118,6 +118,7 @@ inp::Problem load_problem(RunnerInput const& ri)
 
     // Control
     {
+        // NOTE: old celer-sim input is *integrated* over streams
         inp::CoreStateCapacity capacity;
         capacity.primaries = 0;  // Immediately generate initializers
         capacity.initializers = ri.initializer_capacity;
