2f081ee32beebfc44f62548c952f253e9fbfa66c
Seth R. Johnson <johnsonsr@ornl.gov>
2025-02-04 09:13:15 -0500

    Use external G4VG (#1605)
    * Add script to find g4vg
    
    * Fix g4vg include, update vecgeom version, add findg4vg
    
    * Replace internal g4vg
    
    * Update docs and spack recipe
    
    * Update g4vg and cgv versions
    
    * Update url and external version
    
    * Add g4vg to spack dependencies
    
    * Add g4vg to spack scripts
    
    * fixup! Add g4vg to spack dependencies
    
    * Force g4vg to use external for geant4 10.5

--- cmake/CeleritasLibraryUtils.cmake
+++ cmake/CeleritasLibraryUtils.cmake
@@ -137,6 +137,7 @@ if(NOT DEFINED CELERITAS_USE_VecGeom)
   )
 endif()
 
+# Include RDC utils if not included already
 include(CudaRdcUtils)
 
 #-----------------------------------------------------------------------------#
