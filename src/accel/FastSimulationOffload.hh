1a9ffabc8e034f3e1753c922c4e884161cc0f587
Seth R Johnson <johnsonsr@ornl.gov>
2025-02-07 06:59:02 -0500

    Add backward compatibility file

---
+++ src/accel/FastSimulationOffload.hh
@@ -0,0 +1,17 @@
+//------------------------------- -*- C++ -*- -------------------------------//
+// Copyright Celeritas contributors: see top-level COPYRIGHT file for details
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file accel/FastSimulationOffload.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include "FastSimulationOffload.hh"
+
+namespace celeritas
+{
+
+// TODO: Remove in v0.7
+using FastSimulationOffload [[deprecated]] = FastSimulationModel;
+
+}  // namespace celeritas
