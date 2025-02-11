378826870d2cf8d50746c148496bb3d9057aed00
Amanda Lund <alund@anl.gov>
2025-02-11 20:57:31 +0000

    Add tests for more grid inserters

--- test/celeritas/grid/GridInserter.test.cc
+++ test/celeritas/grid/GridInserter.test.cc
@@ -8,6 +8,8 @@
 #include <vector>
 
 #include "corecel/cont/Range.hh"
+#include "celeritas/grid/NonuniformGridInserter.hh"
+#include "celeritas/grid/UniformGridInserter.hh"
 #include "celeritas/grid/XsGridInserter.hh"
 
 #include "celeritas_test.hh"
@@ -25,7 +27,6 @@ class GridInserterTest : public Test
   protected:
     using VecDbl = std::vector<double>;
     Collection<real_type, Ownership::value, MemSpace::host> reals;
-    Collection<XsGridRecord, Ownership::value, MemSpace::host> grids;
 };
 
 //---------------------------------------------------------------------------//
@@ -34,7 +35,7 @@ class GridInserterTest : public Test
 
 TEST_F(GridInserterTest, xs)
 {
-    // TODO: test uniform and nonuniform inserters
+    Collection<XsGridRecord, Ownership::value, MemSpace::host> grids;
     XsGridInserter insert(&reals, &grids);
     {
         VecDbl values = {10, 20, 3};
@@ -70,6 +71,45 @@ TEST_F(GridInserterTest, xs)
     }
     EXPECT_EQ(2, grids.size());
 }
+
+TEST_F(GridInserterTest, uniform)
+{
+    Collection<UniformGridRecord, Ownership::value, MemSpace::host> grids;
+
+    UniformGridInserter insert(&reals, &grids);
+    VecDbl values = {1, 2, 4, 6, 8};
+
+    auto idx = insert(UniformGridData::from_bounds(0.0, 10.0, 5),
+                      make_span(values));
+    EXPECT_EQ(0, idx.unchecked_get());
+    UniformGridRecord const& inserted = grids[idx];
+    EXPECT_EQ(1, grids.size());
+
+    EXPECT_TRUE(inserted);
+    EXPECT_EQ(5, inserted.grid.size);
+    EXPECT_EQ(0, inserted.grid.front);
+    EXPECT_EQ(10, inserted.grid.back);
+    EXPECT_VEC_SOFT_EQ(values, reals[inserted.value]);
+}
+
+TEST_F(GridInserterTest, nonuniform)
+{
+    Collection<NonuniformGridRecord, Ownership::value, MemSpace::host> grids;
+
+    NonuniformGridInserter insert(&reals, &grids);
+    VecDbl const grid = {0, 1, 2, 5, 13};
+    VecDbl const values = {1, 2, 4, 6, 8};
+
+    auto idx = insert(make_span(grid), make_span(values));
+    EXPECT_EQ(0, idx.unchecked_get());
+    NonuniformGridRecord const& inserted = grids[idx];
+    EXPECT_EQ(1, grids.size());
+
+    EXPECT_TRUE(inserted);
+    EXPECT_VEC_SOFT_EQ(grid, reals[inserted.grid]);
+    EXPECT_VEC_SOFT_EQ(values, reals[inserted.value]);
+}
+
 //---------------------------------------------------------------------------//
 }  // namespace test
 }  // namespace celeritas
