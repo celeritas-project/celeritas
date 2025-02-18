ef967ef343fed2450a04909795ee6e48e292b329
Seth R Johnson <johnsonsr@ornl.gov>
2025-02-18 06:56:20 -0500

    Disable pincell test

--- test/geocel/vg/Vecgeom.test.cc
+++ test/geocel/vg/Vecgeom.test.cc
@@ -745,32 +745,6 @@ TEST_F(MultiLevelTest, trace)
 }
 
 //---------------------------------------------------------------------------//
-class PincellTest : public VecgeomGeantTestBase
-{
-    std::string geometry_basename() const final { return "pincell"; }
-};
-
-TEST_F(PincellTest, imager)
-{
-    SafetyImager write_image{this->geometry()};
-
-    ImageInput inp;
-    inp.lower_left = from_cm({-12, -12, 0});
-    inp.upper_right = from_cm({12, 12, 0});
-    inp.rightward = {1.0, 0.0, 0.0};
-    inp.vertical_pixels = 8;
-
-    write_image(ImageParams{inp}, "vg-pincell-xy-mid.jsonl");
-
-    inp.lower_left[2] = inp.upper_right[2] = from_cm(-5.5);
-    write_image(ImageParams{inp}, "vg-pincell-xy-lo.jsonl");
-
-    inp.lower_left = from_cm({-12, 0, -12});
-    inp.upper_right = from_cm({12, 0, 12});
-    write_image(ImageParams{inp}, "vg-pincell-xz-mid.jsonl");
-}
-
-//---------------------------------------------------------------------------//
 
 class SolidsTest
     : public GenericGeoParameterizedTest<VecgeomGeantTestBase, SolidsGeoTest>
