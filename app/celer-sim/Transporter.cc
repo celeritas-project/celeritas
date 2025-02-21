1be8c25a3406a6ed856884a719658b3a75950487
Julien Esseiva <jesseiva@lbl.gov>
2025-02-20 18:16:31 -0800

    revert celer-sim changes

--- app/celer-sim/Transporter.cc
+++ app/celer-sim/Transporter.cc
@@ -20,6 +20,7 @@
 #include "corecel/io/Logger.hh"
 #include "corecel/io/ScopedTimeLog.hh"
 #include "corecel/sys/ScopedSignalHandler.hh"
+#include "corecel/sys/TraceCounter.hh"
 #include "celeritas/Types.hh"
 #include "celeritas/global/ActionSequence.hh"
 #include "celeritas/global/CoreParams.hh"
@@ -107,6 +108,19 @@ auto Transporter<M>::operator()(SpanConstPrimary primaries) -> TransporterResult
             result.initializers.push_back(track_counts.queued);
             result.active.push_back(track_counts.active);
             result.alive.push_back(track_counts.alive);
+            if constexpr (M == MemSpace::host)
+            {
+                auto stream_id
+                    = std::to_string(stepper_->state_ref().stream_id.get());
+                trace_counter(std::string("active-" + stream_id).c_str(),
+                              track_counts.active);
+                trace_counter(std::string("alive-" + stream_id).c_str(),
+                              track_counts.alive);
+                trace_counter(std::string("dead-" + stream_id).c_str(),
+                              track_counts.active - track_counts.alive);
+                trace_counter(std::string("queued-" + stream_id).c_str(),
+                              track_counts.queued);
+            }
         }
         ++result.num_step_iterations;
         result.num_steps += track_counts.active;
