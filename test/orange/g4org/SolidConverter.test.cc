73c38768162a50836afe457bd655a7ef180349fd
Elliott Biondo <veb@ornl.gov>
2025-02-25 15:18:40 -0500

    Update json string in test

--- test/orange/g4org/SolidConverter.test.cc
+++ test/orange/g4org/SolidConverter.test.cc
@@ -242,7 +242,7 @@ TEST_F(SolidConverterTest, ellipsoid)
     this->build_and_test(
         G4Ellipsoid(
             "testEllipsoid", 10 * cm, 20 * cm, 30 * cm, -1 * cm, 29 * cm),
-        R"json({"_type":"solid","interior":{"_type":"ellipsoid","radii":[10.0,20.0,30.0]},"label":"testEllipsoid","z cut":{"lower":-1.0,"upper":29.0}})json",
+        R"json({"_type":"solid","interior":{"_type":"ellipsoid","radii":[10.0,20.0,30.0]},"label":"testEllipsoid","z_slab":{"lower":-1.0,"upper":29.0}})json",
         {{0., 0., 0.},
          {0., 0., -1.1},
          {0., 0, 29.1},
