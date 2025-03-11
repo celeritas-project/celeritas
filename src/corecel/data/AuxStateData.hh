8f3a3e7cbb488bbafe67015021d7072c2e6fc2ce
Seth R Johnson <johnsonsr@ornl.gov>
2025-03-11 11:31:11 -0400

    Add state helpers

--- src/corecel/data/AuxStateData.hh
+++ src/corecel/data/AuxStateData.hh
@@ -78,13 +78,39 @@ make_aux_state(ParamsDataInterface<P> const& params,
 {
     if (m == MemSpace::host)
     {
-        return std::make_unique<AuxStateData<S, MemSpace::host>>(
-            params.host_ref(), stream_id, size);
+        using ASD = AuxStateData<S, MemSpace::host>;
+        return std::make_unique<ASD>(params.host_ref(), stream_id, size);
     }
     else if (m == MemSpace::device)
     {
-        return std::make_unique<AuxStateData<S, MemSpace::device>>(
-            params.host_ref(), stream_id, size);
+        using ASD = AuxStateData<S, MemSpace::device>;
+        return std::make_unique<ASD>(params.host_ref(), stream_id, size);
+    }
+    CELER_ASSERT_UNREACHABLE();
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Create an auxiliary state given a runtime memory space.
+ *
+ * Example:
+ * \code
+    return make_aux_state<FooStateData>(memspace, stream, size);
+ * \endcode
+ */
+template<template<Ownership, MemSpace> class S, template<Ownership, MemSpace> class P>
+std::unique_ptr<AuxStateInterface>
+make_aux_state(MemSpace m, StreamId stream_id, size_type size)
+{
+    if (m == MemSpace::host)
+    {
+        using ASD = AuxStateData<S, MemSpace::host>;
+        return std::make_unique<ASD>(stream_id, size);
+    }
+    else if (m == MemSpace::device)
+    {
+        using ASD = AuxStateData<S, MemSpace::device>;
+        return std::make_unique<ASD>(stream_id, size);
     }
     CELER_ASSERT_UNREACHABLE();
 }
