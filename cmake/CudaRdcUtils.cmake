d30065f4246603d4cfc77ca741df26cb795d0acd
Philippe Canal <pcanal@fnal.gov>
2025-03-13 21:36:24 +0000

    Add missing OR

--- cmake/CudaRdcUtils.cmake
+++ cmake/CudaRdcUtils.cmake
@@ -127,7 +127,7 @@ if(CUDA_RDC_VERSION GREATER _CUDA_RDC_VERSION)
   # A newer version has already been loaded
   return()
 endif()
-if(NOT CUDA_RDC_VERSION NOT COMMAND cuda_rdc_add_library)
+if(NOT CUDA_RDC_VERSION AND NOT COMMAND cuda_rdc_add_library)
   # if this is the first we get here use include_guard
   # to prevent the inclusion of non-version version
   # of this file
