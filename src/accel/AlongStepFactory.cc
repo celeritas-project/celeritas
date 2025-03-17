533372bc986d3508e2cb7011551a07fb14e1ed54
SAKIB RAHMAN <srahman1@bnl.gov>
2025-03-17 07:51:36 -0400

    Add a debug line to print out volumes that has been assigned uniform field

--- src/accel/AlongStepFactory.cc
+++ src/accel/AlongStepFactory.cc
@@ -78,13 +78,18 @@ auto UniformAlongStepFactory::operator()(
             {
                 CELER_ASSERT(lv);
                 auto vol = find_volume(*lv);
-                CELER_VALIDATE(
+		CELER_VALIDATE(
                     vol,
                     << "failed to find volume corresponding to Geant4 volume "
                     << lv->GetName() << " while setting up uniform field");
-                field.volumes.push_back(vol);
+                CELER_LOG(debug)
+		    << "Found volume "
+		    << lv->GetName()
+		    << " that will be assigned a uniform field";
+		field.volumes.push_back(vol);
             }
         }
+
 
         // Create a uniform field
         CELER_LOG(info)
