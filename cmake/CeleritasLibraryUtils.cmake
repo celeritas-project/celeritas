737a2d9fae597bca291493d7900fbb7a4d75248e
Seth R Johnson <johnsonsr@ornl.gov>
2025-03-18 18:07:52 -0400

    We don't need to export object libraries

--- cmake/CeleritasLibraryUtils.cmake
+++ cmake/CeleritasLibraryUtils.cmake
@@ -174,12 +174,6 @@ endfunction()
 
 function(celeritas_add_object_library target)
   add_library(${target} OBJECT ${ARGN})
-  install(TARGETS ${target}
-    EXPORT celeritas-targets
-    ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}"
-    LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}"
-    COMPONENT runtime
-  )
 endfunction()
 
 #-----------------------------------------------------------------------------#
