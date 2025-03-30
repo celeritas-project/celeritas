7a52ba9b7c16e37a5ad8d90cfce4056bcd1e600e
Seth R Johnson <johnsonsr@ornl.gov>
2025-03-30 10:19:24 -0400

    Downgreade more local messages

--- example/offload-template/src/StepDiagnostic.cc
+++ example/offload-template/src/StepDiagnostic.cc
@@ -103,8 +103,6 @@ StepStatistics StepDiagnostic::GetAndReset(CoreStateInterface& state) const
         // Whether the given state is device/host
         constexpr MemSpace M
             = std::remove_reference_t<decltype(derived)>::memspace;
-        CELER_LOG_LOCAL(debug)
-            << "Copying step diagnostics from " << to_cstring(M);
 
         // Get the step data from the core state
         auto& step_state = derived.template aux_data<StepStateData>(aux_id_);
