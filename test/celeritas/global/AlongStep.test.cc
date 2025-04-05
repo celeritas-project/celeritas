bac56316cf7b217db7da7adccfd00834e541f11a
Seth R Johnson <johnsonsr@ornl.gov>
2025-04-05 10:34:35 -0400

    update propagator result and loosen result action

--- test/celeritas/global/AlongStep.test.cc
+++ test/celeritas/global/AlongStep.test.cc
@@ -777,7 +777,10 @@ TEST_F(LeadBoxAlongStepTest, position_change)
         {
             EXPECT_TRUE(scoped_log_.empty()) << scoped_log_;
         }
-        EXPECT_EQ("tracking-cut", result.action);
+        // VecGeom with Geant4 11.0 has eloss-range
+        EXPECT_TRUE(result.action == "tracking-cut"
+                    || result.action == "eloss-range")
+            << result.action;
         EXPECT_SOFT_NEAR(5.38228e-8, result.step, 1e-5);
         EXPECT_EQ(0, result.displacement);
     }
