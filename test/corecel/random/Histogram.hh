7a53e1dbccd18fb9efe7079470e56cbb733ddcec
Seth R Johnson <johnsonsr@ornl.gov>
2025-04-09 05:53:17 -0400

    Adjust histogram
    - Rename histogram method to ensure it's not used as an accessor
    - Use double precision
    - Out-of-range histogram values are counted but in an implicit "outside" bin
    - Inline operator()

--- test/corecel/random/Histogram.hh
+++ test/corecel/random/Histogram.hh
@@ -2,12 +2,13 @@
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file Histogram.hh
+//! \file corecel/random/Histogram.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
 #include <vector>
 
+#include "corecel/Assert.hh"
 #include "corecel/Types.hh"
 #include "corecel/cont/Array.hh"
 
@@ -18,35 +19,57 @@ namespace test
 //---------------------------------------------------------------------------//
 /*!
  * Calculate the histogram of a set of values.
+ *
+ * This uses double precision since values are being accumulated and tallied.
  */
 class Histogram
 {
   public:
     //!@{
     //! \name Type aliases
-    using Real2 = Array<real_type, 2>;
+    using Dbl2 = Array<double, 2>;
     using VecCount = std::vector<size_type>;
-    using VecReal = std::vector<real_type>;
+    using VecDbl = std::vector<double>;
     //!@}
 
   public:
     // Construct with number of bins and range
-    Histogram(size_type num_bins, Real2 range);
+    Histogram(size_type num_bins, Dbl2 range);
 
     // Update the histogram with a value
-    void operator()(real_type value);
+    inline void operator()(double value);
 
     // Get the histogram
     VecCount const& counts() const { return counts_; }
 
     // Get the result as a probability density
-    VecReal density() const;
+    VecDbl calc_density() const;
 
   private:
-    Real2 range_;
-    real_type width_;
+    Dbl2 range_;
+    double width_;
     VecCount counts_;
+    size_type total_counts_{0};
 };
+
+//---------------------------------------------------------------------------//
+/*!
+ * Update the histogram with a value.
+ *
+ * Values outside of \c range are allowable and will show as a deficit in the
+ * resulting tally.
+ */
+void Histogram::operator()(double value)
+{
+    ++total_counts_;
+    if (value < range_[0] || value >= range_[1])
+    {
+        return;
+    }
+    auto index = static_cast<size_type>((value - range_[0]) / width_);
+    CELER_ASSERT(index < counts_.size());
+    ++counts_[index];
+}
 
 //---------------------------------------------------------------------------//
 }  // namespace test
