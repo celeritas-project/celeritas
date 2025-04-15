35255a97d6964c0cb6e9abf8ffa16b4299ac3506
Elliott Biondo <veb@ornl.gov>
2025-04-15 14:25:35 -0400

    Update doc strings

--- src/orange/orangeinp/ConvexHullFinder.hh
+++ src/orange/orangeinp/ConvexHullFinder.hh
@@ -2,7 +2,7 @@
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file corecel/math/ConvexHullFinder.hh
+//! \file orange/orangeinp/ConvexHullFinder.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
--- test/orange/orangeinp/ConvexHullFinder.test.cc
+++ test/orange/orangeinp/ConvexHullFinder.test.cc
@@ -2,7 +2,7 @@
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file corecel/math/ConvexHullFinder.test.cc
+//! \file orange/orangeinp/ConvexHullFinder.test.cc
 //---------------------------------------------------------------------------//
 #include "orange/orangeinp/ConvexHullFinder.hh"
 
