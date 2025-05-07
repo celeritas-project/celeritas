0ff85864d76888c3a775feba3b944d6bc1c3564a
Julien Esseiva <jesseiva@lbl.gov>
2025-05-07 09:32:27 -0700

    fix aggregate initialization of base class

--- src/celeritas/field/CartMapFieldParams.covfie.cc
+++ src/celeritas/field/CartMapFieldParams.covfie.cc
@@ -30,68 +30,65 @@ namespace celeritas
 //---------------------------------------------------------------------------//
 struct CartMapFieldParams::Impl
 {
-  public:
-    using HostRef = ParamsDataInterface<CartMapFieldParamsData>::HostRef;
-    using DeviceRef = ParamsDataInterface<CartMapFieldParamsData>::DeviceRef;
-    using Input = CartMapFieldParams::Input;
+    explicit Impl(Input const& inp)
+        : host_{{inp.driver_options},
+                [&inp] {
+                    Array<size_type, 4> const dims{
+                        inp.num_x,
+                        inp.num_y,
+                        inp.num_z,
+                        static_cast<size_type>(Axis::size_)};
+                    HyperslabIndexer const flat_index{dims};
 
-    Impl(Input const& inp)
-        : host_{[&inp] {
-            HostVal<CartMapFieldParamsData> host;
+                    using builder_t
+                        = detail::CovfieFieldTraits<MemSpace::host>::builder_t;
 
-            Array<size_type, 4> const dims{inp.num_x,
-                                           inp.num_y,
-                                           inp.num_z,
-                                           static_cast<size_type>(Axis::size_)};
-            HyperslabIndexer const flat_index{dims};
-
-            using builder_t
-                = detail::CovfieFieldTraits<MemSpace::host>::builder_t;
-
-            builder_t builder{covfie::make_parameter_pack(
-                builder_t::backend_t::configuration_t{
-                    inp.num_x, inp.num_y, inp.num_z})};
-            builder_t::view_t builder_view{builder};
-            // fill the covfie field data
-            for (auto ix : range(inp.num_x))
-            {
-                for (auto iy : range(inp.num_y))
-                {
-                    for (auto iz : range(inp.num_z))
+                    builder_t builder{covfie::make_parameter_pack(
+                        builder_t::backend_t::configuration_t{
+                            inp.num_x, inp.num_y, inp.num_z})};
+                    builder_t::view_t builder_view{builder};
+                    // fill the covfie field data
+                    for (auto ix : range(inp.num_x))
                     {
-                        auto* fv = builder_view.at(ix, iy, iz).begin();
-                        auto* finp = inp.field.data()
-                                     + flat_index(ix, iy, iz, 0);
-                        std::copy(finp,
-                                  finp + static_cast<size_type>(Axis::size_),
-                                  fv);
+                        for (auto iy : range(inp.num_y))
+                        {
+                            for (auto iz : range(inp.num_z))
+                            {
+                                auto* fv = builder_view.at(ix, iy, iz).begin();
+                                auto* finp = inp.field.data()
+                                             + flat_index(ix, iy, iz, 0);
+                                std::copy(
+                                    finp,
+                                    finp + static_cast<size_type>(Axis::size_),
+                                    fv);
+                            }
+                        }
                     }
-                }
-            }
 
-            using field_real_type = CartMapField::real_type;
-            auto affine_translate = covfie::algebra::affine<3>::translation(
-                static_cast<field_real_type>(-inp.min_x),
-                static_cast<field_real_type>(-inp.min_y),
-                static_cast<field_real_type>(-inp.min_z));
+                    using field_real_type = CartMapField::real_type;
+                    auto affine_translate
+                        = covfie::algebra::affine<3>::translation(
+                            static_cast<field_real_type>(-inp.min_x),
+                            static_cast<field_real_type>(-inp.min_y),
+                            static_cast<field_real_type>(-inp.min_z));
 
-            auto affine_scale = covfie::algebra::affine<3>::scaling(
-                static_cast<field_real_type>((inp.num_x - 1)
-                                             / (inp.max_x - inp.min_x)),
-                static_cast<field_real_type>((inp.num_y - 1)
-                                             / (inp.max_y - inp.min_y)),
-                static_cast<field_real_type>((inp.num_z - 1)
-                                             / (inp.max_z - inp.min_z)));
+                    auto affine_scale = covfie::algebra::affine<3>::scaling(
+                        static_cast<field_real_type>(
+                            (inp.num_x - 1) / (inp.max_x - inp.min_x)),
+                        static_cast<field_real_type>(
+                            (inp.num_y - 1) / (inp.max_y - inp.min_y)),
+                        static_cast<field_real_type>(
+                            (inp.num_z - 1) / (inp.max_z - inp.min_z)));
 
-            using field_t = detail::CovfieFieldTraits<MemSpace::host>::field_t;
-            host.field = std::make_unique<field_t>(covfie::make_parameter_pack(
-                field_t::backend_t::configuration_t(affine_scale
-                                                    * affine_translate),
-                field_t::backend_t::backend_t::configuration_t{},
-                builder.backend()));
-            host.options = inp.driver_options;
-            return host;
-        }()}
+                    using field_t
+                        = detail::CovfieFieldTraits<MemSpace::host>::field_t;
+                    return std::make_unique<field_t>(
+                        covfie::make_parameter_pack(
+                            field_t::backend_t::configuration_t(
+                                affine_scale * affine_translate),
+                            field_t::backend_t::backend_t::configuration_t{},
+                            builder.backend()));
+                }()}
         , host_ref_{{host_.options}, *host_.field}
     {
         if (celeritas::device())
