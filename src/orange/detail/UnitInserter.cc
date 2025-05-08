c37d4d376c329b1208f37bbc1f8f3e53b9068aae
Elliott Biondo <veb@ornl.gov>
2025-05-08 16:40:14 -0400

    Do not build BIH for under 20 volumes

--- src/orange/detail/UnitInserter.cc
+++ src/orange/detail/UnitInserter.cc
@@ -239,7 +239,7 @@ ForceMax const& forced_scalar_max()
  */
 UnitInserter::UnitInserter(UniverseInserter* insert_universe, Data* orange_data)
     : orange_data_(orange_data)
-    , build_bih_tree_{&orange_data_->bih_tree_data}
+    , build_bih_tree_{&orange_data_->bih_tree_data, 20}
     , insert_transform_{&orange_data_->transforms, &orange_data_->reals}
     , build_surfaces_{&orange_data_->surface_types,
                       &orange_data_->real_ids,
