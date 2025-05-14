9a4b15585104b37417b72c455d9659c67d57ff03
lbu <bullerwellle@ornl.gov>
2025-05-14 10:25:50 -0400

    fixup \file description in SDData.hh to be accurate

--- src/celeritas/user/SDData.hh
+++ src/celeritas/user/SDData.hh
@@ -2,7 +2,7 @@
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/user/detail/SDData.hh
+//! \file celeritas/user/SDData.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
