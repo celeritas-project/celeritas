01ca3a1df9ea3db8d282e6b895d729997a5a9eda
Seth R Johnson <johnsonsr@ornl.gov>
2025-05-15 11:19:15 -0400

    Update wildstyle env and default spack to C++20 due to covfie

---
+++ scripts/env/wildstyle.sh
@@ -0,0 +1,3 @@
+export SPACK_ROOT=/projects/spack
+export MODULEPATH=${SPACK_ROOT}/share/spack/lmod/linux-rhel8-x86_64/Core
+export PATH=${SPACK_ROOT}/var/spack/environments/celeritas/.spack-env/view/bin:/usr/local/cuda-12.9/bin:${PATH}
