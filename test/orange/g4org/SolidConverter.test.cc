277d580ff22132b8da64eda05506c1e974aaf6f9
Seth R Johnson <johnsonsr@ornl.gov>
2025-06-01 14:08:32 -0400

    Add test for reflecting/scaled solids

--- test/orange/g4org/SolidConverter.test.cc
+++ test/orange/g4org/SolidConverter.test.cc
@@ -29,6 +29,7 @@
 #include <G4Polyhedra.hh>
 #include <G4ReflectedSolid.hh>
 #include <G4RotationMatrix.hh>
+#include <G4ScaledSolid.hh>
 #include <G4Sphere.hh>
 #include <G4SubtractionSolid.hh>
 #include <G4SystemOfUnits.hh>
@@ -103,6 +104,7 @@ class SolidConverterTest : public ::celeritas::orangeinp::test::ObjectTestBase
   protected:
     Tol tolerance() const { return Tol::from_default(); }
 
+    // Test the solid, the generated hierarchy, and points in space [cm]
     void build_and_test(G4VSolid const& solid,
                         std::string_view json_str = "null",
                         std::initializer_list<Real3> points = {});
@@ -145,7 +147,7 @@ void SolidConverterTest::build_and_test(G4VSolid const& solid,
             inv_scale * pos[0], inv_scale * pos[1], inv_scale * pos[2])));
     };
 
-    // Test user-supplied points
+    // Test user-supplied points [cm]
     for (Real3 const& r : points)
     {
         EXPECT_EQ(calc_g4_sense(r), calc_org_sense(r)) << "at " << r << " [cm]";
@@ -482,7 +484,8 @@ TEST_F(SolidConverterTest, polyhedra)
     static double const z[] = {-0.6, 0.6};
     static double const rmin[] = {0, 0};
     static double const rmax[] = {61.85, 61.85};
-    // flat-top hexagon
+
+    // Flat-top hexagon
     this->build_and_test(
         G4Polyhedra(
             "HGCalEEAbs", 330 * deg, 360 * deg, 6, std::size(z), z, rmin, rmax),
@@ -540,6 +543,54 @@ TEST_F(SolidConverterTest, subtractionsolid)
         G4SubtractionSolid("t1Subtractionb3", &t1, &b3, transform),
         R"json({"_type":"all","daughters":[{"_type":"shape","interior":{"_type":"cylinder","halfheight":5.0,"radius":5.0},"label":"Solid Tube #1"},{"_type":"negated","daughter":{"_type":"transformed","daughter":{"_type":"shape","interior":{"_type":"box","halfwidths":[1.0,2.0,5.0]},"label":"Test Box #3"},"transform":{"_type":"transformation","data":[-1.0,1.2246467991473532e-16,0.0,-1.2246467991473532e-16,-1.0,-0.0,0.0,0.0,1.0,0.0,3.0,0.0]}},"label":""}],"label":"t1Subtractionb3"})json",
         {{0, 0, 0}, {0, 0, 10}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1}});
+}
+
+TEST_F(SolidConverterTest, reflectedsolid)
+{
+    // Triangle, flat top
+    static double const z[] = {10, 50};
+    static double const rmin[] = {0, 0};
+    static double const rmax[] = {10, 10};
+    G4Polyhedra tri("tri", 30 * deg, 360 * deg, 3, std::size(z), z, rmin, rmax);
+
+    // Reflect across xy plane
+    G4ReflectedSolid reflz("tri_refl", &tri, G4ScaleZ3D());
+    this->build_and_test(
+        reflz,
+        R"json({"_type":"transformed","daughter":{"_type":"transformed","daughter":{"_type":"shape","interior":{"_type":"prism","apothem":0.9999999999999999,"halfheight":2.0,"num_sides":3,"orientation":0.25},"label":"tri"},"transform":{"_type":"translation","data":[0.0,0.0,3.0]}},"transform":{"_type":"transformation","data":[1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0]}})json",
+        {
+            {0, 0, 1.1},
+            {0, 0, 5.1},
+            {0, 0, -1.1},
+            {0, 0, -5.1},
+            {0, 1.0, -1.1},
+        });
+
+    // Reflect across yz plane
+    G4ReflectedSolid reflx("tri_refl", &tri, G4ScaleX3D());
+    this->build_and_test(
+        reflx,
+        R"json({"_type":"transformed","daughter":{"_type":"transformed","daughter":{"_type":"shape","interior":{"_type":"prism","apothem":0.9999999999999999,"halfheight":2.0,"num_sides":3,"orientation":0.25},"label":"tri"},"transform":{"_type":"translation","data":[0.0,0.0,3.0]}},"transform":{"_type":"transformation","data":[1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0]}})json",
+        {
+            {0, 0.99, 1.1},
+            {0, -0.99, 5.1},
+            {0, 1.01, 1.1},
+            {0, -1.01, 5.1},
+        });
+}
+
+TEST_F(SolidConverterTest, DISABLED_scaledsolid)
+{
+    G4Box b("box", 10., 20., 30.);
+    G4ScaledSolid ss("scaled", &b, G4Scale3D(0.5, 1.0, 2.0));
+    this->build_and_test(ss,
+                         R"json(null)json",
+                         {
+                             {0.49, 0, 0},
+                             {0.51, 0, 0},
+                             {0.49, 0.99, 2.99},
+                             {0.49, 0.99, 3.01},
+                         });
 }
 
 TEST_F(SolidConverterTest, trap)
