dd2b2f367d457df7d99646f9e4feab9592d6a71f
Amanda Lund <alund@anl.gov>
2025-07-02 23:23:42 +0000

    Add helper function for launching CPU kernel with arbitrary number of threads

--- src/celeritas/global/ActionLauncher.hh
+++ src/celeritas/global/ActionLauncher.hh
@@ -26,6 +26,34 @@ namespace celeritas
 /*!
  * Helper function to run an executor in parallel on CPU.
  *
+ * This allows using a custom number of threads rather than the state size.
+ */
+template<class F>
+void launch_core(size_type num_threads,
+                 std::string_view label,
+                 celeritas::CoreParams const& params,
+                 celeritas::CoreState<MemSpace::host>& state,
+                 F&& execute_thread)
+{
+    MultiExceptionHandler capture_exception;
+#if defined(_OPENMP) && CELERITAS_OPENMP == CELERITAS_OPENMP_TRACK
+#    pragma omp parallel for
+#endif
+    for (size_type i = 0; i < num_threads; ++i)
+    {
+        CELER_TRY_HANDLE_CONTEXT(
+            execute_thread(ThreadId{i}),
+            capture_exception,
+            KernelContextException(
+                params.ref<MemSpace::host>(), state.ref(), ThreadId{i}, label));
+    }
+    log_and_rethrow(std::move(capture_exception));
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Helper function to run an executor in parallel on CPU over all states.
+ *
  * Example:
  * \code
  void FooHelper::step(CoreParams const& params,
@@ -41,20 +69,28 @@ void launch_core(std::string_view label,
                  celeritas::CoreState<MemSpace::host>& state,
                  F&& execute_thread)
 {
-    MultiExceptionHandler capture_exception;
-    size_type const size = state.size();
-#if defined(_OPENMP) && CELERITAS_OPENMP == CELERITAS_OPENMP_TRACK
-#    pragma omp parallel for
-#endif
-    for (size_type i = 0; i < size; ++i)
-    {
-        CELER_TRY_HANDLE_CONTEXT(
-            execute_thread(ThreadId{i}),
-            capture_exception,
-            KernelContextException(
-                params.ref<MemSpace::host>(), state.ref(), ThreadId{i}, label));
-    }
-    log_and_rethrow(std::move(capture_exception));
+    return launch_core(
+        state.size(), label, params, state, std::forward<F>(execute_thread));
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Helper function to run an action in parallel on CPU.
+ *
+ * This allows using a custom number of threads rather than the state size.
+ */
+template<class F>
+void launch_action(CoreStepActionInterface const& action,
+                   size_type num_threads,
+                   celeritas::CoreParams const& params,
+                   celeritas::CoreState<MemSpace::host>& state,
+                   F&& execute_thread)
+{
+    return launch_core(num_threads,
+                       action.label(),
+                       params,
+                       state,
+                       std::forward<F>(execute_thread));
 }
 
 //---------------------------------------------------------------------------//
