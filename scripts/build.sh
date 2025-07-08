de4b42fe9dce42f4acb5881888a957e0a7bb9841
Seth R Johnson <johnsonsr@ornl.gov>
2025-07-08 09:03:58 -0400

    Update build script and user presets

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,20 @@
 #!/bin/sh -e
 
+function log() {
+    local level=$1
+    local message=$2
+    local color=""
+
+    case "$level" in
+        "info") color="\e[32;40m"; level="INFO" ;;
+        "warning") color="\e[33;40m"; level="WARNING" ;;
+        "error") color="\e[31;40m"; level="ERROR" ;;
+        *) color="\e[2;37;40m" ;;
+    esac
+
+    printf "${color}%s:\e[0m %s\n" "$level" "$message" >&2
+}
+
 cd "$(dirname $0)"/..
 
 SYSTEM_NAME=${LMOD_SYSTEM_NAME}
@@ -11,15 +26,19 @@ fi
 # Link user presets for this system if they don't exist
 if [ ! -e "CMakeUserPresets.json" ]; then
   _USER_PRESETS="scripts/cmake-presets/${SYSTEM_NAME}.json"
-  if [ -f "${_USER_PRESETS}" ]; then
-    ln -s "${_USER_PRESETS}" "CMakeUserPresets.json"
+  if [ ! -f "${_USER_PRESETS}" ]; then
+    log info "Creating user presets at ${_USER_PRESETS} . Please update this file for future configurations."
+    cp "scripts/cmake-presets/_dev_.json" "${_USER_PRESETS}"
+    git add "${_USER_PRESETS}" || error "Could not add presets"
   fi
+  log info "Linking presets to ./CMakeUserPresets.json"
+  ln -s "${_USER_PRESETS}" "CMakeUserPresets.json"
 fi
 
 # Source environment script if necessary
 _ENV_SCRIPT="scripts/env/${SYSTEM_NAME}.sh"
 if [ -f "${_ENV_SCRIPT}" ]; then
-  echo "Sourcing environment script at ${_ENV_SCRIPT}" >&2
+  log info "Sourcing environment script at ${_ENV_SCRIPT}" >&2
   . "${_ENV_SCRIPT}"
 fi
 
@@ -27,17 +46,19 @@ fi
 if [ $# -eq 0 ]; then
   echo "Usage: $0 PRESET [config_args...]" >&2
   if hash cmake 2>/dev/null ; then
-    cmake --list-presets >&2
+    cmake --list-presets >&2 \
+      || log error "CMake may be too old or JSON file may be broken"
   else
-    echo "cmake unavailable: cannot call --list-presets" >&2
+    log error "cmake unavailable: cannot call --list-presets" >&2
   fi
   exit 2
 fi
 CMAKE_PRESET=$1
 shift
 
-set -x
-
+log info "Configuring with verbosity"
 cmake --preset=${CMAKE_PRESET} --log-level=VERBOSE "$@"
+log info "Building"
 cmake --build --preset=${CMAKE_PRESET}
+log info "Testing"
 ctest --preset=${CMAKE_PRESET}
