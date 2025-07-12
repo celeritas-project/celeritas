796878fdf33b31ae779b186f69f3066bedf0ab46
Seth R Johnson <johnsonsr@ornl.gov>
2025-07-12 08:23:33 -0400

    Add VolumeToString

--- test/geocel/Volume.test.cc
+++ test/geocel/Volume.test.cc
@@ -8,6 +8,7 @@
 #include "corecel/cont/LabelIdMultiMapUtils.hh"
 #include "geocel/Types.hh"
 #include "geocel/VolumeParams.hh"
+#include "geocel/VolumeToString.hh"
 #include "geocel/inp/Model.hh"
 
 #include "TestMacros.hh"
@@ -23,33 +24,61 @@ namespace test
  * - volumes are alphabetical (A, B, C...)
  * - volume instances are numeric (0, 1, 2...)
  */
-using VolumeTest = ::celeritas::test::Test;
+class VolumeTest : public ::celeritas::test::Test
+{
+  protected:
+    VolumeParams volumes_;
+};
+
+//---------------------------------------------------------------------------//
+class NoVolumeTest : public VolumeTest
+{
+};
 
 /*!
  * No volumes, for unit testing
  */
-TEST_F(VolumeTest, no_volumes)
+TEST_F(NoVolumeTest, params)
 {
-    VolumeParams const params;
+    VolumeParams const& params = volumes_;
     EXPECT_TRUE(params.empty());
     EXPECT_EQ(0, params.num_volumes());
 }
 
+TEST_F(NoVolumeTest, volume_to_string)
+{
+    VolumeToString to_string;
+    EXPECT_EQ("<null>", to_string(VolumeId{}));
+    EXPECT_EQ("<null>", to_string(VolumeInstanceId{}));
+    EXPECT_EQ("v 1", to_string(VolumeId{1}));
+    EXPECT_EQ("vi 2", to_string(VolumeInstanceId{2}));
+}
+
+//---------------------------------------------------------------------------//
 /*!
  * Graph:
  *    A
  */
-TEST_F(VolumeTest, single_volume)
+class SingleVolumeTest : public VolumeTest
 {
-    VolumeParams const params([] {
-        using namespace inp;
-        Volumes in;
-        Volume v;
-        v.label = "A";
-        v.material = GeoMatId{0};
-        in.volumes.push_back(v);
-        return in;
-    }());
+  public:
+    void SetUp()
+    {
+        volumes_ = VolumeParams([] {
+            using namespace inp;
+            Volumes in;
+            Volume v;
+            v.label = "A";
+            v.material = GeoMatId{0};
+            in.volumes.push_back(v);
+            return in;
+        }());
+    }
+};
+
+TEST_F(SingleVolumeTest, params)
+{
+    VolumeParams const& params = volumes_;
 
     EXPECT_FALSE(params.empty());
     EXPECT_EQ(1, params.num_volumes());
@@ -78,6 +107,20 @@ TEST_F(VolumeTest, single_volume)
     }
 }
 
+TEST_F(SingleVolumeTest, volume_to_string)
+{
+    VolumeToString to_string{volumes_};
+    EXPECT_EQ("<null>", to_string(VolumeId{}));
+    EXPECT_EQ("<null>", to_string(VolumeInstanceId{}));
+    EXPECT_EQ("A", to_string(VolumeId{0}));
+
+    if (CELERITAS_DEBUG)
+    {
+        EXPECT_THROW(to_string(VolumeId{1}), DebugError);
+    }
+}
+
+//---------------------------------------------------------------------------//
 /*!
  * Graph:
  * A -> B [0]
@@ -89,47 +132,56 @@ TEST_F(VolumeTest, single_volume)
  *
  * Where bracketed values are volume instance IDs.
  */
-TEST_F(VolumeTest, complex_hierarchy)
+class ComplexVolumeTest : public VolumeTest
 {
-    VolumeParams const params([] {
-        using namespace inp;
-        Volumes in;
+  public:
+    void SetUp()
+    {
+        volumes_ = VolumeParams([] {
+            using namespace inp;
+            Volumes in;
 
-        // Helper to create volumes
-        auto add_volume = [&in](std::string label,
-                                std::vector<VolumeInstanceId> children) {
-            Volume v;
-            v.label = std::move(label);
-            v.material = id_cast<GeoMatId>(in.volumes.size());
-            v.children = std::move(children);
-            in.volumes.push_back(v);
-        };
-        auto add_instance = [&in](VolumeId vol_id) {
-            VolumeInstance vi;
-            vi.label = std::to_string(in.volume_instances.size());
-            vi.volume = vol_id;
-            in.volume_instances.push_back(vi);
-        };
+            // Helper to create volumes
+            auto add_volume = [&in](std::string label,
+                                    std::vector<VolumeInstanceId> children) {
+                Volume v;
+                v.label = std::move(label);
+                v.material = id_cast<GeoMatId>(in.volumes.size());
+                v.children = std::move(children);
+                in.volumes.push_back(v);
+            };
+            auto add_instance = [&in](VolumeId vol_id) {
+                VolumeInstance vi;
+                vi.label = std::to_string(in.volume_instances.size());
+                vi.volume = vol_id;
+                in.volume_instances.push_back(vi);
+            };
 
-        // Create volumes A, B, C, D, E
-        add_volume("A", {VolumeInstanceId{0}, VolumeInstanceId{1}});
-        add_volume("B", {VolumeInstanceId{2}, VolumeInstanceId{3}});
-        add_volume("C", {VolumeInstanceId{4}, VolumeInstanceId{6}});
-        add_volume("D", {});
-        add_volume("E", {});
+            // Create volumes A, B, C, D, E
+            add_volume("A", {VolumeInstanceId{0}, VolumeInstanceId{1}});
+            add_volume("B", {VolumeInstanceId{2}, VolumeInstanceId{3}});
+            add_volume("C", {VolumeInstanceId{4}, VolumeInstanceId{6}});
+            add_volume("D", {});
+            add_volume("E", {});
 
-        // Create volume instances
-        add_instance(VolumeId{1});  // 0 -> B
-        add_instance(VolumeId{2});  // 1 -> C
-        add_instance(VolumeId{2});  // 2 -> C
-        add_instance(VolumeId{2});  // 3 -> C
-        add_instance(VolumeId{3});  // 4 -> D
-        // Add 'null' instance
-        in.volume_instances.push_back(VolumeInstance{});
-        add_instance(VolumeId{4});  // 6 -> E
+            // Create volume instances
+            add_instance(VolumeId{1});  // 0 -> B
+            add_instance(VolumeId{2});  // 1 -> C
+            add_instance(VolumeId{2});  // 2 -> C
+            add_instance(VolumeId{2});  // 3 -> C
+            add_instance(VolumeId{3});  // 4 -> D
+            // Add 'null' instance
+            in.volume_instances.push_back(VolumeInstance{});
+            add_instance(VolumeId{4});  // 6 -> E
 
-        return in;
-    }());
+            return in;
+        }());
+    }
+};
+
+TEST_F(ComplexVolumeTest, params)
+{
+    VolumeParams const& params = volumes_;
 
     static std::string const expected_volume_labels[]
         = {"A", "B", "C", "D", "E"};
@@ -172,6 +224,13 @@ TEST_F(VolumeTest, complex_hierarchy)
 
     static int const expected_volume_mapping[] = {1, 2, 2, 2, 3, -1, 4};
     EXPECT_VEC_EQ(expected_volume_mapping, volume_mapping);
+}
+
+TEST_F(ComplexVolumeTest, volume_to_string)
+{
+    VolumeToString to_string{volumes_};
+    EXPECT_EQ("A", to_string(VolumeId{0}));
+    EXPECT_EQ("1", to_string(VolumeInstanceId{1}));
 }
 
 //---------------------------------------------------------------------------//
