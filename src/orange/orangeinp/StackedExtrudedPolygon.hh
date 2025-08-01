8a755b97b5f611cbaefc68779c844a5359d5d8aa
Elliott Biondo <veb@ornl.gov>
2025-08-01 14:57:55 -0400

    Fix typos in StackedExtrudedPolygon

--- src/orange/orangeinp/StackedExtrudedPolygon.hh
+++ src/orange/orangeinp/StackedExtrudedPolygon.hh
@@ -80,9 +80,9 @@ namespace orangeinp
  *   in the example above might be named `my_shape@1.1.0`. For each level,
  *   additional nodes are created in the form: `label@level.suffix` where
  *   suffixes have the following meanings:
- *   1) .cu : the union of all convex regions on this level,
- *   2) .ncu : the negation of the union of all convex regions on this level,
- *   3) .d : the difference between this level's convex hull and the convex
+ *   1) .cu : the union of all concave regions on this level,
+ *   2) .ncu : the negation of the union of all concave regions on this level,
+ *   3) .d : the difference between this level's convex hull and the concave
  *      regions on this level.
  */
 class StackedExtrudedPolygon final : public ObjectInterface
