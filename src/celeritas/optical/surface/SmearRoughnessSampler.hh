0f90ad82d74bb3ebe8f38439312594df70a94805
Seth R Johnson <johnsonsr@ornl.gov>
2025-08-22 13:29:29 -0400

    Fix doc

--- src/celeritas/optical/surface/SmearRoughnessSampler.hh
+++ src/celeritas/optical/surface/SmearRoughnessSampler.hh
@@ -2,7 +2,7 @@
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/optical/surface/calc/SmearRoughnessSampler.hh
+//! \file celeritas/optical/surface/SmearRoughnessSampler.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
