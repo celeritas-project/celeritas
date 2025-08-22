be23589800927917a9b28166ab944df4cc35f024
Seth R Johnson <johnsonsr@ornl.gov>
2025-08-22 11:54:02 -0400

    Refactor tests using histogram sampler

--- test/celeritas/optical/RoughnessCalculator.test.cc
+++ test/celeritas/optical/RoughnessCalculator.test.cc
@@ -4,12 +4,8 @@
 //---------------------------------------------------------------------------//
 //! \file celeritas/optical/RoughnessCalculator.test.cc
 //---------------------------------------------------------------------------//
-#include <memory>
-#include <random>
-
-#include "corecel/math/ArrayOperators.hh"
-#include "corecel/random/DiagnosticRngEngine.hh"
-#include "celeritas/ext/RootFileManager.hh"
+#include "corecel/random/HistogramSampler.hh"
+#include "geocel/random/IsotropicDistribution.hh"
 #include "celeritas/optical/surface/SurfacePhysicsUtils.hh"
 #include "celeritas/optical/surface/calc/GaussianRoughnessCalculator.hh"
 #include "celeritas/optical/surface/calc/SmearRoughnessCalculator.hh"
@@ -28,100 +24,112 @@ using namespace ::celeritas::test;
 class RoughnessCalculatorTest : public ::celeritas::test::Test
 {
   public:
-    using RandomEngine = DiagnosticRngEngine<std::mt19937>;
-
-    RandomEngine& rng()
-    {
-        rng_.reset_count();
-        return rng_;
-    }
-
-    // Sample a roughness calculator and collect distribution moments
-    template<class C>
-    Array<real_type, 4>
-    sample_moments(C& sample_normal, Real3 const& normal, Real3 const& momentum)
-    {
-        EXPECT_TRUE(dot_product(normal, momentum) < 0);
-
-        auto& rng = this->rng();
-
-        size_type num_samples = 1000;
-
-        // 1st moment of cos with normal
-        // 2nd moment of cos with normal
-        // 1st moment of cos with momentum
-        // 2nd moment of cos with momentum
-        Array<real_type, 4> moments{0, 0, 0, 0};
-
-        for ([[maybe_unused]] auto i : range(num_samples))
-        {
-            Real3 local_normal = sample_normal(rng);
-
-            real_type normal_cos = dot_product(normal, local_normal);
-            real_type momentum_cos = dot_product(momentum, local_normal);
-
-            EXPECT_TRUE(is_soft_unit_vector(local_normal));
-            EXPECT_TRUE(momentum_cos < 0);
-
-            moments[0] += normal_cos;
-            moments[1] += ipow<2>(normal_cos);
-            moments[2] += momentum_cos;
-            moments[3] += ipow<2>(momentum_cos);
-        }
-
-        moments /= real_type(num_samples);
-
-        return moments;
-    }
-
-  private:
-    RandomEngine rng_;
 };
+
+//---------------------------------------------------------------------------//
+// Test whether the surface vs  normal rejection sampler
+TEST_F(RoughnessCalculatorTest, entering_surface)
+{
+    constexpr size_type num_samples = 4000;
+    HistogramSampler calc_histogram(4, {-1, 1}, num_samples);
+
+    std::vector<SampledHistogram> actual;
+
+    // Test over range of incident directions
+    std::vector<Real3> incident_directions
+        = {make_unit_vector(Real3{0, 0, -1}),
+           make_unit_vector(Real3{1, 0, -1}),
+           make_unit_vector(Real3{0, 1, -1}),
+           make_unit_vector(Real3{1, 1, 1}),
+           make_unit_vector(Real3{-1, 0, -1})};
+
+    for (Real3 const& incident_dir : incident_directions)
+    {
+        EnteringSurfaceNormalSampler sample_normal{incident_dir,
+                                                   IsotropicDistribution{}};
+
+        auto transform = [&incident_dir](Real3 const& sampled_normal) {
+            return dot_product(incident_dir, sampled_normal);
+        };
+
+        actual.push_back(calc_histogram(transform, sample_normal));
+    }
+
+    // All sampled normals should satisfy entering surface condition
+    // which means dot_product(sampled_normal, incident_dir) < 0
+    static SampledHistogram const expected[] = {
+        {{0.9595, 1.0405, 0, 0}, 7.987},
+        {{0.998, 1.002, 0, 0}, 8.09},
+        {{0.982, 1.018, 0, 0}, 8.026},
+        {{1.0155, 0.9845, 0, 0}, 7.91},
+        {{0.9925, 1.0075, 0, 0}, 8.016},
+    };
+    EXPECT_REF_EQ(expected, actual);
+}
 
 //---------------------------------------------------------------------------//
 // Test smear roughness model distribution
 TEST_F(RoughnessCalculatorTest, smear)
 {
-    real_type roughness = 0.3;
-    Real3 normal = make_unit_vector(Real3{0, 0, 1});
-    Real3 momentum = make_unit_vector(Real3{0, 1, -1});
+    constexpr size_type num_samples = 10000;
+    HistogramSampler calc_histogram(5, {0, 1}, num_samples);
 
-    EnteringSurfaceNormalSampler sample_normal{
-        momentum, SmearRoughnessCalculator{roughness, normal}};
+    Real3 normal = make_unit_vector(Real3{1, 0, -1});
+    std::vector<SampledHistogram> actual;
 
-    auto moments = this->sample_moments(sample_normal, normal, momentum);
+    // Test over range of roughness values
+    for (real_type roughness : {0.0, 0.1, 0.5, 0.7, 0.9, 1.0})
+    {
+        SmearRoughnessCalculator sample_normal{roughness, normal};
 
-    Array<real_type, 4> expected_moments{
-        0.98210059816854511,
-        0.96466393417949625,
-        -0.69142901275056512,
-        0.48670323495480255,
+        auto transform = [&normal](Real3 const& sampled_normal) {
+            return dot_product(normal, sampled_normal);
+        };
+
+        actual.push_back(calc_histogram(transform, sample_normal));
+    }
+
+    static SampledHistogram const expected[] = {
+        {{0, 0, 0, 0, 5}, 6},
+        {{0, 0, 0, 0, 5}, 6},
+        {{0, 0, 0, 0, 5}, 6},
+        {{0, 0, 0, 0.552, 4.448}, 6},
+        {{0, 0, 0.289, 1.385, 3.326}, 6},
+        {{0.0065, 0.131, 0.498, 1.411, 2.9535}, 6},
     };
 
-    EXPECT_VEC_SOFT_EQ(expected_moments, moments);
+    EXPECT_REF_EQ(expected, actual);
 }
 
 //---------------------------------------------------------------------------//
 // Test Gaussian roughness model distribution
 TEST_F(RoughnessCalculatorTest, gaussian)
 {
-    real_type sigma_alpha = 0.3;
-    Real3 normal = make_unit_vector(Real3{0, 0, 1});
-    Real3 momentum = make_unit_vector(Real3{0, 1, -1});
+    constexpr size_type num_samples = 5000;
+    HistogramSampler calc_histogram(5, {0, 1}, num_samples);
 
-    EnteringSurfaceNormalSampler sample_normal{
-        momentum, GaussianRoughnessCalculator{sigma_alpha, normal}};
+    Real3 normal = make_unit_vector(Real3{1, 0, -1});
+    std::vector<SampledHistogram> actual;
 
-    auto moments = this->sample_moments(sample_normal, normal, momentum);
+    // Test over range of sigma_alpha values
+    for (real_type sigma_alpha : {0.1, 0.7, 1.5, 3.0})
+    {
+        GaussianRoughnessCalculator sample_normal{sigma_alpha, normal};
 
-    Array<real_type, 4> expected_moments{
-        0.955971673785749,
-        0.917180750525598,
-        -0.670429633303795,
-        0.471359786966057,
+        auto transform = [&normal](Real3 const& sampled_normal) {
+            return dot_product(normal, sampled_normal);
+        };
+
+        actual.push_back(calc_histogram(transform, sample_normal));
+    }
+
+    static SampledHistogram const expected[] = {
+        {{0, 0, 0, 0, 5}, 22.0336},
+        {{0.303, 0.495, 0.792, 1.34, 2.07}, 10.5816},
+        {{0.788, 0.9, 1.021, 1.125, 1.166}, 10.1508},
+        {{0.993, 1.012, 0.997, 1.013, 0.985}, 11.2816},
     };
-
-    EXPECT_VEC_SOFT_EQ(expected_moments, moments);
+    EXPECT_REF_EQ(expected, actual);
 }
 
 //---------------------------------------------------------------------------//
