a99b4830620d294028b0a38ed10b551d7bf5d97a
Sakib Rahman <rahmans@myumanitoba.ca>
2025-08-25 13:33:01 -0400

    Update steering file

--- example/ddceler/steeringFile.py
+++ example/ddceler/steeringFile.py
@@ -4,23 +4,21 @@ Minimal proof of concept for DDG4 steering file to run Geant4 with celeritas int
 
 Usage:
 
-CELER_LOG=debug CELER_LOG_LOCAL=debug ddsim --action.run DDcelerRunAction --compactFile=$PWD/install/share/CeleritasDD4hep/celeritas-dd4hep.xml --steering steeringFile.py -N 500 -G --gun.particle e- --gun.etaMax 4 --gun.etaMin 1 --gun.energy 5*GeV --gun.distribution uniform --outputFile output1.root --outputConfig.forceDD4HEP  --action.tracker "Geant4TrackerAction" --action.trackerSDTypes "tracker" --action.calorimeter "Geant4CalorimeterAction" --action.calorimeterSDTypes "calorimeter"
+CELER_LOG=debug CELER_LOG_LOCAL=debug ddsim --action.run DDcelerRunAction --compactFile=$PWD/install/share/CeleritasDD4hep/celeritas-dd4hep.xml --steering steeringFile.py -N 500 -G --gun.particle e- --gun.etaMax 4 --gun.etaMin 1 --gun.energy 5*GeV --gun.distribution uniform --outputFile output.edm4hep.root --outputConfig.forceDD4HEP  --action.tracker "Geant4TrackerAction" --action.trackerSDTypes "tracker" --action.calorimeter "Geant4CalorimeterAction" --action.calorimeterSDTypes "calorimeter"
 
 """
 
-import DDG4
+from DDSim.DD4hepSimulation import DD4hepSimulation
 
-# Create DDG4 kernel
-kernel  = DDG4.Kernel()
+RUNNER = DD4hepSimulation()
 
-# Print detector information
-DDG4.importConstants(kernel.detectorDescription(), debug=False)
-geant4 = DDG4.Geant4(kernel)
-geant4.registerInterruptHandler()
-geant4.printDetectors()
+# Physics configuration
+def setupPhysics(kernel):
+  from DDG4 import PhysicsList,Geant4
+  phys = Geant4(kernel).setupPhysics('QGSP_BERT')
+  celer_phys = PhysicsList(kernel, str('DDcelerTMI'))
+  phys.adopt(celer_phys)
+  phys.dump()
+  return None
 
-# Set up custom physics list for celeritas integration
-phys = geant4.setupPhysics('FTFP_BERT')
-ph = DDG4.PhysicsList(kernel, str('DDcelerTMI'))
-phys.adopt(ph)
-phys.dump()
+RUNNER.physics.setupUserPhysics(setupPhysics)
