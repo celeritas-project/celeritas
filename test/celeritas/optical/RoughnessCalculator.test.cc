5e053631651f67c21e80f25da981618f51581cd5
Seth R Johnson <johnsonsr@ornl.gov>
2025-08-26 09:22:35 -0400

    Update test to have wider range of values (more samples fails due to rejection)

--- test/celeritas/optical/RoughnessCalculator.test.cc
+++ test/celeritas/optical/RoughnessCalculator.test.cc
@@ -127,14 +127,17 @@ TEST_F(RoughnessSamplerTest, smear)
 // Test Gaussian roughness model distribution
 TEST_F(RoughnessSamplerTest, gaussian)
 {
-    constexpr size_type num_samples = 5000;
-    HistogramSampler calc_histogram(5, {0, 1}, num_samples);
+    constexpr size_type num_samples = 1000;
+    HistogramSampler calc_histogram(5, {0.0, 1.0}, num_samples);
 
     Real3 normal = make_unit_vector(Real3{1, 0, -1});
     std::vector<SampledHistogram> actual;
 
     // Test over range of sigma_alpha (stdev in radians) values
-    for (real_type sigma_alpha : {0.1, 0.20594885, 0.79})
+    // A "very rough" crystal in the UNIFIED paper has sigma_alpha of 0.2053
+    // (note that the paper gives the value in degrees), having at most a
+    // deflection angle cosine of ~0.76 (40 degrees)
+    for (real_type sigma_alpha : {0.01, 0.05, 0.1, 0.2053, 0.4})
     {
         GaussianRoughnessSampler sample_normal{normal, sigma_alpha};
 
@@ -144,11 +147,14 @@ TEST_F(RoughnessSamplerTest, gaussian)
 
         actual.push_back(calc_histogram(to_cos_normal, sample_normal));
     }
+    PRINT_EXPECTED(actual);
 
     static SampledHistogram const expected[] = {
-        {{0, 0, 0, 0, 5}, 22.0336},
-        {{0, 0, 0, 0.034, 4.966}, 21.7968},
-        {{0.423, 0.586, 0.891, 1.281, 1.819}, 10.206},
+        {{0, 0, 0, 0, 5}, 22.256},
+        {{0, 0, 0, 0, 5}, 21.438},
+        {{0, 0, 0, 0, 5}, 22.302},
+        {{0, 0, 0, 0.035, 4.965}, 22.152},
+        {{0.01, 0.05, 0.25, 1.01, 3.68}, 15.38},
     };
     if (CELERITAS_REAL_TYPE == CELERITAS_REAL_TYPE_DOUBLE)
     {
