9e2a05f242e1a20a29d51b25a4201b0ee932415a
Hayden Hollenbeck <hollenbeck.hayden@gmail.com>
2025-09-02 12:15:06 -0500

    Revert sincos change in GaussianRoughnessSampler

--- src/celeritas/optical/surface/GaussianRoughnessSampler.hh
+++ src/celeritas/optical/surface/GaussianRoughnessSampler.hh
@@ -103,9 +103,7 @@ CELER_FUNCTION Real3 GaussianRoughnessSampler::operator()(Engine& rng)
             // nonpositive slope are generally vanishingly small)
             alpha = std::fabs(sample_alpha_(rng));
         } while (alpha >= alpha_max_);
-        sin_alpha = std::sin(alpha);
-        cos_alpha = std::cos(alpha);
-        // sincos(alpha, &sin_alpha, &cos_alpha);
+        sincos(alpha, &sin_alpha, &cos_alpha);
 
         // Transform to polar angle using rejection
     } while (RejectionSampler{sin_alpha, f_max_}(rng));
