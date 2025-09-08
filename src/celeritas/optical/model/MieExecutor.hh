282cc906db3bc32326c345a866fbe7f29f56be93
Rashika-Gupta <rashika.gupta.ornl@gmail.com>
2025-09-08 11:13:24 -0400

    add params in the exec

--- src/celeritas/optical/model/MieExecutor.hh
+++ src/celeritas/optical/model/MieExecutor.hh
@@ -10,6 +10,7 @@
 #include "corecel/Macros.hh"
 #include "celeritas/geo/GeoFwd.hh"
 #include "celeritas/optical/CoreTrackView.hh"
+#include "celeritas/optical/ImportedMaterials.hh"
 #include "celeritas/optical/Interaction.hh"
 #include "celeritas/optical/ParticleTrackView.hh"
 #include "celeritas/optical/interactor/MieInteractor.hh"
@@ -22,6 +23,9 @@ namespace optical
 struct MieExecutor
 {
     inline CELER_FUNCTION Interaction operator()(CoreTrackView const&);
+
+    // NativeCRef<ImportedMaterials> imported; // <-- hold material tables
+    ImportedMaterials const* imported;
 };
 
 //---------------------------------------------------------------------------//
@@ -34,8 +38,21 @@ CELER_FUNCTION Interaction MieExecutor::operator()(CoreTrackView const& track)
     auto const& direction = track.geometry().dir();
     auto rng = track.rng();
 
-    MieInteractor interact{particle, direction, Params const & mie_params};
+    // Get current material ID
+    auto mat_id = track.material_record().material_id();
 
+    // Fetch ImportMie for this material
+    //  auto const& import_mie = imported.mie(mat_id);
+    // Get imported mie properties for this material
+    auto const& mie_imp = imported->mie(mat_id);
+
+    // Fill MieInteractor::Params
+    MieInteractor::Params params{static_cast<real_type>(mie_imp.forward_g),
+                                 static_cast<real_type>(mie_imp.backward_g),
+                                 static_cast<real_type>(mie_imp.forward_ratio)};
+
+    // Construct and run the interactor
+    MieInteractor interact{particle, direction, params};
     return interact(rng);
 }
 
