db3c4dc42105f6cff89dea880aee755348c9be5a
Seth R Johnson <johnsonsr@ornl.gov>
2025-09-11 10:48:09 -0400

    Run accel TestEm3 with GPU as well

--- cmake/CeleritasG4Tests.cmake
+++ cmake/CeleritasG4Tests.cmake
@@ -9,6 +9,10 @@ CeleritasG4Tests
 Add all available permutations of tests for executables managed by the Geant4
 run manager.
 
+.. warning:: This cmake file *must* be included in the same scope as it is used
+   due to internal directory-scoped variables. This requirement may be relaxed
+   in the future.
+
 .. command:: celeritas_add_g4_tests
 
   This will add all permutations of available G4 run managers (serial, MT,
@@ -58,6 +62,9 @@ if(Geant4_VERSION VERSION_LESS 11.0)
   set(_rm_avail_task FALSE)
 endif()
 
+# Set number of threads allowed
+set(_rm_threads 2)
+
 function(celeritas_g4_add_one_test test_name target args labels offload rmtype)
   add_test(NAME "${test_name}" COMMAND "$<TARGET_FILE:${target}>" ${args})
   set(_env
@@ -75,8 +82,8 @@ function(celeritas_g4_add_one_test test_name target args labels offload rmtype)
     list(APPEND _env "CELER_KILL_OFFLOAD=1")
   endif()
   if(NOT rmtype STREQUAL "serial")
-    list(APPEND _env "G4FORCENUMBEROFTHREADS=2")
-    list(APPEND _extra_props PROCESSORS 2)
+    list(APPEND _env "G4FORCENUMBEROFTHREADS=${_rm_threads}")
+    list(APPEND _extra_props PROCESSORS ${_rm_threads})
   endif()
 
   set_tests_properties("${test_name}" PROPERTIES
@@ -88,7 +95,7 @@ function(celeritas_g4_add_one_test test_name target args labels offload rmtype)
 endfunction()
 
 function(celeritas_g4_add_tests target)
-  cmake_parse_arguments(PARSE "DISABLE" "NAME" "ARGS;LABELS" ${ARGN})
+  cmake_parse_arguments(PARSE "DISABLE" "NAME" "ARGS;LABELS;RMTYPE;OFFLOAD" ${ARGN})
   if(PARSE_UNPARSED_ARGUMENTS)
     message(SEND_ERROR "Unknown keywords given to celeritas_g4_add_tests(): "
             "\"${PARSE_UNPARSED_ARGUMENTS}\"")
@@ -97,8 +104,14 @@ function(celeritas_g4_add_tests target)
   if(NOT PARSE_NAME)
     set(PARSE_NAME ${target})
   endif()
-  foreach(_offload gpu cpu g4 ko)
-    foreach(_rmtype serial mt task)
+  if(NOT PARSE_RMTYPE)
+    set(PARSE_RMTYPE serial mt task)
+  endif()
+  if(NOT PARSE_OFFLOAD)
+    set(PARSE_OFFLOAD gpu cpu g4 ko)
+  endif()
+  foreach(_offload IN LISTS PARSE_OFFLOAD)
+    foreach(_rmtype IN LISTS PARSE_RMTYPE)
       set(_disable ${PARSE_DISABLE})
       if(_offload STREQUAL "gpu" AND _disable_device)
         set(_disable TRUE)
