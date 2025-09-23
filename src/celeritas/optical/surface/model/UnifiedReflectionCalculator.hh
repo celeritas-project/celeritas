72839fb6a33a7635abca445b83c5447035b17040
Hayden Hollenbeck <hollenbeck.hayden@gmail.com>
2025-09-23 14:09:26 -0500

    UNIFIED Reflection tests

--- src/celeritas/optical/surface/model/UnifiedReflectionCalculator.hh
+++ src/celeritas/optical/surface/model/UnifiedReflectionCalculator.hh
@@ -8,6 +8,7 @@
 
 #include "corecel/Macros.hh"
 #include "corecel/cont/EnumArray.hh"
+#include "corecel/math/ArrayOperators.hh"
 #include "celeritas/optical/Types.hh"
 
 #include "SurfaceInteraction.hh"
@@ -16,55 +17,69 @@ namespace celeritas
 {
 namespace optical
 {
-
+//---------------------------------------------------------------------------//
+/*!
+ * Calculate geometric reflection of an incident vector about a normal.
+ */
 inline CELER_FUNCTION Real3 geometric_reflection(Real3 const& dir,
                                                  Real3 const& normal)
 {
     return dir - 2 * dot_product(dir, normal) * normal;
 }
 
-enum class UnifiedReflectionModes
-{
-    specular_spike,
-    specular_lobe,
-    back_scattering,
-    diffuse_lambertian,
-    size_
-};
-
 //---------------------------------------------------------------------------//
 /*!
- * Brief class description.
+ * Calculator for UNIFIED reflection model.
  *
- * Optional detailed class description, and possibly example usage:
- * \code
-    UnifiedReflectionCalculator ...;
-   \endcode
+ * The model specifies 4 different reflection modes:
+ *  1. Specular spike: geometric reflection about global normal
+ *  2. Specular lobe: geometric reflection about facet normal
+ *  3. Back-scattering: reversed photon direction and polarization
+ *  4. Diffuse Lambertian: reflection following Lambert's cosine law
+ *
+ * Only one reflection mode is selected based on the provided list of
+ * probabilities.
  */
 class UnifiedReflectionCalculator
 {
   public:
+    // Supported reflection modes in UNIFIED model
+    enum class Modes
+    {
+        specular_spike,
+        specular_lobe,
+        back_scattering,
+        diffuse_lambertian,
+        size_
+    };
+
     //!@{
     //! \name Type aliases
-    using ModeProbs = EnumArray<UnifiedReflectionModes, real_type>;
+    using ModeProbs = EnumArray<Modes, real_type>;
     //!@}
 
   public:
+    // Construct from mode probabilities, photon, and surface data
     explicit inline CELER_FUNCTION
     UnifiedReflectionCalculator(ModeProbs const& probs,
                                 PhotonPhasor const& inc_photon,
                                 Real3 const& global_normal,
                                 Real3 const& facet_normal);
 
+    // Sample reflection mode and calculate reflected phasor
     template<class Engine>
     inline CELER_FUNCTION PhotonPhasor operator()(Engine& rng) const;
 
+    // Calculate specular spike reflection
     inline CELER_FUNCTION PhotonPhasor specular_spike() const;
 
+    // Calculate specular lobe reflection
     inline CELER_FUNCTION PhotonPhasor specular_lobe() const;
 
+    // Calculate back-scattering reflection
     inline CELER_FUNCTION PhotonPhasor back_scattering() const;
 
+    // Sample diffuse Lambertian reflection
     template<class Engine>
     inline CELER_FUNCTION PhotonPhasor lambertian_reflection(Engine& rng) const;
 
@@ -74,10 +89,15 @@ class UnifiedReflectionCalculator
     Real3 const& global_normal_;
     Real3 const& facet_normal_;
 
+    // Calculate specular reflection about the given normal
     inline CELER_FUNCTION PhotonPhasor
     specular_reflection(Real3 const& normal) const;
 };
 
+//---------------------------------------------------------------------------//
+/*!
+ * Construct calculator from probabilities, photon, and surface data.
+ */
 CELER_FUNCTION UnifiedReflectionCalculator::UnifiedReflectionCalculator(
     ModeProbs const& probs,
     PhotonPhasor const& inc_photon,
@@ -90,27 +110,55 @@ CELER_FUNCTION UnifiedReflectionCalculator::UnifiedReflectionCalculator(
 {
 }
 
+//---------------------------------------------------------------------------//
+/*!
+ * Sample reflection mode from probabilities and calculate reflection.
+ */
 template<class Engine>
 CELER_FUNCTION PhotonPhasor UnifiedReflectionCalculator::operator()(Engine&) const
 {
     return {};
 }
 
+//---------------------------------------------------------------------------//
+/*!
+ * Calculate specular spike reflection.
+ *
+ * This is geometric reflection about the global normal.
+ */
 CELER_FUNCTION PhotonPhasor UnifiedReflectionCalculator::specular_spike() const
 {
     return {};
 }
 
+//---------------------------------------------------------------------------//
+/*!
+ * Calculate specular lobe reflection.
+ *
+ * This is geometric reflection about the facet normal.
+ */
 CELER_FUNCTION PhotonPhasor UnifiedReflectionCalculator::specular_lobe() const
 {
     return {};
 }
 
+//---------------------------------------------------------------------------//
+/*!
+ * Calculate back-scattering reflection.
+ *
+ * The photon direction and polarization are reversed.
+ */
 CELER_FUNCTION PhotonPhasor UnifiedReflectionCalculator::back_scattering() const
 {
     return {};
 }
 
+//---------------------------------------------------------------------------//
+/*!
+ * Sample diffuse Lambertian reflection.
+ *
+ * Ideal diffuse reflection following Lambert's cosine law.
+ */
 template<class Engine>
 CELER_FUNCTION PhotonPhasor
 UnifiedReflectionCalculator::lambertian_reflection(Engine&) const
@@ -118,6 +166,10 @@ UnifiedReflectionCalculator::lambertian_reflection(Engine&) const
     return {};
 }
 
+//---------------------------------------------------------------------------//
+/*!
+ * Helper function to calculate geometric reflection about a given normal.
+ */
 CELER_FUNCTION PhotonPhasor
 UnifiedReflectionCalculator::specular_reflection(Real3 const&) const
 {
--- test/celeritas/optical/UnifiedReflectionCalculator.test.cc
+++ test/celeritas/optical/UnifiedReflectionCalculator.test.cc
@@ -4,7 +4,7 @@
 //---------------------------------------------------------------------------//
 //! \file celeritas/optical/UnifiedReflectionCalculator.test.cc
 //---------------------------------------------------------------------------//
-// #include "celeritas/optical/UnifiedReflectionCalculator.hh"
+#include "celeritas/optical/surface/model/UnifiedReflectionCalculator.hh"
 
 #include "corecel/random/HistogramSampler.hh"
 #include "celeritas/optical/surface/model/LambertianDistribution.hh"
@@ -49,6 +49,51 @@ TEST_F(UnifiedReflectionCalculatorTest, lambertian)
     if (CELERITAS_REAL_TYPE == CELERITAS_REAL_TYPE_DOUBLE)
     {
         EXPECT_REF_EQ(expected, actual);
+    }
+}
+
+//---------------------------------------------------------------------------//
+// Test specular spike, specular lobe, and back-scattering modes
+TEST_F(UnifiedReflectionCalculatorTest, modes)
+{
+    auto global_normal = make_unit_vector(Real3{-1, 3, 2});
+    auto facet_normal = make_unit_vector(Real3{-1, 4, 2});
+
+    PhotonPhasor photon;
+    photon.direction = make_unit_vector(Real3{1, -1, -2});
+    photon.polarization = make_unit_vector(Real3{2, 0, 1});
+
+    UnifiedReflectionCalculator calc_reflection{
+        {0.3, 0.3, 0.4, 0}, photon, global_normal, facet_normal};
+
+    // Specular spike
+    {
+        auto result = calc_reflection.specular_spike();
+
+        PhotonPhasor expected{{-0.05832118, 0.99146013, 0.11664237},
+                              {-0.89442719, 0, -0.4472136}};
+
+        EXPECT_VEC_SOFT_EQ(expected.direction, result.direction);
+        EXPECT_VEC_SOFT_EQ(expected.polarization, result.polarization);
+    }
+    // Specular lobe
+    {
+        auto result = calc_reflection.specular_lobe();
+
+        PhotonPhasor expected{{0.05832118, 0.99146013, -0.11664237},
+                              {-0.89442719, -0., -0.4472136}};
+
+        EXPECT_VEC_SOFT_EQ(expected.direction, result.direction);
+        EXPECT_VEC_SOFT_EQ(expected.polarization, result.polarization);
+    }
+    // Back scattering
+    {
+        auto result = calc_reflection.back_scattering();
+
+        PhotonPhasor expected{-photon.direction, -photon.polarization};
+
+        EXPECT_VEC_SOFT_EQ(expected.direction, result.direction);
+        EXPECT_VEC_SOFT_EQ(expected.polarization, result.polarization);
     }
 }
 
