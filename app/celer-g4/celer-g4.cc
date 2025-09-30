7a09b9aaf893320062dbc95b465a6ef72762711a
Amanda Lund <alund@anl.gov>
2025-09-30 14:22:18 +0000

    Move celer-g4 setup options loading to after run manager construction
    `GlobalSetup::ReadInput() needs `SharedParams::supported_offload_particles()`, which expects the run manager to already be constructed:
    
    app/celer-g4/celer-g4.cc:271: critical: While running cms-hgcal-g4.inp.json: /home/alund/celeritas_project/celeritas/src/accel/SharedParams.cc:216:
    celeritas: precondition failed: G4RunManager::GetRunManager()

--- app/celer-g4/celer-g4.cc
+++ app/celer-g4/celer-g4.cc
@@ -107,8 +107,6 @@ void run(std::string_view filename, std::shared_ptr<SharedParams> params)
 
     // Construct global setup singleton and make options available to UI
     auto& setup = *GlobalSetup::Instance();
-    // Read user input
-    setup.ReadInput(std::string(filename));
 
     // Start tracing session
     celeritas::TracingSession tracing{setup.input().tracing_file};
@@ -140,6 +138,9 @@ void run(std::string_view filename, std::shared_ptr<SharedParams> params)
 #endif
     }();
     CELER_ASSERT(run_manager);
+
+    // Read user input
+    setup.ReadInput(std::string(filename));
 
     // Set up loggers
     world_logger() = Logger::from_handle_env(make_world_handler(), "CELER_LOG");
