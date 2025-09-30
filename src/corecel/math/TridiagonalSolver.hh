73e92fe595e121cbacaab2a7f37cf9a88e5e4397
Seth R Johnson <johnsonsr@ornl.gov>
2025-09-30 14:59:15 -0400

    Clarify

--- src/corecel/math/TridiagonalSolver.hh
+++ src/corecel/math/TridiagonalSolver.hh
@@ -22,7 +22,8 @@ namespace celeritas
  * This is a simplified form of Gaussian elimination that can solve a
  * tridiagonal system \f$ \mathbf{T} \mathbf{x} = \mathbf{b} \f$ in O(n) time.
  *
- * This class is to be used \em only during setup.
+ * The class is meant for use during setup (originally for the calculation of
+ * spline coefficients) and cannot be used on device.
  */
 class TridiagonalSolver
 {
