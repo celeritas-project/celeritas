53cba46bc22571725927094d64ec5964ddc22c39
Julien Esseiva <jesseiva@lbl.gov>
2025-10-01 18:46:38 +0200

    casting

--- test/celeritas/field/Fields.test.cc
+++ test/celeritas/field/Fields.test.cc
@@ -346,14 +346,17 @@ TEST_F(CartMapFieldTest, all)
     Interpolator interp_z({0, inp.z.min}, {nz_samples - 1, inp.z.max});
     for (size_type ix = 0; ix < nx_samples; ++ix)
     {
-        real_type x = std::min(interp_x(ix), inp.x.max - 1);
+        real_type x
+            = std::min(interp_x(ix), static_cast<real_type>(inp.x.max - 1));
 
         for (size_type iy = 0; iy < ny_samples; ++iy)
         {
-            real_type y = std::min(interp_y(iy), inp.y.max - 1);
+            real_type y = std::min(interp_y(iy),
+                                   static_cast<real_type>(inp.y.max - 1));
             for (size_type iz = 0; iz < nz_samples; ++iz)
             {
-                real_type z = std::min(interp_z(iz), inp.z.max - 1);
+                real_type z = std::min(interp_z(iz),
+                                       static_cast<real_type>(inp.z.max - 1));
 
                 Real3 field = calc_field({x, y, z});
                 for (real_type f : field)
@@ -453,7 +456,7 @@ TEST_F(CartMapFieldTest, all)
 
 TEST_F(CartMapFieldTest, TEST_IF_CELER_DEVICE(device))
 {
-    Real3 n_samples{3, 3, 3};
+    Array<size_type, 3> n_samples{3, 3, 3};
 
     std::vector<real_type> field_values(n_samples[0] * n_samples[1]
                                         * n_samples[2] * 3);
