96a996a9921c5a0f5f95f16732aab427be435ebb
Seth R Johnson <johnsonsr@ornl.gov>
2025-10-02 06:28:16 -0400

    Revert unintentional scintillation changes

--- src/celeritas/optical/gen/detail/MatScintSpecInserter.hh
+++ src/celeritas/optical/gen/detail/MatScintSpecInserter.hh
@@ -10,7 +10,6 @@
 #include "corecel/Types.hh"
 #include "corecel/data/CollectionBuilder.hh"
 #include "corecel/data/DedupeCollectionBuilder.hh"
-#include "corecel/io/Logger.hh"
 #include "corecel/math/SoftEqual.hh"
 #include "celeritas/io/ImportOpticalMaterial.hh"
 
@@ -67,73 +66,55 @@ MatScintSpecInserter::MatScintSpecInserter(Data* data)
  */
 auto MatScintSpecInserter::operator()(ImportMaterialScintSpectrum const& mat)
 {
+    CELER_EXPECT(mat);
+
+    CELER_VALIDATE(mat.yield_per_energy > 0,
+                   << "invalid yield=" << mat.yield_per_energy
+                   << " for scintillation (should be positive)");
+
+    double total_yield{0};
+    std::vector<double> yield_pdf;
+    auto const begin_components = scint_records_.size_id();
+    for (ImportScintComponent const& comp : mat.components)
+    {
+        CELER_VALIDATE(comp.lambda_mean > 0,
+                       << "invalid lambda_mean=" << comp.lambda_mean
+                       << " for scintillation component (should be positive)");
+        CELER_VALIDATE(comp.lambda_sigma > 0,
+                       << "invalid lambda_sigma=" << comp.lambda_sigma
+                       << " (should be positive)");
+        CELER_VALIDATE(comp.rise_time >= 0,
+                       << "invalid rise_time=" << comp.rise_time
+                       << " (should be nonnegative)");
+        CELER_VALIDATE(comp.fall_time > 0,
+                       << "invalid fall_time=" << comp.fall_time
+                       << " (should be positive)");
+        CELER_VALIDATE(comp.yield_frac > 0,
+                       << "invalid yield=" << comp.yield_frac);
+
+        ScintRecord scint;
+        scint.lambda_mean = comp.lambda_mean;
+        scint.lambda_sigma = comp.lambda_sigma;
+        scint.rise_time = comp.rise_time;
+        scint.fall_time = comp.fall_time;
+        scint_records_.push_back(scint);
+
+        yield_pdf.push_back(comp.yield_frac);
+        total_yield += comp.yield_frac;
+    }
+
+    // Normalize yield PDF by total yield
+    for (auto& y : yield_pdf)
+    {
+        y /= total_yield;
+    }
+
     MatScintSpectrum spectrum;
-    if (mat)
-    {
-        // Only if the material has the scintillation spectrum
-        CELER_VALIDATE(mat.yield_per_energy > 0,
-                       << "invalid yield=" << mat.yield_per_energy
-                       << " for scintillation (should be positive)");
+    spectrum.yield_per_energy = mat.yield_per_energy;
+    spectrum.components = {begin_components, scint_records_.size_id()};
+    spectrum.yield_pdf = reals_.insert_back(yield_pdf.begin(), yield_pdf.end());
 
-        double total_yield{0};
-        std::vector<double> yield_pdf;
-        auto const begin_components = scint_records_.size_id();
-        for (ImportScintComponent const& comp : mat.components)
-        {
-            CELER_VALIDATE(comp.lambda_mean > 0,
-                           << "invalid lambda_mean=" << comp.lambda_mean
-                           << " for scintillation component (should be "
-                              "positive)");
-            CELER_VALIDATE(comp.lambda_sigma > 0,
-                           << "invalid lambda_sigma=" << comp.lambda_sigma
-                           << " (should be positive)");
-            CELER_VALIDATE(comp.rise_time >= 0,
-                           << "invalid rise_time=" << comp.rise_time
-                           << " (should be nonnegative)");
-            CELER_VALIDATE(comp.fall_time > 0,
-                           << "invalid fall_time=" << comp.fall_time
-                           << " (should be positive)");
-            CELER_VALIDATE(comp.yield_frac > 0,
-                           << "invalid yield=" << comp.yield_frac);
-
-            ScintRecord scint;
-            scint.lambda_mean = comp.lambda_mean;
-            scint.lambda_sigma = comp.lambda_sigma;
-            scint.rise_time = comp.rise_time;
-            scint.fall_time = comp.fall_time;
-            scint_records_.push_back(scint);
-
-            yield_pdf.push_back(comp.yield_frac);
-            total_yield += comp.yield_frac;
-        }
-        // Normalize yield PDF by total yield
-        for (auto& y : yield_pdf)
-        {
-            y /= total_yield;
-        }
-
-        spectrum.yield_per_energy = mat.yield_per_energy;
-        spectrum.components = {begin_components, scint_records_.size_id()};
-        spectrum.yield_pdf
-            = reals_.insert_back(yield_pdf.begin(), yield_pdf.end());
-
-        CELER_LOG(debug) << "mat spectrum is : " << spectrum.components.size();
-        CELER_ENSURE(spectrum.components.size() == mat.components.size());
-    }
-    else
-    {
-        // No scintillation was found for the material.
-        // setting it to zero
-        spectrum.yield_per_energy = 0.0;
-        spectrum.components
-            = {scint_records_.size_id(), scint_records_.size_id()};
-        spectrum.yield_pdf = {};
-        CELER_LOG(debug)
-            << "No scintillation spectrum found for the material - setting "
-               "the spectrum to "
-            << spectrum.yield_per_energy;
-    }
-
+    CELER_ENSURE(spectrum.components.size() == mat.components.size());
     return materials_.push_back(std::move(spectrum));
 }
 
