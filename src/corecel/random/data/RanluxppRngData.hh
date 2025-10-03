1520da902ff992392a3b7e877ba7dd790b0ad26f
Gregory Davidson <davidsongg@ornl.gov>
2025-10-03 15:04:56 -0400

    Fix documented filename

--- src/corecel/random/data/RanluxppRngData.hh
+++ src/corecel/random/data/RanluxppRngData.hh
@@ -2,7 +2,7 @@
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file corecel/random/data/RanluxppData.hh
+//! \file corecel/random/data/RanluxppRngData.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
