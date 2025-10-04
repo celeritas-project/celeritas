bf8995e6af488cbfa645be58b64b5e5c8b65dfaa
Seth R Johnson <johnsonsr@ornl.gov>
2025-10-04 19:49:45 -0400

    Add unnormalized selector and test enum usage

--- src/corecel/random/distribution/Selector.hh
+++ src/corecel/random/distribution/Selector.hh
@@ -6,13 +6,7 @@
 //---------------------------------------------------------------------------//
 #pragma once
 
-#include <type_traits>
-
-#include "corecel/cont/Range.hh"
-#include "corecel/math/Algorithms.hh"
-#include "corecel/math/SoftEqual.hh"
-
-#include "GenerateCanonical.hh"
+#include "detail/SelectorImpl.hh"
 
 namespace celeritas
 {
@@ -27,7 +21,7 @@ namespace celeritas
  * accumulation algorithm. When building with debug checking, the constructor
  * asserts that the provided "total" value is consistent.
  *
- * The given function *must* return a consistent value for the same given
+ * The given function \em must return a consistent value for the same given
  * argument.
  *
  * \code
@@ -43,103 +37,26 @@ namespace celeritas
                                     pdf.size());
     size_type idx = select_val(rng);
    \endcode
+ * Create a normalized selector from a function and total accumulated value.
  */
 template<class F, class T>
-class Selector
+CELER_FUNCTION detail::Selector<F, T>
+make_selector(F&& func, T size, real_type total = 1)
 {
-  public:
-    //!@{
-    //! \name Type aliases
-    using value_type = T;
-    using real_type = typename std::invoke_result<F, value_type>::type;
-    //!@}
-
-  public:
-    // Construct with function, size, and accumulated value
-    inline CELER_FUNCTION Selector(F&& eval, value_type size, real_type total);
-
-    // Sample from the distribution
-    template<class Engine>
-    inline CELER_FUNCTION T operator()(Engine& rng) const;
-
-  private:
-    using IterT = RangeIter<T>;
-
-    F eval_;
-    IterT last_;
-    real_type total_;
-
-    // Total value, for debug checking
-    inline CELER_FUNCTION real_type debug_accumulated_total() const;
-};
-
-//---------------------------------------------------------------------------//
-/*!
- * Create a selector object from a function and total accumulated value.
- */
-template<class F, class T>
-CELER_FUNCTION Selector<F, T>
-make_selector(F&& func, T size, decltype(func(size)) total = 1)
-{
-    return {celeritas::forward<F>(func), size, total};
-}
-
-//---------------------------------------------------------------------------//
-// INLINE DEFINITIONS
-//---------------------------------------------------------------------------//
-/*!
- * Construct with function, size, and accumulated value.
- */
-template<class F, class T>
-CELER_FUNCTION
-Selector<F, T>::Selector(F&& eval, value_type size, real_type total)
-    : eval_{celeritas::forward<F>(eval)}, last_{size}, total_{total}
-{
-    CELER_EXPECT(last_ != IterT{});
-    CELER_EXPECT(total_ > 0);
-    CELER_EXPECT(
-        celeritas::soft_equal(this->debug_accumulated_total(), total_));
-
-    // Don't accumulate the last value except to assert that the 'total'
-    // isn't out-of-bounds
-    --last_;
+    using NormTag = detail::NormalizedSelectorTag;
+    return {celeritas::forward<F>(func), size, total, NormTag{}};
 }
 
 //---------------------------------------------------------------------------//
 /*!
- * Sample from the distribution.
+ * Create an unnormalized selector that can return \c size if past the end.
  */
 template<class F, class T>
-template<class Engine>
-CELER_FUNCTION T Selector<F, T>::operator()(Engine& rng) const
+CELER_FUNCTION detail::Selector<F, T>
+make_unnormalized_selector(F&& func, T size, real_type total)
 {
-    real_type accum = -total_ * generate_canonical(rng);
-    for (IterT iter{}; iter != last_; ++iter)
-    {
-        accum += eval_(*iter);
-        if (accum > 0)
-            return *iter;
-    }
-
-    return *last_;
-}
-
-//---------------------------------------------------------------------------//
-/*!
- * Accumulate total value for debug checking.
- *
- * This should *only* be used in the constructor before last_ is decremented.
- */
-template<class F, class T>
-CELER_FUNCTION auto Selector<F, T>::debug_accumulated_total() const
-    -> real_type
-{
-    real_type accum = 0;
-    for (IterT iter{}; iter != last_; ++iter)
-    {
-        accum += eval_(*iter);
-    }
-    return accum;
+    using NormTag = detail::UnnormalizedSelectorTag;
+    return {celeritas::forward<F>(func), size, total, NormTag{}};
 }
 
 //---------------------------------------------------------------------------//
