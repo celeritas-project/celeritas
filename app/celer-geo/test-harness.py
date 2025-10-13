b5e41750a55259fe8383580c298e87a4091dfbb1
Seth R Johnson <johnsonsr@ornl.gov>
2025-10-13 16:42:33 -0400

    Fix docstring

--- app/celer-geo/test-harness.py
+++ app/celer-geo/test-harness.py
@@ -3,7 +3,7 @@
 # Copyright 2020-2024 UT-Battelle, LLC, and other Celeritas developers.
 # See the top-level COPYRIGHT file for details.
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-""" """
+"""Run celer-geo as part of a CMake-based test harness."""
 
 import json
 import subprocess
