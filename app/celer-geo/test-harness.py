13f87e73b3fda0502e24327ba86d05a4d7214f74
Seth R Johnson <johnsonsr@ornl.gov>
2025-10-13 09:03:24 -0400

    Add testing of debug output

--- app/celer-geo/test-harness.py
+++ app/celer-geo/test-harness.py
@@ -3,8 +3,8 @@
 # Copyright 2020-2024 UT-Battelle, LLC, and other Celeritas developers.
 # See the top-level COPYRIGHT file for details.
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-"""
-"""
+""" """
+
 import json
 import subprocess
 from os import environ, getcwd
@@ -12,10 +12,16 @@ from sys import exit, argv, stderr
 from pathlib import Path
 
 try:
-    (model_file,) = argv[1:]
+    (model_path,) = argv[1:]
 except TypeError:
     print("usage: {} inp.gdml".format(argv[0]))
     exit(2)
+
+model_path = Path(model_path)
+exe = Path(environ.get("CELERITAS_EXE", "./celer-geo"))
+ext = environ.get("CELER_TEST_EXT", "unknown")
+
+problem_name = "-".join([model_path.stem, ext])
 
 def decode_line(jsonline):
     try:
@@ -26,13 +32,12 @@ def decode_line(jsonline):
         print("fatal:", str(e))
         exit(1)
 
-exe = environ.get("CELERITAS_EXE", "./celer-geo")
-ext = environ.get("CELER_TEST_EXT", "unknown")
 
-problem_name = "-".join([Path(model_file).stem, ext])
+def make_problem_path(ext):
+    return Path("".join([problem_name, ext]))
+
 
 image = {
-    "_units": "cgs",
     "_units": "cgs",
     "lower_left": [-800, 0, -1500],
     "upper_right": [800, 0, 1600],
@@ -40,9 +45,10 @@ image = {
     "vertical_pixels": 128,
 }
 
+
 commands = [
     {
-        "geometry_file": model_file,
+        "geometry_file": str(model_path),
     },
     {
         "_cmd": "orange_stats",
@@ -51,46 +57,76 @@ commands = [
         "_cmd": "trace",
         "image": image,
         "volumes": True,
-        "bin_file": f"{problem_name}.orange.bin",
+        "bin_file": str(make_problem_path(".orange.bin")),
     },
     {
         # Reuse image setup
         "_cmd": "trace",
-        "bin_file": f"{problem_name}.geant4.bin",
+        "bin_file": str(make_problem_path(".geant4.bin")),
         "geometry": "geant4",
     },
     {
         # DEPRECATED: omitting _cmd should work until v1.0
-        "bin_file": f"{problem_name}.vecgeom.bin",
+        "bin_file": str(make_problem_path(".vecgeom.bin")),
         "geometry": "vecgeom",
     },
 ]
 
 env = dict(environ)
+perfetto_path = None
 if env["CMAKE_BUILD_TYPE"].lower() == "release":
-    commands[0]["perfetto_file"] = "out.perfetto"
+    perfetto_path = make_problem_path(".out.perfetto")
+    perfetto_path.unlink(missing_ok=True)
+    commands[0]["perfetto_file"] = str(perfetto_path)
     env["CELER_ENABLE_PROFILING"] = "1"
 
-filename = f"{problem_name}.inp.jsonl"
-with open(filename, 'w') as f:
-    for c in commands:
-        json.dump(c, f)
-        f.write('\n')
+g4orgopt_filename = make_problem_path(".g4orgopt.json")
+g4orgopt = {}
+for k in ['csg', 'org', 'objects']:
+    path = make_problem_path(f".{k}.json")
+    path.unlink(missing_ok=True)
+    g4orgopt[f"{k}_output_file"] = path
 
-print("Running", exe, filename, "from", getcwd(), file=stderr)
-result = subprocess.run([exe, filename],
-                        stdout=subprocess.PIPE,
-                        env=env)
+with open(g4orgopt_filename, "w") as f:
+    json.dump({k: str(v) for k, v in g4orgopt.items()}, f)
+env["G4ORG_OPTIONS"] = str(g4orgopt_filename)
+env["CELER_LOG"] = "debug"
+
+inp_path = make_problem_path(".inp.jsonl")
+with open(inp_path, "w") as f:
+    for c in commands:
+        try:
+            json.dump(c, f)
+        except TypeError:
+            print(repr(c))
+            raise
+        f.write("\n")
+
+print("Running", exe, inp_path, "from", getcwd(), file=stderr)
+result = subprocess.run([exe, inp_path], stdout=subprocess.PIPE, env=env)
 if result.returncode:
     print("Run failed with error", result.returncode)
     exit(result.returncode)
 
 num_bytes = len(result.stdout)
-outname = Path(f'{problem_name}.out.jsonl')
-print(f"Received {num_bytes} bytes of data via stdin and echoed to {outname.absolute()}")
-with open(outname, 'wb') as f:
+outfile = make_problem_path(".out.jsonl")
+print(
+    f"Received {num_bytes} bytes of data via stdin and echoed to {outfile.absolute()}"
+)
+with open(outfile, "wb") as f:
     f.write(result.stdout)
 out_lines = result.stdout.decode().splitlines()
+
+# Check that the ORANGE output is valid
+g4org_debug_out = {}
+for k, outpath in g4orgopt.items():
+    assert outpath.exists(), outpath
+    with open(outpath) as f:
+        g4org_debug_out[k] = json.load(f)
+
+# Check that profiling information was written
+if perfetto_path is not None:
+    assert perfetto_path.exists()
 
 # Geometry diagnostic information
 print(decode_line(out_lines[0]))
@@ -103,5 +139,5 @@ for line in out_lines[1:-1]:
         print("Failure may be ok:", json.dumps(result, indent=1))
 
 summary = decode_line(out_lines[-1])
-summary.pop('runtime')
+summary.pop("runtime")
 print("Run succeeded:", json.dumps(summary, indent=0))
