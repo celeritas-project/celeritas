1e996a7d1fd202b544d48c57d46e1baf1b33fe20
Sakib Rahman <srahman1@bnl.gov>
2025-10-14 09:15:14 -0400

    Add more options within steering file to simplify usage

--- example/ddceler/steeringFile.py
+++ example/ddceler/steeringFile.py
@@ -4,13 +4,34 @@ Minimal proof of concept for DDG4 steering file to run Geant4 with celeritas int
 
 Usage:
 
-CELER_LOG=debug CELER_LOG_LOCAL=debug ddsim --action.run DDcelerRunAction --compactFile=$PWD/install/share/CeleritasDD4hep/celeritas-dd4hep.xml --steering steeringFile.py -N 500 -G --gun.particle e- --gun.etaMax 4 --gun.etaMin 1 --gun.energy 5*GeV --gun.distribution uniform --outputFile output.edm4hep.root --outputConfig.forceDD4HEP  --action.tracker "Geant4TrackerAction" --action.trackerSDTypes "tracker" --action.calorimeter "Geant4CalorimeterAction" --action.calorimeterSDTypes "calorimeter"
+ddsim --compactFile=$PWD/example/ddceler/SiD_ConstantField.xml --steering $PWD/example/ddceler/steeringFile.py --outputFile output.root
 
 """
 
 from DDSim.DD4hepSimulation import DD4hepSimulation
 
 RUNNER = DD4hepSimulation()
+
+# Action configuration
+RUNNER.action.run = "DDcelerRunAction"
+RUNNER.action.tracker = "Geant4TrackerAction"
+RUNNER.action.trackerSDTypes = ["tracker"]
+RUNNER.action.calo = "Geant4CalorimeterAction"
+RUNNER.action.calorimeterSDTypes = ["calorimeter"]
+
+# Output configuration
+RUNNER.outputConfig.forceDD4HEP = True
+
+# Number of events
+RUNNER.numberOfEvents = 20
+
+# Particle gun configuration
+RUNNER.enableGun = True
+RUNNER.gun.particle = "e-"
+RUNNER.gun.energy = "5*GeV"
+RUNNER.gun.distribution = "uniform"
+RUNNER.gun.etaMin = 1
+RUNNER.gun.etaMax = 2
 
 # Physics configuration
 def setupPhysics(kernel):
