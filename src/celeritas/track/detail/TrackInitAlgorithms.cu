7b9a78eb073649de16c1278497c71e8a4a527b9f
LSchwiebert <loren@wayne.edu>
2025-10-21 16:15:37 -0400

    Port some thrust function calls to cub functions for GPU-resident code

--- src/celeritas/track/detail/TrackInitAlgorithms.cu
+++ src/celeritas/track/detail/TrackInitAlgorithms.cu
@@ -6,11 +6,14 @@
 //---------------------------------------------------------------------------//
 #include "TrackInitAlgorithms.hh"
 
+#include <cub/cub.cuh>
 #include <thrust/device_ptr.h>
 #include <thrust/execution_policy.h>
 #include <thrust/partition.h>
-#include <thrust/remove.h>
-#include <thrust/scan.h>
+// #include <thrust/remove.h>
+// #include <thrust/scan.h>
+
+#include "corecel/DeviceRuntimeApi.hh"
 
 #include "corecel/Macros.hh"
 #include "corecel/data/ObserverPtr.device.hh"
@@ -31,21 +34,99 @@ namespace detail
  * Remove all elements in the vacancy vector that were flagged as active
  * tracks.
  */
+
+// Create a functor to omit the active tracks
+//
+struct NotEqual
+{
+    int compare_;
+
+    NotEqual(int compare) : compare_(compare) {}
+
+    CELER_FUNCTION bool operator()(TrackSlotId const& a) const noexcept
+    {
+        return (a.get() != compare_);
+    }
+};
+
 size_type remove_if_alive(
     StateCollection<TrackSlotId, Ownership::reference, MemSpace::device> const&
         vacancies,
     StreamId stream_id)
 {
     ScopedProfiling profile_this{"remove-if-alive"};
-    auto start = device_pointer_cast(vacancies.data());
-    auto end = thrust::remove_if(thrust_execute_on(stream_id),
-                                 start,
-                                 start + vacancies.size(),
-                                 LogicalNot{});
-    CELER_DEVICE_API_CALL(PeekAtLastError());
+    using StreamT = CELER_DEVICE_API_SYMBOL(Stream_t);
+    Stream& s = device().stream(stream_id);
+    StreamT stream = s.get();
+    // There should be a better way to initialize this function than the hard
+    // coded invalid value
+    NotEqual select_op(-1);
 
+    // The first call just computes the number of additional bytes needed for
+    // the in-place selection. The nullptr value causes this instead of running
+    // the function.
+    void* d_temp_storage = nullptr;
+    size_t temp_storage_bytes = 0;
+    // *** For testing with existing code for consistency
+    // *** Instead, we should place the result in the appropriate GPU variable
+    size_type* d_num_not_active = nullptr;
+    // *** For testing with existing code for consistency
+
+    // *** Allocate temporary storage
+    // *** For testing with existing code for consistency
+    d_num_not_active
+        = static_cast<size_type*>(s.malloc_async(sizeof(size_type)));
+    // *** For testing with existing code for consistency
+
+    // *** First call and memory allocation/deallocation for temp space should
+    // *** probably go in ExtendFromSecondariesAction.cu::begin_run()
+    // *** Put the exclusive sum allocation there, too?
+    cub::DeviceSelect::If(d_temp_storage,
+                          temp_storage_bytes,
+                          vacancies.data().get(),
+                          d_num_not_active,
+                          vacancies.size(),
+                          select_op,
+                          stream);
+
+    // Allocate temporary storage
+    d_temp_storage = s.malloc_async(temp_storage_bytes);
+
+    // Run selection
+    cub::DeviceSelect::If(d_temp_storage,
+                          temp_storage_bytes,
+                          vacancies.data().get(),
+                          d_num_not_active,
+                          vacancies.size(),
+                          select_op,
+                          stream);
+
+    // Deallocate temporary storage
+    s.free_async(d_temp_storage);
+
+    // *** For testing with existing code for consistency
+    // *** Replace with the num_vacancies counter
+    auto num = ItemCopier<size_type>{stream_id}(d_num_not_active);
+    s.free_async(d_num_not_active);
+
+    // *** For testing with existing code for consistency
+
+    CELER_DEVICE_API_CALL(PeekAtLastError());
     // New size of the vacancy vector
-    return end - start;
+    return num;
+    // ScopedProfiling profile_this{"remove-if-alive"};
+    // auto start = device_pointer_cast(vacancies.data());
+    // auto end = thrust::remove_if(thrust_execute_on(stream_id),
+    // start,
+    // start + vacancies.size(),
+    // LogicalNot{});
+    // CELER_DEVICE_API_CALL(PeekAtLastError());
+
+    // CELER_LOG(info) << "thrust found and released " << end - start << "
+    // active track slots";
+
+    // // New size of the vacancy vector
+    // return end - start;
 }
 
 //---------------------------------------------------------------------------//
@@ -65,16 +146,57 @@ size_type exclusive_scan_counts(
 {
     ScopedProfiling profile_this{"exclusive-scan-counts"};
     // Exclusive scan:
-    auto data = device_pointer_cast(counts.data());
-    auto stop = thrust::exclusive_scan(thrust_execute_on(stream_id),
-                                       data,
-                                       data + counts.size(),
-                                       data,
-                                       size_type(0));
-    CELER_DEVICE_API_CALL(PeekAtLastError());
+    // To Do:
+    // (1) Store the result in the GPU variable that needs the result
+    // (2) See (1) and change to void function
 
-    // Copy the last element (accumulated total) back to host
-    return ItemCopier<size_type>{stream_id}(stop.get() - 1);
+    // The first call just computes the number of additional bytes needed for
+    // the in-place exclusive scan. The nullptr value causes this instead of
+    // running the function
+
+    using StreamT = CELER_DEVICE_API_SYMBOL(Stream_t);
+    Stream& s = device().stream(stream_id);
+    StreamT stream = s.get();
+
+    void* d_temp_storage = nullptr;
+    size_t temp_storage_bytes = 0;
+    cub::DeviceScan::ExclusiveSum(d_temp_storage,
+                                  temp_storage_bytes,
+                                  counts.data().get(),
+                                  counts.size(),
+                                  stream);
+
+    // Allocate temporary storage
+    d_temp_storage = s.malloc_async(temp_storage_bytes);
+
+    // Run exclusive prefix sum
+    cub::DeviceScan::ExclusiveSum(d_temp_storage,
+                                  temp_storage_bytes,
+                                  counts.data().get(),
+                                  counts.size(),
+                                  stream);
+
+    // Deallocate temporary storage
+    s.free_async(d_temp_storage);
+
+    // Set the counter similar to the following
+    // counters.num_secondaries = ItemCopier<size_type>{stream_id}...;
+    CELER_DEVICE_API_CALL(PeekAtLastError());
+    return ItemCopier<size_type>{stream_id}(counts.data().get() + counts.size()
+                                            - 1);
+
+    // ScopedProfiling profile_this{"exclusive-scan-counts"};
+    // // Exclusive scan:
+    // auto data = device_pointer_cast(counts.data());
+    // auto stop = thrust::exclusive_scan(thrust_execute_on(stream_id),
+    // data,
+    // data + counts.size(),
+    // data,
+    // size_type(0));
+    // CELER_DEVICE_API_CALL(PeekAtLastError());
+
+    // // Copy the last element (accumulated total) back to host
+    // return ItemCopier<size_type>{stream_id}(stop.get() - 1);
 }
 
 //---------------------------------------------------------------------------//
