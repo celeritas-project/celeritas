9cdd7badc0fbc52429fbefae2faeb5449720cfe4
Seth R Johnson <johnsonsr@ornl.gov>
2025-10-22 04:12:13 -0400

    Format python files

--- test/orange/data/field-layers.org.py
+++ test/orange/data/field-layers.org.py
@@ -26,41 +26,47 @@ The geometry looks like this (with 5 boxes, shortened for clarity)::
 """
 
 layer_box = {
-    '_type': 'box',
-    'widths': [18, 1, 18],
+    "_type": "box",
+    "widths": [18, 1, 18],
 }
 
 shapes = []
 cells = []
-for (i, ymid) in enumerate([-4, -2, 0, 2, 4]):
+for i, ymid in enumerate([-4, -2, 0, 2, 4]):
     box = dict(layer_box)
-    box['translate'] = [0, ymid, 0]
-    box['name'] = name = f"layerbox{i}"
+    box["translate"] = [0, ymid, 0]
+    box["name"] = name = f"layerbox{i}"
     shapes.append(box)
-    cells.append({
-        'name': f"layer{i}",
-        'comp': "1",
-        'shapes': [name],
-    })
+    cells.append(
+        {
+            "name": f"layer{i}",
+            "comp": "1",
+            "shapes": [name],
+        }
+    )
 
-shapes.append({
-    '_type': "box",
-    'widths': [20, 40, 20],
-    'name': "worldbox",
-})
+shapes.append(
+    {
+        "_type": "box",
+        "widths": [20, 40, 20],
+        "name": "worldbox",
+    }
+)
 
 world_univ = {
-    '_type': 'unit',
-    'name': "world",
-    'shape': shapes,
-    'cell': cells,
-    'interior': "worldbox",
-    'background': "0", # Fill unassigned space with matid 0
+    "_type": "unit",
+    "name": "world",
+    "shape": shapes,
+    "cell": cells,
+    "interior": "worldbox",
+    "background": "0",  # Fill unassigned space with matid 0
 }
 
-db.update({
-    'geometry': {
-        'global': "world",
-    },
-    'universe': [world_univ]
-})
+db.update(
+    {
+        "geometry": {
+            "global": "world",
+        },
+        "universe": [world_univ],
+    }
+)
