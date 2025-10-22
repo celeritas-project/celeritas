991c8a11ff9007cc21e142067a61f43d3af8049e
Seth R Johnson <johnsonsr@ornl.gov>
2025-10-22 05:46:22 -0400

    Fix header docs

--- app/celer-sim/simple-driver.py
+++ app/celer-sim/simple-driver.py
@@ -3,7 +3,7 @@
 # Copyright 2021-2024 UT-Battelle, LLC, and other Celeritas developers.
 # See the top-level COPYRIGHT file for details.
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-""" """
+"""Test harness for celer-sim for use in CMake."""
 
 import json
 import re
