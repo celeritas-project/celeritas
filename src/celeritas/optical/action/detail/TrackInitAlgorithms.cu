05661a9d0250e1e9171a88e2af2b6a1448c468e7
LSchwiebert <loren@wayne.edu>
2025-11-07 14:17:49 -0500

    Check for hipCUB and build accordingly

--- src/celeritas/optical/action/detail/TrackInitAlgorithms.cu
+++ src/celeritas/optical/action/detail/TrackInitAlgorithms.cu
@@ -6,19 +6,46 @@
 //---------------------------------------------------------------------------//
 #include "TrackInitAlgorithms.hh"
 
+// CUDA has included cub since CUDA 11, but ROCm does not include hipCUB by
+// default, so test for the availability of hipCUB and use thrust instead if
+// it's unavailable. And some further checks for newer cub/hipCUB functions.
+#if CELER_USE_HIP && !CELERITAS_HAVE_HIPCUB
+#    define CELER_USE_THRUST 1
+#else
+#    define CELER_USE_THRUST 0
+#endif
 #if CELERITAS_USE_CUDA
 #    include <cub/device/device_select.cuh>
+#    include <cub/version.cuh>
+#elif CELERITAS_USE_HIP && CELERITAS_HAVE_HIPCUB
+#    include <hipcub/device/device_select.hpp>
+#    include <hipcub/hipcub_version.hpp>
+#endif
+// DeviceTransform is unavailable in older versions of cub/hipcub, so fall back
+// to using thrust::transform instead
+#if CELERITAS_USE_CUDA && CUB_VERSION >= 200800
+#    define CELER_CUB_HAS_TRANSFORM 1
+#else
+#    define CELER_CUB_HAS_TRANSFORM 0
+#endif
+#if CELERITAS_USE_HIP && HIPCUB_VERSION >= 400100
+#    define CELER_HIPCUB_HAS_TRANSFORM 1
+#else
+#    define CELER_HIPCUB_HAS_TRANSFORM 0
+#endif
+#if CELER_CUB_HAS_TRANSFORM
+#    include <cub/device/device_transform.cuh>
+#elif CELER_HIPCUB_HAS_TRANSFORM
+#    include <hipcub/device/device_transform.hpp>
+#else
+#    include <thrust/execution_policy.h>
+#    include <thrust/transform.h>
+#endif
+#include <thrust/device_ptr.h>
+#if CELER_USE_THRUST
+#    include <thrust/copy.h>
 #    include <thrust/iterator/counting_iterator.h>
 #    include <thrust/iterator/transform_iterator.h>
-#elif CELERITAS_USE_HIP
-#    include <hipcub/device/device_select.hpp>
-#    if HIPCUB_VERSION >= 400100
-#        include <hipcub/device/device_transform.hpp>
-#        include <thrust/execution_policy.h>
-#    else
-#        include <thrust/transform.h>
-#    endif
-#    include <thrust/device_ptr.h>
 #endif
 
 #include "corecel/Macros.hh"
@@ -29,10 +56,8 @@
 #include "corecel/sys/Stream.hh"
 #include "corecel/sys/Thrust.device.hh"
 
-#if CELERITAS_USE_CUDA
-using namespace cub;
-#elif CELERITAS_USE_HIP
-using namespace hipcub;
+#if CELERITAS_USE_HIP
+namespace cub = hipcub;
 #endif
 
 namespace celeritas
@@ -41,6 +66,7 @@ namespace optical
 {
 namespace detail
 {
+#if CELER_USE_THRUST
 namespace
 {
 //---------------------------------------------------------------------------//
@@ -51,9 +77,9 @@ struct TransformType
         return TrackSlotId{i};
     }
 };
-
 //---------------------------------------------------------------------------//
 }  // namespace
+#endif
 
 //---------------------------------------------------------------------------//
 /*!
@@ -68,57 +94,33 @@ size_type copy_if_vacant(TrackStatusRef<MemSpace::device> const& status,
     CELER_EXPECT(status.size() == vacancies.size());
 
     ScopedProfiling profile_this{"copy-if-vacant"};
+#if CELER_USE_THRUST
+    auto start = thrust::make_transform_iterator(
+        thrust::make_counting_iterator<size_type>(0), TransformType{});
+    auto result = device_pointer_cast(vacancies.data());
+    auto end = thrust::copy_if(thrust_execute_on(stream_id),
+                               start,
+                               start + vacancies.size(),
+                               device_pointer_cast(status.data()),
+                               result,
+                               IsVacant{});
+    CELER_DEVICE_API_CALL(PeekAtLastError());
 
+    return end - result;
+#else
     // cub functions expect a cudaStream_t pointer for the stream
     using StreamT = CELER_DEVICE_API_SYMBOL(Stream_t);
     StreamT stream = device().stream(stream_id).get();
 
-    // *** For testing with existing code for consistency
     DeviceVector<size_type> num_vacancies{1, stream_id};
-    // *** For testing with existing code for consistency
 
-    // Calling with nullptr causes the function to return the amount of working
-    // space needed instead of invoking the kernel.
-    size_t temp_storage_bytes = 0;
-#if CELERITAS_USE_CUDA
-    auto start = thrust::make_transform_iterator(
-        thrust::make_counting_iterator<size_type>(0), TransformType{});
-    auto flags = device_pointer_cast(status.data());
-    auto result = device_pointer_cast(vacancies.data());
-    DeviceSelect::FlaggedIf(nullptr,
-                            temp_storage_bytes,
-                            start,
-                            flags,
-                            result,
-                            num_vacancies.data(),
-                            vacancies.size(),
-                            IsVacant{},
-                            stream);
-
-    // Allocate temporary storage
-    DeviceAllocation temp_storage(temp_storage_bytes, stream_id);
-
-    DeviceSelect::FlaggedIf(temp_storage.data(),
-                            temp_storage_bytes,
-                            start,
-                            flags,
-                            result,
-                            num_vacancies.data(),
-                            vacancies.size(),
-                            IsVacant{},
-                            stream);
-#elif CELERITAS_USE_HIP
     auto start = device_pointer_cast(status.data());
     DeviceVector<unsigned char> flags{status.size(), stream_id};
-    auto data = device_pointer_cast(vacancies.data());
-#    if CELERITAS_USE_HIP && HIPCUB_VERSION >= 400100
+#    if CELER_CUB_HAS_TRANSFORM || CELER_HIPCUB_HAS_TRANSFORM
+    // HIP defines hipCUB functions as [[nodiscard]], but we defer error checks
     {
-        auto cub_error_code = DeviceTransform::Transform(
+        auto cub_error_code = cub::DeviceTransform::Transform(
             start, flags.data(), status.size(), IsVacant{}, stream);
-        // HIP is particular about checking return codes from hipCUB functions,
-        // so check for an error from the call and proceed accordingly
-        if (cub_error_code)
-            CELER_DEVICE_API_CALL(PeekAtLastError());
     }
 #    else
     thrust::transform(thrust_execute_on(stream_id),
@@ -127,7 +129,20 @@ size_type copy_if_vacant(TrackStatusRef<MemSpace::device> const& status,
                       flags.data(),
                       IsVacant{});
 #    endif
-    auto cub_error_code = DeviceSelect::Flagged(nullptr,
+    // Calling with nullptr causes the function to return the amount of working
+    // space needed instead of invoking the kernel.
+    size_t temp_storage_bytes = 0;
+    auto data = device_pointer_cast(vacancies.data());
+    auto cub_error_code = cub::DeviceSelect::Flagged(nullptr,
+                                                     temp_storage_bytes,
+                                                     data,
+                                                     flags.data(),
+                                                     num_vacancies.data(),
+                                                     vacancies.size(),
+                                                     stream);
+    // Allocate temporary storage
+    DeviceAllocation temp_storage(temp_storage_bytes, stream_id);
+    cub_error_code = cub::DeviceSelect::Flagged(temp_storage.data(),
                                                 temp_storage_bytes,
                                                 data,
                                                 flags.data(),
@@ -135,35 +150,11 @@ size_type copy_if_vacant(TrackStatusRef<MemSpace::device> const& status,
                                                 vacancies.size(),
                                                 stream);
 
-    // HIP is particular about checking return codes from hipCUB functions, so
-    // check for an error from the call and proceed accordingly
-    if (cub_error_code)
-        CELER_DEVICE_API_CALL(PeekAtLastError());
-
-    // Allocate temporary storage
-    DeviceAllocation temp_storage(temp_storage_bytes, stream_id);
-
-    cub_error_code = DeviceSelect::Flagged(temp_storage.data(),
-                                           temp_storage_bytes,
-                                           data,
-                                           flags.data(),
-                                           num_vacancies.data(),
-                                           vacancies.size(),
-                                           stream);
-
-    // HIP is particular about checking return codes from hipCUB functions, so
-    // check for an error from the call and proceed accordingly
-    if (cub_error_code)
-        CELER_DEVICE_API_CALL(PeekAtLastError());
-#endif
-    // *** For testing with existing code for consistency
-    // *** Replace with the appropriate GPU counter
     auto num = ItemCopier<size_type>{stream_id}(num_vacancies.data());
-    // *** For testing with existing code for consistency
-
     CELER_DEVICE_API_CALL(PeekAtLastError());
     // Number of vacancies
     return num;
+#endif
 }
 
 //---------------------------------------------------------------------------//
