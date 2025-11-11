f69e1e3a4f08f01c9fd82ffebb4da19911ef0916
Seth R Johnson <johnsonsr@ornl.gov>
2025-11-11 18:38:41 -0500

    Split 'version' into setup/runtime

---
+++ src/geocel/vg/detail/VecgeomRuntime.hh
@@ -0,0 +1,23 @@
+//------------------------------- -*- C++ -*- -------------------------------//
+// Copyright Celeritas contributors: see top-level COPYRIGHT file for details
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file geocel/vg/detail/VecgeomRuntime.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include <VecGeom/navigation/NavStateFwd.h>
+
+#include "VecgeomTypes.hh"
+
+namespace celeritas
+{
+namespace detail
+{
+//---------------------------------------------------------------------------//
+
+using NavIndex_t = VECGEOM_PRECISION_NAMESPACE::NavIndex_t;
+
+//---------------------------------------------------------------------------//
+}  // namespace detail
+}  // namespace celeritas
