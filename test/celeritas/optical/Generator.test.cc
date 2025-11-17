ec427461d68c65e50ada02fc7e85b7bbe127b7e0
Hayden Hollenbeck <hollenbeck.hayden@gmail.com>
2025-11-17 11:34:07 -0600

    Set direct generator to use baseline comparison variable

--- test/celeritas/optical/Generator.test.cc
+++ test/celeritas/optical/Generator.test.cc
@@ -194,8 +194,7 @@ TEST_F(LArSphereGeneratorTest, direct_generator)
 
     // Get the accumulated counters
     auto result = this->counters(*generate);
-    if (CELERITAS_CORE_GEO != CELERITAS_CORE_GEO_GEANT4
-        && CELERITAS_REAL_TYPE == CELERITAS_REAL_TYPE_DOUBLE)
+    if (reference_configuration)
     {
         EXPECT_EQ(133, result.steps);
         EXPECT_EQ(5, result.step_iters);
