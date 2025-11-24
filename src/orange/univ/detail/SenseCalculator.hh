f91ef1de128eb3bb07d6fbab913bd72e2a884158
Julien Esseiva <jesseiva@lbl.gov>
2025-11-24 15:07:04 -0800

    Rename SenseCalculator, update doc

--- src/orange/univ/detail/SenseCalculator.hh
+++ src/orange/univ/detail/SenseCalculator.hh
@@ -2,7 +2,7 @@
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file orange/univ/detail/LazySenseCalculator.hh
+//! \file orange/univ/detail/SenseCalculator.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
@@ -28,22 +28,23 @@ namespace detail
  * simple *and* complex intersection. Instances of this class are specific to a
  * volume, and a position. Calling an instance evaluates the sense of a
  * volume's face with respect to the given position. This class is used to
- * lazily calculate sense during evaluation of a logic expression, contrary to
- * CachedLazySenseCalculator, this class does not cache the calculated sense:
- * potentially recomputing the same sense value multiple time. The advantage
- * is that we do not need to access global memory to store the cached sense.
+ * lazily calculate sense during evaluation of a logic expression.
+ * This class does not cache the calculated sense, that is senses are
+ * recomputed every time it is requested. The advantage of not caching senses
+ * is that we eliminate global memory usage. With infix evaluation, and
+ * short-circuiting logic, the cost of recomputing senses should be small.
  *
  * The OnFace constructor's parameter is used to store the first face that we
  * are "on".
  */
-class LazySenseCalculator
+class SenseCalculator
 {
   public:
     // Construct from persistent, current, and temporary data
-    inline CELER_FUNCTION LazySenseCalculator(LocalSurfaceVisitor const& visit,
-                                              VolumeView const& vol,
-                                              Real3 const& pos,
-                                              OnFace& face);
+    inline CELER_FUNCTION SenseCalculator(LocalSurfaceVisitor const& visit,
+                                          VolumeView const& vol,
+                                          Real3 const& pos,
+                                          OnFace& face);
 
     // Calculate senses for a single face of the given volume, possibly on a
     // face
@@ -70,10 +71,10 @@ class LazySenseCalculator
  * Construct from persistent, current, and temporary data.
  */
 CELER_FUNCTION
-LazySenseCalculator::LazySenseCalculator(LocalSurfaceVisitor const& visit,
-                                         VolumeView const& vol,
-                                         Real3 const& pos,
-                                         OnFace& face)
+SenseCalculator::SenseCalculator(LocalSurfaceVisitor const& visit,
+                                 VolumeView const& vol,
+                                 Real3 const& pos,
+                                 OnFace& face)
     : visit_{visit}, vol_{vol}, pos_{pos}, face_{face}
 {
 }
@@ -85,7 +86,7 @@ LazySenseCalculator::LazySenseCalculator(LocalSurfaceVisitor const& visit,
  * If the point is exactly on one of the volume's surfaces, the \c face
  * reference passed during instance construction will be set.
  */
-CELER_FUNCTION auto LazySenseCalculator::operator()(FaceId face_id) -> Sense
+CELER_FUNCTION auto SenseCalculator::operator()(FaceId face_id) -> Sense
 {
     CELER_EXPECT(face_id < vol_.num_faces());
     CELER_EXPECT(!face_ || face_.id() < vol_.num_faces());
