3f2543927dc155a28d4f4b5e78ff2c3b8cbe3231
Seth R Johnson <johnsonsr@ornl.gov>
2025-11-30 19:34:00 -0500

    Fix doc

--- src/orange/OrangeInput.hh
+++ src/orange/OrangeInput.hh
@@ -9,7 +9,6 @@
  * \todo This is a weird mix of input and built objects so we can't put it in
  * \c inp . See discussion at
  * https://github.com/celeritas-project/celeritas/pull/2045#discussion_r2437457681
- * .
  */
 //---------------------------------------------------------------------------//
 #pragma once
