6a3c7057a6b0779888158cd83590dd3bdc6e6d28
Seth R Johnson <johnsonsr@ornl.gov>
2025-12-03 14:56:46 -0500

    Only include CUDA RDC utils when using CUDA-enabled VecGeom

--- cmake/CeleritasLibrary.cmake
+++ cmake/CeleritasLibrary.cmake
@@ -49,7 +49,12 @@ if(NOT DEFINED CELERITAS_USE_VecGeom)
   )
 endif()
 
-if(NOT (CELERITAS_USE_VecGeom AND CELERITAS_USE_CUDA))
+if(CELERITAS_USE_VecGeom AND CELERITAS_USE_CUDA AND NOT COMMAND cuda_rdc_add_library)
+  message(FATAL_ERROR "CudaRdcUtils is required but missing")
+endif()
+
+
+if(NOT COMMAND cuda_rdc_add_library)
   # Forward all arguments directly to CMake builtins
   macro(celeritas_add_library)
     add_library(${ARGV})
@@ -70,10 +75,6 @@ if(NOT (CELERITAS_USE_VecGeom AND CELERITAS_USE_CUDA))
     target_compile_options(${ARGV})
   endmacro()
 else()
-  if(NOT COMMAND cuda_rdc_add_library)
-    message(FATAL_ERROR "This file MUST be used with CudaRdcUtils")
-  endif()
-
   # Forward all arguments to RDC utility wrappers
   macro(celeritas_add_library)
     cuda_rdc_add_library(${ARGV})
