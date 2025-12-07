f0430fefaee21cd7dd3c9d54fc12aba4d0c6a455
Amanda Lund <alund@anl.gov>
2025-12-07 20:51:42 +0000

    REVERTME: Add log output for debugging

--- src/celeritas/global/CoreState.hh
+++ src/celeritas/global/CoreState.hh
@@ -16,6 +16,7 @@
 #include "corecel/data/CollectionStateStore.hh"
 #include "corecel/data/DeviceVector.hh"
 #include "corecel/data/Ref.hh"
+#include "corecel/io/Logger.hh"
 #include "corecel/sys/ThreadId.hh"
 #include "celeritas/track/CoreStateCounters.hh"
 
@@ -202,6 +203,8 @@ auto CoreState<M>::aux_data(AuxId auxid) -> StateRef<S>&
     CELER_EXPECT(auxid < aux_state_->size());
 
     // TODO: use "checked static cast" for better runtime performance
+    CELER_LOG(info) << "Accessing aux state with type "
+                    << TypeDemangler<S<Ownership::reference, M>>{}();
     auto* state = dynamic_cast<AuxState<S, M>*>(&aux_state_->at(auxid));
     CELER_ASSERT(state);
 
