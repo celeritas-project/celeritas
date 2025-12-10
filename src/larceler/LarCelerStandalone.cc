51794c183dd0a8e1e319a9f5e7c5cc28741a9875
Seth R Johnson <johnsonsr@ornl.gov>
2025-12-10 06:48:37 -0600

    Try to use the interface

--- src/larceler/LarCelerStandalone.cc
+++ src/larceler/LarCelerStandalone.cc
@@ -2,15 +2,22 @@
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file larceler/empty.cc
+//! \file larceler/LarCelerStandalone.cc
 //---------------------------------------------------------------------------//
+
+#include "LarCelerStandalone.hh"
+
+#include <art/Utilities/ToolMacros.h>
+#include <fhiclcpp/ParameterSet.h>
+#include <lardataobj/Simulation/OpDetBacktrackerRecord.h>
+#include <lardataobj/Simulation/SimEnergyDeposit.h>
 
 namespace celeritas
 {
 //---------------------------------------------------------------------------//
-int larsoft()
-{
-    return 1;
-}
+LarCelerStandalone::LarCelerStandalone(fhicl::ParameterSet const&) {}
+
 //---------------------------------------------------------------------------//
 }  // namespace celeritas
+
+DEFINE_ART_CLASS_TOOL(celeritas::LarCelerStandalone)
---
+++ src/larceler/LarCelerStandalone.hh
@@ -0,0 +1,45 @@
+//------------------------------- -*- C++ -*- -------------------------------//
+// Copyright Celeritas contributors: see top-level COPYRIGHT file for details
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file larceler/LarCelerStandalone.hh
+//---------------------------------------------------------------------------//
+#pragma once
+
+#include <memory>
+#include <vector>
+
+namespace fhicl
+{
+class ParameterSet;
+}
+namespace sim
+{
+class SimEnergyDeposit;
+class OpDetBacktrackerRecord;
+}  // namespace sim
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Run optical photons in a standalone simulation.
+ */
+class LarCelerStandalone
+{
+  public:
+    //!@{
+    //! \name Type aliases
+    using VecSED = std::vector<sim::SimEnergyDeposit>;
+    using UPVecBTR = std::unique_ptr<std::vector<sim::OpDetBacktrackerRecord>>;
+    ///@}
+
+    // Construct with fcl parameters
+    LarCelerStandalone(fhicl::ParameterSet const& p);
+
+    // Execute simulation
+    UPVecBTR execute(VecSED const& edeps) final;
+};
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
