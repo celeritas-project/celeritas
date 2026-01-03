4738886c306e399949b6bd29e1e778ad9a5570e4
Seth R Johnson <johnsonsr@ornl.gov>
2026-01-03 06:21:12 -0500

    Fix InitializedValue semantics

--- src/corecel/cont/InitializedValue.hh
+++ src/corecel/cont/InitializedValue.hh
@@ -9,6 +9,8 @@
 #include <type_traits>
 #include <utility>
 
+#include "corecel/Macros.hh"
+
 namespace celeritas
 {
 namespace detail
@@ -17,7 +19,7 @@ namespace detail
 template<class T>
 struct DefaultFinalize
 {
-    void operator()(T&) const noexcept {}
+    CELER_FORCEINLINE void operator()(T&) const noexcept {}
 };
 //---------------------------------------------------------------------------//
 }  // namespace detail
@@ -30,14 +32,14 @@ struct DefaultFinalize
  * to treat one member data specially but can use default assign/construct for
  * the other elements. The default behavior is just to default-initialize when
  * assigning and clearing the RHS when moving; this is useful for handling
- * managed memory. The *finalizer* is useful when the type has a
- * destructor-type method that has to be called before clearing it.
+ * managed memory. The \em finalizer is called every time a value is lost by
+ * assignment or destruction.
  */
 template<class T, class Finalizer = detail::DefaultFinalize<T>>
 class InitializedValue
 {
   private:
-    static constexpr bool ne_finalize_
+    static constexpr bool noexcept_finalize_
         = noexcept(std::declval<Finalizer>()(std::declval<T&>()));
 
   public:
@@ -48,18 +50,8 @@ class InitializedValue
     InitializedValue() = default;
     //! Implicit construct from lvalue
     InitializedValue(T const& value) : value_(value) {}
-    //! Implicit construct from lvalue and finalizer
-    InitializedValue(T const& value, Finalizer fin)
-        : value_(value), fin_(std::move(fin))
-    {
-    }
     //! Implicit construct from rvalue
     InitializedValue(T&& value) : value_(std::move(value)) {}
-    //! Implicit construct from value type and finalizer
-    InitializedValue(T&& value, Finalizer fin)
-        : value_(std::move(value)), fin_(std::move(fin))
-    {
-    }
 
     //! Default copy constructor
     InitializedValue(InitializedValue const&) noexcept(
@@ -73,7 +65,13 @@ class InitializedValue
     {
     }
 
-    ~InitializedValue() = default;
+    ~InitializedValue() noexcept(noexcept_finalize_)
+    {
+        if (value_ != T{})
+        {
+            Finalizer{}(value_);
+        }
+    }
 
     //!@}
     //!@{
@@ -81,40 +79,26 @@ class InitializedValue
 
     //! Finalize our value when assigning
     InitializedValue& operator=(InitializedValue const& other) noexcept(
-        ne_finalize_ && std::is_nothrow_copy_assignable_v<T>)
+        noexcept_finalize_ && std::is_nothrow_copy_assignable_v<T>)
     {
-        fin_(value_);
+        if (value_ != T{})
+        {
+            Finalizer{}(value_);
+        }
         value_ = other.value_;
-        fin_ = other.fin_;
         return *this;
     }
 
     //! Clear other value on move assign
     InitializedValue& operator=(InitializedValue&& other) noexcept(
-        ne_finalize_ && std::is_nothrow_move_assignable_v<T>)
+        noexcept_finalize_ && std::is_nothrow_move_assignable_v<T>)
     {
-        fin_(value_);
-        value_ = std::exchange(other.value_, {});
-        fin_ = std::exchange(other.fin_, {});
+        if (value_ != T{})
+        {
+            Finalizer{}(value_);
+        }
+        value_ = std::exchange(other.value_, T{});
         return *this;
-    }
-
-    //! Implicit assign from type
-    InitializedValue&
-    operator=(T const& value) noexcept(ne_finalize_
-                                       && std::is_nothrow_copy_assignable_v<T>)
-    {
-        fin_(value_);
-        value_ = value;
-        return *this;
-    }
-
-    //! Swap with another value
-    void swap(InitializedValue& other) noexcept
-    {
-        using std::swap;
-        swap(other.value_, value_);
-        swap(other.fin_, fin_);
     }
 
     //!@}
@@ -128,19 +112,11 @@ class InitializedValue
     //! Explicit reference to stored value
     T const& value() const& { return value_; }
     T& value() & { return value_; }
-    T&& value() && { return value_; }
 
-    //!@}
-
-    //!@{
-    //! Access finalizer
-    Finalizer const& finalizer() const { return fin_; }
-    void finalizer(Finalizer fin) { fin_ = std::move(fin); }
     //!@}
 
   private:
     T value_{};
-    Finalizer fin_{};  // TODO: if C++20, [[no_unique_address]]
 };
 
 //---------------------------------------------------------------------------//
