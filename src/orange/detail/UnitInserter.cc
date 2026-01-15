dabc7ca113beeefcf267eadee7151499e433ffa6
Julien Esseiva <jesseiva@lbl.gov>
2026-01-15 15:25:49 -0800

    fix nowherelogic

--- src/orange/detail/UnitInserter.cc
+++ src/orange/detail/UnitInserter.cc
@@ -574,7 +574,12 @@ VolumeRecord UnitInserter::insert_volume(SurfacesRecord const& surf_record,
         }
     }
 
-    static logic_int const nowhere_logic[] = {logic::ltrue, logic::lnot};
+    static auto const nowhere_logic = []() -> std::array<logic_int, 2> {
+        if (orange_tracking_logic() == LogicNotation::postfix)
+            return {logic::ltrue, logic::lnot};
+        else
+            return {logic::lnot, logic::ltrue};
+    }();
 
     auto input_logic = make_span(v.logic);
     if (v.zorder == ZOrder::background)
