01113670db44039dcba7d01232db0fb2937129aa
LSchwiebert <loren@wayne.edu>
2026-01-19 22:18:20 -0500

    Remove unnecessary header to resolve ROCm compiler issue

--- src/celeritas/global/CoreState.cc
+++ src/celeritas/global/CoreState.cc
@@ -6,9 +6,6 @@
 //---------------------------------------------------------------------------//
 #include "CoreState.hh"
 
-#if CELER_USE_DEVICE
-#    include "corecel/data/ObserverPtr.device.hh"
-#endif
 #include "corecel/io/Logger.hh"
 #include "corecel/sys/ActionRegistry.hh"
 #include "corecel/sys/ScopedProfiling.hh"
