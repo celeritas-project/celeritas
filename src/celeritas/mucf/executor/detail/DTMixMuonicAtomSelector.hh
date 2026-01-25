ea2244fe289f1d79eef1f3358ddb34452aecbc37
Stefano Tognini <togninis@ornl.gov>
2026-01-25 15:37:10 -0500

    Draft muonic atom selection

--- src/celeritas/mucf/executor/detail/DTMixMuonicAtomSelector.hh
+++ src/celeritas/mucf/executor/detail/DTMixMuonicAtomSelector.hh
@@ -7,7 +7,9 @@
 #pragma once
 
 #include "corecel/cont/EnumArray.hh"
+#include "corecel/random/distribution/GenerateCanonical.hh"
 #include "celeritas/Types.hh"
+#include "celeritas/mucf/Types.hh"
 
 namespace celeritas
 {
@@ -16,41 +18,77 @@ namespace detail
 //---------------------------------------------------------------------------//
 /*!
  * Select a muonic atom given the material information.
+ *
+ * This class uses the \f$ q_\text{1S} \f$ formula [ \todo
+ * https://doi.org/10.1134/1.1926428 ]
+ * \f[
+ * q_\text{1s} = \frac{1}{1 + 2.9 C_t},
+ * \f]
+ * where \f$ C_t \f$  is the relative tritium isotope concentration and
+ * \f$ q_\text{1s} \f$ is the fraction muonic deuterium atoms in the ground
+ * state. This expression allows calculating the probability of forming a
+ * muonic deuterium or tritium atom via
+ * \f[
+ * P_\text{d} = C_d * q\text{1s}.
+ * \f]
+ *
+ * If a selected uniform random number is \f$ x \leq P_\text{d} \f$, a muonic
+ * deuterium is formed. Otherwside, a muonic tritium is selected.
  */
 class DTMixMuonicAtomSelector
 {
   public:
-    //! Construct with args; \todo Update documentation
-    inline CELER_FUNCTION DTMixMuonicAtomSelector(/* args */);
+    //! Construct with material information
+    inline CELER_FUNCTION DTMixMuonicAtomSelector(real_type deuterium_fraction,
+                                                  real_type tritium_fraction);
 
     // Select muonic atom
     template<class Engine>
-    inline CELER_FUNCTION MucfMuonicAtom operator()(Engine&);
+    inline CELER_FUNCTION MucfMuonicAtom operator()(Engine& rng);
+
+  private:
+    real_type deuterium_fraction_;
+    real_type tritium_fraction_;
 };
 
 //---------------------------------------------------------------------------//
 // INLINE DEFINITIONS
 //---------------------------------------------------------------------------//
 /*!
- * Construct with args.
-
- * \todo Update documentation
+ * Construct with material information.
  */
-CELER_FUNCTION DTMixMuonicAtomSelector::DTMixMuonicAtomSelector(/* args */)
+CELER_FUNCTION
+DTMixMuonicAtomSelector::DTMixMuonicAtomSelector(real_type deuterium_fraction,
+                                                 real_type tritium_fraction)
+    : deuterium_fraction_(deuterium_fraction)
+    , tritium_fraction_(tritium_fraction)
 {
-    CELER_NOT_IMPLEMENTED("Mucf muonic atom selection");
+    CELER_EXPECT(deuterium_fraction_ >= 0);
+    CELER_EXPECT(tritium_fraction_ >= 0);
+    CELER_EXPECT(deuterium_fraction_ + tritium_fraction_ > 0);
+    CELER_EXPECT(deuterium_fraction_ + tritium_fraction_ <= 1);
 }
 
 //---------------------------------------------------------------------------//
 /*!
- * Return selected muonic atom.
+ * Select a muonic atom.
  */
 template<class Engine>
-CELER_FUNCTION MucfMuonicAtom DTMixMuonicAtomSelector::operator()(Engine&)
+CELER_FUNCTION MucfMuonicAtom DTMixMuonicAtomSelector::operator()(Engine& rng)
 {
     MucfMuonicAtom result{MucfMuonicAtom::size_};
 
-    //! \todo Implement
+    real_type const q1s
+        = real_type{1} / (real_type{1} + real_type{2.9} * tritium_fraction_);
+    real_type const deuterium_probability = deuterium_fraction_ * q1s;
+    if (generate_canonical(rng) <= deuterium_probability)
+    {
+        result = MucfMuonicAtom::deuterium;
+    }
+    else
+    {
+        result = MucfMuonicAtom::tritium;
+    }
 
     CELER_ENSURE(result < MucfMuonicAtom::size_);
     return result;
