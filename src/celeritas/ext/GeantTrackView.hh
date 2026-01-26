c532b5110b028e47006d8c12e3852a4edc46034e
Seth R Johnson <johnsonsr@ornl.gov>
2026-01-26 14:55:25 -0500

    Add mutable geant track view

--- src/celeritas/ext/GeantTrackView.hh
+++ src/celeritas/ext/GeantTrackView.hh
@@ -20,13 +20,24 @@
 namespace celeritas
 {
 //---------------------------------------------------------------------------//
+template<Ownership W>
+class GeantTrackView
+{
+    static_assert(W != Ownership::value, "GeantTrackView cannot own data");
+};
+
+//---------------------------------------------------------------------------//
 /*!
  * Access track data from Geant4 with Celeritas units.
  *
  * This provides a uniform interface to G4Track data using Celeritas types and
  * units. Geant4 data are all in double precision.
+ *
+ * The const_reference version provides read-only access, while the reference
+ * version adds setters.
  */
-class GeantTrackView
+template<>
+class GeantTrackView<Ownership::const_reference>
 {
   public:
     //!@{
@@ -57,9 +68,61 @@ class GeantTrackView
     //! Statistical weight
     real_type weight() const { return track_.GetWeight(); }
 
+    //! Access the G4 track directly
+    G4Track const& track() const { return track_; }
+
   private:
     G4Track const& track_;
 };
+
+//---------------------------------------------------------------------------//
+/*!
+ * Mutable track view with setters.
+ */
+template<>
+class GeantTrackView<Ownership::reference>
+    : public GeantTrackView<Ownership::const_reference>
+{
+    using Base = GeantTrackView<Ownership::const_reference>;
+
+  public:
+    //!@{
+    //! \name Type aliases
+    using Energy = typename Base::Energy;
+    using real_type = typename Base::real_type;
+    //!@}
+
+  public:
+    // Construct from mutable G4Track
+    explicit GeantTrackView(G4Track& track) : Base(track) {}
+
+    // Bring base class getters into scope
+    using Base::dir;
+    using Base::energy;
+    using Base::pos;
+    using Base::time;
+    using Base::weight;
+
+    // Setters
+    inline void pos(Real3 const& position);
+    inline void dir(Real3 const& direction);
+    inline void energy(Energy e);
+    inline void time(real_type t);
+
+    //! Set statistical weight
+    void weight(real_type w) { this->mtrack().SetWeight(w); }
+
+  private:
+    //! Access mutable track reference (safe: constructed from non-const)
+    G4Track& mtrack() { return const_cast<G4Track&>(this->track()); }
+};
+
+//---------------------------------------------------------------------------//
+// TYPE ALIASES
+//---------------------------------------------------------------------------//
+
+using GeantTrackViewConst = GeantTrackView<Ownership::const_reference>;
+using GeantTrackViewMutable = GeantTrackView<Ownership::reference>;
 
 //---------------------------------------------------------------------------//
 // INLINE DEFINITIONS
@@ -67,7 +130,7 @@ class GeantTrackView
 /*!
  * Get particle definition view.
  */
-GeantParticleView GeantTrackView::particle() const
+GeantParticleView GeantTrackView<Ownership::const_reference>::particle() const
 {
     CELER_EXPECT(track_.GetDefinition());
     return GeantParticleView{*track_.GetDefinition()};
@@ -77,7 +140,7 @@ GeantParticleView GeantTrackView::particle() const
 /*!
  * Get position in native Celeritas length units.
  */
-Real3 GeantTrackView::pos() const
+Real3 GeantTrackView<Ownership::const_reference>::pos() const
 {
     return convert_from_geant(track_.GetPosition(), clhep_length);
 }
@@ -86,7 +149,7 @@ Real3 GeantTrackView::pos() const
 /*!
  * Get momentum direction.
  */
-Real3 GeantTrackView::dir() const
+Real3 GeantTrackView<Ownership::const_reference>::dir() const
 {
     return convert_from_geant(track_.GetMomentumDirection(), 1);
 }
@@ -95,7 +158,7 @@ Real3 GeantTrackView::dir() const
 /*!
  * Get kinetic energy in MeV.
  */
-GeantTrackView::Energy GeantTrackView::energy() const
+auto GeantTrackView<Ownership::const_reference>::energy() const -> Energy
 {
     return Energy{convert_from_geant(track_.GetKineticEnergy(), CLHEP::MeV)};
 }
@@ -104,9 +167,45 @@ GeantTrackView::Energy GeantTrackView::energy() const
 /*!
  * Get global time in native Celeritas time units.
  */
-real_type GeantTrackView::time() const
+real_type GeantTrackView<Ownership::const_reference>::time() const
 {
     return convert_from_geant(track_.GetGlobalTime(), clhep_time);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Set position in native Celeritas length units.
+ */
+void GeantTrackView<Ownership::reference>::pos(Real3 const& position)
+{
+    this->mtrack().SetPosition(convert_to_geant(position, clhep_length));
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Set momentum direction.
+ */
+void GeantTrackView<Ownership::reference>::dir(Real3 const& direction)
+{
+    this->mtrack().SetMomentumDirection(convert_to_geant(direction, 1));
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Set kinetic energy in MeV.
+ */
+void GeantTrackView<Ownership::reference>::energy(Energy e)
+{
+    this->mtrack().SetKineticEnergy(convert_to_geant(e.value(), CLHEP::MeV));
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * Set global time in native Celeritas time units.
+ */
+void GeantTrackView<Ownership::reference>::time(real_type t)
+{
+    this->mtrack().SetGlobalTime(convert_to_geant(t, clhep_time));
 }
 
 //---------------------------------------------------------------------------//
