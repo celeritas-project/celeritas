c5b5cb339b017040d965f67597c7fce8965435bf
Seth R Johnson <johnsonsr@ornl.gov>
2026-01-30 11:45:56 -0500

    Fix duplicate symbol when covfie unavailable

--- src/accel/CartMapMagneticField.hh
+++ src/accel/CartMapMagneticField.hh
@@ -49,13 +49,4 @@ using CartMapMagneticField
     = celeritas::MagneticField<CartMapFieldParams, CartAdapterField>;
 
 //---------------------------------------------------------------------------//
-
-#if !CELERITAS_USE_COVFIE
-inline Real3 CartAdapterField::operator()(Real3 const&) const
-{
-    CELER_ASSERT_UNREACHABLE();
-}
-#endif
-
-//---------------------------------------------------------------------------//
 }  // namespace celeritas
