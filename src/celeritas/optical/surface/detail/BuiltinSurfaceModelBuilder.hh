d83576dddfab4e48272c5eb13f00f2242658fa08
Seth R Johnson <johnsonsr@ornl.gov>
2026-02-16 14:01:19 -0500

    Notify user loudly when using fake physics

--- src/celeritas/optical/surface/detail/BuiltinSurfaceModelBuilder.hh
+++ src/celeritas/optical/surface/detail/BuiltinSurfaceModelBuilder.hh
@@ -134,6 +134,8 @@ void BuiltinSurfaceModelBuilder::build_fake(
 {
     if (!layer_map.empty())
     {
+        CELER_LOG(error) << "Using nonphysical placeholder for '" << label
+                         << "' physics: results will be incorrect";
         models_.push_back(std::make_shared<FakeModel<T>>(
             SurfaceModelId(models_.size()), label, layer_map));
         num_surf_ += layer_map.size();
