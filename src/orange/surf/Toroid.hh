a0b2998c60f4fae08d68b83051a0f9444ede238e
Owen Strong <owensanstrong@gmail.com>
2026-02-22 21:23:55 -0600

    Added credit for graphics gems

--- src/orange/surf/Toroid.hh
+++ src/orange/surf/Toroid.hh
@@ -207,7 +207,7 @@ CELER_FUNCTION auto Toroid::calc_intersections(Real3 const& pos,
  * Calculate the coefficients of the polynomial corresponding to the given
  * ray's intersections with the toroid.
  *
- * Written referencing Graphics Gems II.
+ * Written referencing Graphics Gems II\citep{arvo_graphics-gems_1995}.
  */
 CELER_FUNCTION auto
 Toroid::calc_intersection_polynomial(Real3 const& pos,
