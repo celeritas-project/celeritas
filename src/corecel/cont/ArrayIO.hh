b3f41288f0a02ea22d630ae630a057179deb01ae
Seth R Johnson <johnsonsr@ornl.gov>
2026-02-24 05:54:11 -0600

    Provide backward compatibility function removed in #2265

--- src/corecel/cont/ArrayIO.hh
+++ src/corecel/cont/ArrayIO.hh
@@ -6,4 +6,21 @@
 // DEPRECATED: remove in 1.0
 #warning "Deprecated: this file is no longer necessary to include"
 
+#include "corecel/io/StreamToString.hh"
+
 #include "Array.hh"
+
+namespace celeritas
+{
+//---------------------------------------------------------------------------//
+/*!
+ * Convert an array to a string representation for debugging.
+ */
+template<class T, size_type N>
+std::string to_string(Array<T, N> const& a)
+{
+    return stream_to_string(a);
+}
+
+//---------------------------------------------------------------------------//
+}  // namespace celeritas
