1db40c211791b32a0b5a1c79d6f7087b95a994ef
Seth R Johnson <johnsonsr@ornl.gov>
2026-02-24 16:06:38 -0600

    Remove extra tables

--- src/larceler/LarCelerStandalone.cc
+++ src/larceler/LarCelerStandalone.cc
@@ -30,48 +30,18 @@ make_input_from_config(detail::LarCelerStandaloneConfig const& cfg)
 {
     inp::OpticalStandaloneInput result;
 
-#if 0
-    // FIXME: environment config doesn't yet work
-    {
-        fhicl::ParameterSet const& ps = cfg.environment();
-        for (auto const& key : ps.get_names()) {
-            result.environment[key] = ps.get<std::string>(key);
-        }
-    }
-#endif
-
-    // GPU options
-    if (cfg.device().enable())
-    {
-        celeritas::inp::Device d;
-        d.stack_size = cfg.device().stack_size();
-        d.heap_size = cfg.device().heap_size();
-        result.system.device = d;
-    }
-
     // Obtain the GDML filename from the LAr geometry service
     auto geo_handle = lar::providerFrom<geo::Geometry>();
     CELER_VALIDATE(geo_handle, << "LArSoft geometry is not active");
-
     result.problem.model.geometry = geo_handle->GDMLFile();
-    result.problem.generator = inp::OpticalOffloadGenerator{};
 
-    // Optical limits
-    if (auto steps = cfg.optical_limits().steps())
-    {
-        result.problem.limits.steps = steps;
-    }
-    if (auto step_iters = cfg.optical_limits().step_iters())
-    {
-        result.problem.limits.step_iters = step_iters;
-    }
+    result.problem.generator = inp::OpticalOffloadGenerator{};
 
     // Optical capacities
     {
-        auto const& ocfg = cfg.optical_capacity();
-        result.problem.capacity.primaries = ocfg.primaries();
-        result.problem.capacity.tracks = ocfg.tracks();
-        result.problem.capacity.generators = ocfg.generators();
+        result.problem.capacity.primaries = 8192;
+        result.problem.capacity.tracks = 128;
+        result.problem.capacity.generators = 32768;
     }
 
     result.problem.num_streams = 1;
