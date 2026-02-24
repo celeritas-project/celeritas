91be73a7b205a8a405622ebdf7de9b117dc5b3d5
Owen Strong <owensanstrong@gmail.com>
2026-02-24 13:02:07 -0600

    Added static assert to verify raw data method

--- src/orange/surf/Toroid.hh
+++ src/orange/surf/Toroid.hh
@@ -103,7 +103,12 @@ class Toroid
     CELER_FUNCTION real_type ellipse_z_radius() const { return b_; }
 
     //! View of data for type-deleted storage
-    CELER_FUNCTION StorageSpan data() const { return {&origin_[0], 6}; }
+    CELER_FUNCTION StorageSpan data() const
+    {
+        static_assert(offsetof(Toroid, b_) - offsetof(Toroid, origin_)
+                      == sizeof(real_type) * 5);
+        return {&origin_[0], 6};
+    }
 
     //// CALCULATION ///
 
