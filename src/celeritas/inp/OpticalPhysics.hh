3489166d390c2d133e4194ba22fe4a573d1ad98c
Seth R Johnson <johnsonsr@ornl.gov>
2026-02-25 16:55:22 -0600

    Delete todo

--- src/celeritas/inp/OpticalPhysics.hh
+++ src/celeritas/inp/OpticalPhysics.hh
@@ -285,8 +285,6 @@ struct InteractionModels
  *
  * Interstitial materials are the interstitial materials per geometric surface.
  * The last entry is used as the default surface.
- *
- * \todo rename OpticalSurfacePhysics
  */
 struct OpticalSurfacePhysics
 {
