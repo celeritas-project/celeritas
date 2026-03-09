5cf5127f1873af76dfe805fd0ec23a29ec5b9f7a
Seth R Johnson <johnsonsr@ornl.gov>
2026-03-09 08:27:25 -0400

    Use range

--- src/celeritas/g4/MagneticField.hh
+++ src/celeritas/g4/MagneticField.hh
@@ -11,6 +11,7 @@
 
 #include "corecel/Assert.hh"
 #include "corecel/Types.hh"
+#include "corecel/cont/Range.hh"
 #include "geocel/g4/Convert.hh"
 #include "celeritas/Quantities.hh"
 #include "celeritas/ext/GeantUnits.hh"
@@ -71,7 +72,7 @@ void MagneticField<P, F>::GetFieldValue(G4double const pos[3],
     // Calculate the magnetic field value in the native Celeritas unit system
     Real3 field_native
         = calc_field(convert_from_geant<units::ClhepLength, real_type>(pos));
-    for (auto i = 0; i < 3; ++i)
+    for (auto i : range(3))
     {
         // Return values of the field vector in native geant4 units
         field[i] = convert_to_geant<units::ClhepField>(field_native[i]);
