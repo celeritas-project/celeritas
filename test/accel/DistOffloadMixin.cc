3009117d34bce272105941c065e37d2c1299910d
Seth R Johnson <johnsonsr@ornl.gov>
2026-03-27 12:49:28 +0100

    WIP

--- test/accel/DistOffloadMixin.cc
+++ test/accel/DistOffloadMixin.cc
@@ -17,6 +17,7 @@
 #include "geocel/GeoOpticalIdMap.hh"  // IWYU pragma: keep
 #include "geocel/g4/Convert.hh"
 #include "celeritas/ext/GeantParticleView.hh"
+#include "celeritas/g4/Threading.hh"
 #include "celeritas/optical/gen/GeneratorData.hh"
 #include "accel/IntegrationTestBase.hh"
 #include "accel/LocalOpticalGenOffload.hh"
