a7a76f57be23028e4cc1c0d90202615b8d41734f
Seth R Johnson <johnsonsr@ornl.gov>
2026-03-29 06:31:41 -0400

    Try casting and also forbid loading of dynamic extents

--- src/corecel/math/detail/SpanUtilsImpl.hh
+++ src/corecel/math/detail/SpanUtilsImpl.hh
@@ -19,8 +19,9 @@ namespace detail
 //---------------------------------------------------------------------------//
 //! Load a const span into a fixed-size array.
 template<class T, std::size_t N>
-CELER_FUNCTION Array<T, N> load_array(Span<T const, N> s)
+CELER_FUNCTION Array<T, size_type(N)> load_array(Span<T const, N> s)
 {
+    static_assert(N != dynamic_extent);
     Array<T, N> result;
     for (std::size_t i = 0; i != N; ++i)
         result[i] = s[i];
@@ -30,16 +31,19 @@ CELER_FUNCTION Array<T, N> load_array(Span<T const, N> s)
 //! Load a const span into a fixed-size array.
 //! \todo Delete in follow-up PR when implicit const conversion works.
 template<class T, std::size_t N>
-CELER_FUNCTION Array<T, N> load_array(Span<T, N> s)
+CELER_FUNCTION Array<T, size_type(N)> load_array(Span<T, N> s)
 {
+    static_assert(N != dynamic_extent);
     return load_array(make_span(const_cast<T const>(s.data()), s.size()));
 }
 
 //---------------------------------------------------------------------------//
 //! Store a fixed-size array into a span.
-template<class T, std::size_t N>
-CELER_FUNCTION void store_array(Array<T, N> const& a, Span<T, N> dst)
+template<class T, size_type N>
+CELER_FUNCTION void
+store_array(Array<T, N> const& a, Span<T, std::size_t(N)> dst)
 {
+    static_assert(N != dynamic_extent);
     for (std::size_t i = 0; i != N; ++i)
         dst[i] = a[i];
 }
