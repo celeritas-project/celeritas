51de5b7d32f683933a9f98c43438aef764f9ea24
LSchwiebert <loren@wayne.edu>
2026-04-17 14:58:46 -0400

    Remove extraneous semicolon ending a namespace -- old gcc versions produce warnings

--- src/celeritas/em/distribution/BhabhaEnergyDistribution.hh
+++ src/celeritas/em/distribution/BhabhaEnergyDistribution.hh
@@ -60,7 +60,7 @@ class BhabhaEnergyDistribution
         return 1;
     }
 
-};  // namespace BhabhaEnergyDistribution
+}  // namespace BhabhaEnergyDistribution
 
 //---------------------------------------------------------------------------//
 // INLINE DEFINITIONS
