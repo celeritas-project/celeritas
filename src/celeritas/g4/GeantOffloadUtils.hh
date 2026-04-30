a617d63a58d540c9b4be51f5f109ab8fce318cb9
Hayden Hollenbeck <hollenbeck.hayden@gmail.com>
2026-04-29 20:53:45 -0500

    Fix GeantOffloadUtils doc filename

--- src/celeritas/g4/GeantOffloadUtils.hh
+++ src/celeritas/g4/GeantOffloadUtils.hh
@@ -2,7 +2,7 @@
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/g4/G4OffloadUtils.hh
+//! \file celeritas/g4/GeantOffloadUtils.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
