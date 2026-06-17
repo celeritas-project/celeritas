852fb6f8871aac2323f6cd7c05efcb8c57a9cea7
osanstrong <owensanstrong@gmail.com>
2026-06-17 15:53:36 -0400

    Made toroid return right surface type

--- src/orange/surf/Toroid.hh
+++ src/orange/surf/Toroid.hh
@@ -65,10 +65,7 @@ class Toroid
     //// CLASS ATTRIBUTES ////
 
     // Surface type identifier
-    static SurfaceType surface_type()
-    {
-        CELER_NOT_IMPLEMENTED("runtime toroid");
-    }
+    static SurfaceType surface_type() { return SurfaceType::tor; }
 
     //! Safety distance is calculable w/xy of normal and ellipse safety
     //! distance, but this is out of scope at first and might not be trivially
