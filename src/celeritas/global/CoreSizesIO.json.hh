ff9232c96a6f3727483fd707b5f9c62117b36a1d
Amanda Lund <alund@anl.gov>
2026-07-15 22:40:43 +0000

    Fix docs

--- src/celeritas/global/CoreSizesIO.json.hh
+++ src/celeritas/global/CoreSizesIO.json.hh
@@ -2,7 +2,7 @@
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file celeritas/global/CoreSizes.json.hh
+//! \file celeritas/global/CoreSizesIO.json.hh
 //---------------------------------------------------------------------------//
 #pragma once
 
