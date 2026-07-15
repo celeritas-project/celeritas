8784117c3ce50e3e43798da195fee354984e551e
Seth R Johnson <johnsonsr@ornl.gov>
2026-07-15 08:43:46 -0400

    Don't keep empty lines at start of file

--- test/celeritas/track/TrackSort.test.cc
+++ test/celeritas/track/TrackSort.test.cc
@@ -1,4 +1,3 @@
-
 //------------------------------- -*- C++ -*- -------------------------------//
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
