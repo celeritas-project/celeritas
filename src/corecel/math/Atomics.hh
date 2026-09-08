37de04489443f69aaa91f18948acfb57ea294346
Seth R Johnson <johnsonsr@ornl.gov>
2026-09-08 10:46:23 -0400

    Address feedback

--- src/corecel/math/Atomics.hh
+++ src/corecel/math/Atomics.hh
@@ -11,8 +11,9 @@
  * utilities are meant for "kernel" code. Multiple independent events
  * must \em not use these functions to simultaneously modify shared data.
  *
- * \warning Some atomics are unsupported with a compute target less than 6.0,
- * which has been removed from CUDA support for years.
+ * \warning Instantiating double-precision atomics is not supported when using
+ * a compute target less than 6.0, which has been removed from CUDA support for
+ * years.
  *
  * ---------------------------------------------------------------------------*/
 #pragma once
