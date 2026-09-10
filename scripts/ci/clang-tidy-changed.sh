f011e5c38a9cef1f301537c0da5194eaacf08801
Philippe Canal <pcanal@fnal.gov>
2026-09-10 12:08:58 -0500

    fixup! Use clang-tidy-diff for changed files

--- scripts/ci/clang-tidy-changed.sh
+++ scripts/ci/clang-tidy-changed.sh
@@ -48,6 +48,4 @@ git diff --diff-filter=ACM -U0 "$BASE_SHA"..."$HEAD_SHA" \
       -clang-tidy-binary "$CLANG_TIDY" \
       -p 1 \
       -path "$BUILD_DIR" \
-      -extra-arg=-isysroot \
-      -extra-arg=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
       -regex '^(src|app|test)/.*\.(cc|hh)$'
